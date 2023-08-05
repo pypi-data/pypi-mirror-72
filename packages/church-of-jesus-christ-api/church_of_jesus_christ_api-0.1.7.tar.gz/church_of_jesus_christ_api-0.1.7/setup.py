47f90187a24344f525f846c5dd7f774dc14174be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package church-of-jesus-christ-api/0.1.7/church_of_jesus_christ_api-0.1.7.tar.gz

---
+++ packages/church-of-jesus-christ-api/church_of_jesus_christ_api-0.1.7.tar.gz/church_of_jesus_christ_api-0.1.7/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup, find_packages
+from __version__ import __version__
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / 'README.rst').read_text()
+
+# This call to setup() does all the work
+setup(
+    name='church_of_jesus_christ_api',
+    version=__version__,
+    description='Get data from churchofjesuschrist.org',
+    long_description=README,
+    long_description_content_type='text/x-rst',
+    author='Michael Mackliet',
+    author_email='michael@mackliet.com',
+    url='https://github.com/mackliet/church_of_jesus_christ_api',
+    project_urls={
+        "Documentation": "https://church-of-jesus-christ-api.readthedocs.io/en/latest/index.html",
+    },
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['requests'],
+    extras={
+        'documentation': ['sphinx', 'sphinx_rtd_theme']
+    }
+)
