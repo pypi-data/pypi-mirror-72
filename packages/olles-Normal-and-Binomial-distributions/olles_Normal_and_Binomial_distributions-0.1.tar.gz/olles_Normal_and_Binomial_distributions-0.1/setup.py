0e73e5e5614e82edbff6e3135bc97772c4038467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:59 +0000

    Add package olles-Normal-and-Binomial-distributions/0.1/olles_Normal_and_Binomial_distributions-0.1.tar.gz

---
+++ packages/olles-Normal-and-Binomial-distributions/olles_Normal_and_Binomial_distributions-0.1.tar.gz/olles_Normal_and_Binomial_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='olles_Normal_and_Binomial_distributions',
+      version='0.1',
+      description='Normal/Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = "Olle Green",
+      author_email = "olleg.green@gmail.com",
+    zip_safe=False)
