85300f9eb98407281ec673805db019382162f71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:34 +0000

    Add package bitflow/0.6.0/bitflow-0.6.0.tar.gz

---
+++ packages/bitflow/bitflow-0.6.0.tar.gz/bitflow-0.6.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bitflow', 'bitflow.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.9.1,<5.0.0', 'neo4j>=4.0.0,<5.0.0', 'requests', 'urllib3']
+
+setup_kwargs = {
+    'name': 'bitflow',
+    'version': '0.6.0',
+    'description': '',
+    'long_description': None,
+    'author': 'LSaldyt',
+    'author_email': 'lucassaldyt@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
