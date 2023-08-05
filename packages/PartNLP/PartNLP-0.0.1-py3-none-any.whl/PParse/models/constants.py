af6cdc4fa0a3863ee1836871762ee977c1b8acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package PartNLP/0.0.1/PartNLP-0.0.1-py3-none-any.whl

---
+++ packages/PartNLP/PartNLP-0.0.1-py3-none-any.whl/PParse/models/constants.py
@@ -0,0 +1,27 @@
+"""In this script, we are going to add all constant values in which used in the entire project.
+"""
+from PPars.models.hazm_preprocessor import HAZMPreprocessor as HAZM
+from PPars.models.parsivar_preprocessor import PARSIVARPreprocessor as PARSIVAR
+
+
+NAME_TO_PACKAGE_DICT = {'HAZM': HAZM, 'PARSIVAR': PARSIVAR}
+
+NAME_TO_METHODS = {'NORMALIZE': lambda m: m.normalizer(), 'S_TOKENIZE': lambda m: m.sent_tokenize(),
+                         'W_TOKENIZE': lambda m: m.word_tokenize(), 'STEM': lambda m: m.stem(),
+                         'LEMMATIZE': lambda m: m.lemmatize()}
+
+SUPPORTED_LANGUAGES_TO_PACKAGES = {'PERSIAN': ['HAZM', 'PARSIVAR'], 'ENGLISH': []}
+SUPPORTED_PROCESSORS = ['NORMALIZE', 'S_TOKENIZE', 'STEM', 'W_TOKENIZE', 'LEMMATIZE']
+NAME_OF_SUPPORTED_LANGUAGES = ['ENGLISH', 'PERSIAN']
+NAME_OF_SUPPORTED_PACKAGES = ['HAZM', 'PARSIVAR']
+
+
+class Color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    Yellow = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
---
+++ packages/PartNLP/PartNLP-0.0.1-py3-none-any.whl/PParse/models/validation/normalize_validator.py
@@ -0,0 +1,18 @@
+""" This class validates all requirements should be passed by normalize class.
+"""
+from PPars.models.validation.validator import Validator
+
+
+class NormalizeValidator(Validator):
+    def __init__(self, config):
+        # self.package = config['package']
+        self.config = config
+
+    def isvalid(self):
+        """
+        :return: True or False, error_message
+        """
+        return True, '', None
+
+    def get_dependencies(self):
+        return []
