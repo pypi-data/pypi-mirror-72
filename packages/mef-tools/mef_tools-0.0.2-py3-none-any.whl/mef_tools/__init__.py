5a976ada803071d5f288ae177236b32b6bb06560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:01 +0000

    Add package mef-tools/0.0.2/mef_tools-0.0.2-py3-none-any.whl

---
+++ packages/mef-tools/mef_tools-0.0.2-py3-none-any.whl/mef_tools/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2020-present, Mayo Clinic Department of Neurology - Laboratory of Bioelectronics Neurophysiology and Engineering
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from .io import *
+__version__ = '0.0.2'
+
+# Log here changes in versions
