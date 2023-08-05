673aedb903213fb96f71b6a20a8dbb55ebfbaabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package OMS-distributions/0.1/OMS_distributions-0.1.tar.gz

---
+++ packages/OMS-distributions/OMS_distributions-0.1.tar.gz/OMS_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='OMS_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['OMS_distributions'],
+      author='Ori Slasky', 
+      author_email='Orislasky@gmail.com',
+      zip_safe=False)
