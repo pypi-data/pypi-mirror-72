fd4ec2b3b86a9fa26f75cf67a70bc6389ab0c455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package oceandb-mongodb-driver/0.2.1/oceandb_mongodb_driver-0.2.1-py2.py3-none-any.whl

---
+++ packages/oceandb-mongodb-driver/oceandb_mongodb_driver-0.2.1-py2.py3-none-any.whl/oceandb_mongodb_driver/__init__.py
@@ -0,0 +1,7 @@
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+from oceandb_mongodb_driver import plugin
+
+__author__ = """OceanProtocol"""
+__version__ = '0.2.1'
