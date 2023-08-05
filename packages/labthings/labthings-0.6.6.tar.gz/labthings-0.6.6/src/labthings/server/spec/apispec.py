0056d075ed96f53fb31b88efe145ec24354dc036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:32 +0000

    Add package labthings/0.6.6/labthings-0.6.6.tar.gz

---
+++ packages/labthings/labthings-0.6.6.tar.gz/labthings-0.6.6/src/labthings/server/spec/apispec.py
@@ -0,0 +1,94 @@
+from apispec import APISpec
+from .paths import rule_to_path, rule_to_params
+from .utilities import convert_to_schema_or_json
+
+from werkzeug.routing import Rule
+from http import HTTPStatus
+
+
+def rule_to_apispec_path(rule: Rule, spec_dict: dict, apispec: APISpec):
+    """Generate APISpec Path arguments from a flask Rule and View
+    
+    Args:
+        rule (Rule): Flask Rule for path
+        spec_dict (dict): Compiled API spec dictionary
+        apispec (APISpec): APISpec object to generate arguments for
+    
+    Returns:
+        dict: APISpec `path` funtion argument dictionary
+    """
+
+    params = {
+        "path": rule_to_path(rule),
+        "operations": dict_to_apispec_operations(
+            spec_dict.get("_operations", {}), apispec
+        ),
+    }
+
+    # Add URL arguments to operations
+    if rule.arguments:
+        for op in spec_dict.get("_operations", {}).keys():
+            params["operations"][op].update({"parameters": rule_to_params(rule)})
+
+    # Merge in non-special spec elements
+    for k, v in spec_dict.items():
+        if not k.startswith("_"):
+            params[k] = v
+
+    return params
+
+
+def dict_to_apispec_operations(operations_spec_dict: dict, apispec: APISpec):
+    """Generate APISpec `operations` argument from a flask View
+
+    Args:
+        operations_spec_dict (dict): Operations API spec dictionary
+        apispec (APISpec): APISpec object to generate arguments for
+
+    Returns:
+        dict: APISpec `operations` dictionary
+    """
+
+    # Build dictionary of operations (HTTP methods)
+    ops = {}
+    for operation, meth_spec in operations_spec_dict.items():
+
+        ops[operation] = {}
+
+        # Add input schema
+        if "_params" in meth_spec:
+            request_schema = convert_to_schema_or_json(
+                meth_spec.get("_params"), apispec
+            )
+            if request_schema:
+                ops[operation]["requestBody"] = {
+                    "content": {"application/json": {"schema": request_schema}}
+                }
+
+        # Add output schema
+        if "_schema" in meth_spec:
+            ops[operation]["responses"] = {}
+            for code, schema in meth_spec.get("_schema", {}).items():
+                ops[operation]["responses"][code] = {
+                    "description": HTTPStatus(code).phrase,
+                    "content": {
+                        "application/json": {
+                            "schema": convert_to_schema_or_json(schema, apispec)
+                        }
+                    },
+                }
+        else:
+            # If no explicit responses are known, populate with defaults
+            ops[operation]["responses"] = {
+                200: {
+                    "description": meth_spec.get("description")
+                    or HTTPStatus(200).phrase
+                }
+            }
+
+        # Merge in non-special spec elements
+        for k, v in meth_spec.items():
+            if not k.startswith("_"):
+                ops[operation][k] = v
+
+    return ops
---
+++ packages/labthings/labthings-0.6.6.tar.gz/labthings-0.6.6/src/labthings/server/view/builder.py
@@ -0,0 +1,182 @@
+from labthings.server.decorators import (
+    PropertySchema,
+    use_args,
+    Doc,
+    Safe,
+    Idempotent,
+    Semtype,
+)
+from labthings.server.semantics.base import Semantic
+from . import View, ActionView, PropertyView
+from .. import fields
+from ..spec.utilities import compile_view_spec
+
+import os
+import glob
+from flask import send_file, abort
+import uuid
+
+
+def property_of(
+    property_object: object,
+    property_name: str,
+    name: str = None,
+    readonly=False,
+    description=None,
+    semtype=None,
+    schema=fields.Field(),
+    _legacy=False,  # Old structure where POST is used to write property
+):
+
+    # Create a class name
+    if not name:
+        name = type(property_object).__name__ + f"_{property_name}"
+
+    # Create inner functions
+    def _read(self):
+        return getattr(property_object, property_name)
+
+    def _write(self, args):
+        setattr(property_object, property_name, args)
+        return getattr(property_object, property_name)
+
+    def _update(self, args):
+        getattr(property_object, property_name).update(args)
+        return getattr(property_object, property_name)
+
+    # Generate a basic property class
+    generated_class = type(
+        name,
+        (PropertyView, object),
+        {
+            "property_object": property_object,
+            "property_name": property_name,
+            "get": _read,
+        },
+    )
+
+    # Override read-write capabilities
+    if not readonly:
+        # Enable update PUT requests for dictionaries
+        if type(getattr(property_object, property_name)) is dict:
+            generated_class.put = _update
+            generated_class.methods.add("PUT")
+        # If legacy mode, use POST to write property
+        elif _legacy:
+            generated_class.post = _write
+            generated_class.methods.add("POST")
+        # Normally, use PUT to write property
+        else:
+            generated_class.put = _write
+            generated_class.methods.add("PUT")
+
+    # Add decorators for arguments etc
+    if schema:
+        generated_class = PropertySchema(schema)(generated_class)
+
+    if description:
+        generated_class = Doc(description=description, summary=description)(
+            generated_class
+        )
+
+    # Apply semantic type last, to ensure this is always used
+    if semtype:
+        if isinstance(semtype, str):
+            generated_class = Semtype(semtype)(generated_class)
+        elif isinstance(semtype, Semantic):
+            generated_class = semtype(generated_class)
+        else:
+            logging.error(
+                "Unsupported type for semtype. Must be a string or Semantic object"
+            )
+
+    # Compile the generated views spec
+    # Useful if its being attached to something other than a LabThing instance
+    compile_view_spec(generated_class)
+
+    return generated_class
+
+
+def action_from(
+    function,
+    name: str = None,
+    description=None,
+    safe=False,
+    idempotent=False,
+    args=None,
+    semtype=None,
+):
+
+    # Create a class name
+    if not name:
+        name = f"{function.__name__}_action"
+
+    # Create inner functions
+    def _post(self):
+        return function()
+
+    def _post_with_args(self, args):
+        return function(**args)
+
+    # Add decorators for arguments etc
+    if args is not None:
+        generated_class = type(name, (ActionView, object), {"post": _post_with_args})
+        generated_class.post = use_args(args)(generated_class.post)
+    else:
+        generated_class = type(name, (ActionView, object), {"post": _post})
+
+    if description:
+        generated_class = Doc(description=description, summary=description)(
+            generated_class
+        )
+
+    if safe:
+        generated_class = Safe(generated_class)
+
+    if idempotent:
+        generated_class = Idempotent(generated_class)
+
+    # Apply semantic type last, to ensure this is always used
+    if semtype:
+        if isinstance(semtype, str):
+            generated_class = Semtype(semtype)(generated_class)
+        elif isinstance(semtype, Semantic):
+            generated_class = semtype(generated_class)
+        else:
+            logging.error(
+                "Unsupported type for semtype. Must be a string or Semantic object"
+            )
+
+    # Compile the generated views spec
+    # Useful if its being attached to something other than a LabThing instance
+    compile_view_spec(generated_class)
+
+    return generated_class
+
+
+def static_from(static_folder: str, name=None):
+
+    # Create a class name
+    if not name:
+        uid = uuid.uuid4()
+        name = f"static-{uid}"
+
+    # Create inner functions
+    def _get(self, path=""):
+        full_path = os.path.join(static_folder, path)
+        if not os.path.exists(full_path):
+            return abort(404)
+
+        if os.path.isfile(full_path):
+            return send_file(full_path)
+
+        if os.path.isdir(full_path):
+            indexes = glob.glob(os.path.join(full_path, "index.*"))
+            if not indexes:
+                return abort(404)
+            return send_file(indexes[0])
+
+    # Generate a basic property class
+    generated_class = type(name, (View, object), {"get": _get})
+
+    return generated_class
