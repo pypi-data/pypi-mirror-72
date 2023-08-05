73ad467ed54f94133f006870f7ca45ab4fcc7785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:17 +0000

    Add package dsndxy23-probability/0.1/dsndxy23_probability-0.1.tar.gz

---
+++ packages/dsndxy23-probability/dsndxy23_probability-0.1.tar.gz/dsndxy23_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsndxy23_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsndxy23_probability'],
+      author_email = 'vsrworld1999@gmail.com',
+      zip_safe = False)
