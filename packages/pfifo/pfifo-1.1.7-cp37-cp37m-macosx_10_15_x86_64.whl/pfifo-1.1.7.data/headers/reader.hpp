f941b3704b141283407285895ebad9923ad1b72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:57 +0000

    Add package pfifo/1.1.7/pfifo-1.1.7-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/pfifo/pfifo-1.1.7-cp37-cp37m-macosx_10_15_x86_64.whl/pfifo-1.1.7.data/headers/reader.hpp
@@ -0,0 +1,113 @@
+#ifndef __PFIFO_READER_HPP__
+#define __PFIFO_READER_HPP__
+
+#include <string>
+#include <system_error>
+#include <vector>
+
+#include <sys/errno.h>
+#include <sys/fcntl.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace pfifo {
+
+class reader {
+ public:
+  reader(std::string name, mode_t mode) : name(name) {
+    if (mkfifo(name.c_str(), mode) == -1) {
+      int err = errno;
+      if (err != EEXIST) {
+        throw std::system_error(err, std::system_category());
+      }
+    }
+
+    if ((fd = open(name.c_str(), O_RDONLY)) == -1) {
+      int err = errno;
+      throw std::system_error(err, std::system_category());
+    }
+  }
+
+  reader(std::string name) : name(name) {
+    if ((fd = open(name.c_str(), O_RDONLY)) == -1) {
+      int err = errno;
+      throw std::system_error(err, std::system_category());
+    }
+  }
+
+  virtual ~reader() {
+    close(fd);
+    unlink(name.c_str());
+  }
+
+  inline std::size_t read_size() {
+    std::size_t size;
+    char* buf = reinterpret_cast<char*>(&size);
+    std::size_t n = ::read(fd, buf, sizeof(std::size_t));
+
+    if (n < 0) {
+      int err = errno;
+      throw std::system_error(err, std::system_category());
+    }
+
+    if (n != sizeof(std::size_t)) {
+      throw std::system_error(EMSGSIZE, std::system_category());
+    }
+
+    return size;
+  }
+
+  void read_into(char* buf, std::size_t size) {
+    std::size_t readout = 0;
+    while (readout < size) {
+      readout += ::read(fd, buf + readout, size - readout);
+    }
+  }
+
+  template <class Sequence>
+  Sequence read() {
+    std::size_t size = read_size();
+    if (size == 0) {
+      return Sequence();
+    }
+
+    Sequence ret(size / sizeof(typename Sequence::value_type));
+    char* buf = reinterpret_cast<char*>(ret.data());
+
+    read_into(buf, size);
+
+    return ret;
+  }
+
+  std::string reads() {
+    auto v = read<std::vector<char>>();
+    return std::string(v.begin(), v.end());
+  }
+
+ private:
+  std::string name;
+  int fd;
+};
+
+template <>
+char* reader::read() {
+  std::size_t size = read_size();
+  if (size == 0) {
+    return nullptr;
+  }
+
+  char* buf = new char[size];
+
+  std::size_t readout = 0;
+  while (readout < size) {
+    readout += ::read(fd, buf + readout, size - readout);
+  }
+
+  return buf;
+}
+
+}  // namespace pfifo
+
+#endif  // __PFIFO_READER_HPP__
---
+++ packages/pfifo/pfifo-1.1.7-cp37-cp37m-macosx_10_15_x86_64.whl/pfifo-1.1.7.data/headers/writer.hpp
@@ -0,0 +1,92 @@
+#ifndef __PFIFO_WRITER_HPP__
+#define __PFIFO_WRITER_HPP__
+
+#include <string>
+#include <system_error>
+
+#include <sys/errno.h>
+#include <sys/fcntl.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace pfifo {
+
+class writer {
+ public:
+  writer(std::string name, mode_t mode) : name(name) {
+    if (mkfifo(name.c_str(), mode) == -1) {
+      int err = errno;
+      if (err != EEXIST) {
+        throw std::system_error(err, std::system_category());
+      }
+    }
+
+    if ((fd = open(name.c_str(), O_WRONLY)) == -1) {
+      int err = errno;
+      throw std::system_error(err, std::system_category());
+    }
+  }
+
+  writer(std::string name) : name(name) {
+    if ((fd = open(name.c_str(), O_WRONLY)) == -1) {
+      int err = errno;
+      throw std::system_error(err, std::system_category());
+    }
+  }
+
+  ~writer() {
+    close(fd);
+    unlink(name.c_str());
+  }
+
+  inline void write_size(std::size_t size) const {
+    char* buf = reinterpret_cast<char*>(&size);
+    std::size_t n = ::write(fd, buf, sizeof(std::size_t));
+
+    if (n < 0) {
+      int err = errno;
+      throw std::system_error(err, std::system_category());
+    }
+
+    if (n != sizeof(std::size_t)) {
+      throw std::system_error(EMSGSIZE, std::system_category());
+    }
+  }
+
+  template <class Sequence>
+  void write(Sequence msg) const {
+    flock(fd, LOCK_EX);
+
+    std::size_t size = sizeof(typename Sequence::value_type) * msg.size();
+    const char* buf = reinterpret_cast<const char*>(msg.data());
+
+    write_size(size);
+
+    std::size_t written = 0;
+
+    while (written < size) {
+      written += ::write(fd, buf + written, size - written);
+    }
+
+    if (written != size) {
+      throw std::system_error(EMSGSIZE, std::system_category());
+    }
+
+    flock(fd, LOCK_UN);
+  }
+
+ private:
+  std::string name;
+  int fd;
+};
+
+template <>
+void writer::write(const char* msg) const {
+  write(std::string(msg));
+}
+
+}  // namespace pfifo
+
+#endif  // __PFIFO_WRITER_HPP__
