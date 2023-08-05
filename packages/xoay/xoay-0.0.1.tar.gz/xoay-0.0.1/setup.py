012d0bec01e10acd6973010398badd8369e78110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:24 +0000

    Add package xoay/0.0.1/xoay-0.0.1.tar.gz

---
+++ packages/xoay/xoay-0.0.1.tar.gz/xoay-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+  name = 'xoay',
+  version = '0.0.1',
+  description = '',
+  author = 'xoayteam',
+  license = 'GPL-3.0',
+  url = 'https://github.com/xoay/xoay',
+  download_url = 'https://github.com/xoay/xoay/downloads',
+  keywords = ['xoay'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
