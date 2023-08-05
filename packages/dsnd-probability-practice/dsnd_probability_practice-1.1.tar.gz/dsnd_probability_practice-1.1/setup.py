4770a813b6392bae93c691c4400c62017b2f2c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package dsnd-probability-practice/1.1/dsnd_probability_practice-1.1.tar.gz

---
+++ packages/dsnd-probability-practice/dsnd_probability_practice-1.1.tar.gz/dsnd_probability_practice-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_practice',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_practice'],
+      author = 'Jambal Tumurtogoo',
+      author_email = 'tjambal21@gmail.com',
+      zip_safe=False)
