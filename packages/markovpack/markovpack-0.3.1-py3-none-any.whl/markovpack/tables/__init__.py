c89a460413696775ec7851bb6c50467bb4f393f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package markovpack/0.3.1/markovpack-0.3.1-py3-none-any.whl

---
+++ packages/markovpack/markovpack-0.3.1-py3-none-any.whl/markovpack/tables/__init__.py
@@ -0,0 +1,10 @@
+# Imports go here!
+
+
+# Enter the tables of your Pack here!
+available_tables = [
+
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [table.__name__ for table in available_tables]
