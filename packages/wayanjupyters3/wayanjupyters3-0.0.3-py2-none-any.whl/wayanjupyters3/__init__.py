67ef976683ba3d12a000f14a842b6df2ed06dbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:36 +0000

    Add package wayanjupyters3/0.0.3/wayanjupyters3-0.0.3-py2-none-any.whl

---
+++ packages/wayanjupyters3/wayanjupyters3-0.0.3-py2-none-any.whl/wayanjupyters3/__init__.py
@@ -0,0 +1,5 @@
+from .jupyters3 import JupyterS3
+from .jupyters3 import JupyterS3SecretAccessKeyAuthentication
+from .jupyters3 import JupyterS3ECSRoleAuthentication
+from .jupyters3 import AwsCreds
+from .jupyters3 import JupyterS3Authentication
