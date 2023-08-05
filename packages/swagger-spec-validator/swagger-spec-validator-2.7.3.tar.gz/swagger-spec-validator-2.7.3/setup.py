4bc1f0cb9bd04ce82ed1e22a6faeb89e7e3c120d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:14 +0000

    Add package swagger-spec-validator/2.7.3/swagger-spec-validator-2.7.3.tar.gz

---
+++ packages/swagger-spec-validator/swagger-spec-validator-2.7.3.tar.gz/swagger-spec-validator-2.7.3/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import os
+
+from setuptools import find_packages, setup
+
+base_dir = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(base_dir, "swagger_spec_validator", "__about__.py")) as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+
+    description=about['__summary__'],
+
+    url=about['__uri__'],
+
+    author=about['__author__'],
+    author_email=about['__email__'],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={
+        'swagger_spec_validator': [
+            'swagger_spec_validator/schemas/v1.2/*',
+            'swagger_spec_validator/schemas/v2.0/*',
+        ],
+    },
+    include_package_data=True,
+    install_requires=[
+        'jsonschema',
+        'pyyaml',
+        'six',
+    ],
+    extra_dependencies={'python_version<"3"': ["functools32"]},
+    license=about['__license__'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
