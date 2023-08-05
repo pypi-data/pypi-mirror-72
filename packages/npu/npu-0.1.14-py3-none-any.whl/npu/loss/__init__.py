6099fd17415d325b9282df8345dfdc22a50f89cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:28 +0000

    Add package npu/0.1.14/npu-0.1.14-py3-none-any.whl

---
+++ packages/npu/npu-0.1.14-py3-none-any.whl/npu/loss/__init__.py
@@ -0,0 +1,11 @@
+"""
+:synopsis: Loss functions to use with NPU API for training
+.. moduleauthor:: Naval Bhandari <naval@neuro-ai.co.uk>
+"""
+
+CrossEntropyLoss = "CrossEntropyLoss"  #:
+SparseCrossEntropyLoss = "SparseCrossEntropyLoss"  #:
+MSELoss = "MSELoss"  #:
+CTCLoss = "CTCLoss"  #:
+L1Loss = "L1Loss"  #:
+NLLLoss = "NLLLoss"  #:
