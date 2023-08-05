2a82e6c17cb6eb52a02e57fa4288f26ea6c11c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:13 +0000

    Add package probabilityLK/0.1/probabilityLK-0.1.tar.gz

---
+++ packages/probabilityLK/probabilityLK-0.1.tar.gz/probabilityLK-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probabilityLK',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probabilityLK'],
+      author='Lakshya Kwatra',
+      author_email='lkwatra2@gmail.com',
+      zip_safe=False)
