c39428413eb2635b4d94d2680a1dda2f63a49864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:34 +0000

    Add package simple-zuora-client/0.1.5/simple_zuora_client-0.1.5.tar.gz

---
+++ packages/simple-zuora-client/simple_zuora_client-0.1.5.tar.gz/simple_zuora_client-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('LICENSE', 'rt', encoding='utf8') as lic, open('package_description.rst', 'rt', encoding='utf8') as readme:
+    setup(
+        name='simple_zuora_client',
+        maintainer='Sergey Yakimov',
+        maintainer_email='sergwy@gmail.com',
+        version='0.1.5',
+        url='https://gitlab.com/sergwy/simple-zuora-client',
+        description='Simple Zuora Client',
+        long_description_content_type='text/x-rst',
+        long_description=readme.read(),
+        packages=find_packages(),
+        license=lic.read(),
+        install_requires=['requests', 'requests-oauth2', 'requests-oauthlib']
+    )
