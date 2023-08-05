389559cff21161668058ac82abecd266f2aacba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package spela/0.0.5/spela-0.0.5.tar.gz

---
+++ packages/spela/spela-0.0.5.tar.gz/spela-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name='spela',
+    version='0.0.5',
+    packages=['spela', 'utils'],
+    url='https://github.com/kongkip/spela.git',
+    long_description=str(README),
+    long_description_content_type="text/markdown",
+    author='Evans Kiplagat',
+    author_email='evanskiplagat3@gmail.com',
+    license='MIT',
+    description='spectrogram layers',
+    install_requires=['numpy', 'librosa', 'matplotlib']
+)
