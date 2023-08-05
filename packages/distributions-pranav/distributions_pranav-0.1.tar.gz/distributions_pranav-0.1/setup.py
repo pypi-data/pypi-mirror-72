eaf3f73517b5c5be1deb4e65ffbae16e153a1aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:33 +0000

    Add package distributions-pranav/0.1/distributions_pranav-0.1.tar.gz

---
+++ packages/distributions-pranav/distributions_pranav-0.1.tar.gz/distributions_pranav-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_pranav',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pranav'],
+      author_email='nspranav1180@gmail.com',
+      zip_safe=False)
