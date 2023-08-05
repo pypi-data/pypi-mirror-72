52a53ca12457e8cebc138c7062c37004d9925beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package lifebrick/1.1.2/lifebrick-1.1.2.tar.gz

---
+++ packages/lifebrick/lifebrick-1.1.2.tar.gz/lifebrick-1.1.2/setup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+#                            _                _____
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#
+#
+
+import os
+import sys
+import glob
+
+from codecs import open
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist')
+    os.system('twine upload dist/*')
+    os.system('rm -rf dist')
+    os.system('rm -rf *.egg-info')
+    sys.exit()
+
+requires = [
+    # Add predefined packages here.
+    'click',
+    'geoip2',
+    'opencv-python',
+    'numpy',
+    'matplotlib',
+    'schedule',
+    'iso3166'
+]
+
+about = {}  # create a empty dictionary to store the content of the __version__.py file
+# execute the __version__.py file and get the content, save to the dictionary about.
+with open(os.path.join(here, 'lifebrick', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=find_packages(),
+    package_data={
+        # if any package contains LICENSE file, include them.
+        '': ['LICENSE'],
+        # include the mmdb database file within the lifebrick package.
+        "lifebrick": ['data/*.mmdb', 'data/flags/*.png', 'data/flags/*.svg'],
+        # have to add the country flags database (png images)
+    },
+    # package_dir={'startpkg': 'src'},
+    include_package_data=True,
+    python_requires=">=3.5",
+    install_requires=requires,
+    entry_points='''
+        [console_scripts]
+        startpkg=src.cli:cli
+    ''',
+    license=about['__license__'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    project_urls={
+        'Documentation': 'https://azat.ai',
+        'Source': 'https://github.com/AzatAI/lifebrick',
+    },
+)
