5b7e8d4ab576dc4be626e6d2c45d7a57dab17be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:57 +0000

    Add package pyActigraphy/1.0rc1/pyActigraphy-1.0rc1.tar.gz

---
+++ packages/pyActigraphy/pyActigraphy-1.0rc1.tar.gz/pyActigraphy-1.0rc1/pyActigraphy/io/__init__.py
@@ -0,0 +1,31 @@
+"""IO module for reading raw data."""
+
+# Authors: Grégory Hammad <gregory.hammad@uliege.be>
+#
+# License: BSD (3-clause)
+
+# from . import awd
+# from . import rpx
+# from . import reader
+
+from .base import BaseRaw
+from .reader import RawReader
+
+from .reader import read_raw
+from .agd import read_raw_agd
+from .atr import read_raw_atr
+from .awd import read_raw_awd
+from .dqt import read_raw_dqt
+from .mtn import read_raw_mtn
+from .rpx import read_raw_rpx
+
+__all__ = [
+    "BaseRaw", "RawReader",
+    "read_raw",
+    "read_raw_agd",
+    "read_raw_atr",
+    "read_raw_awd",
+    "read_raw_dqt",
+    "read_raw_mtn",
+    "read_raw_rpx"
+]
