ddc6f3b88890f52ad8eb91547f24659fb6c7304a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:33 +0000

    Add package aim-cli/2.0.1/aim-cli-2.0.1.tar.gz

---
+++ packages/aim-cli/aim-cli-2.0.1.tar.gz/aim-cli-2.0.1/aim/sdk/__init__.py
@@ -0,0 +1,9 @@
+from aim.sdk.track import *
+from aim.sdk.init import *
+
+
+# Serializable classes references
+metric = 'Metric'
+hyperparams = 'HyperParameters'
+dataset = 'Dataset'
+checkpoint = 'Checkpoint'
---
+++ packages/aim-cli/aim-cli-2.0.1.tar.gz/aim-cli-2.0.1/aim/sdk/track.py
@@ -0,0 +1,51 @@
+import atexit
+
+from aim.engine.aim_repo import AimRepo
+from aim.artifacts import *
+from aim.artifacts.artifact_writer import ArtifactWriter
+import aim.logger
+from aim.engine.configs import (
+    AIM_BRANCH_ENV_VAR,
+    AIM_COMMIT_ENV_VAR,
+    AIM_AUTOMATED_EXEC_ENV_VAR,
+)
+
+
+repo = None
+
+
+def get_repo():
+    # Get ENV VARS
+    branch_name = os.getenv(AIM_BRANCH_ENV_VAR)
+    commit_hash = os.getenv(AIM_COMMIT_ENV_VAR)
+
+    global repo
+    if repo is None:
+        # Get aim repo from working directory
+        repo = AimRepo.get_working_repo(branch_name, commit_hash)
+        if repo is not None:
+            # Finalize and close storage at program exit
+            atexit.register(repo.close_records_storage)
+    return repo
+
+
+def track(artifact_name: str, *args, **kwargs):
+    if artifact_name not in globals():
+        print('Aim cannot track: \'{0}\''.format(artifact_name))
+        return
+
+    # Get corresponding class
+    obj = globals()[artifact_name]
+
+    # Create an instance
+    inst = obj(*args, **kwargs)
+
+    repo = get_repo()
+    if not repo:
+        print('Aim repository not found \n')
+        return
+
+    writer = ArtifactWriter()
+    writer.save(repo, inst)
+
+    return inst
