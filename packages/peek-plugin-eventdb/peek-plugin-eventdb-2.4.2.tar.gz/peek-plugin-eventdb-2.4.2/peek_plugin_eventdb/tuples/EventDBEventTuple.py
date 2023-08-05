f17b1b8b5aff7d396ba189e9bf72167a3c3a41e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:20 +0000

    Add package peek-plugin-eventdb/2.4.2/peek-plugin-eventdb-2.4.2.tar.gz

---
+++ packages/peek-plugin-eventdb/peek-plugin-eventdb-2.4.2.tar.gz/peek-plugin-eventdb-2.4.2/peek_plugin_eventdb/tuples/EventDBEventTuple.py
@@ -0,0 +1,24 @@
+from vortex.Tuple import Tuple, addTupleType
+
+from peek_plugin_eventdb._private.PluginNames import eventdbTuplePrefix
+
+
+@addTupleType
+class EventDBEventTuple(Tuple):
+    """ Event DB Event Tuple
+
+    This tuple stores a value of a key in the Event DB database.
+
+    datetime: is a timezone aware datetime
+    key: is the key of this alarm or event (Optional)
+    value: is the value of this event.
+
+    """
+    __tupleType__ = eventdbTuplePrefix + 'EventDBEventTuple'
+    __slots__ = ("dateTime", "key", "value")
+
+    @classmethod
+    def sqlCoreLoad(cls, row):
+        return EventDBEventTuple(datetime=row.datetime,
+                                 key=row.key,
+                                 value=row.value)
