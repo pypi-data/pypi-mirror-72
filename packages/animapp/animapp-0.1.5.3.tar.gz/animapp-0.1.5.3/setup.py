6b4d12638e2be0355cb30bfc9eb7d332c077510c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package animapp/0.1.5.3/animapp-0.1.5.3.tar.gz

---
+++ packages/animapp/animapp-0.1.5.3.tar.gz/animapp-0.1.5.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="animapp",
+    version="0.1.5.3",
+    author="Srinivasa Rao",
+    author_email="srinivasarao.rao@gmail.com",
+    description="A package to track the movement of an object (a small animal) in a video",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sraorao/animapp_pyqt5",
+    packages=setuptools.find_packages(),
+    # scripts=['bin/animapp', 'bin/threshold'],
+    entry_points={'console_scripts': ['threshold=animapp.set_thresholds:main', 'animapp=animapp.animapp:main'],},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
