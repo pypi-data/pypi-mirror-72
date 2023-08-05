814a89b8d20dded2f3d4b04d03d42e20d1e5fe67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package itay-bardugo-cron-migration/1.1.25/itay_bardugo_cron_migration-1.1.25-py3-none-any.whl

---
+++ packages/itay-bardugo-cron-migration/itay_bardugo_cron_migration-1.1.25-py3-none-any.whl/cron_migration/revisions/manager.py
@@ -0,0 +1,29 @@
+from .task import Task
+from typing import Type
+
+
+class TaskManager:
+    def __init__(self, task: Type[Task], path):
+        self.task = task
+        self._all_down_revisions = set()
+        self.next: Task = None
+        self.prev: Task = None
+        self.path = path
+
+    def get_down_revision(self):
+        return self.task.get_prev_revision_id()
+
+    def get_revision_id(self):
+        return self.task.get_revision_id()
+
+    def set_next(self, task: Task):
+        self.next = task
+
+    def set_prev(self, task: Task):
+        self.prev = task
+
+    def upgrade(self):
+        self.task().upgrade()
+
+    def downgrade(self):
+        self.task().downgrade()
