a9d7ecfadf634e3ea8c0abe52791092beca8cb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:36 +0000

    Add package ebel-rest/1.0.11/ebel_rest-1.0.11-py3-none-any.whl

---
+++ packages/ebel-rest/ebel_rest-1.0.11-py3-none-any.whl/ebel_rest/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for eBEL API client."""
+
+from ebel_rest.manager.core import connect
+from ebel_rest.manager.export import export_graph
+
+__author__ = """Christian Ebeling"""
+__email__ = 'Christian.Ebeling@scai.fraunhofer.de'
+__version__ = '1.0.11'
