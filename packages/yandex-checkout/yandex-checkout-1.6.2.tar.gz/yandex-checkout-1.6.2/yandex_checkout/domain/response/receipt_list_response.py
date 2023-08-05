d0f74e519b3d92f5fa80483ab922b99e2240255a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:05 +0000

    Add package yandex-checkout/1.6.2/yandex-checkout-1.6.2.tar.gz

---
+++ packages/yandex-checkout/yandex-checkout-1.6.2.tar.gz/yandex-checkout-1.6.2/yandex_checkout/domain/response/receipt_list_response.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from yandex_checkout.domain.common.response_object import ResponseObject
+from yandex_checkout.domain.response.receipt_response import ReceiptResponse
+
+
+class ReceiptListResponse(ResponseObject):
+    __type = None
+
+    __items = None
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value):
+        self.__type = value
+
+    @property
+    def items(self):
+        return self.__items
+
+    @items.setter
+    def items(self, value):
+        if isinstance(value, list):
+            self.__items = [ReceiptResponse(receipt) for receipt in value]
+        else:
+            self.__items = value
---
+++ packages/yandex-checkout/yandex-checkout-1.6.2.tar.gz/yandex-checkout-1.6.2/yandex_checkout/receipt.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import uuid
+
+from yandex_checkout.client import ApiClient
+from yandex_checkout.domain.common.http_verb import HttpVerb
+from yandex_checkout.domain.request.receipt_request import ReceiptRequest
+from yandex_checkout.domain.response.receipt_response import ReceiptResponse
+from yandex_checkout.domain.response.receipt_list_response import ReceiptListResponse
+
+
+class Receipt:
+    base_path = '/receipts'
+
+    def __init__(self):
+        self.client = ApiClient()
+
+    @classmethod
+    def list(cls, params):
+        if ('payment_id' in params or 'refund_id' in params) is False:
+            raise TypeError('Invalid params value')
+
+        instance = cls()
+        path = cls.base_path
+
+        response = instance.client.request(HttpVerb.GET, path, params)
+        return ReceiptListResponse(response)
+
+    @classmethod
+    def create(cls, params, idempotency_key=None):
+        """
+        Create receipt
+
+        :param params: data passed to API
+        :param idempotency_key:
+        :return: ReceiptResponse
+        """
+        instance = cls()
+        path = cls.base_path
+
+        if not idempotency_key:
+            idempotency_key = uuid.uuid4()
+
+        headers = {
+            'Idempotence-Key': str(idempotency_key)
+        }
+
+        if isinstance(params, dict):
+            params_object = ReceiptRequest(params)
+        elif isinstance(params, ReceiptRequest):
+            params_object = params
+        else:
+            raise TypeError('Invalid params value type')
+
+        response = instance.client.request(HttpVerb.POST, path, None, headers, params_object)
+        return ReceiptResponse(response)
