5f3f05df305d7a322dbb6f3dde3fc582b06da997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:37 +0000

    Add package toggl-python/0.2.3/toggl_python-0.2.3.tar.gz

---
+++ packages/toggl-python/toggl_python-0.2.3.tar.gz/toggl_python-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['toggl_python']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.13.3,<0.14.0', 'pydantic[email]>=1.5.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'toggl-python',
+    'version': '0.2.3',
+    'description': 'Python wraper for Toggl API.',
+    'long_description': 'Toggl Python API\n================\n\n![https://pypi.python.org/pypi/toggl_python](https://img.shields.io/pypi/v/toggl_python.svg) ![https://travis-ci.com/evrone/toggl_python](https://img.shields.io/travis/evrone/toggl_python.svg) ![https://toggl-python.readthedocs.io/en/latest/?badge=latest](https://readthedocs.org/projects/toggl-python/badge/?version=latest) ![https://pyup.io/repos/github/evrone/toggl_python/](https://pyup.io/repos/github/evrone/toggl_python/shield.svg)\n\nToggl Python API\n----------------\n\n* Based on open Toggl API documentation: https://github.com/toggl/toggl_api_docs/blob/master/toggl_api.md\n* Free software: MIT license\n* Documentation: https://toggl-python.readthedocs.io.\n\nInstallation\n------------\n`pip install toggl-python` or use [poetry](https://python-poetry.org) `poetry add toggl-python`\n\nFeatures\n--------\n\n- Get TimeEntries.\n\n```python\nfrom toggl_python import TokenAuth, TimeEntries\n\nif __name__ == "__main__":\n    auth = TokenAuth(\'AUTH_TOKEN\')\n    print(TimeEntries(auth=auth).list())\n```\n\n* *TODO*\n\nCredits\n-------\n\nThis package was created with [Cookiecutter](https://github.com/audreyr/cookiecutter) and the [audreyr/cookiecutter-pypackage](https://github.com/audreyr/cookiecutter-pypackage) project template.\n',
+    'author': 'Ivlev Denis',
+    'author_email': 'me@dierz.pro',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/evrone/toggl_python',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/toggl-python/toggl_python-0.2.3.tar.gz/toggl_python-0.2.3/toggl_python/repository.py
@@ -0,0 +1,196 @@
+from functools import partial
+
+import httpx
+
+from .api import Api
+from .entities import (
+    BaseEntity,
+    Client,
+    Dashboard,
+    Group,
+    Project,
+    ProjectUser,
+    Tag,
+    Task,
+    TimeEntry,
+    User,
+    Workspace,
+    WorkspaceUser,
+)
+from .exceptions import MethodNotAllowed, NotSupported
+
+
+class BaseRepository(Api):
+    LIST_URL = ""
+    DETAIL_URL = None
+    ENTITY_CLASS = BaseEntity
+    ADDITIONAL_METHODS = {}
+    EXCLUDED_METHODS = ()
+
+    def __init__(self, base_url=None, auth=None):
+        super().__init__(base_url=base_url, auth=auth)
+        if not self.DETAIL_URL:
+            self.DETAIL_URL = self.LIST_URL + "/{id}"
+
+    def __getattr__(self, method: str):
+        if method in self.EXCLUDED_METHODS:
+            raise MethodNotAllowed
+        try:
+            method = super().__getattr__(method)
+        except AttributeError:
+            if method in self.ADDITIONAL_METHODS.keys():
+                method = partial(
+                    self.additionat_method, **self.ADDITIONAL_METHODS[method]
+                )
+            else:
+                raise AttributeError(f"No such method ({method})!")
+        return method
+
+    def additionat_method(
+        self,
+        url: str,
+        _id: int = None,
+        additional_id: int = None,
+        entity: object = None,
+        detail: bool = False,
+        single_item: bool = False,
+        data_key: str = None,
+        params: dict = None,
+        data: dict = None,
+        files: dict = None,
+    ) -> httpx.Response:
+        """
+        Call additional method with specified url and params
+        """
+
+        if detail:
+            _url = (self.DETAIL_URL + "/" + url + "/{additional_id}").format(
+                id=_id, additional_id=additional_id
+            )
+            return self._retrieve(_url, entity, headers=self.HEADERS)
+        elif _id:
+            _url = (self.DETAIL_URL + "/" + url).format(id=_id)
+            return self._list(_url, entity, headers=self.HEADERS)
+        elif single_item:
+            _url = str(self.BASE_URL) + "/" + url
+            return self._retrieve(
+                _url, entity, headers=self.HEADERS, data_key="data"
+            )
+        else:
+            raise NotSupported
+
+    def _retrieve(self, _url, entity_class, data_key: str = "data", **kwargs):
+        response = self.get(_url)
+        data = response.json()
+        if data_key:
+            data = data[data_key]
+            return entity_class(**data)
+        else:
+            return entity_class(**data)
+
+    def retrieve(self, id: int = None, **kwargs):
+        full_url = self.BASE_URL.join(self.DETAIL_URL.format(id=id))
+        return self._retrieve(full_url, self.ENTITY_CLASS)
+
+    def _list(self, _url, entity_class, **kwargs):
+        response = self.get(_url)
+        return [entity_class(**entity) for entity in response.json()]
+
+    def list(self, **kwargs):
+        if "list" in self.EXCLUDED_METHODS:
+            raise MethodNotAllowed
+        full_url = self.BASE_URL.join(self.LIST_URL)
+        return self._list(full_url, self.ENTITY_CLASS)
+
+    def create(self, entity: ENTITY_CLASS):
+        if "create" in self.EXCLUDED_METHODS:
+            raise MethodNotAllowed
+        full_url = self.BASE_URL.join(self.LIST_URL)
+        response = self.post(full_url, data=entity.dict())
+        return self.ENTITY_CLASS(**response.json())
+
+    def update(self, entity: ENTITY_CLASS):
+        if "update" in self.EXCLUDED_METHODS:
+            raise MethodNotAllowed
+        full_url = self.BASE_URL.join(self.DETAIL_URL.format(id=entity.id))
+        response = self.put(full_url, data=entity.dict())
+        return self.ENTITY_CLASS(**response.json())
+
+    def partial_update(self, entity: ENTITY_CLASS):
+        if "partial_update" in self.EXCLUDED_METHODS:
+            raise MethodNotAllowed
+        full_url = self.BASE_URL.join(self.DETAIL_URL.format(id=entity.id))
+        response = self.patch(full_url, data=entity.dict())
+        return self.ENTITY_CLASS(**response.json())
+
+
+class Clients(BaseRepository):
+    LIST_URL = "clients"
+    ENTITY_CLASS = Client
+
+
+class Groups(BaseRepository):
+    LIST_URL = "groups"
+    ENTITY_CLASS = Group
+
+
+class Projects(BaseRepository):
+    LIST_URL = "projects"
+    ENTITY_CLASS = Project
+
+
+class ProjectUsers(BaseRepository):
+    LIST_URL = "project_users"
+    ENTITY_CLASS = ProjectUser
+
+
+class Tags(BaseRepository):
+    LIST_URL = "tags"
+    ENTITY_CLASS = Tag
+
+
+class Tasks(BaseRepository):
+    LIST_URL = "tasks"
+    ENTITY_CLASS = Task
+
+
+class TimeEntries(BaseRepository):
+    LIST_URL = "time_entries"
+    ENTITY_CLASS = TimeEntry
+
+
+class Users(BaseRepository):
+    EXCLUDED_METHODS = ("list", "create", "update", "partial_update")
+    LIST_URL = "users"
+    ENTITY_CLASS = User
+    ADDITIONAL_METHODS = {
+        "me": {"url": "me", "entity": User, "single_item": True},
+    }
+
+
+class Workspaces(BaseRepository):
+    LIST_URL = "workspaces"
+    ENTITY_CLASS = Workspace
+    ADDITIONAL_METHODS = {
+        "projects": {"url": "projects", "entity": Project, "detail": False},
+        "users": {"url": "users", "entity": User, "detail": False},
+        "clients": {"url": "clients", "entity": Client, "detail": False},
+        "groups": {"url": "groups", "entity": Group, "detail": False},
+        "tasks": {"url": "tasks", "entity": Task, "detail": False},
+        "tags": {"url": "tags", "entity": Tag, "detail": False},
+        "workspace_users": {
+            "url": "workspace_users",
+            "entity": WorkspaceUser,
+            "detail": False,
+        },
+    }
+
+
+class WorkspaceUsers(BaseRepository):
+    LIST_URL = "workspace_users"
+    ENTITY_CLASS = WorkspaceUser
+
+
+class Dashboards(BaseRepository):
+    LIST_URL = "dashboards"
+    ENTITY_CLASS = Dashboard
