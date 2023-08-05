f2865a97890217a4bbcc2bddb4434ebd2287c46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:24 +0000

    Add package django-inteliger/0.0.1/django_inteliger-0.0.1-py3-none-any.whl

---
+++ packages/django-inteliger/django_inteliger-0.0.1-py3-none-any.whl/log/middleware.py
@@ -0,0 +1,11 @@
+from core.views import Inteliger
+
+class LogMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        Inteliger().salvar_log(request=request, response=response)
+        return response
