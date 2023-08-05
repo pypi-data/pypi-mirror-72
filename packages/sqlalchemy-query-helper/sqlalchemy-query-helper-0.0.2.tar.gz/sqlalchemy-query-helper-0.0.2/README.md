bc424bd8469579592d4bafcf34daff8785072f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:17 +0000

    Add package sqlalchemy-query-helper/0.0.2/sqlalchemy-query-helper-0.0.2.tar.gz

---
+++ packages/sqlalchemy-query-helper/sqlalchemy-query-helper-0.0.2.tar.gz/sqlalchemy-query-helper-0.0.2/README.md
@@ -0,0 +1,17 @@
+# SQLAlchemy query helpermake
+
+## Usage
+
+## Development
+
+- Install `pipenv`
+
+- `make install`
+
+- Make your changes and open PR
+
+## Test
+
+- Install `sqllite3`
+
+- `make test`
---
+++ packages/sqlalchemy-query-helper/sqlalchemy-query-helper-0.0.2.tar.gz/sqlalchemy-query-helper-0.0.2/sqlalchemy_query_helper/query_generator.py
@@ -0,0 +1,38 @@
+from sqlalchemy import inspect
+import datetime
+
+
+op_mapping = {
+    "eq": lambda attr, value: attr == value,
+    "neq": lambda attr, value: attr != value,
+    "gt": lambda attr, value: attr > value,
+    "gte": lambda attr, value: attr >= value,
+    "lt": lambda attr, value: attr < value,
+    "lte": lambda attr, value: attr <= value,
+}
+
+
+def generate_query(session, model, raw_query):
+    return _generate_query(model, session.query(model), raw_query)
+
+
+def _generate_query(model, query, raw_query):
+    inst = inspect(model)
+    for c_attr in inst.mapper.column_attrs:
+        prop = c_attr.key
+        if prop in raw_query:
+            simple_query = raw_query[prop]
+            operation = op_mapping[simple_query["op"]]
+            value = simple_query["value"]
+            if c_attr.columns[0].type.python_type == datetime.datetime and isinstance(
+                simple_query["value"], str
+            ):
+                value = datetime.datetime.fromisoformat(value)
+            query = query.filter(operation(getattr(model, prop), simple_query["value"]))
+    for r_attr in inst.mapper.relationships:
+        prop = r_attr.key
+        if prop in raw_query:
+            query = query.join(getattr(model, prop))
+            query = _generate_query(r_attr.entity.class_, query, raw_query[prop])
+
+    return query
