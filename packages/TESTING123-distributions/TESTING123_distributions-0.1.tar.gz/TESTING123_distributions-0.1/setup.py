197cc28aa546e92e580bfc19956120158df10bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:45 +0000

    Add package TESTING123-distributions/0.1/TESTING123_distributions-0.1.tar.gz

---
+++ packages/TESTING123-distributions/TESTING123_distributions-0.1.tar.gz/TESTING123_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='TESTING123_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['TESTING123_distributions'],
+      zip_safe=False)
