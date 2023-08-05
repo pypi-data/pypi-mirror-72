3275ce3a0e55db326336460b04a79cbdf0a0de51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:37 +0000

    Add package myth/0.0.1/myth-0.0.1.tar.gz

---
+++ packages/myth/myth-0.0.1.tar.gz/myth-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+  name = 'myth',
+  version = '0.0.1',
+  description = '',
+  author = 'acegik',
+  license = 'GPL-3.0',
+  url = 'https://github.com/acegik/myth',
+  download_url = 'https://github.com/acegik/myth/downloads',
+  keywords = ['ansible', 'kubernetes', 'big-data'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
