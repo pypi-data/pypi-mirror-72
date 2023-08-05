fd3b7a87402f3bf0ea28f15e24d8d2e3e5cb64ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package pygdtf/1.0.2/pygdtf-1.0.2.tar.gz

---
+++ packages/pygdtf/pygdtf-1.0.2.tar.gz/pygdtf-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='pygdtf',
+    version='1.0.2',
+    description='General Device Type Format library',
+    long_description='Basic library for interfacing with the General Device Type Format developed by MA, Robe and '
+                     'Vectorworks. For more information on the format see gdtf-share.com',
+    license='MIT',
+    author='Jack Page',
+    author_email='jdpboc98@gmail.com',
+    packages=['pygdtf']
+)
