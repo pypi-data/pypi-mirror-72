faff386232e792b5994e5b96b7e15b36e57023da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:56 +0000

    Add package meo-distributions/0.1/meo_distributions-0.1.tar.gz

---
+++ packages/meo-distributions/meo_distributions-0.1.tar.gz/meo_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='meo_distributions',
+      version='0.1',
+      description='Gaussian distribution and Binomial Distribution',
+      packages=['meo_distributions'],
+      author= 'Moses Egbo',
+      zip_safe=False)
