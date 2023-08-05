cafea5927913e38c10dcebea6e65031b3c42deb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:48 +0000

    Add package jon-dist-prob/0.1/jon_dist_prob-0.1.tar.gz

---
+++ packages/jon-dist-prob/jon_dist_prob-0.1.tar.gz/jon_dist_prob-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jon_dist_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author="Mukhammad Khalimudin",
+      author_email="khalimudin@gmail.com",
+      zip_safe=False)
