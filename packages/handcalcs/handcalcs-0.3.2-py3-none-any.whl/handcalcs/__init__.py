a36441de6c1424d27842a09225ef9250205b1ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:31 +0000

    Add package handcalcs/0.3.2/handcalcs-0.3.2-py3-none-any.whl

---
+++ packages/handcalcs/handcalcs-0.3.2-py3-none-any.whl/handcalcs/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.3.2"
+from handcalcs.handcalcs import handcalc
+__all__ = ["handcalc"]
+
---
+++ packages/handcalcs/handcalcs-0.3.2-py3-none-any.whl/handcalcs/render.py
@@ -0,0 +1,51 @@
+from . import handcalcs as hand
+from . import numpy_and_pandas_patches as patches
+from IPython.core.magic import (Magics, magics_class, cell_magic, register_cell_magic)
+from IPython import get_ipython
+from IPython.core.magics.namespace import NamespaceMagics
+from IPython.display import Latex, Markdown, display
+from IPython.utils.capture import capture_output
+import sys
+
+try:
+    _nms = NamespaceMagics()
+    _Jupyter = get_ipython()
+    _nms.shell = _Jupyter.kernel.shell
+    cell_capture = capture_output(stdout=True, stderr=True, display=True)
+except AttributeError:
+    raise ImportError("handcalcs.render is intended for a Jupyter environment."
+    " Use 'from handcalcs import handcalc' for the decorator interface.")
+
+@register_cell_magic
+def render(line, cell):
+    # Run the cell
+    with cell_capture:
+        _nms.shell.run_cell(cell)
+
+    # Retrieve variables from the local namespace
+    var_list = _nms.who_ls()
+    var_dict = {v: _nms.shell.user_ns[v] for v in var_list}
+    renderer = hand.LatexRenderer(cell, var_dict)
+    latex_code = renderer.render()
+    display(Latex(latex_code))
+
+
+@register_cell_magic
+def tex(line, cell):
+    # Run the cell
+    _nms.shell.run_cell(cell)
+    
+    # Retrieve variables from the local namespace
+    var_list = _nms.who_ls()
+    var_dict = {v: _nms.shell.user_ns[v] for v in var_list}
+    renderer = hand.LatexRenderer(cell, var_dict)
+    latex_code = renderer.render()
+    print(latex_code)
+
+def load_ipython_extension(ipython):
+    """This function is called when the extension is
+    loaded. It accepts an IPython InteractiveShell
+    instance. We can register the magic with the
+    `register_magic_function` method of the shell
+    instance."""
+    ipython.register_magic_function(render, 'cell')
