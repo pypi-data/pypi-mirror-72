dfcb802d81dbc5f623eb35e4b1a50a6ade0b4780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:06 +0000

    Add package stat-dist333/0.1/stat_dist333-0.1.tar.gz

---
+++ packages/stat-dist333/stat_dist333-0.1.tar.gz/stat_dist333-0.1/README.md
@@ -0,0 +1,19 @@
+# stat-dist (i.e. Statistical Distributions) package
+
+Summary of the package
+
+
+# Files
+
+Explanation of the files in the package
+
+
+# Installation
+
+Install information goes here 
+
+
+# Usage:
+
+Use demo
+
---
+++ packages/stat-dist333/stat_dist333-0.1.tar.gz/stat_dist333-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'stat_dist333',
+      version = '0.1',
+      description = 'Statistical distributions - Gaussian, Binomial etc..',
+      packages = ['stat_dist333'],
+      author = 'Amanpreet Singh',
+      author_email = 'amanpreetsingh459@gmail.com',
+      zip_safe = False)
