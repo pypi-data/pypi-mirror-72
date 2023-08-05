6c34fef609ddb281fa8a0ce7a8c502e13165d830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:07 +0000

    Add package lintreview/2.34.5/lintreview-2.34.5-py2.py3-none-any.whl

---
+++ packages/lintreview/lintreview-2.34.5-py2.py3-none-any.whl/lintreview/tools/ansible.py
@@ -0,0 +1,42 @@
+import os
+import logging
+import lintreview.docker as docker
+from lintreview.tools import Tool, process_quickfix
+
+log = logging.getLogger(__name__)
+
+
+class Ansible(Tool):
+
+    name = 'ansible'
+
+    def check_dependencies(self):
+        """
+        See if python2 container exists
+        """
+        return docker.image_exists('python2')
+
+    def match_file(self, filename):
+        base = os.path.basename(filename)
+        name, ext = os.path.splitext(base)
+        return ext == '.yml'
+
+    def process_files(self, files):
+        """
+        Run code checks with ansible-lint.
+        Only a single process is made for all files
+        to save resources.
+        """
+        command = ['ansible-lint', '-p']
+        if self.options.get('ignore'):
+            command += ['-x', self.options.get('ignore')]
+        command += files
+        output = docker.run('python2', command, self.base_path)
+        if not output:
+            log.debug('No ansible-lint errors found.')
+            return False
+
+        output = output.split("\n")
+        output.sort()
+
+        process_quickfix(self.problems, output, docker.strip_base)
