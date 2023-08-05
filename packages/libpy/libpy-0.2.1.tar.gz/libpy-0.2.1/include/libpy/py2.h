344db30deb36688aa1e6e425707520121a2baef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:43 +0000

    Add package libpy/0.2.1/libpy-0.2.1.tar.gz

---
+++ packages/libpy/libpy-0.2.1.tar.gz/libpy-0.2.1/include/libpy/py2.h
@@ -0,0 +1,9 @@
+#include "libpy/detail/python.h"
+
+#if PY_MAJOR_VERSION == 2
+#define Py_RETURN_NOTIMPLEMENTED                                                         \
+    Py_INCREF(Py_NotImplemented);                                                        \
+    return Py_NotImplemented;
+
+using Py_hash_t = long;
+#endif
