28ee7cae6f065e7cffc467d1da313bb0d56bb74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:46 +0000

    Add package django-gisserver/0.8/django_gisserver-0.8-py2.py3-none-any.whl

---
+++ packages/django-gisserver/django_gisserver-0.8-py2.py3-none-any.whl/gisserver/parsers/fes20/filters.py
@@ -0,0 +1,82 @@
+from typing import AnyStr, Optional, Union
+from xml.etree.ElementTree import Element, QName
+
+from defusedxml.ElementTree import ParseError, fromstring
+
+from gisserver.parsers.base import FES20, tag_registry
+from gisserver.parsers.utils import expect_tag
+from . import expressions, identifiers, operators, query
+
+FilterPredicates = Union[expressions.Function, operators.Operator]
+
+
+class Filter:
+    """The <fes:Filter> element.
+
+    As this is a wrapper, it only contains a "predicate" element with the contents.
+    """
+
+    query_language = "urn:ogc:def:queryLanguage:OGC-FES:Filter"
+
+    predicate: FilterPredicates
+    source: Optional[AnyStr]
+
+    def __init__(self, predicate: FilterPredicates, source: Optional[AnyStr] = None):
+        self.predicate = predicate
+        self.source = source
+
+    @classmethod
+    def from_string(cls, text: AnyStr) -> "Filter":
+        """Parse an XML <fes20:Filter> string.
+
+        This uses defusedxml by default, to avoid various XML injection attacks.
+
+        :raises ValueError: When data is incorrect, or XML has syntax errors.
+        :raises NotImplementedError: When unsupported features are called.
+        """
+        try:
+            root_element = fromstring(text)
+        except ParseError as e:
+            # Offer consistent results for callers to check for invalid data.
+            raise ValueError(str(e)) from e
+        return Filter.from_xml(root_element, source=text)
+
+    @classmethod
+    @expect_tag(FES20, "Filter")
+    def from_xml(cls, element: Element, source: Optional[AnyStr] = None) -> "Filter":
+        """Parse the <fes20:Filter> element."""
+        if len(element) > 1 or element[0].tag == QName(FES20, "ResourceId"):
+            # fes20:ResourceId is the only element that may appear multiple times.
+            return Filter(
+                predicate=operators.IdOperator(
+                    [identifiers.Id.from_child_xml(child) for child in element]
+                ),
+                source=source,
+            )
+        else:
+            return Filter(
+                predicate=tag_registry.from_child_xml(
+                    element[0], allowed_types=(expressions.Function, operators.Operator)
+                ),
+                source=source,
+            )
+
+    def compile_query(self) -> query.CompiledQuery:
+        """Collect the data to perform a Django ORM query."""
+        compiler = query.CompiledQuery()
+
+        # Function, Operator, IdList
+        q_object = self.predicate.build_query(compiler)
+        if q_object is not None:
+            compiler.add_lookups(q_object)
+
+        return compiler
+
+    def __repr__(self):
+        return f"Filter(predicate={self.predicate!r}, source={self.source})"
+
+    def __eq__(self, other):
+        if isinstance(other, Filter):
+            return self.predicate == other.predicate
+        else:
+            return NotImplemented
