c727041ee2166a9350812833243731ed269281f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:02 +0000

    Add package pyazureutils/0.0.0.1/pyazureutils-0.0.0.1-py3-none-any.whl

---
+++ packages/pyazureutils/pyazureutils-0.0.0.1-py3-none-any.whl/pyazureutils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Python Microsoft Azure Web Services Utilities
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pyazureutils is a collection of utilities for interacting with Microsoft Azure Web Services
+"""
---
+++ packages/pyazureutils/pyazureutils-0.0.0.1-py3-none-any.whl/pyazureutils/version.py
@@ -0,0 +1,2 @@
+""" This file was generated when pyazureutils was built """
+VERSION = '0.0.0.1'
