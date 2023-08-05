137fba00e3a47d253a87427ccc306055549ebf6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package pickle5/0.0.11/pickle5-0.0.11.tar.gz

---
+++ packages/pickle5/pickle5-0.0.11.tar.gz/pickle5-0.0.11/README.rst
@@ -0,0 +1,30 @@
+==========================================================
+`pickle5` -- A backport of the pickle 5 protocol (PEP 574)
+==========================================================
+
+This package backports all features and APIs added in the ``pickle`` module
+in Python 3.8.3, including the
+`PEP 574 <https://www.python.org/dev/peps/pep-0574/>`_ additions.  It should
+work with Python 3.5, 3.6 and 3.7.
+
+Basic usage is similar to the ``pickle`` module, except that the module
+to be imported is ``pickle5``:
+
+.. code-block:: python
+
+   import pickle5 as pickle
+
+   pb = pickle.PickleBuffer(b"foo")
+   data = pickle.dumps(pb, protocol=5)
+   assert pickle.loads(data) == b"foo"
+
+Detailed documentation can be found in PEP 574 and the standard
+`pickle documentation <https://docs.python.org/3.8/library/pickle.html>`_.
+
+.. image:: https://travis-ci.org/pitrou/pickle5-backport.svg?branch=master
+   :target: https://travis-ci.org/pitrou/pickle5-backport
+   :align: left
+
+.. image:: https://ci.appveyor.com/api/projects/status/hk4lrl81wp0q283o/branch/master?svg=true
+   :target: https://ci.appveyor.com/project/pitrou/pickle5-backport/branch/master
+   :align: left
---
+++ packages/pickle5/pickle5-0.0.11.tar.gz/pickle5-0.0.11/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, Extension, find_packages
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+extra_compile_args = []
+if os.name == 'posix':
+    # Assume a gcc/clang-compatible CLI and enable C99 on old compilers
+    extra_compile_args.append('-std=c99')
+
+ext_modules = [
+    Extension('pickle5._pickle',
+              ['pickle5/_pickle.c', 'pickle5/picklebufobject.c'],
+              depends=['pickle5/picklebufobject.h',
+                       'pickle5/compat.h'],
+              extra_compile_args=extra_compile_args),
+    ]
+
+setup(
+    name="pickle5",
+    version="0.0.11",
+    author="Antoine Pitrou",
+    author_email="antoine@python.org",
+    description="Backport of the pickle 5 protocol (PEP 574) and other pickle changes",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/pitrou/pickle5-backport",
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+)
