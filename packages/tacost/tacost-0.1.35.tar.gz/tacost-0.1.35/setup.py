97f3875ec6877dd43e4ab16de491b434ee76fcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package tacost/0.1.35/tacost-0.1.35.tar.gz

---
+++ packages/tacost/tacost-0.1.35.tar.gz/tacost-0.1.35/setup.py
@@ -0,0 +1,32 @@
+"""Setup for the chocobo package."""
+import tacost
+import setuptools
+
+version_number = tacost.__version__
+
+setuptools.setup(
+    author="Thejasvi Beleyur",
+    author_email="thejasvib@gmail.com",
+    name='tacost',
+    license="MIT",
+    description='tact: Test ACoustic Tracking: Create simulated sound sources/ trajectories to test the accuracy of an acoustic tracking system',
+    version=version_number,
+	long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",    
+    url='https://github.com/thejasvibr/test_acoustictracking.git',
+    packages=setuptools.find_packages(),
+	install_requires=['numpy','pandas','pyyaml','scipy'],
+    python_requires=">=3.5",
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Science/Research',
+    ],
+)
---
+++ packages/tacost/tacost-0.1.35.tar.gz/tacost-0.1.35/tacost/tests/test_make_sim_audio.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+""" Tests for make_sim_audio
+Created on Thu May 21 00:35:36 2020
+
+@author: tbeleyur
+"""
+
+
