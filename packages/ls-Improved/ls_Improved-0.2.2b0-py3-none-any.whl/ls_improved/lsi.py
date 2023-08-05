4d9a47b6d0e2f24405176418f67495c7475fb3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:56 +0000

    Add package ls-Improved/0.2.2b0/ls_Improved-0.2.2b0-py3-none-any.whl

---
+++ packages/ls-Improved/ls_Improved-0.2.2b0-py3-none-any.whl/ls_improved/lsi.py
@@ -0,0 +1,104 @@
+import os
+import argparse
+from glob import glob
+
+
+# Other settings
+class pycolor:
+    red = '\033[31m'
+    green = '\033[32m'
+    yellow = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    END = '\033[0m'
+    BOLD = '\038[1m'
+    UNDERLINE = '\033[4m'
+    INVISIBLE = '\033[08m'
+    REVERCE = '\033[07m'
+
+
+class Lsi():
+    def __init__(self, dir, is_all=False):
+        self.dir = dir
+        self.is_all = is_all
+
+        self.desc_name = '.description.lsi'
+        self.c_dir = pycolor.CYAN
+        self.c_desc = pycolor.yellow
+        self.c_end = pycolor.END
+        self.c_under = pycolor.UNDERLINE
+
+    # Raise Error
+    def _assert_dir_existance(self, dir):
+        if not os.path.isdir(dir):
+            print('error: ' + dir[:-1] + ' is not a directory.')
+            exit()
+
+    # Get children
+    def _get_children_of_dir(self, dir):
+        pathes = []
+        if self.is_all:
+            pathes = glob(dir+'.*')
+        pathes += glob(dir+'*')
+        children_d = [p for p in pathes if os.path.isdir(p)]
+        children_f = [p for p in pathes if os.path.isfile(p)]
+        return children_d, children_f
+
+    # load descriprion
+    def _read_description(self, desc_path, dir_length):
+        if os.path.exists(desc_path):
+            with open(desc_path, 'r') as f:
+                description = f.read().replace('\n', '\n       '+' '*dir_length)
+        else:
+            description = 'Dir'
+        return description
+    
+    def _print_children_d(self, children_d):
+        for dir in children_d:
+            dir_name = dir.split('/')[-1]
+            dir_length = len(dir_name)
+            dir_name = self.c_dir + self.c_under + dir_name + self.c_end
+            desc_path = dir +'/' + self.desc_name
+            description = self._read_description(desc_path, dir_length)
+            ## 最終行が空白のみの場合除去
+            if set(description.split('\n')[-1])==set(' '):
+                description = '\n'.join(description.split('\n')[:-1])
+            ## descriptionが指定されているなら色を付ける
+            description = self.c_desc + description + self.c_end if description != 'Dir' else description
+            output = ' -- ' + dir_name + ' / ' + description
+            print(output)
+
+    def _print_children_f(self, children_f):
+        for file in children_f:
+            file_name = file.split('/')[-1]
+            description = 'File'
+            description = self.c_desc + description + self.c_end if description != 'File' else description
+            output = ' -- ' + file_name +' / '+description
+            print(output)
+
+    def run(self):
+        self._assert_dir_existance(self.dir)
+        children_d, children_f = self._get_children_of_dir(self.dir)
+        self._print_children_d(children_d)
+        self._print_children_f(children_f)
+
+
+def main():
+    # Parser setting
+    parser = argparse.ArgumentParser(description="lsi ==lsImproved==")
+    parser.add_argument('dir', type=str, nargs='?', default="./", metavar='DirectoryPath', help='directory where you want to look. (default: current directory)')
+    parser.add_argument('-a','--all', action='store_true', help='show hidden files and directories. (default: Hidden)')
+    args = parser.parse_args()
+
+    # Get parser arguments
+    dir = args.dir
+    dir = dir+'/' if dir[-1] != '/' else dir
+    is_all = args.all
+
+    lsi = Lsi(dir, is_all=is_all)
+    lsi.run()
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ls-Improved/ls_Improved-0.2.2b0-py3-none-any.whl/ls_improved/mkdiri.py
@@ -0,0 +1,75 @@
+import os
+import argparse
+
+
+# Other settings
+class pycolor:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    END = '\033[0m'
+    BOLD = '\038[1m'
+    UNDERLINE = '\033[4m'
+    INVISIBLE = '\033[08m'
+    REVERCE = '\033[07m'
+
+class Mkdiri():
+    def __init__(self, dir, description, is_add_mode=False):
+        self.dir = dir
+        self.description = description
+        self.is_add_mode = is_add_mode
+
+        self.desc_name = '.description.lsi'
+
+    def _write_description(self, desc_path, description):
+        with open(desc_path, 'w') as f:
+            f.write(description)
+
+    def _mkdiri(self, dir_path, description):
+        os.mkdir(dir_path)
+        self._write_description(dir_path+self.desc_name, description)
+
+    def _raise_file_exists_error(self, dir_path, description):
+        if self.is_add_mode:
+            self._write_description(dir_path+self.desc_name, description)
+        else:
+            print('error: "' + dir_path[:-1] + '" already exists.')
+            print('if you want to add a description, use "-a" option please.')
+            exit()
+
+    def _raise_path_not_found_error(self):
+        print('error: invalid path.')
+
+    def run(self):
+        try:
+            self._mkdiri(self.dir, self.description)
+        except OSError as e:
+            e = str(e)
+            if 'File exists' in e:
+                self._raise_file_exists_error(self.dir, self.description)
+            if 'No such file or directory' in e:
+                self._raise_path_not_found_error()
+
+def main():
+    # Parser setting
+    parser = argparse.ArgumentParser(description="mkdiri ==mkdirImproved==")
+    parser.add_argument('dir', type=str, default="./", metavar='DirectoryPath', help='directory where you want to look. (default: current directory)')
+    parser.add_argument('description', type=str, nargs='?', default='Dir', help='description message (default:No description.)')
+    parser.add_argument('-a','--add', action='store_true', help='add description if directory already exists. (default:False)')
+    args = parser.parse_args()
+
+    # Get parser arguments
+    dir = args.dir
+    dir = dir+'/' if dir[-1] != '/' else dir
+    description = args.description
+    is_add_mode = args.add
+
+    mkdiri = Mkdiri(dir, description, is_add_mode)
+    mkdiri.run()
+
+if __name__ == '__main__':
+    main()
