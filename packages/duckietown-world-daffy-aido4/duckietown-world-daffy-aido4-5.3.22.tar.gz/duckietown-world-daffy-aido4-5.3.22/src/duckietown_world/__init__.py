3e23e19f83fb91879f1299486b19a39755ecf4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package duckietown-world-daffy-aido4/5.3.22/duckietown-world-daffy-aido4-5.3.22.tar.gz

---
+++ packages/duckietown-world-daffy-aido4/duckietown-world-daffy-aido4-5.3.22.tar.gz/duckietown-world-daffy-aido4-5.3.22/src/duckietown_world/__init__.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+__version__ = "5.3.22"
+
+# import zuper_json
+import logging
+
+
+logging.basicConfig()
+logger = logging.getLogger("dt-world")
+logger.setLevel(logging.DEBUG)
+
+logger.info("duckietown-world %s" % __version__)
+# for templating to work
+# from zuper_ipce import logger as l
+# _ = l
+
+# remove noisy logging
+from duckietown_serialization_ds1 import logger as dslogger
+
+dslogger.setLevel(logging.CRITICAL)
+
+from contracts import disable_all, __version__ as contracts_version
+
+# logger.info('contracts %s ' % contracts_version)
+disable_all()
+
+
+from .geo import *
+from .seqs import *
+from .svg_drawing import *
+from .world_duckietown import *
