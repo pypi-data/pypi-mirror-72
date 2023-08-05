0a048a8736ac521d9a96c02dc6569705e78b4e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:12 +0000

    Add package standard-stats-distributions/0.1/standard_stats_distributions-0.1.tar.gz

---
+++ packages/standard-stats-distributions/standard_stats_distributions-0.1.tar.gz/standard_stats_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='standard_stats_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['standard_stats_distributions'],
+      author="Mickey Sompong",
+      zip_safe=False)
