73952fc04d38bbe399be831c40f1ef31109b7cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:15 +0000

    Add package litenet/0.0.2/litenet-0.0.2.tar.gz

---
+++ packages/litenet/litenet-0.0.2.tar.gz/litenet-0.0.2/litenet/cli.py
@@ -0,0 +1,29 @@
+import click
+from litenet.server import LiteNetServer
+from litenet.client import LiteNetClient
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.argument()
+@click.option("-h", "--host", required=True, type=str, help="The ip to run the server on. (Must have access to it.)")
+@click.option("-p", "--port", default=5050, type=int, help="The port to run the server on. (Must be a valid port.)")
+@click.option("--header", "header", default=64, type=int, help="The size of the server message header. (Adjust as needed.)")
+def server(host, port, header):
+    litenet = LiteNetServer(host, port, header=header)
+    litenet.start()
+
+@click.argument()
+@click.option("-h", "--host", required=True, type=str, help="The ip the server's running on. (Must have access to it.)")
+@click.option("-p", "--port", default=5050, type=int, help="The port the server's running on. (Must be a valid port.)")
+@click.option("--header", "header", default=64, type=int, help="The size of the server message header. (Must match the server's.)")
+def client(host, port, header):
+    client = LiteNetClient(host, port, header)
+    client.start()
+
+
+cli.add_argument(server)
+cli.add_argument(client)
