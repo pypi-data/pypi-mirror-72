7ed44ad638a28aad4a040ef96dcf67f99cf0fc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package pycryptodome/3.9.8/pycryptodome-3.9.8.tar.gz

---
+++ packages/pycryptodome/pycryptodome-3.9.8.tar.gz/pycryptodome-3.9.8/src/x.h
@@ -0,0 +1,7 @@
+/* This file was automatically generated, do not edit */
+#include "common.h"
+const unsigned p256_n_tables;
+const unsigned p256_window_size;
+const unsigned p256_points_per_table;
+const uint64_t p256_tables[52][32][2][4];
+
