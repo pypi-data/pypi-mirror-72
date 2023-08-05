2333fdfcba44221abaaa81b017fe34d71a50ce89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package pytrustplatform/0.0.0.2/pytrustplatform-0.0.0.2-py3-none-any.whl

---
+++ packages/pytrustplatform/pytrustplatform-0.0.0.2-py3-none-any.whl/pytrustplatform/__init__.py
@@ -0,0 +1,6 @@
+"""
+Python Trust Platform utilities
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pytrustplatform is a collection of utilities for interacting with Microchip Trust Platform and Microchip CryptoAuthentication™ devices
+"""
