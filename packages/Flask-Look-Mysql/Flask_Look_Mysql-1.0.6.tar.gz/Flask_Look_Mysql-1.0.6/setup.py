0e3fb775a2ea5bf166992208ee588bdece219177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package Flask-Look-Mysql/1.0.6/Flask_Look_Mysql-1.0.6.tar.gz

---
+++ packages/Flask-Look-Mysql/Flask_Look_Mysql-1.0.6.tar.gz/Flask_Look_Mysql-1.0.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/16 0016 11:04
+# @File    : setup.py
+# @author  : dfkai
+# @Software: PyCharm
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask_Look_Mysql',
+    version='1.0.6',
+    keywords='flask look mysql html',
+    description='a look mysql html',
+    license='MIT License',
+    url='https://github.com/libaibuaidufu/Flask-Look-Mysql',
+    author='libaibuaidufu',
+    author_email='libaibuaidufu@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=["flask", "pymysql", "cachelib"],
+)
+
+"""
+python setup.py sdist bdist_wheel 
+twine upload dist/*
+# or python setup.py sdist bdist_wheel upload
+"""
