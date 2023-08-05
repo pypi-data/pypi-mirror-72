ec71c2d2da4a2b4ed70362ef1b9c0dc879ce3130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package kora/0.2.14/kora-0.2.14.tar.gz

---
+++ packages/kora/kora-0.2.14.tar.gz/kora-0.2.14/kora/selenium.py
@@ -0,0 +1,13 @@
+# install chromium, its driver, and selenium
+import os
+os.system('apt install chromium-chromedriver')
+os.system('pip install selenium')
+# set options to be headless, ..
+from selenium import webdriver
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+# create a webdriver instance, ready to use
+wd = webdriver.Chrome('chromedriver',options=options)
+wd.select = wd.find_elements_by_css_selector
