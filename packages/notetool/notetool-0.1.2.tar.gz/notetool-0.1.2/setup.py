092f91cc5f50970ee7188d102a516f3c6edf7aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:11 +0000

    Add package notetool/0.1.2/notetool-0.1.2.tar.gz

---
+++ packages/notetool/notetool-0.1.2.tar.gz/notetool-0.1.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/03/30 12:10
+# @Author  : niuliangtao
+# @Site    :
+# @File    : setup.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+
+install_requires = ['IPython', 'matplotlib', 'pycurl', 'cryptography', 'six']
+
+setup(name='notetool',
+      version='0.1.2',
+      description='notetool',
+      author='niuliangtao',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+
+      packages=find_packages(),
+      install_requires=install_requires
+      )
