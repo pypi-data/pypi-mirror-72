c23e8398f9c9cc87f982c0287b2bc7f17e3222f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package bestpix/1.5.1/bestpix-1.5.1.tar.gz

---
+++ packages/bestpix/bestpix-1.5.1.tar.gz/bestpix-1.5.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bestpix',
+    version='1.5.01',
+    author='Michael Chuprin',
+    author_email='mkchuprin@gmail.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mkchuprin/BestPix',
+    scripts=['scripts/reveal', 'scripts/cleanup'],
+    license='GNU GPLv3',
+    packages=['bestpix'],
+    include_package_data=True,
+    python_requires='>=3',
+)
