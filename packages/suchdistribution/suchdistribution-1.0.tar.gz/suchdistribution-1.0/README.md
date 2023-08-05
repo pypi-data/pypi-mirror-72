2c2b39d532401832d8e1b131c9dd08cc1b654d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:59 +0000

    Add package suchdistribution/1.0/suchdistribution-1.0.tar.gz

---
+++ packages/suchdistribution/suchdistribution-1.0.tar.gz/suchdistribution-1.0/README.md
@@ -0,0 +1,8 @@
+#probability_distributions package
+
+This is a package that does many wonderful things that normal people have no interest in whatsoever.
+If you count yourself amongst them, then all you need to know is:
+
+'Funny code big good.'
+
+Enjoy!
---
+++ packages/suchdistribution/suchdistribution-1.0.tar.gz/suchdistribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='suchdistribution',
+      version='1.0',
+      description='All the things regarding Gaussian and Binomial distributions',
+      packages=['suchdistribution'],
+      author='Behan Boshoff',
+      author_email = 'bhnboshoff@gmail.com',
+      zip_safe=False)
