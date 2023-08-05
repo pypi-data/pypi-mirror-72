3ea8f235304a878b75cde0b6df0aa54ace4e819f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package globmatch-temp/0.1.1/globmatch_temp-0.1.1.tar.gz

---
+++ packages/globmatch-temp/globmatch_temp-0.1.1.tar.gz/globmatch_temp-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['globmatch_temp']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'globmatch-temp',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Damien Krystkiewicz',
+    'author_email': 'damien.krystkiewicz@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
