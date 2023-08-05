1759335d2e2131e6947a49d20b04c842908d3d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:59 +0000

    Add package plone.namedfile/5.4.0/plone.namedfile-5.4.0-py2.py3-none-any.whl

---
+++ packages/plone.namedfile/plone.namedfile-5.4.0-py2.py3-none-any.whl/plone/namedfile/testing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from plone.testing import Layer
+from plone.testing import publisher
+from plone.testing import zca
+
+from plone.testing import zope
+from zope.configuration import xmlconfig
+
+
+class NamedFileTestLayer(Layer):
+
+    defaultBases = (zope.STARTUP, publisher.PUBLISHER_DIRECTIVES)
+
+    def setUp(self):
+        zca.pushGlobalRegistry()
+
+        import plone.namedfile
+        xmlconfig.file('testing.zcml', plone.namedfile)
+
+    def tearDown(self):
+        # Zap the stacked zca context
+        zca.popGlobalRegistry()
+
+
+PLONE_NAMEDFILE_FIXTURE = NamedFileTestLayer()
+
+PLONE_NAMEDFILE_INTEGRATION_TESTING = zope.IntegrationTesting(
+    bases=(PLONE_NAMEDFILE_FIXTURE, ),
+    name='plone.namedfile:NamedFileTestLayerIntegration',
+)
+
+PLONE_NAMEDFILE_FUNCTIONAL_TESTING = zope.FunctionalTesting(
+    bases=(PLONE_NAMEDFILE_FIXTURE, ),
+    name='plone.namedfile:NamedFileTestLayerFunctional',
+)
