5d6a14799c70f873b85d25491048e0a33f0c4fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:22 +0000

    Add package gmplot/1.4.0/gmplot-1.4.0.tar.gz

---
+++ packages/gmplot/gmplot-1.4.0.tar.gz/gmplot-1.4.0/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+__version__ = '1.4.0'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def get_requirements(requirements_file):
+    with open(requirements_file) as f:
+        return f.read().strip().splitlines()
+
+setup(
+    name = 'gmplot',
+    version = __version__,
+    author = 'gmplot contributors',
+    url = 'https://github.com/gmplot/gmplot',
+    description = 'A matplotlib-like interface to plot data with Google Maps.',
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='python google-maps visualization',
+    packages = find_packages(),
+    include_package_data=True,
+    package_data = {
+        'gmplot': ['markers/*.png'],
+    },
+    install_requires=get_requirements('requirements.txt'),
+)
