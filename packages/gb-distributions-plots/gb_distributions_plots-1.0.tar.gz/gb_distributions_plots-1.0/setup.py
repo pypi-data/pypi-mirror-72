510d2e7d010ae07995059ed42d52ff04cc17f53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:57 +0000

    Add package gb-distributions-plots/1.0/gb_distributions_plots-1.0.tar.gz

---
+++ packages/gb-distributions-plots/gb_distributions_plots-1.0.tar.gz/gb_distributions_plots-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gb_distributions_plots',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_distributions_plots'],
+      author = 'Kaustubh Katkar',
+      zip_safe=False)
