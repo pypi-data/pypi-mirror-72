39dd8e47b339ad3052566bcb8da5a04d14564215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:34 +0000

    Add package probdist-metrics/0.1/probdist_metrics-0.1.tar.gz

---
+++ packages/probdist-metrics/probdist_metrics-0.1.tar.gz/probdist_metrics-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probdist_metrics',
+      version='0.1',
+      description='Gaussian and Binomial distribution metrics',
+      packages=['probdist_metrics'],
+      author='Vikash Sharma',
+      author_email='vikash.sharma.inbox@gmail.com',
+      zip_safe=False)
