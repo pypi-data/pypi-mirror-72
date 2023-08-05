333801793e660280a6f6c512bf2670753b87ec5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:00 +0000

    Add package binary-refinery/0.2.1/binary_refinery-0.2.1-py3-none-any.whl

---
+++ packages/binary-refinery/binary_refinery-0.2.1-py3-none-any.whl/refinery/units/formats/pe/perc.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pefile
+from fnmatch import fnmatch
+
+from ... import arg, Unit
+
+
+class rsrc:
+    RESOURCE_TYPES = {
+        'CURSOR': 1,
+        'BITMAP': 2,
+        'ICON': 3,
+        'MENU': 4,
+        'DIALOG': 5,
+        'STRING': 6,
+        'FONTDIR': 7,
+        'FONT': 8,
+        'ACCELERATOR': 9,
+        'RCDATA': 10,
+        'MESSAGETABLE': 11,
+        'VERSION': 16,
+        'DLGINCLUDE': 17,
+        'PLUGPLAY': 19,
+        'VXD': 20,
+        'ANICURSOR': 21,
+        'ANIICON': 22,
+        'HTML': 23,
+        'MANIFEST': 24,
+    }
+
+    @staticmethod
+    def to_name(number):
+        for key in rsrc.RESOURCE_TYPES:
+            if rsrc.RESOURCE_TYPES[key] == number:
+                return key
+        return str(number)
+
+    def __init__(self, path):
+        self.root = False
+        self.needles = tuple(
+            self._parse(k, x)
+            for k, x in enumerate(path.split('/'))
+        )
+
+    def relax(self):
+        return rsrc(F'*/{self}')
+
+    def __str__(self):
+        return '/'.join(self.to_name(x) for x in self.needles)
+
+    def _parse(self, index, s):
+        if not index:
+            try:
+                translation = self.RESOURCE_TYPES[s]
+            except KeyError:
+                pass
+            else:
+                self.root = True
+                return translation
+        try:
+            return int(s, 0)
+        except ValueError:
+            pass
+        return s
+
+
+class perc(Unit):
+    """
+    Extract PE file resources.
+    """
+
+    def __init__(self, *entries: arg(metavar='entry', type=rsrc, help=(
+        'A resource path for the resource to be extracted. May contain numeric '
+        'literals and name strings with wildcards.'
+    ))):
+        super().__init__(entries=entries)
+
+    def _match(self, level, needle, e):
+        if e.id == needle or str(e.id) == needle:
+            return True
+        if level == 0 and rsrc.to_name(needle) == str(e.name):
+            return True
+        elif fnmatch(str(e.name), str(needle)):
+            return True
+        return False
+
+    def _search(self, pe, directory, needles, level=0, path=('.rsrc',)):
+        try:
+            needle = needles[level]
+        except IndexError:
+            if level >= 3:
+                self.log_warn(F'unexpected resource tree level {level + 1:d}')
+            needle = '*'
+
+        for e in directory.entries:
+            if self._match(level, needle, e):
+                id_string = e.id if level else rsrc.to_name(e.id)
+                new_path = path + (str(e.name if e.name else id_string),)
+                if e.struct.DataIsDirectory:
+                    yield from self._search(pe, e.directory, needles, level + 1, new_path)
+                elif needles:
+                    yield dict(
+                        data=pe.get_data(e.data.struct.OffsetToData, e.data.struct.Size),
+                        path='/'.join(new_path[1:])
+                    )
+                else:
+                    yield '/'.join(new_path[1:]).encode('UTF8')
+
+    def process(self, data):
+        pe = pefile.PE(data=data)
+        if not self.args.entries:
+            yield from self._search(pe, pe.DIRECTORY_ENTRY_RESOURCE, [])
+            return
+        for entry in self.args.entries:
+            while len(entry.needles) <= 3:
+                self.log_info('searching pattern:', str(entry))
+                yield from self._search(pe, pe.DIRECTORY_ENTRY_RESOURCE, entry.needles)
+                entry = entry.relax()
