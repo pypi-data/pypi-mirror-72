2c36b4220c7698c03be4a2a68a9974c279e5ab87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:26 +0000

    Add package raylab/0.8.4/raylab-0.8.4-py3-none-any.whl

---
+++ packages/raylab/raylab-0.8.4-py3-none-any.whl/raylab/utils/annotations.py
@@ -0,0 +1,13 @@
+"""Collection of type annotations."""
+from typing import Callable
+from typing import Tuple
+
+from torch import Tensor
+
+RewardFn = Callable[[Tensor, Tensor, Tensor], Tensor]
+TerminationFn = Callable[[Tensor, Tensor, Tensor], Tensor]
+DynamicsFn = Callable[[Tensor, Tensor], Tuple[Tensor, Tensor]]
+DetPolicy = Callable[[Tensor], Tensor]
+StochasticPolicy = Callable[[Tensor], Tuple[Tensor, Tensor]]
+ActionValue = Callable[[Tensor, Tensor], Tensor]
+StateValue = Callable[[Tensor], Tensor]
