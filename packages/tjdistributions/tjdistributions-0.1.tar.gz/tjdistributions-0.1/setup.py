185167a8747369275c51db872bb872c321e284e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:33 +0000

    Add package tjdistributions/0.1/tjdistributions-0.1.tar.gz

---
+++ packages/tjdistributions/tjdistributions-0.1.tar.gz/tjdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tjdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tjdistributions'],
+      zip_safe=False)
