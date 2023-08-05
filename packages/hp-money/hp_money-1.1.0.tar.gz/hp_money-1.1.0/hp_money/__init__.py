b636bec390d9b02650428d491a73a24a921476de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:53 +0000

    Add package hp-money/1.1.0/hp_money-1.1.0.tar.gz

---
+++ packages/hp-money/hp_money-1.1.0.tar.gz/hp_money-1.1.0/hp_money/__init__.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+""" 货币
+
+        1. 币种
+        2. 金额
+"""
+
+from .amount import Amount
+
+
+__author__ = 'Qingxu Kuang<kuangqingxu@transfereasy.com>'
+__version__ = '1.1.0'
+
+
+class Money(object):
+
+    def __init__(self, *, currency, amount, origin=False):
+
+        self._currency = currency
+        self._amount = Amount(
+            amount,
+            currency.accuracy if not origin else 1
+        )
+
+    @property
+    def currency(self):
+        return self._currency
+
+    @property
+    def amount(self):
+        return self._amount
+
+    def __str__(self):
+        return f'{self._amount} {self._currency}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __add__(self, other):
+        if not isinstance(other, Money):
+            raise TypeError(
+                'Cannot add a Money instance with a non-Money instance'
+            )
+        if self._currency != other.currency:
+            raise TypeError(
+                f'Cannot add money with different currencies {self._currency} and {other.currency}'
+            )
+
+        return self.__class__(
+            currency=self._currency,
+            amount=self._amount.value + other.amount.value,
+            origin=True
+        )
+
+    def __radd__(self, other):
+
+        return self.__add__(other)
+
+    def __sub__(self, other):
+
+        return self.__add__(-other)
+
+    def __rsub__(self, other):
+
+        return other - self
+
+    def __abs__(self):
+
+        return self.__class__(
+            currency=self._currency,
+            amount=abs(self._amount.value),
+            origin=True
+        )
+
+    def __neg__(self):
+
+        return self.__class__(
+            currency=self._currency,
+            amount=-self._amount.value,
+            origin=True
+        )
+
+    def __eq__(self, other):
+
+        if isinstance(other, Money):
+            return (
+                self._currency == other.currency
+            ) and (
+                self._amount == other.amount
+            )
+        return False
+
+    def __ne__(self, other):
+
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+
+        if not isinstance(other, Money):
+            raise TypeError(
+                'Money instance cannot be compared with non-Money instance.'
+            )
+        if self._currency == other.currency:
+            return self._amount > other.amount
+        else:
+            raise TypeError(
+                'Different currency instance cannot be compared.'
+            )
+
+    def __lt__(self, other):
+
+        if not isinstance(other, Money):
+            raise TypeError(
+                'Money instance cannot be compared with non-Money instance.'
+            )
+        if self._currency == other.currency:
+            return self._amount < other.amount
+        else:
+            raise TypeError(
+                'Different currency instance cannot be compared.'
+            )
+
+    def __ge__(self, other):
+
+        return (self == other) or (self > other)
+
+    def __le__(self, other):
+
+        return (self == other) or (self < other)
+
+    def exchange(self, forex_rate):
+
+        if self.currency != forex_rate.base_currency:
+            raise ValueError(
+                f'{self.currency.iso_code} Not Equals {forex_rate.base_currency.iso_code}'
+            )
+
+        amount = self.amount.exchange(forex_rate) / forex_rate.target_currency.accuracy
+
+        return Money(
+            currency=forex_rate.target_currency,
+            amount=amount
+        )
---
+++ packages/hp-money/hp_money-1.1.0.tar.gz/hp_money-1.1.0/tests/test_money.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+from hp_money import Money
+from hp_money.currency import Currency
+import pytest
+
+
+HKD = Currency('HKD', '344', 100)
+JPY = Currency('JPY', '392', 1)
+
+amount1 = 8.88
+amount2 = '8.88'
+amount3 = '8.8800'
+amount4 = 9.99
+
+
+def test_currency_stringify():
+    """ 测试币种字符串输出 """
+
+    assert str(HKD) == 'HKD', 'Currency stringify test error'
+    assert str(HKD) != 'JPY', 'Currency stringify test error'
+    assert str(JPY) == 'JPY', 'Currency stringify test error'
+
+
+def test_currency_equality():
+    """ 测试币种相等性 """
+
+    assert HKD == HKD, 'Currency equivalent test error'
+    assert HKD == Currency('HKD', '344', 100), 'Currency equality test error'
+    assert HKD != Currency('HKD', '355', 100), 'Currency equality test error'
+    assert HKD != Currency('HKD', '355', 1), 'Currency equality test error'
+    assert HKD != JPY, 'Currency equivalent test error'
+
+
+def test_money_equality():
+
+    money1 = Money(
+        amount=amount1,
+        currency=HKD
+    )
+    money2 = Money(
+        amount=amount3,
+        currency=HKD
+    )
+    money3 = Money(
+        amount=amount1,
+        currency=JPY
+    )
+    money4 = Money(
+        amount=amount4,
+        currency=HKD
+    )
+
+    assert str(money2.amount.value) == '888', 'Money equality test error'
+    assert money1 == money2, 'Money equality test error'
+    assert money1 != money3, 'Money equality test error'
+    assert money4 != money1, 'Money equality test error'
+
+
+def test_money_calculation():
+
+    money1 = Money(
+        amount=amount1,
+        currency=HKD
+    )
+    money2 = Money(
+        amount=amount4,
+        currency=HKD
+    )
+    money3 = Money(
+        amount=amount4,
+        currency=JPY
+    )
+
+    sum1 = money1 + money2
+
+    diff1 = money1 - money2
+    diff2 = money2 - money1
+
+    assert sum1 == Money(currency=HKD, amount='18.87'), 'ERROR'
+    assert diff1 == -Money(currency=HKD, amount='1.11')
+    assert diff2 == Money(currency=HKD, amount='1.11')
+    assert abs(diff1) == Money(currency=HKD, amount='1.11')
+    assert abs(diff1) == abs(diff2)
+
+    with pytest.raises(TypeError) as exc_info:
+        sum2 = money1 + money3
+
+    with pytest.raises(TypeError) as exc_info:
+        sum2 = money1 + '2'
