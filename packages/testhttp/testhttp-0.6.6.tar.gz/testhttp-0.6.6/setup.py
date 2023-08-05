0d87ccc9ac9f2ba4d35024e68f98d6c8d4607114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:40 +0000

    Add package testhttp/0.6.6/testhttp-0.6.6.tar.gz

---
+++ packages/testhttp/testhttp-0.6.6.tar.gz/testhttp-0.6.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='testhttp',
+      version='0.6.6',
+      description='HTTP testing tool',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type="text/markdown",
+      url='http://github.com/faisalraja/testhttp',
+      author='Faisal Raja',
+      author_email='support@altlimit.com',
+      license='MIT',
+      packages=['testhttp'],
+      install_requires=['requests'],
+      keywords='http test api',
+      zip_safe=False,
+      entry_points={
+          'console_scripts': ['testhttp = testhttp:cmd']
+      }
+      )
