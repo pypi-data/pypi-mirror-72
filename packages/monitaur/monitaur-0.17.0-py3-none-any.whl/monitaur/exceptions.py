3410c6969158b9cf484e73dbbf3c3ab535c51192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package monitaur/0.17.0/monitaur-0.17.0-py3-none-any.whl

---
+++ packages/monitaur/monitaur-0.17.0-py3-none-any.whl/monitaur/exceptions.py
@@ -0,0 +1,26 @@
+class ClientAuthError(Exception):
+    def __init__(self, message, errors=None):
+        self.message = message
+        self.errors = errors
+        super().__init__(f"message: {self.message}, errors: {self.errors}")
+
+
+class ClientValidationError(Exception):
+    def __init__(self, message, errors=None):
+        self.message = message
+        self.errors = errors
+        super().__init__(f"message: {self.message}, errors: {self.errors}")
+
+
+class FileError(Exception):
+    def __init__(self, message, errors=None):
+        self.message = message
+        self.errors = errors
+        super().__init__(f"message: {self.message}, errors: {self.errors}")
+
+
+class CustomInfluencesError(Exception):
+    def __init__(self, message, errors=None):
+        self.message = message
+        self.errors = errors
+        super().__init__(f"message: {self.message}, errors: {self.errors}")
