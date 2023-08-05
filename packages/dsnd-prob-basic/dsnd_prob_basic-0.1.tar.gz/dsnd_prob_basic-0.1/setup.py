28d297c707c43521a2e15feb54e515c38241686a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package dsnd-prob-basic/0.1/dsnd_prob_basic-0.1.tar.gz

---
+++ packages/dsnd-prob-basic/dsnd_prob_basic-0.1.tar.gz/dsnd_prob_basic-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_prob_basic',
+      version='0.1',
+      description='Gaussian distributions',
+      author = 'Ahmed Mohsen',
+      packages=['distributions'],
+      zip_safe=False)
