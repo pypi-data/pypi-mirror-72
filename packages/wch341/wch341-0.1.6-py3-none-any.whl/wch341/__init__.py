4ef6ed83be585b5e130082b80bfd6d35557e96d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:53 +0000

    Add package wch341/0.1.6/wch341-0.1.6-py3-none-any.whl

---
+++ packages/wch341/wch341-0.1.6-py3-none-any.whl/wch341/__init__.py
@@ -0,0 +1,24 @@
+import os
+import platform
+from time import sleep
+import subprocess
+def exist():
+    sys = platform.system()
+    if sys=="Darwin":
+        kexts = "_".join(os.listdir("/Library/Extensions/"))
+        return "usbserial.kext" in kexts
+    elif sys=="Windows":
+        return "ch341ser.inf" in "_".join(os.listdir("C:\Windows\System32\DriverStore\FileRepository"))
+    return True
+
+def install():
+    if exist():
+        return True
+    sys = platform.system()
+    if sys=="Darwin":
+        subprocess.call(["/usr/bin/open",os.path.dirname(__file__)+"/driver/ch34x_install.pkg"])
+    elif sys=="Windows":
+        subprocess.call([os.path.dirname(__file__)+"\driver\ch34x_install.exe","/sw","/se"])
+    while not exist():
+        sleep(1)
+    return True
