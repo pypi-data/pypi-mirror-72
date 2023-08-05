28d2b86b44941c6fb1d86de1f26f9b48555e9c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package jupyterlmod/1.9.0/jupyterlmod-1.9.0-py3-none-any.whl

---
+++ packages/jupyterlmod/jupyterlmod-1.9.0-py3-none-any.whl/jupyterlmod-1.9.0.data/data/share/jupyter/nbextensions/jupyterlmod/main.js
@@ -0,0 +1,260 @@
+define(function(require) {
+    "use strict";
+    var $ = require('jquery');
+    var ui = require('jquery-ui');
+    var dialog = require('base/js/dialog');
+    var lmod_class = require('./lmod');
+    var utils = require('base/js/utils');
+
+    var base_url = utils.get_body_data("baseUrl");
+
+    var lmod = new lmod_class.Lmod(base_url);
+    var search_source = null;
+
+    var server_proxy_infos = {};
+    var server_proxy_launcher = {};
+
+    const lmod_tab_html = $([
+'<div id="lmod" class="tab-pane">',
+'    <div id="lmod_toolbar" class="row list_toolbar">',
+'        <div class="col-sm-18 no-padding">',
+'            <input id="modules" class="form-control input-lg" placeholder="Search available modules..." style="width:100%">',
+'        </div>',
+'    </div>',
+'    <div class="list_container" id="lmod_list">',
+'        <div class="row list_header" id="list_header">',
+'            <div class="col-sm-8">Loaded Modules</div>',
+'            <div class="col-sm-4 no-padding tree-buttons">',
+'                <div class="pull-right">',
+'                    <div class="btn-group">',
+'                        <button class="btn btn-default btn-xs" id="save-button">Save</button>',
+'                    </div> ',
+'                    <div class="btn-group">',
+'                        <button class="dropdown-toggle btn btn-default btn-xs" id="restore-buttons" data-toggle="dropdown">',
+'                            <span>Restore </span>',
+'                            <span class="caret"/>',
+'                        </button>',
+'                        <ul class="dropdown-menu", id="restore-menu"/>',
+'                    </div>',
+'                </div>',
+'            </div>',
+'        </div>',
+'    </div>',
+'</div>',
+    ].join('\n'));
+
+    const lmod_list_line = $([
+'<div class="list_item row">',
+'   <div class="col-md-12">',
+'       <a href="#lmod_list"/>',
+'       <div class="item_buttons pull-right">',
+'           <button class="btn btn-warning btn-xs">Unload</button>',
+'       </div>',
+'   </div>',
+'</div>',
+    ].join('\n'));
+
+    const save_dialog_body = $([
+'<div>',
+'   <p class="save-message">Enter a new collection name:</p>',
+'   <br/>',
+'   <input type="text" size="25" class="form-control" placeholder="default">',
+'</div>'
+    ].join('\n'));
+
+    function save_collection(event) {
+        var d = dialog.modal({
+            title: "Save Collection",
+            body: save_dialog_body,
+            keyboard_manager: this.keyboard_manager,
+            default_button: "Cancel",
+            buttons : {
+                "Cancel": {},
+                "Save": {
+                    class: "btn-primary",
+                    click: function () {
+                        var name = d.find('input').val();
+                        lmod.save(name ? name : 'default').then(refresh_restore_list);
+                    }
+                }
+            },
+            open : function () {
+                d.find('input[type="text"]').keydown(function (event) {
+                    if (event.which === keyboard.keycodes.enter) {
+                        d.find('.btn-primary').first().click();
+                        return false;
+                    }
+                });
+                d.find('input[type="text"]').focus().select();
+            }
+        });
+    }
+
+    function refresh_restore_list() {
+        lmod.savelist()
+        .then(values => {
+            let list = $("#restore-menu").html("");
+            values.map(item => {
+                let li = $('<li>').append($('<a>', {'href': '#', "text" : item}))
+                                  .click(e => lmod.restore(item).then(refresh_module_ui));
+                list.append(li);
+            })
+        });
+    }
+
+    function refresh_module_ui() {
+        Promise.all([lmod.avail(), lmod.list()])
+        .then(values => {
+            let avail_set = new Set(values[0]);
+            let modulelist = values[1].sort();
+
+            $("#list_header").nextAll().remove();
+            let list = $("#lmod_list");
+
+            modulelist.map(item => {
+                let li = lmod_list_line.clone();
+                li.find('a').text(item).click(e => show_module(item));
+                li.find('button').click(e => lmod.unload(item).then(refresh_module_ui));
+                list.append(li);
+                avail_set.delete(item)
+            });
+
+            refresh_kernel_menu(modulelist);
+
+            search_source = Array.from(avail_set);
+        });
+    }
+
+    function refresh_kernel_menu(modulelist) {
+        $('[id^="kernel-"]').remove();
+        IPython.new_notebook_widget.request_kernelspecs()
+
+        let menu = $('.tree-buttons').find('.dropdown-menu');
+
+        for (let server_key in server_proxy_infos) {
+            let is_enabled = modulelist.some(module => { return module.toLowerCase().includes(server_key) });
+            if (is_enabled) {
+                if(!server_proxy_launcher.hasOwnProperty(server_key)) {
+                    menu.append(server_proxy_infos[server_key]);
+                    server_proxy_launcher[server_key] = $('.new-' + server_key);
+                }
+            } else if (server_key in server_proxy_launcher) {
+                server_proxy_launcher[server_key].remove();
+                delete server_proxy_launcher[server_key];
+            }
+        }
+    }
+
+    async function show_module(module) {
+        let data = await lmod.show(module);
+        let datalist = data.split('\n');
+        let textarea = $('<pre/>').text($.trim(datalist.slice(3).join('\n')));
+        let dialogform = $('<div/>').append(textarea);
+        let path = datalist[1].slice(0, -1);
+        dialog.modal({
+            title: path,
+            body: dialogform,
+            default_button: "Ok",
+            buttons : {
+                "Ok" : {}
+            }
+        });
+    }
+
+    function split( val ) {
+      return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+
+    async function setup_proxy_infos() {
+        const response = await fetch(base_url + 'server-proxy/servers-info');
+        if (!response.ok) {
+            console.log('jupyter-lmod: could not communicate with jupyter-server-proxy API.');
+            return;
+        }
+        const data = await response.json();
+        for (let server_process of data.server_processes) {
+            /* create our list item */
+            let entry_container = $('<li>')
+                .attr('role', 'presentation')
+                .addClass('new-' + server_process.name);
+
+            /* create our list item's link */
+            let entry_link = $('<a>')
+                .attr('role', 'menuitem')
+                .attr('tabindex', '-1')
+                .attr('href', base_url + server_process.name + '/')
+                .attr('target', '_blank')
+                .text(server_process.launcher_entry.title);
+
+            entry_container.append(entry_link);
+            server_proxy_infos[server_process.name] = entry_container;
+        }
+    }
+
+    function load() {
+        if (!IPython.notebook_list) return;
+        $(".tab-content").append(lmod_tab_html);
+        $("#save-button").click(save_collection);
+        $("#tabs").append(
+            $('<li>')
+            .append(
+                $('<a>')
+                .attr('href', '#lmod')
+                .attr('data-toggle', 'tab')
+                .text('Softwares')
+                .click(function (e) {
+                    window.history.pushState(null, null, '#lmod');
+                })
+            )
+        );
+        $( "#modules" )
+        // don't navigate away from the field on tab when selecting an item
+        .on( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).autocomplete( "instance" ).menu.active ) {
+              event.preventDefault();
+            }
+        })
+        .on('keyup', function (event) {
+            if (event.keyCode == $.ui.keyCode.ENTER) {
+                var modules = split($.trim(event.target.value));
+                modules.pop();
+
+                lmod.load(modules).then(refresh_module_ui);
+                event.target.value = "";
+            }
+        })
+        .autocomplete({
+            minLength: 0,
+            source: function( request, response ) {
+              // delegate back to autocomplete, but extract the last term
+              response( $.ui.autocomplete.filter(
+                search_source, extractLast( request.term ) ) );
+            },
+            focus: function() {
+              // prevent value inserted on focus
+              return false;
+            },
+            select: function( event, ui ) {
+              var terms = split( this.value );
+              // remove the current input
+              terms.pop();
+              // add the selected item
+              terms.push( ui.item.value );
+              // add placeholder to get the comma-and-space at the end
+              terms.push( "" );
+              this.value = terms.join( ", " );
+              return false;
+            }
+        });
+        setup_proxy_infos();
+        refresh_module_ui();
+        refresh_restore_list();
+    }
+    return {
+        load_ipython_extension: load
+    };
+});
---
+++ packages/jupyterlmod/jupyterlmod-1.9.0-py3-none-any.whl/lmod/__init__.py
@@ -0,0 +1,120 @@
+import os # require by lmod output evaluated by exec()
+import sys
+
+from functools import partial, wraps
+from subprocess import Popen, PIPE
+
+LMOD_SYSTEM_NAME = os.environ.get('LMOD_SYSTEM_NAME', '')
+SITE_POSTFIX = os.path.join('lib', 'python'+sys.version[:3], 'site-packages')
+
+def module(command, *args):
+    cmd = (os.environ['LMOD_CMD'], 'python', '--terse', command)
+
+    result = Popen(cmd + args, stdout=PIPE, stderr=PIPE)
+    if command in ('load', 'unload', 'purge', 'restore', 'save'):
+        try:
+            exec(result.stdout.read())
+        except NameError:
+            pass
+
+    return result.stderr.read().decode()
+
+def update_sys_path(env_var, postfix=''):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kargs):
+            if env_var in os.environ:
+                orig_paths = os.environ[env_var].split(os.pathsep)
+                orig_paths = [os.path.join(path, postfix) for path in orig_paths]
+            else:
+                orig_paths = []
+            output = function(*args, **kargs)
+            if env_var in os.environ:
+                new_paths = os.environ[env_var].split(os.pathsep)
+                new_paths = [os.path.join(path, postfix) for path in new_paths]
+            else:
+                new_paths = []
+
+            paths_to_del = set(orig_paths) - set(new_paths)
+            paths_to_add = set(new_paths) - set(orig_paths)
+            paths_to_add = [path for path in new_paths if path in paths_to_add]
+            for path in paths_to_del:
+                try:
+                    sys.path.remove(path)
+                except ValueError:
+                    continue
+            sys.path.extend(paths_to_add)
+            return output
+        return wrapper
+    return decorator
+
+def avail(*args):
+    string = module('avail', *args)
+    modules = []
+    for entry in string.split():
+        if not (entry.startswith('/') or
+                entry.endswith('/') or
+                "@" in entry):
+            modules.append(entry)
+    modules.sort(key=lambda v: v.split('/')[0])
+    return modules
+
+def list(hide_hidden=False):
+    string = module('list').strip()
+    if string != "No modules loaded":
+        modules = string.split()
+        if hide_hidden:
+            modules = [m for m in modules if m.rsplit('/', 1)[-1][0] != '.']
+        return modules
+    return []
+
+def freeze():
+    header = ["import lmod", "lmod.purge(force=True)"]
+    modules = list(hide_hidden=True)
+    return "\n".join(header +
+                     ["lmod.load('{}')".format(m) for m in modules])
+
+@update_sys_path('PYTHONPATH')
+@update_sys_path('EBPYTHONPREFIXES', SITE_POSTFIX)
+def load(*args):
+    output = module('load', *args)
+    if output:
+        print(output)
+
+@update_sys_path('PYTHONPATH')
+@update_sys_path('EBPYTHONPREFIXES', SITE_POSTFIX)
+def reset():
+    output = module('reset')
+    if output:
+        print(output)
+
+@update_sys_path('PYTHONPATH')
+@update_sys_path('EBPYTHONPREFIXES', SITE_POSTFIX)
+def restore(*args):
+    output = module('restore', *args)
+    if output:
+        print(output)
+
+def savelist():
+    return module('savelist').split()
+
+@update_sys_path('PYTHONPATH')
+@update_sys_path('EBPYTHONPREFIXES', SITE_POSTFIX)
+def unload(*args):
+    output = module('unload', *args)
+    if output:
+        print(output)
+
+@update_sys_path('PYTHONPATH')
+@update_sys_path('EBPYTHONPREFIXES', SITE_POSTFIX)
+def purge(force=False):
+    if force:
+        args = ('--force',)
+    else:
+        args = ()
+    output = module('purge', *args)
+    if output:
+        print(output)
+
+show = partial(module, 'show')
+save = partial(module, 'save')
