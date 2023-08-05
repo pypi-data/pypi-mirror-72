decc6d859d73f187a481cda779dc32041b88a5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:59 +0000

    Add package algops/0.0.1/algops-0.0.1.tar.gz

---
+++ packages/algops/algops-0.0.1.tar.gz/algops-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+  name = 'algops',
+  version = '0.0.1',
+  description = '',
+  author = 'algops',
+  license = 'GPL-3.0',
+  url = 'https://github.com/algops/algops',
+  download_url = 'https://github.com/algops/algops/downloads',
+  keywords = ['algorithm-problem-solving', 'ansible', 'big-data'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
