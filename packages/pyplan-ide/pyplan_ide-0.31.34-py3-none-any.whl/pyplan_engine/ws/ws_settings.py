714ce7523a756ed077592a7ee2b972529b74fee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package pyplan-ide/0.31.34/pyplan_ide-0.31.34-py3-none-any.whl

---
+++ packages/pyplan-ide/pyplan_ide-0.31.34-py3-none-any.whl/pyplan_engine/ws/ws_settings.py
@@ -0,0 +1,7 @@
+class ws_settings:
+    MSG_TYPE_MESSAGE = 0  # For standard messages
+    MSG_TYPE_SESSION_KILLED = 1  # For session killed messages
+    MSG_TYPE_LEAVE = 2
+    MSG_TYPE_ENTER = 3
+    MSG_TYPE_PROGRESSBAR = 4  # For progressbar messages
+    MSG_TYPE_DEBUG_MODE_INFO = 6  # For debug mode messages
