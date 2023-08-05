eaab37602427c513a7d23ee4506a9e02fafc12c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package aiocheck/0.0.9/aiocheck-0.0.9-py3-none-any.whl

---
+++ packages/aiocheck/aiocheck-0.0.9-py3-none-any.whl/aiocheck/__init__.py
@@ -0,0 +1,18 @@
+"""
+    aiocheck
+
+    A python asyncio host health checker using native ping commands.
+
+    Example:
+    ```
+    import aiocheck
+    aiocheck.cli.main('localhost')
+    ```
+
+    :copyright: 2020 kruserr
+    :license: MIT
+"""
+
+from aiocheck.Database import Database
+from aiocheck.Host import Host
+from aiocheck import cli
