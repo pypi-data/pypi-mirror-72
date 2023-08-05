8c428656a2bdeeefb07828946ac4bb2f7f624606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package mangoes101/0.1/mangoes101-0.1.tar.gz

---
+++ packages/mangoes101/mangoes101-0.1.tar.gz/mangoes101-0.1/mangoes101/mangoes.py
@@ -0,0 +1,5 @@
+
+def hello():
+    print("Do you like mangoes: ")
+    input()
+    print(" I Love You ")
---
+++ packages/mangoes101/mangoes101-0.1.tar.gz/mangoes101-0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='mangoes101',
+      version='0.1',
+      description='',
+      packages=['mangoes101'])
