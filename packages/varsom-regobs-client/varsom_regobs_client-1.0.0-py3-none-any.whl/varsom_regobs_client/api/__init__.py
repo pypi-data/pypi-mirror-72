7c8c31db0bb1ce2a340a207e1d0802205f8faee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:59 +0000

    Add package varsom-regobs-client/1.0.0/varsom_regobs_client-1.0.0-py3-none-any.whl

---
+++ packages/varsom-regobs-client/varsom_regobs_client-1.0.0-py3-none-any.whl/varsom_regobs_client/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from varsom_regobs_client.api.account_api import AccountApi
+from varsom_regobs_client.api.attachment_api import AttachmentApi
+from varsom_regobs_client.api.general_obs_api import GeneralObsApi
+from varsom_regobs_client.api.geo_code_api import GeoCodeApi
+from varsom_regobs_client.api.helptext_api import HelptextApi
+from varsom_regobs_client.api.kdv_elements_api import KdvElementsApi
+from varsom_regobs_client.api.location_api import LocationApi
+from varsom_regobs_client.api.registration_api import RegistrationApi
+from varsom_regobs_client.api.search_api import SearchApi
+from varsom_regobs_client.api.token_api import TokenApi
+from varsom_regobs_client.api.trip_api import TripApi
