6d1a36df856829301c05ae29c22299a1add8a44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package analyze-distributions/0.1/analyze_distributions-0.1.tar.gz

---
+++ packages/analyze-distributions/analyze_distributions-0.1.tar.gz/analyze_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='analyze_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['analyze_distributions'],
+      author = 'Soufiane NAITTOULAHYANE',
+      author_email = 'naittou.soufiane@gmail.com',
+      zip_safe=False)
