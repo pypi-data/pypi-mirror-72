d1a9649001e8ff6dcdac9da13811fadfaa84a3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:50 +0000

    Add package vin-dsnd-probability/0.1/vin-dsnd-probability-0.1.tar.gz

---
+++ packages/vin-dsnd-probability/vin-dsnd-probability-0.1.tar.gz/vin-dsnd-probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vin-dsnd-probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vin-dsnd-probability'],
+      zip_safe=False)
