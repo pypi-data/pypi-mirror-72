b8f05d2b17764533ee5c0cb876fb754a06bc49e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package github-webhook2/0.0.3/github_webhook2-0.0.3-py3-none-any.whl

---
+++ packages/github-webhook2/github_webhook2-0.0.3-py3-none-any.whl/webhook/webhook.py
@@ -0,0 +1,53 @@
+import subprocess
+import hmac
+from flask import Flask, jsonify
+from flask import request
+import json
+from os import path
+import sys
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+def __do_deploy():
+    """
+        部署
+    """
+    # curdir = path.dirname(path.abspath(__file__))
+    deploy_script_file = app.config['deploy_script']
+    cmd = f"bash  {deploy_script_file}"
+
+    (status, output) = subprocess.getstatusoutput(cmd)
+    return status, output
+
+
+@app.route('/deploy', methods=['POST'])
+def deploy():
+    signature = request.headers.get('X-Hub-Signature')
+    sha, signature = signature.split('=')
+    secret = str.encode(app.config.get('webhook_sk'))
+    hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
+
+    if not hmac.compare_digest(hashhex, signature):
+        print("部署失败")
+        return json.dumps({"success": 1, "message": "accessKey error"}), 500
+    deploy_result = {}
+    print("开始部署")
+    deploy_result['success'], deploy_result['message'] = __do_deploy()
+    print("部署成功")
+    status_code = 500 if deploy_result['success']==1 else 200
+    return jsonify(deploy_result), status_code
+
+
+def main(port=50001, sk='', deploy_script=''):
+    app.config['webhook_sk'] = sk
+    app.config['deploy_script'] = deploy_script
+    app.run(port=port)
+
+
+if __name__=="__main__":
+    sk = sys.argv[1]
+    main(sk=sk)
+
