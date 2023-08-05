42ebe6bf55b600283486b9dde17d1bb601f3a9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:54 +0000

    Add package JavPy/0.5.7/JavPy-0.5.7-py2.py3-none-any.whl

---
+++ packages/JavPy/JavPy-0.5.7-py2.py3-none-any.whl/JavPy/app/javpy-react/build/precache-manifest.514d1c95e79d8448b132cb58fe992a49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c885b225d17918e737eae1f9d63c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfcf0e7faff8f46f2902",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "fbe8a57e3a10218b26b9",
+    "url": "/static/css/main.12301873.chunk.css"
+  },
+  {
+    "revision": "dfcf0e7faff8f46f2902",
+    "url": "/static/js/2.88715c02.chunk.js"
+  },
+  {
+    "revision": "69aceadffa80df17147cfa73022ec51e",
+    "url": "/static/js/2.88715c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe8a57e3a10218b26b9",
+    "url": "/static/js/main.9004d92d.chunk.js"
+  },
+  {
+    "revision": "00e68b54667811347eae",
+    "url": "/static/js/runtime-main.39a99049.js"
+  }
+]);
