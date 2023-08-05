e0c3bd71c83aafcce0c8d246eaa15deebb9e37cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package wikipack/1.0.0/wikipack-1.0.0-py3-none-any.whl

---
+++ packages/wikipack/wikipack-1.0.0-py3-none-any.whl/wikipack/commands/__init__.py
@@ -0,0 +1,10 @@
+# Imports go here!
+# from .example import ExampleCommand
+
+# Enter the commands of your Pack here!
+available_commands = [
+    # ExampleCommand,
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [command.__name__ for command in available_commands]
