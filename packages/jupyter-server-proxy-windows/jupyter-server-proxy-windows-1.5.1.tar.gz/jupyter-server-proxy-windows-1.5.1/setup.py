d7a5144f2f533c1ace27d05cd99f9da702bca10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:44 +0000

    Add package jupyter-server-proxy-windows/1.5.1/jupyter-server-proxy-windows-1.5.1.tar.gz

---
+++ packages/jupyter-server-proxy-windows/jupyter-server-proxy-windows-1.5.1.tar.gz/jupyter-server-proxy-windows-1.5.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from glob import glob
+
+setuptools.setup(
+    name="jupyter-server-proxy-windows",
+    version='1.5.1',
+    url="https://github.com/jupyterhub/jupyter-server-proxy",
+    author="Ryan Lovett & Yuvi Panda",
+    author_email="rylo@berkeley.edu",
+    license="BSD 3-Clause",
+    description="Jupyter server extension to supervise and proxy web services",
+    packages=setuptools.find_packages(),
+    install_requires=['notebook', 'simpervisor>=0.2', 'aiohttp'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Framework :: Jupyter',
+    ],
+    data_files=[
+        ('share/jupyter/nbextensions/jupyter_server_proxy', glob('jupyter_server_proxy/static/*')),
+        ('etc/jupyter/jupyter_notebook_config.d', ['jupyter_server_proxy/etc/jupyter-server-proxy-serverextension.json']),
+        ('etc/jupyter/nbconfig/tree.d', ['jupyter_server_proxy/etc/jupyter-server-proxy-nbextension.json'])
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
