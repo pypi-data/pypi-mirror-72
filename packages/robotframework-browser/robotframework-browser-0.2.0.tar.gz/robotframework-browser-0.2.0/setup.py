4e3488c78e305ca9ed6d3b160b1825069e26d9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:23 +0000

    Add package robotframework-browser/0.2.0/robotframework-browser-0.2.0.tar.gz

---
+++ packages/robotframework-browser/robotframework-browser-0.2.0.tar.gz/robotframework-browser-0.2.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup  # type: ignore
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+packages = \
+    ['Browser', 'Browser.generated', 'Browser.locators']
+
+package_data = \
+    {'': ['*'], 'Browser': ['wrapper/*', 'wrapper/generated/*']}
+
+install_requires = \
+    open("requirements.txt").readlines()
+
+setup_kwargs = {
+    'name': 'robotframework-browser',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'author': 'Mikko Korpela',
+    'author_email': 'mikko.korpela@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MarketSquare/robotframework-browser',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': {'console_scripts': ['rfbrowser=Browser.entry:run']},
+    'python_requires': '>=3.8,<4.0',
+    'classifiers': [
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Testing",
+        "Framework :: Robot Framework",
+        "Framework :: Robot Framework :: Library"
+    ]
+}
+
+
+setup(**setup_kwargs)
