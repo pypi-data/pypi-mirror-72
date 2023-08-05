62b0728063f7eacd732da89ac258ebb1fcefe270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:11 +0000

    Add package targetDB/1.3.1/targetDB-1.3.1-py3-none-any.whl

---
+++ packages/targetDB/targetDB-1.3.1-py3-none-any.whl/targetDB/utils/druggability_ml.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pkg_resources
+from pathlib import Path
+
+
+def generate_model():
+    # Model generated with optimised parameters (see .ipynb file)
+    rf_model = RandomForestClassifier(n_estimators=1000, max_depth=21, max_features=3, min_samples_leaf=2,
+                                      min_samples_split=5)
+
+    # recover the training data from the data file
+
+    pck_path = Path(str(pkg_resources.resource_filename('targetDB.utils', ''))).parent
+    ml_data = pck_path.joinpath('ml_data')
+    ml_data = ml_data.joinpath('ml_training_data_13_01_2020.zip')
+
+    training_df = pd.read_json(ml_data, compression='zip')
+
+    training_set, training_labels = training_df.drop('DRUGGABLE', axis=1), training_df['DRUGGABLE']
+
+    return rf_model.fit(training_set, training_labels)
+
+
+def predict(model, data):
+    df = data.copy()
+    df.index = df.Target_id
+    df.drop(columns=['Target_id'], inplace=True)
+    df.replace({True: 1, False: 0}, inplace=True)
+    df = df.fillna(0)
+    # Only columns to consider in the model (see .ipynb file for selection of the columns)
+    col_to_drop = ['OT_max_association_score', 'Heart_alert', 'Liver_alert', 'Kidney_alert', 'dis_AScore',
+                    'bio_EScore', 'safe_EScore', 'chembl_selective_M', 'chembl_selective_G', 'chembl_selective_E',
+                    'bindingDB_phase2','commercial_potent', 'information_score','gen_AQualScore', 'genetic_NORM']
+    df = df.drop(columns=col_to_drop,axis=1)
+
+    return model.predict(df)
+
+
+def predict_prob(model, data):
+    df = data.copy()
+    df.index = df.Target_id
+    df.drop(columns=['Target_id'], inplace=True)
+    df.replace({True: 1, False: 0}, inplace=True)
+    df = df.fillna(0)
+    # Only columns to consider in the model (see .ipynb file for selection of the columns
+    col_to_drop = ['OT_max_association_score', 'Heart_alert', 'Liver_alert', 'Kidney_alert', 'dis_AScore',
+                    'bio_EScore', 'safe_EScore', 'chembl_selective_M', 'chembl_selective_G', 'chembl_selective_E',
+                    'bindingDB_phase2','commercial_potent', 'information_score','gen_AQualScore', 'genetic_NORM']
+    df = df.drop(columns=col_to_drop, axis=1)
+    return model.predict_proba(df)
+
+
+def in_training_set(data):
+
+    pck_path = Path(str(pkg_resources.resource_filename('targetDB.utils', ''))).parent
+    ml_data = pck_path.joinpath('ml_data')
+    ml_data = ml_data.joinpath('ml_training_data_13_01_2020.zip')
+
+    training_df = pd.read_json(ml_data, compression='zip')
+
+    df = data.copy()
+    df.index = df.Target_id
+
+    df['Is_in_training_set'] = 'No'
+    df.loc[df.index.isin(training_df.index),['Is_in_training_set']] = 'Yes'
+    return df['Is_in_training_set'].values
---
+++ packages/targetDB/targetDB-1.3.1-py3-none-any.whl/targetDB/utils/gene2id.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import sqlite3
+import pandas as pd
+import numpy as np
+
+
+def gene_to_id(list_of_genes,targetDB_path=None):
+	print('[NAME CONVERSION]: Converting gene names into IDs (Uniprot,Ensembl,HGNC)')
+	connector = sqlite3.connect(targetDB_path)
+	gene_list = []
+	required_columns = ['alias_symbol', 'ensembl_gene_id', 'prev_symbol', 'symbol','uniprot_ids']
+	for gene in list_of_genes:
+		gene = gene.rstrip('\n')
+		gene = gene.rstrip('\r')
+		gene_list.append(gene)
+	gene_ids = "','".join(gene_list)
+	gene_id_query = """SELECT * FROM hgnc as hgn WHERE hgn.hgnc_id in (SELECT hg.hgnc_id FROM hgnc as hg WHERE hg.xref_value in ('%s'))""" % gene_ids
+	gene_xref = pd.read_sql(gene_id_query, con=connector)
+	xref_df = gene_xref[gene_xref.xref_name.isin(['symbol', 'uniprot_ids', 'ensembl_gene_id', 'prev_symbol', 'alias_symbol'])]
+	xref_piv = xref_df.pivot_table(index='hgnc_id', values='xref_value', columns='xref_name',
+	                               aggfunc=lambda x: ';'.join(x), fill_value='')
+	for col in required_columns:
+		if col not in xref_piv.columns:
+			xref_piv[col] = ''
+	xref_piv.uniprot_ids = xref_piv.uniprot_ids.apply(lambda x: list(filter(None, x.split(';'))))
+	connector.close()
+	list_of_ids = []
+	for gene in gene_list:
+		gene_id = xref_piv[xref_piv.symbol == gene].index.tolist()
+		if len(gene_id) == 0:
+			gene_id = xref_df[(xref_df.xref_value == gene) & (
+						(xref_df.xref_name == 'prev_symbol') | (xref_df.xref_name == 'alias_symbol'))][
+				'hgnc_id'].tolist()
+		if len(gene_id) != 0:
+			list_of_ids.extend(gene_id)
+	output = xref_piv.loc[list_of_ids]
+	print('[NAME CONVERSION]: Conversion Done')
+	output = output.reset_index()
+	output.drop_duplicates(subset=['hgnc_id','ensembl_gene_id','symbol'],inplace=True)
+	output.index = output['hgnc_id']
+	not_present = [x for x in gene_list if x not in list(xref_piv.symbol)]
+	return output,not_present
+
+
+def gene_to_id_all(targetDB_path=None):
+	print('[NAME CONVERSION]: Converting gene names into IDs (Uniprot,Ensembl,HGNC)')
+	connector = sqlite3.connect(targetDB_path)
+	required_columns = ['alias_symbol', 'ensembl_gene_id', 'prev_symbol', 'symbol', 'uniprot_ids']
+	gene_id_query = """SELECT * FROM hgnc"""
+	gene_xref = pd.read_sql(gene_id_query, con=connector)
+	xref_df = gene_xref[gene_xref.xref_name.isin(required_columns)]
+	xref_piv = xref_df.pivot_table(index='hgnc_id', values='xref_value', columns='xref_name',
+	                               aggfunc=lambda x: ';'.join(x), fill_value='')
+	xref_piv = xref_piv.replace('', np.nan)
+	xref_piv = xref_piv.dropna(subset=['uniprot_ids'])
+	xref_piv = xref_piv.replace(np.nan,'')
+	for col in required_columns:
+		if col not in xref_piv.columns:
+			xref_piv[col] = ''
+	xref_piv.uniprot_ids = xref_piv.uniprot_ids.apply(lambda x: list(filter(None, x.split(';'))))
+	connector.close()
+	output = xref_piv.reset_index()
+	output.drop_duplicates(subset=['hgnc_id', 'ensembl_gene_id', 'symbol'], inplace=True)
+	output.index = output['hgnc_id']
+	return output
