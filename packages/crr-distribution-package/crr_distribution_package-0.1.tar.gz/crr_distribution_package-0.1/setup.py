cda38441341a2a6a469e98a3ff5757901af4e160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:18 +0000

    Add package crr-distribution-package/0.1/crr_distribution_package-0.1.tar.gz

---
+++ packages/crr-distribution-package/crr_distribution_package-0.1.tar.gz/crr_distribution_package-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='crr_distribution_package',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['crr_distribution_package'],
+      author = 'Chetan Raj Rupakheti',
+      author_email = 'chetanrrk@gmail.com',
+      zip_safe=False)
