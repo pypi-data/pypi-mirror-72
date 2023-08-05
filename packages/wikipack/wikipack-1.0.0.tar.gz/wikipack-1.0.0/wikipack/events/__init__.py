9487f2be5f391eca73b169d8fa7ecc96929d9964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package wikipack/1.0.0/wikipack-1.0.0.tar.gz

---
+++ packages/wikipack/wikipack-1.0.0.tar.gz/wikipack-1.0.0/wikipack/events/__init__.py
@@ -0,0 +1,10 @@
+# Imports go here!
+# from .example import ExampleEvent
+
+# Enter the commands of your Pack here!
+available_events = [
+    # ExampleEvent,
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [command.__name__ for command in available_events]
