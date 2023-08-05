690c869eb9388fe83567d3800d071fd4ae39b83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package pypeln/0.4.2/pypeln-0.4.2-py3-none-any.whl

---
+++ packages/pypeln/pypeln-0.4.2-py3-none-any.whl/pypeln/thread/utils.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+from dataclasses import dataclass
+from multiprocessing import get_context
+import multiprocessing
+from multiprocessing.queues import Empty, Full
+import typing as tp
+
+from pypeln import utils as pypeln_utils
+
+# multiprocessing = get_context("fork")
+MANAGER = multiprocessing.Manager()
+
+
+def Namespace(**kwargs) -> tp.Any:
+    return MANAGER.Namespace(**kwargs)
