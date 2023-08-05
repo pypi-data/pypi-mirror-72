0c32d9be0b6aa9b66ecc0bf5560f85a17e389c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package tarun-probdist/0.0.1/tarun_probdist-0.0.1.tar.gz

---
+++ packages/tarun-probdist/tarun_probdist-0.0.1.tar.gz/tarun_probdist-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tarun_probdist',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['tarun_probdist'],
+      author='Tharun Saranga',
+      author_email='tarun.saranga116@gmail.com',
+      zip_safe=False)
