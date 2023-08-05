3eda5e17339c42c90e394eb849a74bf61e6c3fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:07 +0000

    Add package simple-socket/0.0.3/simple_socket-0.0.3.tar.gz

---
+++ packages/simple-socket/simple_socket-0.0.3.tar.gz/simple_socket-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='simple_socket',
+    version='0.0.3',
+    # 0.0.3 - Added SSL server/client
+    # 0.0.2 - Added readme.md
+    # 0.0.1 - init release
+    packages=['simple_socket'],
+    url='https://github.com/GrantGMiller/simple_socket',
+    license='MIT',
+    author='Grant Miller',
+    author_email='grant@grant-miller.com',
+    description='Simple event-based socket objects'
+)
+
+# python -m setup.py sdist bdist_wheel
+# twine upload dist/*
