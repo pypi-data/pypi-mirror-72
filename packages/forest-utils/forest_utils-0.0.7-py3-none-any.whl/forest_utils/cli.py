38cb27701a4656a64d036dfec12bf4058efde077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:45 +0000

    Add package forest-utils/0.0.7/forest_utils-0.0.7-py3-none-any.whl

---
+++ packages/forest-utils/forest_utils-0.0.7-py3-none-any.whl/forest_utils/cli.py
@@ -0,0 +1,35 @@
+import click
+from pathlib import Path
+
+from utils import create
+from utils import pull
+
+def get_version():
+    with open('VERSION.txt') as file:
+        return file.read().split("==")[-1].strip()
+
+INFORMATION = {
+    'name': "forest",
+    'version': get_version(),
+}
+
+@click.group(help='A CLI Tool for uploading model to model zoo')
+@click.version_option(INFORMATION['version'])
+@click.pass_context
+def main(ctx):
+    pass
+
+@cli.command(help='Initialize zoo repository')
+@click.pass_context
+def init(ctx):
+    create.create_dir_tree()
+
+
+@cli.command(help='Pull models from model zoo')
+@click.pass_context
+@click.option('--all', '-a', is_flag=True, help="Pull all the files from the github origin")
+def pull(ctx, docker):
+    pull.get_files()
+
+if __name__ == "__main__":
+    main()
