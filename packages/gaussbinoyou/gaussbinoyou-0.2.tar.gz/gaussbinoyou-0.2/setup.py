7927af0c98f85b62e511f90fe11ab12d4c3b6850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package gaussbinoyou/0.2/gaussbinoyou-0.2.tar.gz

---
+++ packages/gaussbinoyou/gaussbinoyou-0.2.tar.gz/gaussbinoyou-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussbinoyou',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussbinoyou'],
+      zip_safe=False)
