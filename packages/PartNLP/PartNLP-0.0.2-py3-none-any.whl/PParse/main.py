0f975267f0971b607cd2ac4afda1df4a5ae46e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package PartNLP/0.0.2/PartNLP-0.0.2-py3-none-any.whl

---
+++ packages/PartNLP/PartNLP-0.0.2-py3-none-any.whl/PParse/main.py
@@ -0,0 +1,4 @@
+from PPars.models.core1 import Pipeline
+
+if __name__ == "__main__":
+    obj = Pipeline(lang='', package='', text='برای بدست آوردن نتایج بهتر میتوان. از پیش پردازش بهره برد', processors=[])
