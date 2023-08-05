4d9ca97a3960724f4322df9a34c2bdd1c8e69966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:29 +0000

    Add package letslog/0.0.1/letslog-0.0.1.tar.gz

---
+++ packages/letslog/letslog-0.0.1.tar.gz/letslog-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='letslog',
+    packages = ['letslog'],
+    version='0.0.1',
+    license='MIT',
+    author='YiFei Li',
+    author_email='yifeil@berkeley.edu',
+    description='A logger package',
+    url='https://github.com/yifeili98/letslog',
+    download_url='https://github.com/yifeili98/letslog/archive/0.0.1.tar.gz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+          'datetime',
+          'pathlib',
+          'logging',
+      ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
