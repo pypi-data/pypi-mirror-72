5cc568c5d7df14d7918a6a48f880ba2162a38e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package generallibrary/1.3.5/generallibrary-1.3.5-py3.8.egg

---
+++ packages/generallibrary/generallibrary-1.3.5-py3.8.egg/generallibrary/__init__.py
@@ -0,0 +1,5 @@
+
+from generallibrary.base.iterables import iterable, depth, dictFirstValue, iterFirstValue
+from generallibrary.base.time import Timer, sleep
+from generallibrary.base.types import strToDynamicType, typeChecker
+import test
