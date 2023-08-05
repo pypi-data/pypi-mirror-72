298405f20420185f723004aa2f9fc1ed86e424ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:25 +0000

    Add package mensolutions-distribution/1.0/mensolutions_distribution-1.0.tar.gz

---
+++ packages/mensolutions-distribution/mensolutions_distribution-1.0.tar.gz/mensolutions_distribution-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mensolutions_distribution',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['mensolutions_distribution'],
+      long_description='long_description to be placed here',
+      author='Emmanuel Ameh',
+      author_email='a@b.com',
+      zip_safe=False)
