91b41d9f10e5f056c1f5d3c4e2102e50c9aa3514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:16 +0000

    Add package zeblonk-probability/0.2/zeblonk_probability-0.2.tar.gz

---
+++ packages/zeblonk-probability/zeblonk_probability-0.2.tar.gz/zeblonk_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zeblonk_probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['zeblonk_probability'],
+      author = 'Jason Ho',
+      author_emaikl = 'jason_c_ho@yahoo.com',
+      zip_safe=False)
