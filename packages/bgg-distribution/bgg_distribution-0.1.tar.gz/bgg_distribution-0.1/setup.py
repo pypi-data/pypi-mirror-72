7ec8ce65105c5d519f6ff5fc118cda900ccdd4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:59 +0000

    Add package bgg-distribution/0.1/bgg_distribution-0.1.tar.gz

---
+++ packages/bgg-distribution/bgg_distribution-0.1.tar.gz/bgg_distribution-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bgg_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bgg_distribution'],
+      author= 'Noe Durocher',
+      zip_safe=False)
