7a1d0b2e2373c95994ff0493a9be3074311d72c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:14 +0000

    Add package aibolit/1.1.2/aibolit-1.1.2.tar.gz

---
+++ packages/aibolit/aibolit-1.1.2.tar.gz/aibolit-1.1.2/aibolit/metrics/ncss/ncss.py
@@ -0,0 +1,50 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Aibolit
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from aibolit.utils.ast_builder import build_ast
+
+
+class NCSSMetric():
+    def __init__(self):
+        pass
+
+    def value(self, filename: str):
+        if len(filename) == 0:
+            raise ValueError('Empty file for analysis')
+
+        tree = build_ast(filename)
+
+        metric = 0
+        for _, node in tree:
+            node_type = str(type(node))
+            if 'Statement' in node_type:
+                metric += 1
+            elif 'VariableDeclarator' == node_type:
+                metric += 1
+            elif 'Assignment' == node_type:
+                metric += 1
+            elif 'Declaration' in node_type and 'LocalVariableDeclaration' not in node_type \
+                 and 'PackageDeclaration' not in node_type:
+                metric += 1
+
+        return metric
