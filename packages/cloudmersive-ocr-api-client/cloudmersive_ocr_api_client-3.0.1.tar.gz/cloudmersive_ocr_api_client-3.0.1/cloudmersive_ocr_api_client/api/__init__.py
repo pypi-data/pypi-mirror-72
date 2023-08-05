e6bc79d2fc2725d93dbd23acb9668cad8fd23be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:34 +0000

    Add package cloudmersive-ocr-api-client/3.0.1/cloudmersive_ocr_api_client-3.0.1.tar.gz

---
+++ packages/cloudmersive-ocr-api-client/cloudmersive_ocr_api_client-3.0.1.tar.gz/cloudmersive_ocr_api_client-3.0.1/cloudmersive_ocr_api_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cloudmersive_ocr_api_client.api.image_ocr_api import ImageOcrApi
+from cloudmersive_ocr_api_client.api.pdf_ocr_api import PdfOcrApi
+from cloudmersive_ocr_api_client.api.preprocessing_api import PreprocessingApi
+from cloudmersive_ocr_api_client.api.receipts_api import ReceiptsApi
