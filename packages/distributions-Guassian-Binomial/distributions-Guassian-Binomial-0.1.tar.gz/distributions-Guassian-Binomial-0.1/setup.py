ca964219aaf6e2eb0c262f17dd59f7b0ce9eb249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:45 +0000

    Add package distributions-Guassian-Binomial/0.1/distributions-Guassian-Binomial-0.1.tar.gz

---
+++ packages/distributions-Guassian-Binomial/distributions-Guassian-Binomial-0.1.tar.gz/distributions-Guassian-Binomial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-Guassian-Binomial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-Guassian-Binomial'],
+      zip_safe=False)
