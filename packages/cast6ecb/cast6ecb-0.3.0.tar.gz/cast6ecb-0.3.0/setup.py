526893a3601716e92ae7035aa4200b6a1df0ff94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package cast6ecb/0.3.0/cast6ecb-0.3.0.tar.gz

---
+++ packages/cast6ecb/cast6ecb-0.3.0.tar.gz/cast6ecb-0.3.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from setuptools import setup, Extension
+
+VERSION = "0.3.0"
+
+setup(
+    name="cast6ecb",
+    version=VERSION,
+    description="Python3 interface to mcrypt library, only for CAST-256 (CAST6) algorithm, with ECB mode.",
+    author="Remi Paulmier",
+    author_email="pypi-ops@blablacar.com",
+    license="LGPL",
+    url="https://github.com/shtouff/python-cast6ecb",
+    long_description= \
+        """
+        Partial python3 interface to mcrypt library. This can be used only for CAST-256 (CAST6) algorithm, with ECB mode.
+        """,
+    ext_modules=[
+        Extension(
+            "cast6ecb",
+            ["cast6ecb.c"],
+            libraries=["mcrypt"],
+            define_macros=[("VERSION", '"%s"' % VERSION)]
+        )
+    ],
+    classifiers=['Topic :: Security :: Cryptography'],
+)
