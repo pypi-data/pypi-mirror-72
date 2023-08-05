7296defaf11bf240f54e33d9ba14ed5f270c0368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:21 +0000

    Add package logentries-cli/1.2.1/logentries-cli-1.2.1.tar.gz

---
+++ packages/logentries-cli/logentries-cli-1.2.1.tar.gz/logentries-cli-1.2.1/setup.py
@@ -0,0 +1,50 @@
+# -*- encoding:utf-8 -*-
+from __future__ import print_function
+import io
+
+from setuptools import setup
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.rst')
+
+setup(
+    name='logentries-cli',
+    version='1.2.1',
+    description='Get your logs from Logentries on the comandline',
+    author='Adam Johnson',
+    author_email='me@adamj.eu',
+    url='http://github.com/adamchainz/logentries-cli/',
+    license='GPLv3',
+    install_requires=[
+        'parsedatetime>=1.4',
+        'python-dateutil>=2.4.0',
+        'requests>=2.5.1',
+    ],
+    scripts=['bin/logentries'],
+    long_description=long_description,
+    packages=[],
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Logging',
+    ],
+)
