83a09a2c1db057485a35128fafe441b0453f4f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:12 +0000

    Add package splunksecrets/0.5.0/splunksecrets-0.5.0.tar.gz

---
+++ packages/splunksecrets/splunksecrets-0.5.0.tar.gz/splunksecrets-0.5.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+VERSION = "0.5.0"
+
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="splunksecrets",
+    version=VERSION,
+    author="Steve McMaster",
+    author_email="mcmaster@hurricanelabs.com",
+    py_modules=["splunksecrets"],
+    description="splunksecrets - Encrypt / Decrypt Splunk encrypted passwords",
+    long_description=long_description,
+    install_requires=[
+        "cryptography",
+        "pcrypt",
+        "six>=1.12.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "splunksecrets = splunksecrets:main",
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    bugtrack_url="https://github.com/HurricaneLabs/splunksecrets/issues",
+)
