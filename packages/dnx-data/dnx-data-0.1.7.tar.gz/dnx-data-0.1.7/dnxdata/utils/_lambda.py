107caf8901fd7c36bb81fc8f795e1603f6caf9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:51 +0000

    Add package dnx-data/0.1.7/dnx-data-0.1.7.tar.gz

---
+++ packages/dnx-data/dnx-data-0.1.7.tar.gz/dnx-data-0.1.7/dnxdata/utils/_lambda.py
@@ -0,0 +1,25 @@
+from dnxdata.resource import lambda_client
+from dnxdata.logger import Logger
+
+
+class Lambda:
+
+    def __init__(self):
+        self.logger = Logger("DNX Lambda => ")
+
+    def invoke(self, name):
+        self.logger.info("Starting Invoke Lambda")
+        response = lambda_client.invoke_lambda.invoke(
+            FunctionName=name,
+            InvocationType='Event'
+        )
+
+        if response.get("StatusCode") == 202:
+            self.logger.info("Invoke Lambda Success")
+        else:
+            self.logger.info(
+                "Failed Invoke Lambda {}"
+                .format(response)
+            )
+
+        self.logger.info("Finishing Invoke Lambda")
