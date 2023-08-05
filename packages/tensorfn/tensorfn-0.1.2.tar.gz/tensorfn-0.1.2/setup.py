fdbfc8877a8e65a3fc7d4b805d1649020c4c0622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:58 +0000

    Add package tensorfn/0.1.2/tensorfn-0.1.2.tar.gz

---
+++ packages/tensorfn/tensorfn-0.1.2.tar.gz/tensorfn-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='tensorfn',
+    version='0.1.2',
+    description='Non-opionated utility library for PyTorch',
+    url='https://github.com/rosinality/tensorfn',
+    author='Kim Seonghyeon',
+    author_email='kim.seonghyeon@navercorp.com',
+    license='MIT',
+    install_requires=[
+        'torch>=1.1',
+        'pydantic>=1.5',
+        'pyhocon>=0.3.54'
+    ]
+)
