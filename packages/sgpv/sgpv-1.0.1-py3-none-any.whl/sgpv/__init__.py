58c9bce706157081bb5f289661f3b643b2abe5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:42 +0000

    Add package sgpv/1.0.1/sgpv-1.0.1-py3-none-any.whl

---
+++ packages/sgpv/sgpv-1.0.1-py3-none-any.whl/sgpv/__init__.py
@@ -0,0 +1,14 @@
+"""
+The sgpv-module is a translation of the same named R-library of the same name.
+The original R-library can be found here https://github.com/weltybiostat/sgpv
+The authors of the R-library and this module for Python are not connected with each other.
+There is a translation of the R-library into Stata with additional features by the author of this module.
+The Stata version of this module can be found here https://github.com/skbormann/stata-tools/tree/master/sgpv
+
+This module contains the following functions:
+            sgpv.value   - calculate the SGPVs
+            sgpv.power   - power functions for the SGPVs
+            sgpv.risk    - false confirmation/discovery risks for the SGPVs
+            sgpv.plot    - plot the SGPVs
+
+"""
