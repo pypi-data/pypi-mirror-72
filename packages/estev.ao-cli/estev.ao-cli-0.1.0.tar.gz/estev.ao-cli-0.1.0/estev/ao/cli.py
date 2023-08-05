0b26b5ce84e3cd5b3b012449d8c8105f79946df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:09 +0000

    Add package estev.ao-cli/0.1.0/estev.ao-cli-0.1.0.tar.gz

---
+++ packages/estev.ao-cli/estev.ao-cli-0.1.0.tar.gz/estev.ao-cli-0.1.0/estev/ao/cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""estev.ao
+
+Usage:
+  estev <url>
+  estev (-h | --help)
+  estev (-v | --version)
+
+Options:
+  -h --help         Show this screen.
+  -v --version      Show version.
+"""
+from docopt import docopt
+import requests
+import pyperclip
+
+from estev.ao import VERSION
+
+
+def create_link(url: str) -> str:
+    """Create a shorted version of the url"""
+    result = requests.post('http://api.estev.ao/links', json={
+        'url': url
+    }).json()
+
+    return result['link']
+
+
+def main():
+    """
+    Main entry point for the tool.
+    """
+    arguments = docopt(__doc__, version=f"{VERSION}")
+
+    link = create_link(url=arguments['<url>'])
+
+    pyperclip.copy(link)
+
+    print(link)
