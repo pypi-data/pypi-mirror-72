22f78206e95bcb5f14fbbe375caca6e1c0f3e499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:06 +0000

    Add package npu/0.1.12/npu-0.1.12.tar.gz

---
+++ packages/npu/npu-0.1.12.tar.gz/npu-0.1.12/npu/loss/__init__.py
@@ -0,0 +1,10 @@
+"""
+:synopsis: Loss functions to use with NPU API for training
+.. moduleauthor:: Naval Bhandari <naval@neuro-ai.co.uk>
+"""
+
+CrossEntropyLoss = "CrossEntropyLoss"  #:
+MSELoss = "MSELoss"  #:
+CTCLoss = "CTCLoss"  #:
+L1Loss = "L1Loss"  #:
+NLLLoss = "NLLLoss"  #:
---
+++ packages/npu/npu-0.1.12.tar.gz/npu-0.1.12/npu/vision/models.py
@@ -0,0 +1,14 @@
+"""
+:synopsis: Global models available
+.. moduleauthor:: Naval Bhandari <naval@neuro-ai.co.uk>
+"""
+
+from ..core import Model
+
+resnet18 = Model("resnet18")
+""":resnet18: Resnet 18 model. Access pretrained version `resnet18(pretrained=True)` """
+
+vgg16 = Model("vgg16")
+""":vgg16: VGG16 model. Access pretrained version `vgg16(pretrained=True)` """
+
+
