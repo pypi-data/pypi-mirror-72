9a28f96ccfb81a60400e1d7bf455b862e3e923b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package slidingwindow/0.0.14/slidingwindow-0.0.14.tar.gz

---
+++ packages/slidingwindow/slidingwindow-0.0.14.tar.gz/slidingwindow-0.0.14/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name = 'slidingwindow',
+	version = '0.0.14',
+	description = 'Sliding Window library for image processing in Python',
+	url = 'https://github.com/adamrehn/slidingwindow',
+	author = 'Adam Rehn',
+	author_email = 'adam@adamrehn.com',
+	license = 'MIT',
+	packages = ['slidingwindow'],
+	zip_safe = True,
+	install_requires = [
+		'numpy',
+		'psutil'
+	]
+)
