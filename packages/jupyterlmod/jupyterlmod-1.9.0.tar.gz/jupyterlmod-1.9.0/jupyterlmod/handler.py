1678dcbb31e9515b5fa2e05125637a888c399f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package jupyterlmod/1.9.0/jupyterlmod-1.9.0.tar.gz

---
+++ packages/jupyterlmod/jupyterlmod-1.9.0.tar.gz/jupyterlmod-1.9.0/jupyterlmod/handler.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+import tornado.web
+import lmod
+
+from functools import partial
+from jupyter_core.paths import jupyter_path
+from notebook.base.handlers import IPythonHandler
+
+ACTIONS = {
+    "avail"    : lmod.avail,
+    "list"     : partial(lmod.list, hide_hidden=True),
+    "freeze"   : lmod.freeze,
+    "show"     : lmod.show,
+    "load"     : lmod.load,
+    "unload"   : lmod.unload,
+    "purge"    : lmod.purge,
+    "savelist" : lmod.savelist,
+    "save"     : lmod.save,
+    "restore"  : lmod.restore,
+    "reset"    : lmod.reset
+}
+
+class LmodActionHandler(IPythonHandler):
+    @tornado.web.authenticated
+    def get(self, action):
+        func = ACTIONS.get(action, None)
+        if func:
+            args = self.get_arguments("args")
+            result = func(*args)
+            self.finish(json.dumps(result))
+
+    @tornado.web.authenticated
+    def post(self, action):
+        func = ACTIONS.get(action, None)
+        if func:
+            args = self.get_arguments('args')
+            if args:
+                jpath_old = os.environ.get('JUPYTER_PATH')
+                func(*args)
+                # If JUPYTER_PATH has been modified by func
+                # the kernel directory list is updated.
+                if jpath_old != os.environ.get('JUPYTER_PATH'):
+                    self.kernel_spec_manager.kernel_dirs = jupyter_path('kernels')
+                self.finish(json.dumps('SUCCESS'))
+
+_action_regex = r"/lmod/(?P<action>{})".format("|".join(ACTIONS.keys()))
+default_handlers = [(_action_regex, LmodActionHandler)]
+
+if __name__ == "__main__":
+    import tornado.ioloop
+    app = tornado.web.Application([ (r"/", LmodActionHandler), ])
+    app.listen(12345)
+    tornado.ioloop.IOLoop.current().start()
---
+++ packages/jupyterlmod/jupyterlmod-1.9.0.tar.gz/jupyterlmod-1.9.0/jupyterlmod/static/lmod.js
@@ -0,0 +1,99 @@
+function getCookie(name) {
+  var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+  return r ? r[1] : undefined;
+}
+
+var $ = require('jquery');
+
+class Lmod {
+  constructor(base_url) {
+    this._available = null;
+    this._loaded = null;
+    this._savelist = null;
+    this.url = base_url + 'lmod/'
+    this._xsrf = getCookie("_xsrf");
+  }
+
+  avail() {
+    if(this._available == null){
+      this._available = this.get('avail', {});
+    }
+    return this._available;
+  }
+
+  list() {
+    if(this._loaded == null) {
+      this._loaded = this.get('list', {});
+    }
+    return this._loaded;
+  }
+
+  load(modules) {
+    this._available = null;
+    this._loaded = null;
+    return this.post('load', {'args' : modules});
+  }
+
+  restore(name) {
+    this._available = null;
+    this._loaded = null;
+    return this.post('restore', {'args' : name});
+  }
+
+  save(name) {
+    this._savelist = null;
+    return this.post('save', {'args' : name});
+  }
+
+  savelist() {
+    if (this._savelist == null) {
+      this._savelist = this.get('savelist', {});
+    }
+    return this._savelist;
+  }
+
+  show(module) {
+    return this.get('show', {'args' : module});
+  }
+
+  freeze() {
+    return this.get('freeze', {});
+  }
+
+  unload(modules) {
+    this._available = null;
+    this._loaded = null;
+    return this.post('unload', {'args' : modules});
+  }
+
+  get(action, options) {
+    return $.ajax({
+            url: this.url + action,
+            type: "GET",
+            dataType: "json",
+            data: options,
+            traditional: true
+        });
+  }
+
+  post(action, options) {
+    options['_xsrf'] = this._xsrf;
+        return $.ajax({
+            url: this.url + action,
+            type: "POST",
+            dataType: "json",
+            data: options,
+            traditional: true
+        });
+  }
+}
+
+define({
+  Lmod : Lmod
+});
+
+// Export is only used for TS. Try/Catch to avoid error in JS
+try {
+  exports.Lmod = Lmod;
+}
+catch (ReferenceError) {}
