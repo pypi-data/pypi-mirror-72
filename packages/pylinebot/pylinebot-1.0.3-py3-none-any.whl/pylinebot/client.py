88ef07b801a9c7cdce38a49484d94598bde0367d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:48 +0000

    Add package pylinebot/1.0.3/pylinebot-1.0.3-py3-none-any.whl

---
+++ packages/pylinebot/pylinebot-1.0.3-py3-none-any.whl/pylinebot/client.py
@@ -0,0 +1,15 @@
+# third party
+from linebot import LineBotApi, WebhookParser
+
+# local
+from .service import Service
+
+class LINE(Service):
+
+    def __init__(self, channel_access_token, channel_secret):
+        self.client = LineBotApi(channel_access_token)
+        self.parser = WebhookParser(channel_secret)
+        self.initialize()
+
+    def initialize(self):
+        Service.__init__(self)
