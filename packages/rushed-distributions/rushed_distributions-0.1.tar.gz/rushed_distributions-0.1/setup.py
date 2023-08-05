4f161d3dc1b8c1c6d1d923c552cbdce6d5ed40bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package rushed-distributions/0.1/rushed_distributions-0.1.tar.gz

---
+++ packages/rushed-distributions/rushed_distributions-0.1.tar.gz/rushed_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'rushed_distributions',
+      version = '0.1',
+      description = 'Binomial and Gaussian distributions',
+      packages = ['rushed_distributions'],
+      author = 'Rushikesh Deotale',
+      zip_safe = False)
