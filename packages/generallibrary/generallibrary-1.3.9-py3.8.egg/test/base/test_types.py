db6b7b8a701fe1ed267ae3af2866244a7845b914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package generallibrary/1.3.9/generallibrary-1.3.9-py3.8.egg

---
+++ packages/generallibrary/generallibrary-1.3.9-py3.8.egg/test/base/test_types.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+from generallibrary import strToDynamicType, typeChecker
+
+class TypesTest(unittest.TestCase):
+    def test_strToDynamicType(self):
+        pass
+
+    def test_typeChecker(self):
+        self.assertTrue(typeChecker("hello", (bool, str)))
+        self.assertTrue(typeChecker(["hello"], list, (bool, str)))
+        self.assertFalse(typeChecker(["hello"], (tuple, dict), (bool, str), raiseTypeError=False))
