36d257c2f499d815b2ec7c7bc12b8993df73d25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:59 +0000

    Add package litNlp/0.8.3/litNlp-0.8.3.tar.gz

---
+++ packages/litNlp/litNlp-0.8.3.tar.gz/litNlp-0.8.3/README.md
@@ -0,0 +1,48 @@
+[![996.icu](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu)
+
+# litNlp
+## 简介
+litNlp是基于Tensorflow2.0实现的一个轻量级的深度情感极性推理模型,类别默认2分极性训练，
+GPU和CPU平台通用，是搭建文本分类模型baseline的快速方案。
+1. 内置情感分析模型-利用深度模型优化语义建模，使用字符级减少tokenizer的大小
+2. 直接提供模型训练，默认Text-CNN字符级卷积网络作为baseline，自带早停操作，使用少的参数即可开始训练多分类模型
+
+# 使用方法
+> 1. pip install  litNlp
+> 2. 模型需要先通过训练，保存在sa_model里面，然后就可以批预测，具体的使用见example文件内容
+
+    from litNlp.predict import SA_Model_Predict
+    # 批处理文本
+    predict_text = ['这个我不喜欢', '这个我喜欢不']
+    # 初始化并加载模型
+    tokenizer_path = 'sa_model/tokenizer.pickle'
+    sa_model_path = 'sa_model/c_cnn_m.h5'
+    model = SA_Model_Predict(tokenizer_path,sa_model_path_m=sa_model_path)
+    sa_score = model.predict(predict_text)
+    print([i[1] for i in sa_score])
+ 
+# 参数解释
+    # 最大句子长度
+    maxlen = 100
+    # 最大的tokenizer字典长度
+    max_words = 1000
+    # 设置embedding大小
+    embedding_dim = 300
+    # 模型的保存位置，后续用于推理
+    sa_model_path_m = 'sa_model/c_cnn_m.h5'
+    # 离线保存tokenizer
+    tokenize_path ='sa_model/tokenizer.pickle'
+    # 分类的类别数
+    num_classes = 2
+    # train_method : 模型训练方式，默认textcnn，可选：bilstm , gru
+    train_method = 'textcnn'
+# 2个epoch 的二分类性能
+
+<div align=center><img  src="https://github.com/CarryChang/litNlp/blob/master/pic/auc_2poch.png"></div>
+
+# jupyter 加载
+> 1. 情感分析，优化语义的情感推理
+<div align=center><img  src="https://github.com/CarryChang/litNlp/blob/master/pic/tools.png"></div>
+
+ 
+### [pipy code](https://pypi.org/project/litNlp/)
---
+++ packages/litNlp/litNlp-0.8.3.tar.gz/litNlp-0.8.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import setuptools
+# with open("README.md", "r", encoding='utf-8') as fh:
+#     long_description = fh.read()
+REQUIRED_PACKAGES = [
+    'h5py', 'requests'
+]
+setuptools.setup(
+  name="litNlp",
+  version="0.8.3",
+  packages=['litNlp', 'litNlp.model_structure'],
+  author="CarryChang",
+  author_email="coolcahng@gmail.com",
+  url='https://github.com/CarryChang/litNlp',
+  include_package_data=True,
+  description='A fast tool for text classification model with tensorflow ',
+  long_description='litNlp是基于Tensorflow2.0实现的一个轻量级的深度文本分类模型,支持多分类，并默认二分类，是搭建文本分类模型的快速方案',
+  install_requires=['tensorflow==2.0.1', 'numpy==1.17.0', 'pandas', 'sklearn'],
+  python_requires=">=3.4",
+  zip_safe=True,
+  classifiers=(
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+  extras_require={
+        "cpu": ["tensorflow==2.0.0"],
+        "gpu": ["tensorflow-gpu==2.0.0"],
+    },
+  entry_points={
+    },
+  license="Apache-2.0",
+  keywords=['text classification', 'nlp','batch predict',
+              'deep learning', 'tensorflow', 'ml',],
+)
