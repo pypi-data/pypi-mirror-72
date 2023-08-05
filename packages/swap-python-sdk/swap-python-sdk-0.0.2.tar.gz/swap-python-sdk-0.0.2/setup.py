1003074348fe57449b2bedd5e9005095e556cc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package swap-python-sdk/0.0.2/swap-python-sdk-0.0.2.tar.gz

---
+++ packages/swap-python-sdk/swap-python-sdk-0.0.2.tar.gz/swap-python-sdk-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name='swap-python-sdk',
+    version='0.0.2',
+    author='',
+    author_email='',
+    description='SWAP Python SDK',
+    # long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        'google-cloud-pubsub==1.1.0',
+        'jsonschema==3.2.0',
+        'marshmallow==3.3.0',
+        'python-dotenv==0.10.5',
+        'requests==2.22.0'
+    ],
+    package_data={'swap': ['common/static/schemas/*']},
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ]
+)
