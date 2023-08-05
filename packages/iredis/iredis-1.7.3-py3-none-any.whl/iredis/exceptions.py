8edc5c4edfc75288c1b10a08e6edef9c5f347c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package iredis/1.7.3/iredis-1.7.3-py3-none-any.whl

---
+++ packages/iredis/iredis-1.7.3-py3-none-any.whl/iredis/exceptions.py
@@ -0,0 +1,18 @@
+class IRedisException(Exception):
+    pass
+
+
+class UsageError(IRedisException):
+    pass
+
+
+class InvalidArguments(IRedisException):
+    """Invalid argument(s)"""
+
+
+class NotRedisCommand(IRedisException):
+    """Not a Redis command"""
+
+
+class AmbiguousCommand(IRedisException):
+    """Command is not finished, don't it's command's name"""
