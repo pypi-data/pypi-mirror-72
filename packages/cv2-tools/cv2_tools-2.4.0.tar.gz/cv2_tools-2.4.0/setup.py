acbdf9487b2bb88fe0f8589ab6496b1960c82d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:46 +0000

    Add package cv2-tools/2.4.0/cv2_tools-2.4.0.tar.gz

---
+++ packages/cv2-tools/cv2_tools-2.4.0.tar.gz/cv2_tools-2.4.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='cv2_tools',
+    version='2.4.0',
+    author='Fernando Pérez',
+    author_email='fernaperg@gmail.com',
+    description='Library to help the drawing process with OpenCV. Thought to add labels to the images. Classification of images, etc.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fernaper/opencv-draw-tools',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        #'opencv-python',
+        'numpy',
+        'python-constraint',
+        #'pillow',
+        'imagehash'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
