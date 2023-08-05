6758a01eab9d01b8cec86be4aeba461a3245dbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:25 +0000

    Add package autodyna/2.5.7/autodyna-2.5.7.tar.gz

---
+++ packages/autodyna/autodyna-2.5.7.tar.gz/autodyna-2.5.7/autodyna/__init__.py
@@ -0,0 +1,9 @@
+from . import Beams, initialize
+import os
+import sys
+
+# Insert relevant path
+initPath = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, initPath)
+
+# Import project libraries
