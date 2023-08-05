f80949a1648f5f975e880f655c5588110585ba39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package pyformlang/0.1.20/pyformlang-0.1.20-py3-none-any.whl

---
+++ packages/pyformlang/pyformlang-0.1.20-py3-none-any.whl/pyformlang/regular_expression/__init__.py
@@ -0,0 +1,25 @@
+"""
+:mod:`pyformlang.regular_expression`
+====================================
+
+This module deals with regular expression.
+
+By default, this module does not use the standard way to write regular
+expressions. Please read the documentation of Regex for more information.
+
+Available Classes
+-----------------
+
+Regex
+    A regular expression
+MisformedRegexError
+    An error occurring when the input regex is incorrect
+
+"""
+
+
+from .regex import Regex
+from .regex_objects import MisformedRegexError
+from .python_regex import PythonRegex
+
+__all__ = ["Regex", "PythonRegex", "MisformedRegexError"]
