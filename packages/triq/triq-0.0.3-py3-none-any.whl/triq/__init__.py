0f222e93647b3b67d7a0e8e66e6a1098eb95833e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:55 +0000

    Add package triq/0.0.3/triq-0.0.3-py3-none-any.whl

---
+++ packages/triq/triq-0.0.3-py3-none-any.whl/triq/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.3'
+__description__ = 'Use trio async framework with PySide2/PyQt5'
+__url__ = "https://github.com/innodatalabs/triq/"
+__author__ = 'Mike Kroutikov'
+__author_email__ = 'mkroutikov@innodata.com'
+__keywords__ = 'pyqt pyside async trio'
+
+
+from .triq import run, exit, call_async
