291670f9294990109c14537f68bc5d78e8704233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:16 +0000

    Add package whiterabbit/0.0.1/whiterabbit-0.0.1.tar.gz

---
+++ packages/whiterabbit/whiterabbit-0.0.1.tar.gz/whiterabbit-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="whiterabbit",
+    version="0.0.1",
+    license='Apache License, Version 2.0',
+    author="TaeHwan-Jung",
+    author_email="nlkey2022@gmail.com",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+)
