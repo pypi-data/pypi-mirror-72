81ecee80821603bb416abee00b132441bf757fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package dgds-distributions/0.1/dgds_distributions-0.1.tar.gz

---
+++ packages/dgds-distributions/dgds_distributions-0.1.tar.gz/dgds_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dgds_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dgds_distributions'],
+      author='Daniel Grewal',
+      author_email='daniel.grewal1503@icloud.com',
+      zip_safe=False)
