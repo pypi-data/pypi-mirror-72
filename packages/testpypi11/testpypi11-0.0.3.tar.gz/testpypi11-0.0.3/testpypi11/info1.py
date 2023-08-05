75b288074a6ec9dbf3d4976186325f9b2543d4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package testpypi11/0.0.3/testpypi11-0.0.3.tar.gz

---
+++ packages/testpypi11/testpypi11-0.0.3.tar.gz/testpypi11-0.0.3/testpypi11/info1.py
@@ -0,0 +1,9 @@
+
+class Info1:
+	def __init__(self):
+		pass
+	
+	@staticmethod
+	def print_hello():
+		print('hello this is Info1.')
+
---
+++ packages/testpypi11/testpypi11-0.0.3.tar.gz/testpypi11-0.0.3/testpypi11/test_package2/info2.py
@@ -0,0 +1,9 @@
+
+class Info2:
+	def __init__(self):
+		pass
+	
+	@staticmethod
+	def print_hello2():
+		print('Hellow, this is Info2.')
+
