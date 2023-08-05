d23fd10dd6261cb84c4e20e0354ef0832cf96f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package Distributions-Pack-Testing/1.0/Distributions_Pack_Testing-1.0.tar.gz

---
+++ packages/Distributions-Pack-Testing/Distributions_Pack_Testing-1.0.tar.gz/Distributions_Pack_Testing-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Distributions_Pack_Testing',
+      version='1.0',
+      description='Gaussian Distributions_Pack_Testing',
+      packages=['Distributions_Pack_Testing'],
+      author='Harish Mohanaselvam',
+      author_email='harish.msl@hotmail.com',
+      zip_safe=False)
