4c1fd704e9368bae3e659ecca5404c223b390ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:39 +0000

    Add package gauss-bino-distributions-ercelik/0.1/gauss_bino_distributions_ercelik-0.1.tar.gz

---
+++ packages/gauss-bino-distributions-ercelik/gauss_bino_distributions_ercelik-0.1.tar.gz/gauss_bino_distributions_ercelik-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'gauss_bino_distributions_ercelik',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['gauss_bino_distributions_ercelik'],
+      author = 'Dilay Fidan Ercelik',
+      author_email = 'dilay.ercelik@gmail.com',
+      zip_safe = False)
