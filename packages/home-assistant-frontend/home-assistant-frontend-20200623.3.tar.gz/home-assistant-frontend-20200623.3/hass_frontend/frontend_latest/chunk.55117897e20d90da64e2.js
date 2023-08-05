39749feb1078a9e29affb420904e0730e4d79d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package home-assistant-frontend/20200623.3/home-assistant-frontend-20200623.3.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20200623.3.tar.gz/home-assistant-frontend-20200623.3/hass_frontend/frontend_latest/chunk.55117897e20d90da64e2.js
@@ -0,0 +1,2 @@
+(self.webpackJsonp=self.webpackJsonp||[]).push([[38],{813:function(e,r,n){"use strict";n.r(r),n.d(r,"codeMirror",(function(){return c})),n.d(r,"codeMirrorCss",(function(){return i}));var s=n(504),o=n.n(s),t=n(712),a=(n(713),n(714),n(13));o.a.commands.save=e=>{Object(a.a)(e.getWrapperElement(),"editor-save")};const c=o.a,i=t.a}}]);
+//# sourceMappingURL=chunk.55117897e20d90da64e2.js.map
