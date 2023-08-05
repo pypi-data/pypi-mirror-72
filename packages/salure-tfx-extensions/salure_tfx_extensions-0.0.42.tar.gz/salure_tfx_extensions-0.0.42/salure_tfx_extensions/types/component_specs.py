0c81030b21f8971e4c12d2a3780b9227603375ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:21 +0000

    Add package salure-tfx-extensions/0.0.42/salure_tfx_extensions-0.0.42.tar.gz

---
+++ packages/salure-tfx-extensions/salure_tfx_extensions-0.0.42.tar.gz/salure_tfx_extensions-0.0.42/salure_tfx_extensions/types/component_specs.py
@@ -0,0 +1,18 @@
+"""Standard Component specs for the salure_tfx_extensions library"""
+
+from tfx.types import ComponentSpec
+from tfx.types.component_spec import ChannelParameter
+from tfx.types import standard_artifacts
+
+
+class BaseSpec(ComponentSpec):
+    """Salure_tfx_extensions BaseComponent spec"""
+
+    PARAMETERS = {}
+    INPUTS = {
+        'examples': ChannelParameter(type=standard_artifacts.Examples)
+    }
+    OUTPUTS = {
+        'output_examples': ChannelParameter(type=standard_artifacts.Examples)
+    }
+
