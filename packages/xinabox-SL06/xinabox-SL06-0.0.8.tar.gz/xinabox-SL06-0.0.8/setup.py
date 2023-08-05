9cc3a8bbbc45db2cb43a4d6126f980ef55dc9064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package xinabox-SL06/0.0.8/xinabox-SL06-0.0.8.tar.gz

---
+++ packages/xinabox-SL06/xinabox-SL06-0.0.8.tar.gz/xinabox-SL06-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import sys
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = list()
+if sys.platform == "linux2" or sys.platform == "linux":
+    install_requires = ["smbus2", "xinabox-CORE",]
+else:
+    install_requires = ["xinabox-CORE",]
+
+setuptools.setup(
+    name="xinabox-SL06",
+    version="0.0.8",
+    author="Luqmaan Baboo",
+    author_email="luqmaanbaboo@gmail.com",
+    description="Proximity, gesture, light and colour sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinabox/Python-SL06",
+    install_requires=install_requires,
+    py_modules=["xSL06",],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
