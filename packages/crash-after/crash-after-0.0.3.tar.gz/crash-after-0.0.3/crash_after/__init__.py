acf63d9a2586381a935baf66074b6f6bb3601b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package crash-after/0.0.3/crash-after-0.0.3.tar.gz

---
+++ packages/crash-after/crash-after-0.0.3.tar.gz/crash-after-0.0.3/crash_after/__init__.py
@@ -0,0 +1,20 @@
+import datetime
+
+from decorator import decorator
+
+
+class DateTimeException(Exception):
+    message = "This method has stopped worked"
+
+
+@decorator
+def crash_after(func, date: str = None, *args, **kwargs):
+    if date is None:
+        return func(*args, **kwargs)
+
+    now = datetime.datetime.now()
+    _crash_after = datetime.datetime.strptime(date, "%m/%d/%y")
+    if _crash_after < now:
+        raise DateTimeException
+
+    return func(*args, **kwargs)
