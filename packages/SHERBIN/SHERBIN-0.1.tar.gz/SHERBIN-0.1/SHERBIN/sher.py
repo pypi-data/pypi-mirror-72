acc0bfeb066a69bcd4305248897d906138b0e7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package SHERBIN/0.1/SHERBIN-0.1.tar.gz

---
+++ packages/SHERBIN/SHERBIN-0.1.tar.gz/SHERBIN-0.1/SHERBIN/sher.py
@@ -0,0 +1,5 @@
+def sherbin(str):
+    if str.count('@')==1 and str.count('.')==1:
+        return True
+    else:
+        return False
---
+++ packages/SHERBIN/SHERBIN-0.1.tar.gz/SHERBIN-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='SHERBIN',
+      version='0.1',
+      description='SHER_EMAIL',
+      packages=['SHERBIN'],
+      zip_safe=False)
