7e0b8529ff53be9656a8f60626070bc2bd8a249f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:50 +0000

    Add package afaans-distribution/0.1/afaans_distribution-0.1.tar.gz

---
+++ packages/afaans-distribution/afaans_distribution-0.1.tar.gz/afaans_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='afaans_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['afaans_distribution'],
+      author='Affan Shariff',
+      author_email='afaanshariff29@gmail.com',
+      zip_safe=False)
