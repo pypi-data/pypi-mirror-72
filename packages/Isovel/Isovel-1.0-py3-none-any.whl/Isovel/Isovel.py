1d91725b7a3cbec2b118e343b25e9bc6e3db7826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:20 +0000

    Add package Isovel/1.0/Isovel-1.0-py3-none-any.whl

---
+++ packages/Isovel/Isovel-1.0-py3-none-any.whl/Isovel/Isovel.py
@@ -0,0 +1,15 @@
+# This to start documentation
+import numpy as np
+from scipy.ndimage.interpolation import rotate
+
+def mult(a,b):
+    """
+    multiplication
+
+    Args: 
+        a (float): first number
+        b (float): second number
+    Returns:
+        float: multiplication
+    """
+    return a*b
