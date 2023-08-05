4bfb2164d580154afd96f7ce44e210491d329f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:12 +0000

    Add package test-gaussian-distributions/0.1/test_gaussian_distributions-0.1.tar.gz

---
+++ packages/test-gaussian-distributions/test_gaussian_distributions-0.1.tar.gz/test_gaussian_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='test_gaussian_distributions',
+      version='0.1',
+      description='Kamal lamichhane test Gaussian distributions',
+      packages=['test_gaussian_distributions'],
+      author = 'Kamal Lamichane',
+      author_email = 'Kamal.Lamichane@leddartech.com',
+      zip_safe=False)
