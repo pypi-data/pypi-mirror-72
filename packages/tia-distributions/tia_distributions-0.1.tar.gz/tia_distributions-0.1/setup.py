dc43a7ef487918dfd46e9362d722aa3996290255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package tia-distributions/0.1/tia_distributions-0.1.tar.gz

---
+++ packages/tia-distributions/tia_distributions-0.1.tar.gz/tia_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tia_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tia_distributions'],
+      author='Iddi Abdul Aziz',
+      author_email='iddiabdulaziz1@gmail.com',
+   zip_safe=False)
