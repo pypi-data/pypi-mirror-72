33509e54d0a7266bca090294d0e7f9d76dd90d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package onapsdk/1.0.0/onapsdk-1.0.0-py3-none-any.whl

---
+++ packages/onapsdk/onapsdk-1.0.0-py3-none-any.whl/onapsdk/aai/business/__init__.py
@@ -0,0 +1,7 @@
+"""A&AI business package."""
+from .customer import Customer, ServiceSubscription
+from .instance import Instance
+from .owning_entity import OwningEntity
+from .service import ServiceInstance
+from .vf_module import VfModuleInstance
+from .vnf import VnfInstance
