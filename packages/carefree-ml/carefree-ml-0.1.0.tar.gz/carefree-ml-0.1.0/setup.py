7a8dff54b145638c43dc43bdaf9ce46786dfbb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package carefree-ml/0.1.0/carefree-ml-0.1.0.tar.gz

---
+++ packages/carefree-ml/carefree-ml-0.1.0.tar.gz/carefree-ml-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+
+DESCRIPTION = "Machine Learning algorithms implemented with numpy"
+with open("README.md", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="carefree-ml",
+    version="0.1.0",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[
+        "carefree-data>=0.1.2",
+        "carefree-toolkit>=0.1.5",
+        "dill", "future", "psutil", "pillow",
+        "cython>=0.29.12", "numpy>=1.16.2", "scipy>=1.2.1",
+        "scikit-learn>=0.20.3", "matplotlib>=3.0.3",
+        "mkdocs", "mkdocs-material", "mkdocs-minify-plugin",
+        "Pygments", "pymdown-extensions"
+    ],
+    author="carefree0910",
+    author_email="syameimaru_kurumi@pku.edu.cn",
+    url="https://github.com/carefree0910/carefree-ml",
+    download_url=f"https://github.com/carefree0910/carefree-ml/archive/v{VERSION}.tar.gz",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords="python machine-learning numpy"
+)
