b0dbb8b7d6714f4518707df6669e8a516251ba9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:50 +0000

    Add package notetool/0.3.0/notetool-0.3.0.tar.gz

---
+++ packages/notetool/notetool-0.3.0.tar.gz/notetool-0.3.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+def get_version(argv):
+    if len(argv) <= 1:
+        return
+    if argv[1] != 'build':
+        return
+    from os import path
+    here = path.abspath(path.dirname(__file__))
+    version_path = path.join(here, '__version__.md')
+    if path.exists(version_path):
+        with open(version_path, 'r') as f:
+            version1 = [int(i) for i in f.read().split('.')]
+    else:
+        version1 = [0, 0, 1]
+    version2 = version1[0] * 100 + version1[1] * 10 + version1[2] + 1
+
+    version1[2] = version2 % 10
+    version1[1] = int(version2 / 10) % 10
+    version1[0] = int(version2 / 100)
+    version3 = '{}.{}.{}'.format(*version1)
+    with open(version_path, 'w') as f:
+        f.write(version3)
+    return version3
+
+
+version = get_version(['a', 'build'])
+
+install_requires = ['IPython', 'matplotlib', 'pycurl', 'cryptography', 'six']
+
+setup(name='notetool',
+      version=version,
+      description='notetool',
+      author='niuliangtao',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+
+      packages=find_packages(),
+      install_requires=install_requires
+      )
