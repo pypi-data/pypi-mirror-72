5607ede814908b8ce5099310a4cbb9abf19f80e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:33 +0000

    Add package first-test-package/0.12/first_test_package-0.12.tar.gz

---
+++ packages/first-test-package/first_test_package-0.12.tar.gz/first_test_package-0.12/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='first_test_package',
+      version='0.12',
+      description='Gaussian distributions',
+      packages=['first_test_package'],
+      zip_safe=False)
