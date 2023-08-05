cf99be4a5823c3cb0111117d37660829f3d7c9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package napoleontoolbox/2.5.11/napoleontoolbox-2.5.11.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.5.11.tar.gz/napoleontoolbox-2.5.11/napoleontoolbox/forecasting/forecasting_utility.py
@@ -0,0 +1,48 @@
+import lightgbm as lgb
+
+import numpy as np
+
+from scipy.stats import uniform, randint
+
+from sklearn.datasets import load_breast_cancer, load_diabetes, load_wine
+from sklearn.metrics import auc, accuracy_score, confusion_matrix, mean_squared_error
+from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, RandomizedSearchCV, train_test_split
+from sklearn.linear_model import Lasso
+import xgboost as xgb
+from sklearn import datasets, linear_model
+
+from napoleontoolbox.forecasting import lgbm_model_wrapper
+from napoleontoolbox.forecasting import arima_model_wrapper
+from napoleontoolbox.forecasting import sma_model_wrapper
+
+
+from abc import ABC, abstractmethod
+
+
+def instantiate_model(method = 'default',idios = {}):
+    if method == 'lgbm':
+        return lgbm_model_wrapper.LGBMModel(**idios)
+    if method == 'arima':
+        return arima_model_wrapper.ArimaModel(**idios)
+    if method == 'sma':
+        return sma_model_wrapper.SmaModel(**idios)
+
+
+class AbstractForecasterWrapper(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def calibrate(self, X, y):#, method = 'standard'):
+        pass
+
+    @abstractmethod
+    def fit(self, X_train, y_train, X_val, y_val):#, method = 'standard'):
+        pass
+
+    @abstractmethod
+    def predict(self, X_test):#, method = 'standard'):
+        pass
+
+    @abstractmethod
+    def get_features_importance(self, features_names):#, method = 'standard'):
+        pass
