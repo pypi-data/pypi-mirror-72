1405b6ad1f5143ca1f9d6687d2b74ab2f08c74e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:28 +0000

    Add package pygooglecloudutils/0.0.0.1/pygooglecloudutils-0.0.0.1-py3-none-any.whl

---
+++ packages/pygooglecloudutils/pygooglecloudutils-0.0.0.1-py3-none-any.whl/pygooglecloudutils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Python Google Cloud Platform Utilities
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+pygooglecloudutils is a collection of utilities for interacting with Google Cloud Platform
+"""
---
+++ packages/pygooglecloudutils/pygooglecloudutils-0.0.0.1-py3-none-any.whl/pygooglecloudutils/version.py
@@ -0,0 +1,2 @@
+""" This file was generated when pygooglecloudutils was built """
+VERSION = '0.0.0.1'
