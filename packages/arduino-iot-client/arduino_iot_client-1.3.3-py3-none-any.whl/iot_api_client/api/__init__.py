5a0d97f354764888dda0aedc1d13eaf946327e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package arduino-iot-client/1.3.3/arduino_iot_client-1.3.3-py3-none-any.whl

---
+++ packages/arduino-iot-client/arduino_iot_client-1.3.3-py3-none-any.whl/iot_api_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from iot_api_client.api.devices_v2_api import DevicesV2Api
+from iot_api_client.api.properties_v2_api import PropertiesV2Api
+from iot_api_client.api.series_v2_api import SeriesV2Api
+from iot_api_client.api.things_v2_api import ThingsV2Api
