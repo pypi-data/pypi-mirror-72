615197e387788c1646197ae8d63a1023b71647cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package fundamentalanalysis/0.2.6/FundamentalAnalysis-0.2.6.tar.gz

---
+++ packages/fundamentalanalysis/FundamentalAnalysis-0.2.6.tar.gz/FundamentalAnalysis-0.2.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FundamentalAnalysis",
+    packages=["FundamentalAnalysis"],
+    version="0.2.6",
+    license="MIT",
+    description="Fully-fledged Fundamental Analysis package capable of collecting 20 years of Company Profiles,\
+    Financial Statements, Ratios and Stock Data of 13.000+ companies.",
+    author="JerBouma",
+    author_email="jer.bouma@gmail.com",
+    url="https://github.com/JerBouma/FundamentalAnalysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["fundamental", "analysis", "finance"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ],
+)
