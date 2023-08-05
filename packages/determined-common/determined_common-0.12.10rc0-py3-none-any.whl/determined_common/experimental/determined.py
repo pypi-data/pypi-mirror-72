4f9ccd631c50672f98231464734cb03d5bcd8577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:58 +0000

    Add package determined-common/0.12.10rc0/determined_common-0.12.10rc0-py3-none-any.whl

---
+++ packages/determined-common/determined_common-0.12.10rc0-py3-none-any.whl/determined_common/experimental/determined.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from determined_common.experimental.checkpoint import Checkpoint, get_checkpoint
+from determined_common.experimental.experiment import ExperimentReference
+from determined_common.experimental.session import Session
+from determined_common.experimental.trial import TrialReference
+
+
+class Determined:
+    """
+    Determined gives access to Determined API objects.
+
+    Arguments:
+        master (string, optional): The URL of the Determined master. If
+            this argument is not specified, the environment variables ``DET_MASTER`` and
+            ``DET_MASTER_ADDR`` will be checked for the master URL in that order.
+        user (string, optional): The Determined username used for
+            authentication. (default: ``determined``)
+    """
+
+    def __init__(
+        self, master: Optional[str] = None, user: Optional[str] = None,
+    ):
+        self._session = Session(master, user)
+
+    def get_experiment(self, experiment_id: int) -> ExperimentReference:
+        """
+        Get the :class:`~determined.experimental.ExperimentReference` representing the
+        experiment with the provided experiment ID.
+        """
+        return ExperimentReference(experiment_id, self._session._master)
+
+    def get_trial(self, trial_id: int) -> TrialReference:
+        """
+        Get the :class:`~determined.experimental.TrialReference` representing the
+        trial with the provided trial ID.
+        """
+        return TrialReference(trial_id, self._session._master)
+
+    def get_checkpoint(self, uuid: str) -> Checkpoint:
+        """
+        Get the :class:`~determined.experimental.Checkpoint` representing the
+        checkpoint with the provided UUID.
+        """
+        return get_checkpoint(uuid, self._session._master)
