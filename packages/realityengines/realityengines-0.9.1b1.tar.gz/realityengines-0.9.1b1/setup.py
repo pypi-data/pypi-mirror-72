b9682ccdad048183249592f80fa387718f55dd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package realityengines/0.9.1b1/realityengines-0.9.1b1.tar.gz

---
+++ packages/realityengines/realityengines-0.9.1b1.tar.gz/realityengines-0.9.1b1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(name='realityengines',
+      version='0.9.1-BETA1',
+      description='RealityEngines.AI Python Client Library',
+      url='https://realityengines.ai',
+      author='RealityEngines.AI',
+      author_email='dev@realityengines.ai',
+      license='MIT',
+      packages=['realityengines'],
+      install_requires=['packaging', 'requests'],
+      zip_safe=True,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ])
