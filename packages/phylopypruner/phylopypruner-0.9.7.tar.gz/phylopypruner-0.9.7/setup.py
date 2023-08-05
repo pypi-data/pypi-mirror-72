1d869689ea53aec6fb64a987ae4ab91131c3cce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package phylopypruner/0.9.7/phylopypruner-0.9.7.tar.gz

---
+++ packages/phylopypruner/phylopypruner-0.9.7.tar.gz/phylopypruner-0.9.7/setup.py
@@ -0,0 +1,27 @@
+"Build script for setuptools."
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name="phylopypruner",
+    version="0.9.7",
+    author="Felix Thalen",
+    author_email="felix.thalen@uni-goettingen.de",
+    license="GPL 3",
+    description="tree-based orthology inference",
+    scripts=['scripts/phylopypruner'],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/fethalen/phylopypruner",
+    keywords=["orthology inference", "orthologs", "tree-based",
+              "phylogenetics", "phylogenomics", "orthology"],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
