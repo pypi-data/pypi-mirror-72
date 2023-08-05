dc3c496bb51643f62259d9bd2d3afe36034b3891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:28 +0000

    Add package django-hashids/0.1.1/django-hashids-0.1.1.tar.gz

---
+++ packages/django-hashids/django-hashids-0.1.1.tar.gz/django-hashids-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_hashids']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['hashids>=1.0.2']
+
+setup_kwargs = {
+    'name': 'django-hashids',
+    'version': '0.1.1',
+    'description': 'Non-intrusive hashids library Django',
+    'long_description': None,
+    'author': 'Shen Li',
+    'author_email': 'dustet@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4',
+}
+
+
+setup(**setup_kwargs)
