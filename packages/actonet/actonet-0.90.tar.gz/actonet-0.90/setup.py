781e80d34acc47971d9e41ca337eed231887478f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package actonet/0.90/actonet-0.90.tar.gz

---
+++ packages/actonet/actonet-0.90.tar.gz/actonet-0.90/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(name="actonet",
+    version="0.90",
+    description = "Abduction-based control of Boolean networks fixpoints",
+    install_requires = [
+        "algorecell_types",
+        "asprin",
+        "colomoto_jupyter",
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="computational systems biology",
+
+    include_package_data = True,
+    packages = find_packages()
+)
+
