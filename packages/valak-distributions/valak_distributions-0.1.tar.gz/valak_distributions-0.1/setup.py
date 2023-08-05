5f7d3aa6be7a9dd621a7d66d02d34cfac96fb8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:27 +0000

    Add package valak-distributions/0.1/valak_distributions-0.1.tar.gz

---
+++ packages/valak-distributions/valak_distributions-0.1.tar.gz/valak_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='valak_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['valak_distributions'],
+      zip_safe=False)
