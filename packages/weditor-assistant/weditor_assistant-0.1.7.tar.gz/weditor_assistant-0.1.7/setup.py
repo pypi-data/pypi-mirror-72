4501b0c7dd6bd5a0373a81476b80e65a377d03ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:50 +0000

    Add package weditor-assistant/0.1.7/weditor_assistant-0.1.7.tar.gz

---
+++ packages/weditor-assistant/weditor_assistant-0.1.7.tar.gz/weditor_assistant-0.1.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='weditor_assistant',
+      version='0.1.7',
+      description='string to date',
+      author='zxw',
+      author_email='761479698@qq.com',
+      license='MIT',
+      packages=['weditor_assistant'])
