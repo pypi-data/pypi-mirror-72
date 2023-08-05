2324d463682bb19fa54ee6a7de30b09ce920925e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:27 +0000

    Add package henry-test-tool/0.1.2/henry_test_tool-0.1.2.tar.gz

---
+++ packages/henry-test-tool/henry_test_tool-0.1.2.tar.gz/henry_test_tool-0.1.2/henry_test_tool/testit.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+
+def test_hello():
+    print("hello")
+
+def test_say_hi():
+    print('hi')
+
+def test_token():
+    print('token')
+''' pip包管理
+https://blog.csdn.net/fengmm521/article/details/79144407
+
+
+
+from henry_test_tool import testit  
+testit.test_hello()  
+
+# 升级包
+pip3 install --upgrade henry-test-tool
+
+
+# 安装制定版本
+pip3 install henry-test-tool==0.1.0
+
+
+
+'''
