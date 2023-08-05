1dd09de61e7b92ae45b40b9edb99fcbbcf6ad257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package sdc-dp-helpers/0.3.0/sdc_dp_helpers-0.3.0.tar.gz

---
+++ packages/sdc-dp-helpers/sdc_dp_helpers-0.3.0.tar.gz/sdc_dp_helpers-0.3.0/setup.py
@@ -0,0 +1,32 @@
+"""
+    Set up the sdc_helpers package
+"""
+from setuptools import setup
+
+setup(
+    name='sdc_dp_helpers',
+    packages=
+    [
+        'sdc_dp_helpers',
+        'sdc_dp_helpers.google_analytics',
+        'sdc_dp_helpers.api_utilities',
+    ],
+    install_requires=[
+        'apiclient',
+        'httplib2',
+        'pathlib',
+        'oauth2client',
+        'numpy',
+        'pandas'
+    ],
+    description='A module for developing data pipelines from external api\'s',
+    version='0.3.0',
+    url='http://github.com/RingierIMU/sdc-dataPipeline-helpers',
+    author='Ringier South Africa',
+    author_email='tools@ringier.co.za',
+    keywords=[
+        'pip',
+        'datapipeline',
+        'helpers',
+    ]
+)
