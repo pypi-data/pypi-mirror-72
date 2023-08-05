4cddb08ab7b8654e7faa27738d679dd8c19d06af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package distro-probability/0.1/distro_probability-0.1.tar.gz

---
+++ packages/distro-probability/distro_probability-0.1.tar.gz/distro_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distro_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distro_probability'],
+      author="Ernest Abah",
+      author_email="abahernesto@gmail.com",
+      zip_safe=False)
