dd54b2e395d90caf5b7a342348c5228eaaa89874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:39 +0000

    Add package festin/0.0.6/festin-0.0.6-py3-none-any.whl

---
+++ packages/festin/festin-0.0.6-py3-none-any.whl/festin/black_list.py
@@ -0,0 +1,25 @@
+BLACK_LIST_DOMAINS = {
+    "www.google.com",
+    "www.googletagmanager.com",
+    "cdnjs.cloudflare.com",
+    "ajax.googleapis.com",
+    "maxcdn.bootstrapcdn.com",
+    "cdn.statuspage.io",
+    "static.getclicky.com"
+}
+
+BLACK_LIST_TLD = {
+    "cloudfront.net",
+    "etclicky.com",
+    "eurolandir.com",
+    "trafficmanager.net",
+    "googleapis.com",
+    "gstatic.com",
+    "facebook.com",
+    "elb.amazonaws.com",
+    "amakaiedge.net",
+}
+
+BLACK_LIST_PREFISES = {
+    "cdn"
+}
