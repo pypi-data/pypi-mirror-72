88cd3b26dc15b3b94a6465b9e946b1c32746819e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package aibolit/1.1.2rc1/aibolit-1.1.2rc1.tar.gz

---
+++ packages/aibolit/aibolit-1.1.2rc1.tar.gz/aibolit-1.1.2rc1/aibolit/patterns/if_return_if_detection/if_detection.py
@@ -0,0 +1,32 @@
+import javalang
+from aibolit.utils.java_parser import JavalangImproved
+
+
+class newJavalangImproved(JavalangImproved):
+
+    def filter(self, ntypes):
+        nodes = self.tree_to_nodes()
+        array = []
+        for index, i in enumerate(nodes):
+            if (type(i.node) in ntypes):
+                if type(i.node.then_statement) in [javalang.tree.BlockStatement] and i.node.else_statement is not None:
+                    for check_return in i.node.then_statement.statements:
+                        if type(check_return) in [javalang.tree.ReturnStatement]:
+                            array.append(nodes[index].line)
+        return array
+
+
+class CountIfReturn:
+    '''
+    Returns lines with if statement which has also return statement
+    and other conditions with else.
+    '''
+    def __init__(self):
+        pass
+
+    def value(self, filename: str):
+        ''''''
+        tree = newJavalangImproved(filename)
+        if_decls = tree.filter([javalang.tree.IfStatement])
+
+        return if_decls
