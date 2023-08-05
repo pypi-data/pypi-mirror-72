7f0b800ea5d42c797b79dfea2bc7a992e5aee497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package sample-oeg-pip-package/0.1/sample_oeg_pip_package-0.1.tar.gz

---
+++ packages/sample-oeg-pip-package/sample_oeg_pip_package-0.1.tar.gz/sample_oeg_pip_package-0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='sample_oeg_pip_package',
+      version='0.1',
+      description='Sample OEG pip package',
+      url='http://github.com/oeg-upm/sample-oeg-pip-package',
+      author='Daniel Doña',
+      author_email='ddona@delicias.dia.fi.upm.es',
+      license='MIT',
+      packages=['sample_oeg_pip_package'],
+      zip_safe=False)
