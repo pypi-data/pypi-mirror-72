90b23d9ae1bdce0226ce262fdb7fa8f8bf02623a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:31 +0000

    Add package formaldict/0.1.3/formaldict-0.1.3.tar.gz

---
+++ packages/formaldict/formaldict-0.1.3.tar.gz/formaldict-0.1.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['formaldict', 'formaldict.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['kmatch>=0.3.0,<0.4.0',
+ 'prompt-toolkit>=3.0.2,<4.0.0',
+ 'python-dateutil>=2.8.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'formaldict',
+    'version': '0.1.3',
+    'description': 'Formal structured dictionaries parsed from a schema',
+    'long_description': None,
+    'author': 'Jyve Engineering',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
