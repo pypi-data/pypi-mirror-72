9ad55ae9d7a0984065096f6595f9a0780677e65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:04 +0000

    Add package munter.py/2.1.0/munter.py-2.1.0-py3-none-any.whl

---
+++ packages/munter.py/munter.py-2.1.0-py3-none-any.whl/munter/__init__.py
@@ -0,0 +1,3 @@
+"""Main package for Munter.py"""
+__version__ = "2.1.0"
+from .munter import time_calc
---
+++ packages/munter.py/munter.py-2.1.0-py3-none-any.whl/munter/__main__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""entry point for program and library"""
+
+from .munter import main
+main()
+
