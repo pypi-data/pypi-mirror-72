90d6ef87e6ca1a316bc2f69fcb002aaf7739b7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:17 +0000

    Add package imdata-collector/0.1/imdata_collector-0.1.tar.gz

---
+++ packages/imdata-collector/imdata_collector-0.1.tar.gz/imdata_collector-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='imdata_collector',
+      version='0.1',
+      description='A tiny tool to collect images and videos from youtube and google image engine',
+      author='Kathy.Chen',
+      author_email='v_xchen_v@pku.edu.cn',
+      zip_safe=False
+      )
