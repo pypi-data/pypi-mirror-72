8df3a030ba778827f0a85bee796fa68ab128daa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:33 +0000

    Add package aim-cli/2.0.1/aim_cli-2.0.1-py2.py3-none-any.whl

---
+++ packages/aim-cli/aim_cli-2.0.1-py2.py3-none-any.whl/aim/cli/configs.py
@@ -0,0 +1,8 @@
+INIT_NAME = 'init'
+EXPERIMENT_NAME = 'experiment'
+COMMIT_NAME = 'commit'
+RESET_NAME = 'reset'
+VERSION_NAME = 'version'
+DE_NAME = 'de'
+STATUS_NAME = 'status'
+CONFIG_NAME = 'config'
