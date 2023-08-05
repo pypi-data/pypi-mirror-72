fb97be8e30ff414a4f27f05c06fde6e3904fe10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package JavPy/0.4.4/JavPy-0.4.4-py2.py3-none-any.whl

---
+++ packages/JavPy/JavPy-0.4.4-py2.py3-none-any.whl/JavPy/app/javpy-react/build/precache-manifest.86ec8cadf6e2a1bc869857699b75f375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34dec70ad8665dd5b7a22286b4a2620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3dbc1daca364702954f",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "dbcc7ab4a5e8ba50443c",
+    "url": "/static/css/main.789ec1f2.chunk.css"
+  },
+  {
+    "revision": "c3dbc1daca364702954f",
+    "url": "/static/js/2.efeff5a4.chunk.js"
+  },
+  {
+    "revision": "69aceadffa80df17147cfa73022ec51e",
+    "url": "/static/js/2.efeff5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcc7ab4a5e8ba50443c",
+    "url": "/static/js/main.d85f1cbc.chunk.js"
+  },
+  {
+    "revision": "00e68b54667811347eae",
+    "url": "/static/js/runtime-main.39a99049.js"
+  }
+]);
