31f59c1c0a61f98ddc099ab7e5864cb396be57bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package teste-marinonigrande/0.0.1/teste_marinonigrande-0.0.1.tar.gz

---
+++ packages/teste-marinonigrande/teste_marinonigrande-0.0.1.tar.gz/teste_marinonigrande-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="teste_marinonigrande", # Replace with your own username
+    version="0.0.1",
+    author="Nícolas Grande",
+    author_email="nmgrande@hotmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires  = ['Django==2.1.7',], # List all your dependencies inside the list
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
