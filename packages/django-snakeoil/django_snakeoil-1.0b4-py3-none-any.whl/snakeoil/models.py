23f23bfb7911e61ed46fbe4348a62d5b135b02d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:53 +0000

    Add package django-snakeoil/1.0b4/django_snakeoil-1.0b4-py3-none-any.whl

---
+++ packages/django-snakeoil/django_snakeoil-1.0b4-py3-none-any.whl/snakeoil/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class SEOModel(models.Model):
+    meta_tags = models.JSONField(default=dict, verbose_name=_("meta tags"))
+
+    class Meta:
+        abstract = True
+
+
+class SEOPath(SEOModel):
+    path = models.CharField(primary_key=True, max_length=255, verbose_name=_("path"))
+
+    class Meta:
+        verbose_name = _("SEO path")
+        verbose_name_plural = _("SEO paths")
+
+    def __str__(self):
+        return self.path
