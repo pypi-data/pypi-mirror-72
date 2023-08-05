525c53ee9e6dd1035599fa007e3465f26b1087f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:42 +0000

    Add package firefly-framework/1.0.9/firefly-framework-1.0.9.tar.gz

---
+++ packages/firefly-framework/firefly-framework-1.0.9.tar.gz/firefly-framework-1.0.9/src/firefly_test/todo/application/middleware/authenticate.py
@@ -0,0 +1,37 @@
+#  Copyright (c) 2019 JD Williams
+#
+#  This file is part of Firefly, a Python SOA framework built by JD Williams. Firefly is free software; you can
+#  redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+#  Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+#
+#  Firefly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+#  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details. You should have received a copy of the GNU Lesser General Public
+#  License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  You should have received a copy of the GNU General Public License along with Firefly. If not, see
+#  <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+from typing import Optional
+
+import firefly as ff
+
+
+@ff.authenticator()
+class Authenticate(ff.Handler):
+    def handle(self, message: ff.Message) -> Optional[bool]:
+        if 'fail_authentication' in message.headers:
+            raise ff.UnauthenticatedError()
+
+        return True
+
+
+@ff.authenticator()
+class Authenticate2(ff.Handler):
+    def handle(self, message: ff.Message) -> Optional[bool]:
+        if 'fail_authentication2' in message.headers:
+            raise ff.UnauthenticatedError()
+
+        return True
