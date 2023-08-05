8541839a2fe4afb224efaff9c895a57a0d6be447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:48 +0000

    Add package Bioplots/0.1.6.7/Bioplots-0.1.6.7-py3-none-any.whl

---
+++ packages/Bioplots/Bioplots-0.1.6.7-py3-none-any.whl/Bioplots/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+################################################################################
+# Created Date : Friday February 7th 2020                                      #
+# Author: Jingxin Fu (jingxinfu.tj@gmail.com)                                  #
+# ----------                                                                   #
+# Last Modified: Friday February 7th 2020 2:22:38 pm                           #
+# Modified By: Jingxin Fu (jingxinfu.tj@gmail.com)                             #
+# ----------                                                                   #
+# Copyright (c) Jingxin Fu 2020                                                #
+################################################################################
+
+
+__doc__=""" 
+""" 
+
+__version__ = '0.1.6.7'
+
+from .utils import *
+from .distribution import *
+from .cluster import *
