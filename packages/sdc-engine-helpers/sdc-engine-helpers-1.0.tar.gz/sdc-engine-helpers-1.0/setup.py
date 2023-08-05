84f5ffa3ca0dcd54a34465d72cf9b4c97113eb5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package sdc-engine-helpers/1.0/sdc-engine-helpers-1.0.tar.gz

---
+++ packages/sdc-engine-helpers/sdc-engine-helpers-1.0.tar.gz/sdc-engine-helpers-1.0/setup.py
@@ -0,0 +1,34 @@
+"""
+    Set up the sdc_engine_helpers package
+"""
+from setuptools import setup
+
+setup(
+    name='sdc-engine-helpers',
+    packages=[
+        'sdc_engine_helpers',
+        'sdc_engine_helpers.personalize',
+        'sdc_engine_helpers.personalize.maintenance',
+        'sdc_engine_helpers.personalize.event',
+        'sdc_engine_helpers.personalize.recommendations',
+        'sdc_engine_helpers.sagemaker',
+        'sdc_engine_helpers.sagemaker.recommendations'
+    ],
+    install_requires=[
+        'sdc-helpers==1.0.2'
+    ],
+    description='AWS Recommendation Engine Helpers',
+    url='http://github.com/RingierIMU/sdc-recommend-engine-helpers',
+    version='1.0',
+    author='Ringier South Africa',
+    author_email='tools@ringier.co.za',
+    keywords=[
+        'pip',
+        'helpers',
+        'aws',
+        'recommendation',
+        'personalize',
+        'sageMaker'
+    ],
+    download_url='https://github.com/RingierIMU/sdc-recommend-engine-helpers/archive/v1.0.zip'
+)
