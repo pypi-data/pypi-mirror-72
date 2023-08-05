2042b6608640ed1528901985024ef8e31f93604f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package mytestlib-AkioTest/1.0.0/mytestlib-AkioTest-1.0.0.tar.gz

---
+++ packages/mytestlib-AkioTest/mytestlib-AkioTest-1.0.0.tar.gz/mytestlib-AkioTest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-AkioTest',
+    version='1.0.0',
+    url='http://dummy.org',
+    author='Akio Shuto',
+    author_email='syuto@vir.bekkoame.ne.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
