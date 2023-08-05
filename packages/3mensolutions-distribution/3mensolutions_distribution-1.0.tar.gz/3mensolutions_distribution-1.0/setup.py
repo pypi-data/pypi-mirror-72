5c5a5e7edcbccf7b86b49346a508502ac79de772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package 3mensolutions-distribution/1.0/3mensolutions_distribution-1.0.tar.gz

---
+++ packages/3mensolutions-distribution/3mensolutions_distribution-1.0.tar.gz/3mensolutions_distribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='3mensolutions_distribution',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['3mensolutions_distribution'],
+      author='Emmanuel Ameh',
+      author_email='a@b.com',
+      zip_safe=False)
