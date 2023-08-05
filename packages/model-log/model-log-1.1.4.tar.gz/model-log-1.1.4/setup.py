ac70f775d79be39a09aff7bd52d6378406f32563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:46 +0000

    Add package model-log/1.1.4/model-log-1.1.4.tar.gz

---
+++ packages/model-log/model-log-1.1.4.tar.gz/model-log-1.1.4/setup.py
@@ -0,0 +1,33 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+
+VERSION = '1.1.4'
+
+
+setup(name='model-log',
+      version=VERSION,
+      description="test description",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python,machine-learning,deep-learning,metric,loss',
+      author='mantch',
+      author_email='mantchs@163.com',
+      url='https://github.com/NLP-LOVE/Model_Log',
+      license='Apache License 2.0',
+      packages=['model_log'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'flask >= 0.11'
+      ],
+      entry_points={
+        'console_scripts':[
+            'model-log = model_log.modellog_web:main'
+        ]
+      }
+)
