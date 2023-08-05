d93cc773018ed6ba6be1187d17ae78fa6cc42d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package caty/0.6.1/caty-0.6.1.tar.gz

---
+++ packages/caty/caty-0.6.1.tar.gz/caty-0.6.1/caty/plugins/__init__.py
@@ -0,0 +1,27 @@
+from .csv import CSV
+from .ini import INI
+from .json import JSON
+from .pickle import PICKLE
+from .sqlite import SQLITE
+from .toml import TOML
+from .txt import TXT
+from .yaml import YAML
+from .bin import BIN, OCT, DEC, HEX
+from .cfg import CFG
+
+Plugins = (
+    CSV,
+    INI,
+    JSON,
+    PICKLE,
+    SQLITE,
+    TOML,
+    TXT,
+    YAML,
+    BIN,
+    OCT,
+    DEC,
+    HEX,
+    CFG,
+)
+
