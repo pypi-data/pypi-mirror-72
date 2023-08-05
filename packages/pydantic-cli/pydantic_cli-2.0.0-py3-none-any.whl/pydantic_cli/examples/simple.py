d9df332aa45a94bfd0fd9b242d10982db83760a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package pydantic-cli/2.0.0/pydantic_cli-2.0.0-py3-none-any.whl

---
+++ packages/pydantic-cli/pydantic_cli-2.0.0-py3-none-any.whl/pydantic_cli/examples/simple.py
@@ -0,0 +1,22 @@
+"""
+Simple Example of using Pydantic to generate a CLI
+
+This example will generate a CLI tool with 2 (required) positional arguments
+"""
+from pydantic import BaseModel
+
+from pydantic_cli import run_and_exit
+
+
+class Options(BaseModel):
+    input_file: str
+    max_records: int
+
+
+def example_runner(opts: Options) -> int:
+    print(f"Mock example running with {opts}")
+    return 0
+
+
+if __name__ == "__main__":
+    run_and_exit(Options, example_runner, description=__doc__, version="0.1.0")
