36db68a85c575eb12961b160de89213395c2a76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:38 +0000

    Add package sentry-auth-gitlab-v2/0.2.4/sentry-auth-gitlab-v2-0.2.4.tar.gz

---
+++ packages/sentry-auth-gitlab-v2/sentry-auth-gitlab-v2-0.2.4.tar.gz/sentry-auth-gitlab-v2-0.2.4/auth_gitlab/provider.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+
+from sentry.auth.providers.oauth2 import (
+    OAuth2Callback, OAuth2Provider, OAuth2Login
+)
+
+from .constants import (
+    AUTHORIZE_URL, ACCESS_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, SCOPE
+)
+from .views import FetchUser
+
+
+class GitLabOAuth2Provider(OAuth2Provider):
+    name = 'Gitlab'
+
+    def get_auth_pipeline(self):
+        return [
+            OAuth2Login(AUTHORIZE_URL, client_id=CLIENT_ID, scope=SCOPE),
+            OAuth2Callback(
+                access_token_url=ACCESS_TOKEN_URL,
+                client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+            ),
+            FetchUser()
+        ]
+
+    def get_refresh_token_url(self):
+        return ACCESS_TOKEN_URL
+
+    def build_config(self, config):
+        return {}
+
+    def build_identity(self, state):
+        data = state['data']
+        user_data = state['user']
+        return {
+            'id': user_data['id'],
+            'email': user_data['email'],
+            'name': user_data['name'],
+            'data': self.get_oauth_data(data),
+        }
---
+++ packages/sentry-auth-gitlab-v2/sentry-auth-gitlab-v2-0.2.4.tar.gz/sentry-auth-gitlab-v2-0.2.4/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""
+sentry-auth-gitlab-v2
+==================
+
+:copyright: (c) 2015 Functional Software, Inc
+"""
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+import os
+
+tests_require = [
+    'pytest',
+    'mock',
+]
+
+install_requires = [
+    'sentry',
+]
+
+# The directory containing this file
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+# The text of the README file
+with open(HERE + "/README.rst", 'r') as f:
+    README = f.read()
+
+setup(
+    name='sentry-auth-gitlab-v2',
+    version='0.2.4',
+    author='Zakhar Bessarab',
+    author_email='zekker6@gmail.com',
+    url='https://github.com/zekker6',
+    description='Gitlab authentication provider for Sentry',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'tests': tests_require},
+    include_package_data=True,
+    entry_points={
+        'sentry.apps': [
+            'auth_gitlab = auth_gitlab.apps.Config',
+         ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
