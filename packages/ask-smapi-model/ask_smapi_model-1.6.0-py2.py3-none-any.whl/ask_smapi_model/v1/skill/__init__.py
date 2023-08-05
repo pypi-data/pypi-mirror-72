7a1c7b64d66dcc5119639128ea653de08c87c933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:29 +0000

    Add package ask-smapi-model/1.6.0/ask_smapi_model-1.6.0-py2.py3-none-any.whl

---
+++ packages/ask-smapi-model/ask_smapi_model-1.6.0-py2.py3-none-any.whl/ask_smapi_model/v1/skill/__init__.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+#
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License'). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+#
+from __future__ import absolute_import
+
+from .agreement_type import AgreementType
+from .update_skill_with_package_request import UpdateSkillWithPackageRequest
+from .validation_feature import ValidationFeature
+from .skill_status import SkillStatus
+from .build_step_name import BuildStepName
+from .response_status import ResponseStatus
+from .ssl_certificate_payload import SSLCertificatePayload
+from .build_step import BuildStep
+from .publication_status import PublicationStatus
+from .standardized_error import StandardizedError
+from .manifest_last_update_request import ManifestLastUpdateRequest
+from .validation_details import ValidationDetails
+from .submit_skill_for_certification_request import SubmitSkillForCertificationRequest
+from .interaction_model_last_update_request import InteractionModelLastUpdateRequest
+from .image_attributes import ImageAttributes
+from .validation_failure_type import ValidationFailureType
+from .reason import Reason
+from .hosted_skill_deployment_status_last_update_request import HostedSkillDeploymentStatusLastUpdateRequest
+from .skill_summary import SkillSummary
+from .publication_method import PublicationMethod
+from .image_dimension import ImageDimension
+from .create_skill_request import CreateSkillRequest
+from .image_size import ImageSize
+from .hosted_skill_provisioning_status import HostedSkillProvisioningStatus
+from .import_response_skill import ImportResponseSkill
+from .instance import Instance
+from .skill_messaging_credentials import SkillMessagingCredentials
+from .hosted_skill_deployment_status import HostedSkillDeploymentStatus
+from .status import Status
+from .regional_ssl_certificate import RegionalSSLCertificate
+from .import_response import ImportResponse
+from .withdraw_request import WithdrawRequest
+from .export_response_skill import ExportResponseSkill
+from .build_details import BuildDetails
+from .skill_interaction_model_status import SkillInteractionModelStatus
+from .create_skill_with_package_request import CreateSkillWithPackageRequest
+from .create_skill_response import CreateSkillResponse
+from .format import Format
+from .upload_response import UploadResponse
+from .skill_credentials import SkillCredentials
+from .skill_summary_apis import SkillSummaryApis
+from .validation_failure_reason import ValidationFailureReason
+from .export_response import ExportResponse
+from .list_skill_response import ListSkillResponse
+from .hosted_skill_deployment_details import HostedSkillDeploymentDetails
+from .validation_endpoint import ValidationEndpoint
+from .manifest_status import ManifestStatus
+from .action import Action
+from .hosted_skill_provisioning_last_update_request import HostedSkillProvisioningLastUpdateRequest
+from .image_size_unit import ImageSizeUnit
+from .validation_data_types import ValidationDataTypes
+from .resource_import_status import ResourceImportStatus
+from .skill_resources_enum import SkillResourcesEnum
