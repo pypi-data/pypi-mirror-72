016b8e2352aae846b51d9f96bc10212ee58216d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:55 +0000

    Add package django-snakeoil/1.0b5/django_snakeoil-1.0b5-py3-none-any.whl

---
+++ packages/django-snakeoil/django_snakeoil-1.0b5-py3-none-any.whl/tests/test_jinja2.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from snakeoil.models import SEOPath
+
+
+class MetaTemplateTagTestCase(TestCase):
+    def test_jinja2(self):
+        SEOPath.objects.create(
+            path="/jinja2/",
+            meta_tags={"en": {"description": "jinja2 path description"}},
+        )
+        response = self.client.get("/jinja2/")
+        self.assertContains(
+            response, '<meta name="description" content="jinja2 path description">'
+        )
