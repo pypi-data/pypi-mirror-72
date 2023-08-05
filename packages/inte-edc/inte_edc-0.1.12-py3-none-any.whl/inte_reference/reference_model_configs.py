32481a16b54cd0261e58a31e6976e701dd7a3b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:05 +0000

    Add package inte-edc/0.1.12/inte_edc-0.1.12-py3-none-any.whl

---
+++ packages/inte-edc/inte_edc-0.1.12-py3-none-any.whl/inte_reference/reference_model_configs.py
@@ -0,0 +1,12 @@
+from edc_reference import site_reference_configs
+
+site_reference_configs.register_from_visit_schedule(
+    visit_models={"edc_appointment.appointment": "inte_subject.subjectvisit"}
+)
+
+configs = {
+    "inte_subject.carestatus": ["hiv_result", "diabetic", "hypertensive"],
+}
+
+for reference_name, fields in configs.items():
+    site_reference_configs.add_fields_to_config(name=reference_name, fields=fields)
