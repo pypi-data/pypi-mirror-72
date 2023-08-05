076331872e7870a16454e77d80e5d71ba8c8c3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:56 +0000

    Add package wch341/0.1.2/wch341-0.1.2-py3-none-any.whl

---
+++ packages/wch341/wch341-0.1.2-py3-none-any.whl/wch341/__init__.py
@@ -0,0 +1,22 @@
+import os
+import platform
+from time import sleep
+import subprocess
+def exist():
+    sys = platform.system()
+    if sys=="Darwin":
+        kexts = os.listdir("/Library/Extensions/")
+        return "usbserial.kext" in kexts
+    elif sys=="Windows":
+        return False
+    return True
+
+def install():
+    sys = platform.system()
+    if sys=="Darwin":
+        subprocess.call(["/usr/bin/open",os.path.dirname(__file__)+"/driver/ch34x_install.pkg"])
+    elif sys=="Windows":
+        subprocess.call([os.path.dirname(__file__)+"/driver/ch34x_install.exe"])
+    while exist():
+        sleep(1)
+    return
