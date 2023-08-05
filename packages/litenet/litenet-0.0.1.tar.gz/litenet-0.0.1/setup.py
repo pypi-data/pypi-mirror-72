87c84d24eb0fd2cd0a03dfd147f9bfe59da337f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:13 +0000

    Add package litenet/0.0.1/litenet-0.0.1.tar.gz

---
+++ packages/litenet/litenet-0.0.1.tar.gz/litenet-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='litenet',
+    version='0.0.1',
+    packages=['litenet'],
+    url='https://github.com/GrandMoff100/LiteNet',
+    license='',
+    author='Quantum_Wizard',
+    author_email='',
+    description='A CL version of LiteNet',
+    entry_points={
+        "console_scripts": ["litenet=litenet.cli:cli"]
+    }
+)
