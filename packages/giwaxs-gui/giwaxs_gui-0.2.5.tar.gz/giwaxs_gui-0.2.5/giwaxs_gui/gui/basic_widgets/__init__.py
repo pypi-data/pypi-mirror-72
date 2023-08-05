2feaf846125ac89ff586ea984fa09429f15b67cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:10 +0000

    Add package giwaxs-gui/0.2.5/giwaxs_gui-0.2.5.tar.gz

---
+++ packages/giwaxs-gui/giwaxs_gui-0.2.5.tar.gz/giwaxs_gui-0.2.5/giwaxs_gui/gui/basic_widgets/__init__.py
@@ -0,0 +1,9 @@
+from .buttons import RoundedPushButton, InfoButton, DeleteButton, ConfirmButton
+from .plots_1d import Custom1DPlot, Smooth1DPlot, PlotBC
+from .plots_2d import CustomImageViewer
+from .sliders import DoubleSlider, LabeledSlider, ControlSlider, ParametersSlider
+from .setup_widgets import AbstractInputParametersWidget, BasicInputParametersWidget
+from .toolbars import ToolBar, BlackToolBar
+from .spinner import Spinner
+from .popups import PopUpWrapper, TextNotification
+from .labels import LabelButton, Label, HyperlinkLabel
