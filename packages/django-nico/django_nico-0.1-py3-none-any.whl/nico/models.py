027514bff4ba6bbb55a8b06aaa988ab0ee4023e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package django-nico/0.1/django_nico-0.1-py3-none-any.whl

---
+++ packages/django-nico/django_nico-0.1-py3-none-any.whl/nico/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class Status(models.Model):
+    status = models.BooleanField(null=True, default=True)
+    class Meta:
+        abstract = True
+
+
+class DatLog(Status):
+    dat_insercao = models.DateTimeField(auto_now_add=True, null=True)
+    dat_edicao = models.DateTimeField(auto_now=True, null=True)
+    dat_delete = models.DateTimeField(null=True)
+    class Meta:
+        abstract = True
+
+
+class UsrLog(Status):
+    usr_insercao = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True, related_name='usr_insercao')
+    usr_edicao = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True, related_name='usr_edicao')
+    usr_delete = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True, related_name='usr_delete')
+    class Meta:
+        abstract = True
+
+
+class Log(DatLog, UsrLog):
+    class Meta:
+        abstract = True
+
+
+class Profile(AbstractUser, Log):
+    nm_completo = models.CharField(max_length=200, null=True)
+    class Meta:
+        abstract = True
