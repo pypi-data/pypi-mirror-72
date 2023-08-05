573af696cc3ef4d838323d10233ebdf4552813dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package awsnd-sst-probdist/0.2/awsnd_sst_probdist-0.2.tar.gz

---
+++ packages/awsnd-sst-probdist/awsnd_sst_probdist-0.2.tar.gz/awsnd_sst_probdist-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='awsnd_sst_probdist',
+      version='0.2',
+      description='Binomial and Gaussian probability distributions',
+      packages=['awsnd_sst_probdist'],
+      athor='Saulo S. Toledo',
+      author_email='saulotoledo@gmail.com',
+      zip_safe=False)
