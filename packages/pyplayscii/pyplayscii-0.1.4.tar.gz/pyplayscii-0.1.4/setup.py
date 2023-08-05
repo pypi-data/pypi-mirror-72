c123be8003f36ac150de4c07e7b6dbe324d2d117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package pyplayscii/0.1.4/pyplayscii-0.1.4.tar.gz

---
+++ packages/pyplayscii/pyplayscii-0.1.4.tar.gz/pyplayscii-0.1.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+def get_readme():
+    here = path.dirname(__file__)
+    with open(path.join(here, 'README.md'), encoding='UTF8') as readme_file:
+        readme = readme_file.read()
+        return readme
+
+
+def get_requirements():
+    here = path.dirname(__file__)
+    with open(path.join(here, 'requirements.txt'), encoding='UTF8') as req_file:
+        req = req_file.read()
+        return req
+
+
+setup(
+    name="pyplayscii",
+    version="0.1.4",
+    author="SeBeom Lee",
+    description="Object oriented ascii art python game engine",
+    long_description=get_readme(),
+    install_requires=get_requirements(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/k2sebeom/pyplayscii",
+    packages=find_packages(include=['playscii', 'playscii.games']),
+    test_suite="tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
