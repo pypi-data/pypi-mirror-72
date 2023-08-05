7a9bbc3114deb35ccb9b334ca667473dba866310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:28 +0000

    Add package biosteam/2.19.24/biosteam-2.19.24.tar.gz

---
+++ packages/biosteam/biosteam-2.19.24.tar.gz/biosteam-2.19.24/biosteam/tests/utility_tests.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# BioSTEAM: The Biorefinery Simulation and Techno-Economic Analysis Modules
+# Copyright (C) 2020, Yoel Cortes-Pena <yoelcortes@gmail.com>
+# 
+# This module is under the UIUC open-source license. See 
+# github.com/BioSTEAMDevelopmentGroup/biosteam/blob/master/LICENSE.txt
+# for license details.
+"""
+"""
+import biosteam as bst
+import doctest
+
+def test_heat_utility():
+    from biosteam import _heat_utility
+    doctest.testmod(_heat_utility)
+    
+def test_power_utility():
+    from biosteam import _power_utility
+    doctest.testmod(_power_utility)
+    
+def test_utilities():
+    test_heat_utility()
+    test_power_utility()
+    
+if __name__ == '__main__':
+    test_utilities()
