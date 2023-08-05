958670346cee35e8b831b82a4d93b05f99e2b346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:27 +0000

    Add package spacerocks/0.7.4/spacerocks-0.7.4.tar.gz

---
+++ packages/spacerocks/spacerocks-0.7.4.tar.gz/spacerocks-0.7.4/spacerocks/constants.py
@@ -0,0 +1,16 @@
+from astropy.constants import c
+from astropy import units as u
+from astropy.coordinates import Angle
+
+# standard gravitational parameter, GM. This value comes directly from Horizons.
+mu_bary = 0.00029630927492415936 * u.radian**2 * u.au**3 / u.day**2
+mu_helio = 0.00029591220828559093 * u.radian**2 * u.au**3 / u.day**2
+
+# obliquity of the Earth. From Horizons.
+epsilon = Angle(84381.448, u.arcsec).radian
+
+# speed of light
+c = c.to(u.au / u.year)
+
+# G parameter for magnitude estimation
+G = 0.15
