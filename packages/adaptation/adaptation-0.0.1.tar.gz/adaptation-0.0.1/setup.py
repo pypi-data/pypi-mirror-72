f62de1dfe732895c42d820c68bff642a3d2f55db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:07 +0000

    Add package adaptation/0.0.1/adaptation-0.0.1.tar.gz

---
+++ packages/adaptation/adaptation-0.0.1.tar.gz/adaptation-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='adaptation',
+    version='0.0.1',
+    description='Awesome Domain Adaptation Package Toolbox',
+    url='https://github.com/antoinedemathelin/adapt.git',
+    author='Antoine de Mathelin',
+    author_email='antoine.de_mathelin@cmla.ens-cachan.fr',
+    license='unlicense',
+    packages=['adapt'],
+    zip_safe=False
+)
