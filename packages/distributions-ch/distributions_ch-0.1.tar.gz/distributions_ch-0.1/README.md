36a1a4fb020018964f65643aafa387387faf912d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package distributions-ch/0.1/distributions_ch-0.1.tar.gz

---
+++ packages/distributions-ch/distributions_ch-0.1.tar.gz/distributions_ch-0.1/README.md
@@ -0,0 +1,11 @@
+#Need To complete later
+
+Summery of the package
+
+#Files
+
+Explaination of the files
+
+#Installation Guide
+
+installation guides for the package
---
+++ packages/distributions-ch/distributions_ch-0.1.tar.gz/distributions_ch-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ch',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_ch'],
+      zip_safe=False)
