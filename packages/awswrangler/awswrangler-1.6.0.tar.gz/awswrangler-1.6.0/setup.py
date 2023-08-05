ea0df1721042c9321ca0414fc98c8c1a7ce28ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:05 +0000

    Add package awswrangler/1.6.0/awswrangler-1.6.0.tar.gz

---
+++ packages/awswrangler/awswrangler-1.6.0.tar.gz/awswrangler-1.6.0/setup.py
@@ -0,0 +1,27 @@
+import os
+from io import open
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+path = os.path.join(here, "awswrangler", "__metadata__.py")
+with open(file=path, mode="r", encoding="utf-8") as f:
+    exec(f.read(), about)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author="Igor Tavares",
+    url="https://github.com/awslabs/aws-data-wrangler",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=about["__license__"],
+    packages=find_packages(include=["awswrangler", "awswrangler.*"], exclude=["tests"]),
+    include_package_data=True,
+    python_requires=">=3.6, <3.9",
+    install_requires=[open("requirements.txt").read().strip().split("\n")]
+)
