1d8a936192eeefe2e9733bf45feec6925cd2575e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:25 +0000

    Add package kuafu/0.0.12/kuafu-0.0.12.tar.gz

---
+++ packages/kuafu/kuafu-0.0.12.tar.gz/kuafu-0.0.12/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+from kuafu import __version__
+
+setup(
+      name='kuafu',
+      version=__version__,
+      description='PDF manager',
+      keywords='poppler-qt5',
+      url='',
+      author='Yinlin Hu',
+      author_email='huyinlin@gmail.com',
+      license='GNU GPLv3',
+      packages=['kuafu'],
+      install_requires=['PyQt5', 'numpy'], #external packages as dependencies
+      classifiers=[
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python :: 3',
+      ],
+      entry_points={
+          'console_scripts': ['kuafu=kuafu.main:main'],
+      },
+      data_files=[
+                 ('share/applications', ['kuafu/resources/kuafu.desktop']),
+                 ('share/icons', ['kuafu/resources/icons/kuafu.svg']),
+                 ('resources', ['kuafu/resources/ui_main.py', 'kuafu/resources/ui_library.py', 
+                                'kuafu/resources/ui_document.py', 'kuafu/resources/resources_rc.py',
+                                'kuafu/resources/ui_annotation_item.py'])
+      ],
+      include_package_data=True,
+      zip_safe=False
+)
