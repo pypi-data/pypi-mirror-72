7f657a9ccae5244a4d084597c0863229eb6643c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:53 +0000

    Add package ohsome2label/1.0.2/ohsome2label-1.0.2-py3-none-any.whl

---
+++ packages/ohsome2label/ohsome2label-1.0.2-py3-none-any.whl/ohsome2label/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.0.2'
+from .config import *
+from .utils import *
+from .label import *
+from .tile import *
+from .visualize import *
+from .palette import palette
+from .overpass import *
+from .quality import *
