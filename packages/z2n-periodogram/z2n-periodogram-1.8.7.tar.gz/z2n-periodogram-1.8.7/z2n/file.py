2644361a33daa4e572d5829b50568658e7e070b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:54 +0000

    Add package z2n-periodogram/1.8.7/z2n-periodogram-1.8.7.tar.gz

---
+++ packages/z2n-periodogram/z2n-periodogram-1.8.7.tar.gz/z2n-periodogram-1.8.7/z2n/file.py
@@ -0,0 +1,169 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# Other Libraries
+from astropy.table import Table
+import numpy as np
+
+
+def load_file(series) -> None:
+    """
+    Open file and store time series.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    table = Table.read(series.input)
+    series.time = table['TIME'].data
+    series.time = series.time.astype(series.time.dtype.name)
+
+
+def load_ascii(series) -> None:
+    """
+    Open ascii file and store time series.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    table = Table.read(series.input, names=('TIME',), format='ascii')
+    series.time = table['TIME'].data
+    series.time = series.time.astype(series.time.dtype.name)
+
+
+def load_csv(series) -> None:
+    """
+    Open csv file and store time series.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    table = Table.read(series.input, names=('TIME',), format='csv')
+    series.time = table['TIME'].data
+    series.time = series.time.astype(series.time.dtype.name)
+
+
+def load_fits(series) -> None:
+    """
+    Open fits file and store time series.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    events = Table.read(series.input, format='fits')
+    series.time = events['TIME'].data
+    series.time = series.time.astype(series.time.dtype.name)
+
+
+def load_hdf5(series) -> None:
+    """
+    Open hdf5 file and store time series.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    table = Table.read(series.input, format='hdf5')
+    series.time = table['TIME'].data
+    series.time = series.time.astype(series.time.dtype.name)
+
+
+def save_ascii(series) -> None:
+    """
+    Save the periodogram to ascii file.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    array = np.column_stack((series.bins, series.z2n))
+    table = Table(array, names=('FREQUENCY', 'POTENCY'))
+    table.write(f'{series.output}.txt', format='ascii')
+
+
+def save_csv(series) -> None:
+    """
+    Save the periodogram to csv file.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    array = np.column_stack((series.bins, series.z2n))
+    table = Table(array, names=('FREQUENCY', 'POTENCY'))
+    table.write(f'{series.output}.csv', format='csv')
+
+
+def save_fits(series) -> None:
+    """
+    Save the periodogram to fits file.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    array = np.column_stack((series.bins, series.z2n))
+    table = Table(array, names=('FREQUENCY', 'POTENCY'))
+    table.write(f'{series.output}.fits', format='fits')
+
+
+def save_hdf5(series) -> None:
+    """
+    Save the periodogram to hdf5 file.
+
+    Parameters
+    ----------
+    series : Series
+        A time series object.
+
+    Returns
+    -------
+    None
+    """
+    array = np.column_stack((series.bins, series.z2n))
+    table = Table(array, names=('FREQUENCY', 'POTENCY'))
+    table.write(f'{series.output}.hdf5', format='hdf5', compression=True)
