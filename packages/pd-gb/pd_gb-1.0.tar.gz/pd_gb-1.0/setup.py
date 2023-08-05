8eea2009d4eb92b92ad3b17dc2d66565aa2f683d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:08 +0000

    Add package pd-gb/1.0/pd_gb-1.0.tar.gz

---
+++ packages/pd-gb/pd_gb-1.0.tar.gz/pd_gb-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pd_gb',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pd_gb'],
+      author='Charleen Lozi',
+      author_email='clozymwangs@gmail.com',
+      zip_safe=False)
