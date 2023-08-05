1a4f35dc6da51784305b843d2cf62facce3b79aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:12 +0000

    Add package babelfont/0.0.2/babelfont-0.0.2.tar.gz

---
+++ packages/babelfont/babelfont-0.0.2.tar.gz/babelfont-0.0.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+config = {
+    'name': 'babelfont',
+    'author': 'Simon Cozens',
+    'author_email': 'simon@simon-cozens.org',
+    'url': 'https://github.com/simoncozens/babelfont',
+    'description': 'Interrogate and manipulate UFO, TTF and OTF fonts with a common interface',
+    'long_description': open('README.rst', 'r').read(),
+    'license': 'MIT',
+    'version': '0.0.2',
+    'install_requires': [
+        "defcon",
+        "fontparts",
+        "fonttools"
+    ],
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta"
+
+    ],
+    'package_dir': {'': 'lib'},
+    'packages': find_packages("lib"),
+}
+
+if __name__ == '__main__':
+    setup(**config)
