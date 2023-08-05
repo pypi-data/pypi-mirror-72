5864abb4e04122f08a700c818c12246373cabfbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:59 +0000

    Add package jb-probablity/0.1/jb_probablity-0.1.tar.gz

---
+++ packages/jb-probablity/jb_probablity-0.1.tar.gz/jb_probablity-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jb_probablity',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jb_probablity'],
+      author= 'Jay Bagrecha',
+      zip_safe=False)
