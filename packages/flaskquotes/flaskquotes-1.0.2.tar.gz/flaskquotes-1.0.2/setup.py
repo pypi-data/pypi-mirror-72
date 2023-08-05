cba14049e41fd462c7d341c11b48d36f3cc9a315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:23 +0000

    Add package flaskquotes/1.0.2/flaskquotes-1.0.2.tar.gz

---
+++ packages/flaskquotes/flaskquotes-1.0.2.tar.gz/flaskquotes-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = {}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("./flaskquotes/version.py", "r") as vh:
+    exec(vh.read(), version)
+
+setup(name="flaskquotes",
+      description="Get AFI top 100 quotes",
+      author="Jeff Moorhead",
+      author_email="jeff.moorhead1@gmail.com",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      version = version.get("__version__", "0.0.0"),
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'quotes=flaskquotes.console:main'
+          ]
+      }, install_requires=['BeautifulSoup4', 'Flask']
+      )
