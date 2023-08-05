10541e0e4422638b8aa8b4f42170c4ccac8de511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:56 +0000

    Add package awsm-distributions/0.1/awsm_distributions-0.1.tar.gz

---
+++ packages/awsm-distributions/awsm_distributions-0.1.tar.gz/awsm_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='awsm_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['awsm_distributions'],
+      author='Paulo Alves',
+      zip_safe=False)
