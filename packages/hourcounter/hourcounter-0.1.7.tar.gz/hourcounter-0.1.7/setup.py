21646bf84b0468a5ae9ada5213092bbfe04b6bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:39 +0000

    Add package hourcounter/0.1.7/hourcounter-0.1.7.tar.gz

---
+++ packages/hourcounter/hourcounter-0.1.7.tar.gz/hourcounter-0.1.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='hourcounter',
+      version='0.1.7',
+      description='Count the number of items in the time interval',
+      long_description='Count the number of items in the time interval',
+      long_description_content='Count the number of items in the time interval',
+      packages=['hourcounter'],
+      author='Davuluri Hemanth Chowdary',
+      author_email='hemanthdavuluri98@gmail.com',
+      zip_safe=False)
