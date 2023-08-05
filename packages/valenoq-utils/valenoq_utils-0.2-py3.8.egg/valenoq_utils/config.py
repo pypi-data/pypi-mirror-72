2cd5b5347cd8aacfd9b4a4ec666e7d918ac6e33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package valenoq-utils/0.2/valenoq_utils-0.2-py3.8.egg

---
+++ packages/valenoq-utils/valenoq_utils-0.2-py3.8.egg/valenoq_utils/config.py
@@ -0,0 +1,16 @@
+import os
+import json
+import urllib.request
+
+CONFIG_HOST = os.environ.get("CONFIG_HOST")
+CONFIG_PORT = os.environ.get("CONFIG_PORT")
+PARENT_HOST = os.environ.get("PARENT_HOST")
+DEVELOP_HOST = os.environ.get("DEVELOP_HOST")
+PRODUCTION_HOST = os.environ.get("PRODUCTION_HOST")
+
+def get_config(name_container):
+    address = "http://%s:%s?name=%s" %(CONFIG_HOST, CONFIG_PORT, name_container)
+    if PARENT_HOST == DEVELOP_HOST:
+        address += "&type=development"
+    reply = urllib.request.urlopen(address).read()
+    return json.loads(reply.decode("utf-8"))
