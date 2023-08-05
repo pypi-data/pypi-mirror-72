8402b8d29d94dcc50d414725d4052f45ce1cec5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:03 +0000

    Add package starcli/2.3.2/starcli-2.3.2.tar.gz

---
+++ packages/starcli/starcli-2.3.2.tar.gz/starcli-2.3.2/starcli/__main__.py
@@ -0,0 +1,28 @@
+""" starcli.__main__ """
+
+# Internal application imports
+from .list_layout import list_layout
+from .table_layout import table_layout
+from .search import search
+from .parser import args
+
+
+def cli(args):
+    if not args.stars:  # if args.stars is not present
+        args.stars = ">=50"
+    tmp_repos = search(args.lang, args.date, args.stars, args.debug)
+    if not tmp_repos:  # if search() returned None
+        return
+
+    repos = []
+    for i in range(args.limit_results):
+        repos.append(tmp_repos[i])
+
+    if args.layout == "table":
+        table_layout(repos)
+        return
+    list_layout(repos)
+
+
+if __name__ == "__main__":
+    cli(args)
