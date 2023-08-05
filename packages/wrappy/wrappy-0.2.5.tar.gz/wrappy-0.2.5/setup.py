c2cc9c2498c872552ebf6e36407f528df79a50fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:37 +0000

    Add package wrappy/0.2.5/wrappy-0.2.5.tar.gz

---
+++ packages/wrappy/wrappy-0.2.5.tar.gz/wrappy-0.2.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wrappy",
+    version="0.2.5",
+    author="Ernie Thornhill",
+    author_email="pepsimixt@gmail.com",
+    description="Decorators for common developer utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ErnieThornhill/wrappy",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'wasabi>=0.4',
+        'dill>=0.3.1.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
