33c43bf51f3a1e3117b0c8dbb0e6a35ae8fd7b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package coral-ordinal/0.1.3/coral-ordinal-0.1.3.tar.gz

---
+++ packages/coral-ordinal/coral-ordinal-0.1.3.tar.gz/coral-ordinal-0.1.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from setuptools import setup
+import os
+import sys
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+version = {}
+with open(os.path.join(_here, 'coral_ordinal', 'version.py')) as f:
+    exec(f.read(), version)
+    
+    
+with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'coral-ordinal',
+    url = 'https://github.com/ck37/coral_ordinal',
+    author = 'Chris Kennedy',
+    author_email = 'chrisken@gmail.com',
+    packages = setuptools.find_packages(),
+    install_requires = ['numpy',
+        'pandas',
+        'tensorflow>=2.2',
+        'scipy'],
+    version = version['__version__'],
+    long_description_content_type = "text/markdown",
+    license = 'MIT',
+    description = 'TF.Keras implementation of CORAL ordinal classification output layer',
+    long_description = long_description,
+    python_requires = '>=3.6',
+)
