828a14da9412ef62a78288b3c9f3bfc89952609d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package kcli/99.0.202006230901/kcli-99.0.202006230901-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202006230901-py3-none-any.whl/kvirt/jinjafilters/__init__.py
@@ -0,0 +1,46 @@
+import os
+
+
+def basename(path):
+    return os.path.basename(path)
+
+
+def dirname(path):
+    return os.path.dirname(path)
+
+
+def none(value):
+    return value if value is not None else ''
+
+
+def _type(value):
+    if value is None:
+        return None
+    elif isinstance(value, str):
+        return 'string'
+    elif isinstance(value, int):
+        return 'int'
+    elif isinstance(value, dict):
+        return 'dict'
+    elif isinstance(value, list):
+        return 'list'
+
+
+def ocpnodes(cluster, platform, masters, workers):
+    masters = ['%s-master-%d' % (cluster, num) for num in range(masters)]
+    workers = ['%s-worker-%d' % (cluster, num) for num in range(workers)]
+    if platform in ['kubevirt', 'openstack', 'vsphere', 'packet']:
+        return ["%s-bootstrap-helper" % cluster] + ["%s-bootstrap" % cluster] + masters + workers
+    else:
+        return ["%s-bootstrap" % cluster] + masters + workers
+
+
+def certificate(value):
+    if 'BEGIN CERTIFICATE' in value:
+        return value
+    else:
+        return "-----BEGIN CERTIFICATE-----\n%s\n-----END CERTIFICATE-----" % value
+
+
+jinjafilters = {'basename': basename, 'dirname': dirname, 'ocpnodes': ocpnodes, 'none': none, 'type': _type,
+                'certificate': certificate}
