7d7b30849f8fdfa07b9824f53ba2ad598af7fba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:00 +0000

    Add package weditor-assistant/0.1.9/weditor_assistant-0.1.9.tar.gz

---
+++ packages/weditor-assistant/weditor_assistant-0.1.9.tar.gz/weditor_assistant-0.1.9/weditor_assistant/test.py
@@ -0,0 +1,6 @@
+import weditor_assistant as wa
+wa.get_image()
+import sys
+
+import time
+wa.get_image()
---
+++ packages/weditor-assistant/weditor_assistant-0.1.9.tar.gz/weditor_assistant-0.1.9/weditor_assistant/weditor_assistant.py
@@ -0,0 +1,4 @@
+
+value='wa.element_click(device,"text","相机")'
+x = value.split(".", 1)
+print(x[1])
