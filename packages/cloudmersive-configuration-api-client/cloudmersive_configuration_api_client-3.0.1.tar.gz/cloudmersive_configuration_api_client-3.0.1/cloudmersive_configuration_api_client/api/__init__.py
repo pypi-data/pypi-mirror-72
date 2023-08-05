1f84b2146714dc4375cca69fc4bb8858156796e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:33 +0000

    Add package cloudmersive-configuration-api-client/3.0.1/cloudmersive_configuration_api_client-3.0.1.tar.gz

---
+++ packages/cloudmersive-configuration-api-client/cloudmersive_configuration_api_client-3.0.1.tar.gz/cloudmersive_configuration_api_client-3.0.1/cloudmersive_configuration_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cloudmersive_configuration_api_client.api.orchestrator_api import OrchestratorApi
+from cloudmersive_configuration_api_client.api.settings_api import SettingsApi
