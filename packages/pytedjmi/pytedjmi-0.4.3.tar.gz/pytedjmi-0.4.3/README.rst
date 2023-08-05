4e708c9797756ece669337fdcc1bd40427eebd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package pytedjmi/0.4.3/pytedjmi-0.4.3.tar.gz

---
+++ packages/pytedjmi/pytedjmi-0.4.3.tar.gz/pytedjmi-0.4.3/README.rst
@@ -0,0 +1,34 @@
+========================
+Pytest Django Migrations
+========================
+
+ - version number: 0.4.3
+ - author: Kit La Touche
+
+Overview
+--------
+
+Test Django migrations through Pytest.
+
+Installation / Usage
+--------------------
+
+To install use pip::
+
+    $ pip install pytedjmi
+
+
+Or clone the repo::
+
+    $ git clone https://github.com/wlonk/pytest-django-migrations.git
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ packages/pytedjmi/pytedjmi-0.4.3.tar.gz/pytedjmi-0.4.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytedjmi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=3.0.0', 'pytest-django>=3.9.0', 'pytest>=5.4.3']
+
+entry_points = \
+{'pytest11': ['pytedjmi = pytedjmi.core']}
+
+setup_kwargs = {
+    'name': 'pytedjmi',
+    'version': '0.4.3',
+    'description': 'Test Django migrations through Pytest.',
+    'long_description': '========================\nPytest Django Migrations\n========================\n\n - version number: 0.4.3\n - author: Kit La Touche\n\nOverview\n--------\n\nTest Django migrations through Pytest.\n\nInstallation / Usage\n--------------------\n\nTo install use pip::\n\n    $ pip install pytedjmi\n\n\nOr clone the repo::\n\n    $ git clone https://github.com/wlonk/pytest-django-migrations.git\n    $ python setup.py install\n    \nContributing\n------------\n\nTBD\n\nExample\n-------\n\nTBD\n',
+    'author': 'Kit La Touche',
+    'author_email': 'kit@transneptune.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
