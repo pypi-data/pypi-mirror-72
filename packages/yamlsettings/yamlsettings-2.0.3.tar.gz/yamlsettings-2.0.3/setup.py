71ff4baa402f90d98d8a6cfc5fab0f5fc04f21a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package yamlsettings/2.0.3/yamlsettings-2.0.3.tar.gz

---
+++ packages/yamlsettings/yamlsettings-2.0.3.tar.gz/yamlsettings-2.0.3/setup.py
@@ -0,0 +1,56 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = open('README.rst').read()
+
+requirements = {
+    "package": [
+        "PyYAML>=5",
+        "six",
+    ],
+    "test": [
+        "nose",
+        "mock",
+        "pytest",
+        "pytest-mock",
+        "pytest-pudb",
+    ],
+    "setup": [
+        "pytest-runner",
+    ],
+}
+
+requirements.update(all=sorted(set().union(*requirements.values())))
+
+setup(
+    name='yamlsettings',
+    version='2.0.3',
+    description='Yaml Settings Configuration Module',
+    long_description=readme,
+    author='Kyle James Walker',
+    author_email='KyleJamesWalker@gmail.com',
+    url='https://github.com/KyleJamesWalker/yamlsettings',
+    packages=['yamlsettings', 'yamlsettings.extensions'],
+    package_dir={'yamlsettings':
+                 'yamlsettings'},
+    include_package_data=True,
+    install_requires=requirements['package'],
+    extras_require=requirements,
+    setup_requires=requirements['setup'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='tests',
+    tests_require=requirements['test'],
+)
