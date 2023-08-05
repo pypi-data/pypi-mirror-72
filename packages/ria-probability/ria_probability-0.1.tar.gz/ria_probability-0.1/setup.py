8778243cf75f8a3fd2644660247611921d58b5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:15 +0000

    Add package ria-probability/0.1/ria_probability-0.1.tar.gz

---
+++ packages/ria-probability/ria_probability-0.1.tar.gz/ria_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ria_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ria_probability'],
+      author = 'Alfonso Ridolfo',
+      author_email = 'alfonso.ridolfo@gmail.com',
+      zip_safe=False)
