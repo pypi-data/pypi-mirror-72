7b8bcde4872ce91cc42f847132e36573e74c16a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:24 +0000

    Add package nrz-distributions/0.1/nrz_distributions-0.1.tar.gz

---
+++ packages/nrz-distributions/nrz_distributions-0.1.tar.gz/nrz_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nrz_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nrz_distributions'],
+      author = 'Rosalie Zhu',
+      zip_safe=False)
