a34715f3d7701884f32ee1655df21832d4dd3f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package cabean/0.90/cabean-0.90.tar.gz

---
+++ packages/cabean/cabean-0.90.tar.gz/cabean-0.90/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+NAME = 'cabean'
+
+setup(name=NAME,
+    version='0.90',
+    author = "Loïc Paulevé",
+    author_email = "loic.pauleve@labri.fr",
+    url = "https://github.com/algorecell/pyCabean",
+    description = "Python interface to CABEAN, A Software Tool for the Control of Asynchronous Boolean Networks",
+    install_requires = [
+        "colomoto_jupyter",
+        "algorecell_types",
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="computational systems biology",
+
+    include_package_data = True,
+    packages = find_packages(),
+    py_modules = ["cabean_setup"]
+)
+
