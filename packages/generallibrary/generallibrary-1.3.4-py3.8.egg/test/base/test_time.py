5dd1accff70811c23c1d41b99fe82fde23a44c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package generallibrary/1.3.4/generallibrary-1.3.4-py3.8.egg

---
+++ packages/generallibrary/generallibrary-1.3.4-py3.8.egg/test/base/test_time.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+from generallibrary import Timer, sleep
+
+class TimeTest(unittest.TestCase):
+    def test_timer(self):
+        timer = Timer()
+        sleep(0.05)
+        secs = timer.seconds()
+        self.assertEqual(round(secs, 2), 0.05)
+
+    def test_sleep(self):
+        pass
