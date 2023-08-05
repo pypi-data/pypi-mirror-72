1294863d1ade97ed84e69eeef49e42bd0a3ce603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package gpplot/0.2.1/gpplot-0.2.1.tar.gz

---
+++ packages/gpplot/gpplot-0.2.1.tar.gz/gpplot-0.2.1/gpplot/__init__.py
@@ -0,0 +1,7 @@
+"""import gpplot imports both the plots and style modules"""
+from .plots import *
+from .style import *
+
+__author__ = """Peter C DeWeirdt"""
+__email__ = 'petedeweirdt@gmail.com'
+__version__ = '0.2.1'
