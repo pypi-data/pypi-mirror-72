1459b2bc48861902236f2943626c68ca8ada3d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package markovpack/0.3.2/markovpack-0.3.2-py3-none-any.whl

---
+++ packages/markovpack/markovpack-0.3.2-py3-none-any.whl/markovpack/stars/__init__.py
@@ -0,0 +1,8 @@
+# Imports go here!
+
+# Enter the PageStars of your Pack here!
+available_page_stars = [
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [command.__name__ for command in available_page_stars]
