f301dce3e19d115994f071d855fe45261665c61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package distributions-joseMarin-exercise/0.1/distributions-joseMarin-exercise-0.1.tar.gz

---
+++ packages/distributions-joseMarin-exercise/distributions-joseMarin-exercise-0.1.tar.gz/distributions-joseMarin-exercise-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions-joseMarin-exercise',
+      version='0.1',
+        description='Gaussian and Binomial distributions',
+        packages=['distributions'],
+        author='Jose MARIN',
+        author_email= 'josermarinr18@gmail.com',
+        python_requires='>=3.4^',
+        zip_safe=False)
