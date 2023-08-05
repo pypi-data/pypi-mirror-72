4f57ff4a1f14c79178e4c73b0935ca5928e6f2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package jwaigz-distributions/0.1/jwaigz-distributions-0.1.tar.gz

---
+++ packages/jwaigz-distributions/jwaigz-distributions-0.1.tar.gz/jwaigz-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jwaigz-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jwaigz-distributions'],
+      author='John Njeri',
+      author_email ='waiganjojohn11@gmail.com',
+      zip_safe=False)
