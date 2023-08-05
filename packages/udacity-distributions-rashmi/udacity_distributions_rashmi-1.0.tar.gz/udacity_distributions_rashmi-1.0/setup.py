b5251a808c86bf95c074e4bb567e4ad93a8738c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package udacity-distributions-rashmi/1.0/udacity_distributions_rashmi-1.0.tar.gz

---
+++ packages/udacity-distributions-rashmi/udacity_distributions_rashmi-1.0.tar.gz/udacity_distributions_rashmi-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_rashmi',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['udacity_distributions_rashmi'],
+      zip_safe=False)
