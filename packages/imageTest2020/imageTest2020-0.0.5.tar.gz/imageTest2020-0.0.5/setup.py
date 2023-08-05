d6730ed10cbc618bce95b080668ab9edaf0005f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:23 +0000

    Add package imageTest2020/0.0.5/imageTest2020-0.0.5.tar.gz

---
+++ packages/imageTest2020/imageTest2020-0.0.5.tar.gz/imageTest2020-0.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='imageTest2020',
+      version='0.0.5',
+      description='string to date',
+      author='zxw',
+      author_email='761479698@qq.com',
+      license='MIT',
+      packages=['imageTest2020'])
