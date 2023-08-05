42da793adf14eff26ac55f8cd94dccdf0c8c3ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:36 +0000

    Add package z-distributions/0.1/z_distributions-0.1.tar.gz

---
+++ packages/z-distributions/z_distributions-0.1.tar.gz/z_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='z_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['z_distributions'],
+      author='Chiazokam Echeta',
+      zip_safe=False)
