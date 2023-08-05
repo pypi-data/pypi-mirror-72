d540e49b35e1521ff1c41513feb27e038f0b3709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package django-cid/2.1/django-cid-2.1.tar.gz

---
+++ packages/django-cid/django-cid-2.1.tar.gz/django-cid-2.1/cid/backends/postgresql/base.py
@@ -0,0 +1,12 @@
+from django.db.backends.postgresql.base import DatabaseWrapper as BaseDatabaseWrapper
+
+from ...cursor import CidCursorWrapper
+
+
+class DatabaseWrapper(BaseDatabaseWrapper):
+    # Don't warn about abstract `_start_transaction_under_autocommit`
+    # pylint: disable=abstract-method
+
+    def create_cursor(self, name=None):
+        base_cursor = super().create_cursor(name)
+        return CidCursorWrapper(base_cursor)
