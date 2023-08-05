08ed0d4563703c0db854eb946d54f3515f566c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:31 +0000

    Add package gilda/0.4/gilda-0.4-py3-none-any.whl

---
+++ packages/gilda/gilda-0.4-py3-none-any.whl/gilda/app/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, Response, abort, jsonify, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+from gilda.api import *
+from gilda import __version__ as version
+
+app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False
+Bootstrap(app)
+
+
+class GroundForm(FlaskForm):
+    text = StringField('Text', validators=[DataRequired()])
+    context = TextAreaField('Context')
+    submit = SubmitField('Submit')
+
+    def get_matches(self):
+        return ground(self.text.data, self.context.data)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def info():
+    form = GroundForm()
+    if form.validate_on_submit():
+        matches = form.get_matches()
+        return render_template('matches.html', matches=matches, form=form,
+                               version=version)
+    return render_template('home.html', form=form, version=version)
+
+
+@app.route('/ground', methods=['POST'])
+def ground_endpoint():
+    if request.json is None:
+        abort(Response('Missing application/json header.', 415))
+    # Get input parameters
+    text = request.json.get('text')
+    context = request.json.get('context')
+    scored_matches = ground(text, context)
+    res = [sm.to_json() for sm in scored_matches]
+    return jsonify(res)
+
+
+@app.route('/get_names', methods=['POST'])
+def get_names_endpoint():
+    if request.json is None:
+        abort(Response('Missing application/json header.', 415))
+    # Get input parameters
+    kwargs = {key: request.json.get(key) for key in {'db', 'id', 'status',
+                                                     'source'}}
+    names = get_names(**kwargs)
+    return jsonify(names)
+
+
+@app.route('/models', methods=['GET', 'POST'])
+def models():
+    return jsonify(get_models())
---
+++ packages/gilda/gilda-0.4-py3-none-any.whl/gilda/tests/test_grounder.py
@@ -0,0 +1,85 @@
+from gilda.grounder import Grounder
+from . import appreq
+
+
+gr = Grounder()
+
+
+def test_grounder():
+    entries = gr.lookup('kras')
+    statuses = [e.status for e in entries]
+    assert 'assertion' in statuses
+    for entry in entries:
+        if entry.status == 'assertion':
+            assert entry.id == '6407', entry
+
+    scores = gr.ground('kras')
+    assert len(scores) == 1, scores
+    assert appreq(scores[0].score, 0.9845), scores
+    scores = gr.ground('k-ras')
+    assert len(scores) == 1, scores
+    assert appreq(scores[0].score, 0.9936), scores
+    scores = gr.ground('KRAS')
+    assert len(scores) == 1, scores
+    assert appreq(scores[0].score, 1.0), scores
+    scores = gr.ground('bRaf')
+    assert len(scores) == 1, scores
+    assert appreq(scores[0].score, 0.9936), scores
+
+
+def test_grounder_bug():
+    # Smoke test to make sure the 'NA' entry in grounding terms doesn't get
+    # turned into a None
+    gr.ground('Na')
+
+
+def test_grounder_num_entries():
+    entries = gr.lookup('NPM1')
+    assert len(entries) == 1, entries
+    entries = gr.lookup('H4')
+    assert len(entries) == 5, entries
+
+
+def test_grounder_depluralize():
+    entries = gr.lookup('RAFs')
+    assert len(entries) == 2, entries
+    for entry in entries:
+        assert entry.norm_text == 'raf'
+
+
+def test_disambiguate_adeft():
+    matches = gr.ground('IR')
+    matches = gr.disambiguate('IR', matches, 'Insulin Receptor (IR)')
+    for match in matches:
+        assert match.disambiguation is not None
+        assert match.disambiguation['type'] == 'adeft'
+        assert match.disambiguation['match'] in ('grounded', 'ungrounded')
+        assert match.disambiguation['score'] is not None
+        if match.term.db == 'HGNC' and match.term.id == '6091':
+            assert match.disambiguation['match'] == 'grounded'
+            assert match.disambiguation['score'] == 1.0
+
+
+def test_disambiguate_gilda():
+    matches = gr.ground('NDR1')
+    matches = gr.disambiguate('NDR1', matches, 'STK38')
+    for match in matches:
+        assert match.disambiguation['type'] == 'gilda'
+        assert match.disambiguation['match'] == 'grounded'
+        if match.term.db == 'HGNC' and match.term.id == '17847':
+            assert match.disambiguation['score'] > 0.99
+        if match.term.db == 'HGNC' and match.term.id == '7679':
+            assert match.disambiguation['score'] < 0.01
+
+
+def test_rank_namespace():
+    matches = gr.ground('interferon-gamma')
+    assert matches[0].term.db == 'HGNC'
+
+
+def test_aa_synonym():
+    matches = gr.ground('WN')
+    assert '141447' not in {m.term.id for m in matches}
+
+    matches = gr.ground('W-N')
+    assert '141447' not in {m.term.id for m in matches}
