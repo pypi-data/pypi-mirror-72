899283d305bf1780d581a659c9142ecb42666e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package generallibrary/1.4.7/generallibrary-1.4.7-py3.8.egg

---
+++ packages/generallibrary/generallibrary-1.4.7-py3.8.egg/test/test_iterables.py
@@ -0,0 +1,19 @@
+
+import unittest
+
+from generallibrary.iterables import iterable, depth, dictFirstValue, iterFirstValue
+
+class IterablesTest(unittest.TestCase):
+    def test_iterable(self):
+        pass
+
+    def test_depth(self):
+        self.assertEqual(depth(5), 0)
+
+    def test_dictFirstValue(self):
+        pass
+
+    def test_iterFirstValue(self):
+        pass
+
+
