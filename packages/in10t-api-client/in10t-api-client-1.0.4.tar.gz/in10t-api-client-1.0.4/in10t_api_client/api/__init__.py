098a6742c3b2f20034eff0f05c927b9f97050095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:17 +0000

    Add package in10t-api-client/1.0.4/in10t-api-client-1.0.4.tar.gz

---
+++ packages/in10t-api-client/in10t-api-client-1.0.4.tar.gz/in10t-api-client-1.0.4/in10t_api_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from in10t_api_client.api.data_api import DataApi
+from in10t_api_client.api.file_data_api import FileDataApi
+from in10t_api_client.api.files_api import FilesApi
+from in10t_api_client.api.security_api import SecurityApi
+from in10t_api_client.api.shapefile_data_api import ShapefileDataApi
+from in10t_api_client.api.trials_api import TrialsApi
+from in10t_api_client.api.trial_ids_api import TrialIdsApi
