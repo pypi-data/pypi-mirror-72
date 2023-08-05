1131ae0551637509d73fa6bc93aabb16883cf762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package EllucianEthosPythonClient/0.1.2/EllucianEthosPythonClient-0.1.2.tar.gz

---
+++ packages/EllucianEthosPythonClient/EllucianEthosPythonClient-0.1.2.tar.gz/EllucianEthosPythonClient-0.1.2/EllucianEthosPythonClient/ResourceWrappers/BaseResourceWrapper.py
@@ -0,0 +1,57 @@
+import json
+import copy
+
+class BaseResourceWrapper():
+  clientAPIInstance = None
+  dict = None
+  version = None
+  resourceName = None
+  resourceID = None
+  def __init__(self, clientAPIInstance, dict, version, resourceName):
+    self.clientAPIInstance = clientAPIInstance
+    self.dict = dict
+    self.version = version
+    self.resourceName = resourceName
+
+    if "id" not in dict:
+      raise Exception("Invalid resource dict (missing id)")
+    if not isinstance(version , str):
+      raise Exception("Version passed must be a string")
+
+    self.resourceID = dict["id"]
+
+  def getMajorVersion(self):
+    return self.version.split(".")[0]
+
+  def _getDictForPut(self):
+    return copy.deepcopy(self.dict)
+
+  def save(self, loginSession):
+    def injectHeaderFN(headers):
+      headers["Accept"] = "application/vnd.hedtech.integration.v" + self.getMajorVersion() + "+json"
+      headers["Content-Type"] = "application/vnd.hedtech.integration.v" + self.getMajorVersion() + "+json"
+      ##print(headers)
+
+    url = "/api/" + self.resourceName + "/" + self.resourceID
+
+    result = self.clientAPIInstance.sendPutRequest(
+      url=url,
+      loginSession=loginSession,
+      injectHeadersFn=injectHeaderFN,
+      data=json.dumps(self._getDictForPut())
+    )
+    if result.status_code != 200:
+      self.clientAPIInstance.raiseResponseException(result)
+
+    self.dict = json.loads(result.content)
+
+  def delete(self, loginSession):
+    url = "/api/" + self.resourceName + "/" + self.resourceID
+
+    result = self.clientAPIInstance.sendDeleteRequest(
+      url=url,
+      loginSession=loginSession,
+      injectHeadersFn=None,
+    )
+    if result.status_code != 200:
+      self.clientAPIInstance.raiseResponseException(result)
---
+++ packages/EllucianEthosPythonClient/EllucianEthosPythonClient-0.1.2.tar.gz/EllucianEthosPythonClient-0.1.2/README.md
@@ -0,0 +1,28 @@
+# EllucianEthosPythonClient
+Python Client Library for interfacing with Ellucian Ethos
+
+In development - not ready for production use
+
+## Aims
+
+This library aims to provied:
+
+ - Wrapper class to handle Ethos authorizations
+ - Wrapper classes that call Ellucian API's to fetch resources
+ - Common updates on resources
+ - Provide mock running mode to allow for testing without calling Ethos
+ 
+## Usage examples
+
+For a quick start usage example using the python REPL follow the [Quickstart](./docs/QUICKSTART.md) guide.
+
+
+
+## Background
+
+I started putting this python library after an Ethos user group meeting. There are a number of other Ellucian customers
+all writing clients and I wanted to take the oppertunity of digging out exmaples I had written for myself and my team
+and sharing them with the community.
+  
+ 
+
