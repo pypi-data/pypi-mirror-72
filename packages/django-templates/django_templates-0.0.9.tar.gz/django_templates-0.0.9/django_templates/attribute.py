624e63b4acf1e6a29567f5bcaa1445790cfc574a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:24 +0000

    Add package django-templates/0.0.9/django_templates-0.0.9.tar.gz

---
+++ packages/django-templates/django_templates-0.0.9.tar.gz/django_templates-0.0.9/django_templates/attribute.py
@@ -0,0 +1,11 @@
+class AttributeTemplate:
+    def __init__(self, html_file, action):
+        self.data['html_file'] = html_file
+        self.data['action'] = action
+
+    def get_field(self, field_name):
+        if field_name in self.data.keys():
+            return self.data[field_name]
+        else:
+            raise Exception(
+                'Class {} has no field {}'.format(self.__name__, field_name))
