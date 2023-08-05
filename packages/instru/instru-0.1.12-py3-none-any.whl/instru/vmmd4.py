8469d03a8c36b4d257343728d131972424ea2708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package instru/0.1.12/instru-0.1.12-py3-none-any.whl

---
+++ packages/instru/instru-0.1.12-py3-none-any.whl/instru/vmmd4.py
@@ -0,0 +1,50 @@
+"""
+ViewMarq MD4-0424 module from http://www.automationdirect.com/
+"""
+
+from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadBuilder, BinaryPayloadDecoder
+from time import sleep
+
+CMD_STRING_BUFFER_ADDR = 411000 - 400001
+
+what = 100
+
+TEST_STRING = f"<ID 1><CLR><WIN 0 0 287 31><POS 0 0><LJ><BL N><CS 0><GRN><T>AOGGGG!!!???:{what}</T><POS 0 8><LJ><T>DISPLAY</T><POS 0 16><LJ><T>TEST</T><POS 0 24><LJ><T>OK</T>\r"
+
+
+class VmMd40424:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def __enter__(self):
+        self.vm = ModbusTcpClient(self.host, self.port)
+        self.vm.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.vm.close()
+
+    @property
+    def command_string_buffer(self):
+        pass
+        # result = self.vm.read_holding_registers(CMD_STRING_BUFFER_ADDR, 117)
+        # decoder = BinaryPayloadDecoder.fromRegisters(result.registers,
+        #                                              byteorder=Endian.Big, wordorder=Endian.Little)
+        #
+        # return decoder.decode_string(234)
+
+    @command_string_buffer.setter
+    def command_string_buffer(self, value):
+        builder = BinaryPayloadBuilder(repack=True)
+        builder.add_string(value)
+        payload = builder.to_registers()
+        result = self.vm.write_registers(CMD_STRING_BUFFER_ADDR, payload)
+        print(f'write into LED command string with response of {result}')
+
+
+if __name__ == '__main__':
+    with VmMd40424('192.168.153.250', '502') as vm:
+        vm.command_string_buffer = TEST_STRING
