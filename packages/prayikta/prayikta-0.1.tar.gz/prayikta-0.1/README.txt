316cfd44fa0e7c756e06b5100b891df9358d53b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package prayikta/0.1/prayikta-0.1.tar.gz

---
+++ packages/prayikta/prayikta-0.1.tar.gz/prayikta-0.1/README.txt
@@ -0,0 +1,3 @@
+## Prayikta
+
+By using Prayikta module you can  calculate Gaussian distribution, Binomial Probability, pdf and can draw histograms.
---
+++ packages/prayikta/prayikta-0.1.tar.gz/prayikta-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prayikta',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prayikta'],
+      author = 'Manish Soni',
+      author_email = 'manish.soni8403@gmail.com',
+      zip_safe=False)
