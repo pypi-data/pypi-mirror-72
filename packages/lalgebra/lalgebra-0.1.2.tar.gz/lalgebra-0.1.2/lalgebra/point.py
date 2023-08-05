90527427e82c6f90247a27594b58c1d1a3db719e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package lalgebra/0.1.2/lalgebra-0.1.2.tar.gz

---
+++ packages/lalgebra/lalgebra-0.1.2.tar.gz/lalgebra-0.1.2/lalgebra/point.py
@@ -0,0 +1,8 @@
+class Point:
+	x = 0
+	y = 0
+	coord = property(lambda self: f'({self.x}, {self.y})')
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
---
+++ packages/lalgebra/lalgebra-0.1.2.tar.gz/lalgebra-0.1.2/lalgebra/polygon.py
@@ -0,0 +1,13 @@
+from lalgebra.point import Point
+from lalgebra.line import Line
+
+class InvalidPolygonError(Exception):
+    pass
+
+class Polygon:
+    vertices = []
+    area = property(lambda self: sum([self.vertices[v].x + self.vertices[v+1].y for v in range(0, len(self.vertices)-1)]) + sum([-1*(self.vertices[v].x + self.vertices[v-1].y) for v in range(1, len(self.vertices))]))
+
+    def __init__(self, vertices):
+        if len(vertices) < 3: raise InvalidPolygonError("A polygon must have at least 3 vertices!")
+        self.vertices = vertices
