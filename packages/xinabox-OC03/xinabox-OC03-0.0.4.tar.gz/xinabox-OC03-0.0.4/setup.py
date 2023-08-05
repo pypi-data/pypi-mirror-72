d9ba3b7842b33673a29a1bc2a24b5cebc2afcfe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:30 +0000

    Add package xinabox-OC03/0.0.4/xinabox-OC03-0.0.4.tar.gz

---
+++ packages/xinabox-OC03/xinabox-OC03-0.0.4.tar.gz/xinabox-OC03-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xinabox-OC03",
+    version="0.0.4",
+    author="Luqmaan Baboo",
+    author_email="luqmaanbaboo@gmail.com",
+    description="Solid state relay",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinabox/Python-OC03",
+    install_requires=["xinabox-CORE",],
+    py_modules=["xOC03",],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
