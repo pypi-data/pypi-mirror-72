54f55ae13b7662fc82659b93aa2f1a700234b924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:42 +0000

    Add package robotframework-seleniumtestability/1.1.0/robotframework-seleniumtestability-1.1.0.tar.gz

---
+++ packages/robotframework-seleniumtestability/robotframework-seleniumtestability-1.1.0.tar.gz/robotframework-seleniumtestability-1.1.0/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+SeleniumTestability
+"""
+import versioneer
+from pathlib import Path
+from setuptools import setup
+
+LIBRARY_NAME = "SeleniumTestability"
+CWD = Path(__file__).parent
+
+requirements_file = CWD / "requirements.txt"
+readme_file = CWD / "README.md"
+
+# Get requirements
+with requirements_file.open(encoding="utf-8") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+# Get the long description from the README file
+with readme_file.open(encoding="utf-8") as f:
+    long_description = f.read()
+
+CLASSIFIERS = """
+Development Status :: 3 - Alpha
+Topic :: Software Development :: Testing
+Operating System :: OS Independent
+License :: OSI Approved :: Apache Software License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Topic :: Software Development :: Testing
+Framework :: Robot Framework
+Framework :: Robot Framework :: Library
+""".strip().splitlines()
+
+setup(
+    name="robotframework-%s" % LIBRARY_NAME.lower(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="SeleniumTestability library that helps speed up tests with" "asyncronous evens",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rasjani/robotframework-%s" % LIBRARY_NAME.lower(),
+    author="Jani Mikkonen",
+    author_email="jani.mikkonen@gmail.com",
+    license="Apache License 2.0",
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIREMENTS,
+    keywords="robot framework testing automation selenium seleniumlibrary"
+    "testability async javascript softwaretesting",
+    platforms="any",
+    packages=[LIBRARY_NAME],
+    package_dir={"": "src"},
+    package_data={"": ["js/*.*"]},
+)
---
+++ packages/robotframework-seleniumtestability/robotframework-seleniumtestability-1.1.0.tar.gz/robotframework-seleniumtestability-1.1.0/src/Helpers/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from psutil import Process, wait_procs, NoSuchProcess
+from SeleniumLibrary.base.robotlibcore import DynamicCore, keyword
+from SeleniumTestability.types import ProcessType
+
+
+class Helpers(DynamicCore):
+    def __init__(self: "Helpers") -> None:
+        DynamicCore.__init__(self, [])
+
+    @keyword
+    def die_die_die(self: "Helpers", parent: ProcessType) -> None:
+        if isinstance(parent, int):
+            try:
+                par = Process(parent)
+            except NoSuchProcess:
+                self.warn("Unable to kill process id:{}".format(parent))
+                return
+        else:
+            par = parent
+        for child_process in par.children():
+            self.die_die_die(child_process)
+            if child_process.is_running():
+                child_process.terminate()
+                _, alive = wait_procs([child_process], timeout=3)
+                for p in alive:
+                    p.kill()
