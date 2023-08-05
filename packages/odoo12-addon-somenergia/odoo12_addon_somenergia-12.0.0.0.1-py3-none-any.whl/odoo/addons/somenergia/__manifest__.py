431aeda52a72a1d3bac42fac099752f35bfdc746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:38 +0000

    Add package odoo12-addon-somenergia/12.0.0.0.1/odoo12_addon_somenergia-12.0.0.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-somenergia/odoo12_addon_somenergia-12.0.0.0.1-py3-none-any.whl/odoo/addons/somenergia/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Odoo Som Energia customizations",
+    'version': '12.0.0.0.1',
+    'depends': ['account_asset_management'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """ 
+    Odoo Som Energia customizations.
+    """,
+    "license": "AGPL-3",
+    'data': [
+        'views/account_asset_profile.xml'
+    ],  
+}
+
