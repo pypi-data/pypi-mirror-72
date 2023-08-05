bb0cf551f5f6d4faa06e15eb5e32a375bd7bb6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package autolens/1.0.4/autolens-1.0.4-py3-none-any.whl

---
+++ packages/autolens/autolens-1.0.4-py3-none-any.whl/test_autolens/integration/tests/imaging/lens__source/lens_light_mass__source.py
@@ -0,0 +1,38 @@
+import autofit as af
+import autolens as al
+from test_autolens.integration.tests.imaging import runner
+
+test_type = "lens__source"
+test_name = "lens_light_mass__source"
+data_name = "lens_sie__source_smooth"
+instrument = "vro"
+
+
+def make_pipeline(name, folders, search=af.PySwarmsGlobal()):
+
+    phase1 = al.PhaseImaging(
+        phase_name="phase_1",
+        folders=setup.folders,
+        galaxies=dict(
+            lens=al.GalaxyModel(
+                redshift=0.5,
+                light=al.lp.SphericalDevVaucouleurs,
+                mass=al.mp.EllipticalIsothermal,
+            ),
+            source=al.GalaxyModel(redshift=1.0, light=al.lp.EllipticalSersic),
+        ),
+        sub_size=8,
+        search=search,
+    )
+
+    phase1.search.const_efficiency_mode = True
+    phase1.search.n_live_points = 60
+    phase1.search.facc = 0.8
+
+    return al.PipelineDataset(name, phase1)
+
+
+if __name__ == "__main__":
+    import sys
+
+    runner.run(sys.modules[__name__])
