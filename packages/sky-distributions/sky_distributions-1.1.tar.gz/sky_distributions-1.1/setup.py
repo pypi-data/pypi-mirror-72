5da0205c6b96bfc7bbfd911eb520ba172d8bbe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:24 +0000

    Add package sky-distributions/1.1/sky_distributions-1.1.tar.gz

---
+++ packages/sky-distributions/sky_distributions-1.1.tar.gz/sky_distributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sky_distributions',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['sky_distributions'],
+      zip_safe=False)
