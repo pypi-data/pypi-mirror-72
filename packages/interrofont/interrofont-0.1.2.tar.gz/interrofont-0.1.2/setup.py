0083cdd311b62975b832c346743f9c9a0eaa99b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package interrofont/0.1.2/interrofont-0.1.2.tar.gz

---
+++ packages/interrofont/interrofont-0.1.2.tar.gz/interrofont-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['interrofont']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fonttools']
+
+entry_points = \
+{'console_scripts': ['interrofont = interrofont:main']}
+
+setup_kwargs = {
+    'name': 'interrofont',
+    'version': '0.1.2',
+    'description': 'Interrogate a font file',
+    'long_description': None,
+    'author': 'Simon Cozens',
+    'author_email': 'simon@simon-cozens.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
