0bd9c2abb4969e1f28d364b1837d7be14923953b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package django-nico/0.8/django-nico-0.8.tar.gz

---
+++ packages/django-nico/django-nico-0.8.tar.gz/django-nico-0.8/nico/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class DatLog(models.Model):
+    dat_insercao = models.DateTimeField(auto_now_add=True, null=True)
+    dat_edicao = models.DateTimeField(auto_now=True, null=True)
+    dat_delete = models.DateTimeField(null=True)
+    class Meta:
+        abstract = True
+
+
+class UsrLog(models.Model):
+    usr_insercao = models.ForeignKey('nico.Profile', on_delete=models.SET_NULL, null=True, related_name='%(app_label)s_%(class)s_usr_insercao')
+    usr_edicao = models.ForeignKey('nico.Profile', on_delete=models.SET_NULL, null=True, related_name='%(app_label)s_%(class)s_usr_edicao')
+    usr_delete = models.ForeignKey('nico.Profile', on_delete=models.SET_NULL, null=True, related_name='%(app_label)s_%(class)s_usr_delete')
+    class Meta:
+        abstract = True
+
+
+class Log(DatLog, UsrLog):
+    status = models.BooleanField(null=True, default=True)
+    class Meta:
+        abstract = True
+
+
+class Profile(AbstractUser, Log):
+    nm_completo = models.CharField(max_length=200, null=True)
+
+
+class Funcionario(Profile):
+    cr = models.CharField(max_length=200, null=True)
+    profile = models.OneToOneField(to=Profile, parent_link=True, related_name='funcionario', on_delete=models.CASCADE)
+
+
+class Cliente(Profile):
+    cpf = models.CharField(max_length=200, null=True)
+    profile = models.OneToOneField(to=Profile, parent_link=True, related_name='cliente', on_delete=models.CASCADE)
