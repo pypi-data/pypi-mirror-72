7aec0919905a3c01f56fb90d09d96d61a8176228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package punit/1.0.3/punit-1.0.3.tar.gz

---
+++ packages/punit/punit-1.0.3.tar.gz/punit-1.0.3/modi/batch.py
@@ -0,0 +1,16 @@
+def punit():
+           print("Punit modi")
+           return
+
+def abhi():
+           print("Abhishek anora")
+           return
+
+def deep():
+           print("Deependra singh")
+           return
+
+def keertika():
+           print("keertika sirohiya")
+           return
+           
