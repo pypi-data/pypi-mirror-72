18bd9401c7648dd582425dc634f50d8d648884fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:17 +0000

    Add package pymsbuild/0.0.19/pymsbuild-0.0.19.tar.gz

---
+++ packages/pymsbuild/pymsbuild-0.0.19.tar.gz/pymsbuild-0.0.19/pymsbuild/_build.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+from pathlib import PurePath, Path
+
+
+def locate():
+    exe = Path(os.getenv("MSBUILD", ""))
+    if exe.is_file():
+        return exe
+    for part in os.getenv("PATH", "").split(os.path.pathsep):
+        p = Path(part)
+        if p.is_dir():
+            exe = p / "msbuild.exe"
+            if exe.is_file():
+                return exe
+    vswhere = Path(os.getenv("ProgramFiles(x86)"), "Microsoft Visual Studio", "Installer", "vswhere.exe")
+    if vswhere.is_file():
+        out = Path(subprocess.check_output([
+            str(vswhere),
+            "-nologo",
+            "-property", "installationPath",
+            "-latest",
+            "-prerelease",
+            "-products", "*",
+            "-requires", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
+            "-utf8",
+        ], encoding="utf-8", errors="strict").strip())
+        if out.is_dir():
+            exe = out / "MSBuild" / "Current" / "Bin" / "msbuild.exe"
+            if exe.is_file():
+                return exe
+
+    raise RuntimeError("Unable to locate msbuild.exe. Please provide it as %MSBUILD%")
