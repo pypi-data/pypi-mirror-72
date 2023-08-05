a3edc04a8eb5187664fc388c9e62a6344eb27b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package pkg-distributions/0.2/pkg_distributions-0.2.tar.gz

---
+++ packages/pkg-distributions/pkg_distributions-0.2.tar.gz/pkg_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pkg_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['pkg_distributions'],
+      author='Rafaela Sousa',
+      author_email = 'rafaelaprm@gmail.com',
+      zip_safe=False)
