6d1e5dbe85c7ad29a72e48d01988cd37472fc552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:28 +0000

    Add package mlflow/1.9.1/mlflow-1.9.1-py3-none-any.whl

---
+++ packages/mlflow/mlflow-1.9.1-py3-none-any.whl/mlflow/server/js/build/precache-manifest.45232c49ba92a84f6b623fc4f848a985.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf7d6c3de5e0c95af44b",
+    "url": "static-files/static/css/main.b8c9609e.chunk.css"
+  },
+  {
+    "revision": "bf7d6c3de5e0c95af44b",
+    "url": "static-files/static/js/main.dd34ca00.chunk.js"
+  },
+  {
+    "revision": "e2753c5829ad0c22cd41",
+    "url": "static-files/static/js/runtime~main.34eeba64.js"
+  },
+  {
+    "revision": "bfba1882039ded16cc7b",
+    "url": "static-files/static/css/2.9d69fc75.chunk.css"
+  },
+  {
+    "revision": "bfba1882039ded16cc7b",
+    "url": "static-files/static/js/2.3c607361.chunk.js"
+  },
+  {
+    "revision": "246bfb44ab6d6f0973e0516ae6436ec0",
+    "url": "static-files/static/media/404-overflow.246bfb44.svg"
+  },
+  {
+    "revision": "7a9dd62d43af29b22609a5aa93325481",
+    "url": "static-files/static/media/home-logo.7a9dd62d.png"
+  },
+  {
+    "revision": "424b3291d91fa569c4d525fa01abb44d",
+    "url": "static-files/static/media/notebook.424b3291.svg"
+  },
+  {
+    "revision": "91661969e9b84d8458d5be802ce436d6",
+    "url": "static-files/static/media/empty.91661969.svg"
+  },
+  {
+    "revision": "0e9e57fc119af79203e8a87520122dd4",
+    "url": "static-files/static/media/laptop.0e9e57fc.svg"
+  },
+  {
+    "revision": "4c3d159fcacd3cf13f13adbfac20d124",
+    "url": "static-files/static/media/project.4c3d159f.svg"
+  },
+  {
+    "revision": "ff2120e55a4e8abcfdc99eea208eedd0",
+    "url": "static-files/static/media/job.ff2120e5.svg"
+  },
+  {
+    "revision": "f8c411a9a70c61fd7000bb3082b16e23",
+    "url": "static-files/static/media/default-error.f8c411a9.svg"
+  },
+  {
+    "revision": "74bba15d71d39cdeae9e3edce246e3ac",
+    "url": "static-files/static/media/permission-denied-lock.74bba15d.svg"
+  },
+  {
+    "revision": "a1bbaadbf82282f9b9a44f18d932a893",
+    "url": "static-files/static/media/no-experiments.a1bbaadb.svg"
+  },
+  {
+    "revision": "80d06002929ce4f31f20be6b803fe6ff",
+    "url": "static-files/index.html"
+  }
+];
