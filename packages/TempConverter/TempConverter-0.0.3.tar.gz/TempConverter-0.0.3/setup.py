f80fc6c64ba98d58a0a7d4b334448995c0d65422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package TempConverter/0.0.3/TempConverter-0.0.3.tar.gz

---
+++ packages/TempConverter/TempConverter-0.0.3.tar.gz/TempConverter-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TempConverter",
+    version="0.0.3",
+    author="Akash",
+    author_email="akash9129@gmail.com",
+    description="A small package to convert temperature",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
