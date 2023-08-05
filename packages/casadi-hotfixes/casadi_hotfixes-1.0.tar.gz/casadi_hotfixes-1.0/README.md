0847572aeed09cb23db107ee8fbc1ce323cd1e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:18 +0000

    Add package casadi-hotfixes/1.0/casadi_hotfixes-1.0.tar.gz

---
+++ packages/casadi-hotfixes/casadi_hotfixes-1.0.tar.gz/casadi_hotfixes-1.0/README.md
@@ -0,0 +1,14 @@
+Collection of hotfixes (monkeypatches) for CasADi
+
+Usage:
+
+```python
+
+import casadi_hotfixes
+from casadi import *
+```
+
+version 1.0, June 24 2020
+ * workaround for incompatibility with numpy 1.19
+
+
---
+++ packages/casadi-hotfixes/casadi_hotfixes-1.0.tar.gz/casadi_hotfixes-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='casadi_hotfixes',
+      version='1.0',
+      description='Collections of patches for CasADi Python',
+      url='http://github.com/storborg/funniest',
+      author='Joris Gillis',
+      author_email='joris.gillis42@gmail.com',
+      license='MIT',
+      packages=['casadi_hotfixes'],
+      zip_safe=True)
+
