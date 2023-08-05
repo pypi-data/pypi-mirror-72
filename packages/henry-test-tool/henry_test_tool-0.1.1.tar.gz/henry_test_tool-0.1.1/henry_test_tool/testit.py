6dfd2f0c6208ff8a30e3b96004601e0458d2550d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:21:40 +0000

    Add package henry-test-tool/0.1.1/henry_test_tool-0.1.1.tar.gz

---
+++ packages/henry-test-tool/henry_test_tool-0.1.1.tar.gz/henry_test_tool-0.1.1/henry_test_tool/testit.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+def test_hello():
+    print("hello")
+
+def test_say_hi():
+    print('hi')
+
+''' pip包管理
+https://blog.csdn.net/fengmm521/article/details/79144407
+
+
+
+from henry_test_tool import testit  
+testit.test_hello()  
+
+
+'''
---
+++ packages/henry-test-tool/henry_test_tool-0.1.1.tar.gz/henry_test_tool-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: henry.wang
+# Mail: shanandone@qq.com
+# Created Time:  2020-06-25
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "henry_test_tool",
+    version = "0.1.1",
+    keywords = ("pip", "henrytest","henrytool"),
+    description = "henry used for teest",
+    long_description = "long long desc",
+    license = "MIT Licence",
+
+    url = "https://github.com/yunsansheng/henrytest",
+    author = "henry.wang",
+    author_email = "shanandone@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
+
