4d8172a4f47fa7d05e7fd7acb975c7fb405e9024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package stat-prob-dist/0.1.1/stat_prob_dist-0.1.1.tar.gz

---
+++ packages/stat-prob-dist/stat_prob_dist-0.1.1.tar.gz/stat_prob_dist-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'stat_prob_dist',
+      version = '0.1.1',
+      description = 'Statistical distributions - Gaussian, Binomial etc..',
+      packages = ['stat_prob_dist'],
+      author = 'Amanpreet Singh',
+      author_email = 'amanpreetsingh459@gmail.com',
+      zip_safe = False)
---
+++ packages/stat-prob-dist/stat_prob_dist-0.1.1.tar.gz/stat_prob_dist-0.1.1/stat_prob_dist/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'statistical_distributions',
+      version = '0.1',
+      description = 'Statistical distributions - Gaussian, Binomial etc..',
+      packages = ['statistical_distributions'],
+      author = 'Amanpreet Singh',
+      author_email = 'amanpreetsingh459@gmail.com',
+      zip_safe = False)
