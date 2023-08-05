ce04c4fe1c0ad5d586e1984b944a6f2c96a2661d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package ai-transformersx/0.5.28/ai_transformersx-0.5.28-py2.py3-none-any.whl

---
+++ packages/ai-transformersx/ai_transformersx-0.5.28-py2.py3-none-any.whl/transformersx/train/__init__.py
@@ -0,0 +1,3 @@
+from .trainer import TaskTrainer, TaskTrainLoop, TaskTrainedCounter, TaskTrainEpoch, TaskTrainStep, TaskTrainerContext, \
+    TaskTrainerOptimizers, TaskTrainerSaver, TaskTrainerEvaluator, TaskTrainerLogger, TaskTrainerDataLoaders, \
+    TaskTrainingArguments, TaskEvalPrediction, TaskPredictionOutput, TaskTrainOutput, TaskDefaultDataCollatorx
