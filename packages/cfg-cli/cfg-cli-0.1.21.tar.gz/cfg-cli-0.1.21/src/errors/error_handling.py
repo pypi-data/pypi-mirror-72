2065f48ab3d34fa35cd35873fb98a0fae7ba2f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:25 +0000

    Add package cfg-cli/0.1.21/cfg-cli-0.1.21.tar.gz

---
+++ packages/cfg-cli/cfg-cli-0.1.21.tar.gz/cfg-cli-0.1.21/src/errors/error_handling.py
@@ -0,0 +1,37 @@
+import requests
+from typing import Union
+
+from .exceptions import InvalidCredentials, NotLoggedIn
+
+
+def catch_error(resp: requests.Response, service="prism") -> requests.Response:
+    """[summary]
+
+    Args:
+        resp (requests.Response): [description]
+        service (str, optional): [description]. Defaults to "prism".
+
+    Raises:
+        NotLoggedIn: [description]
+        InvalidCredentials: [description]
+        e: [description]
+
+    Returns:
+        requests.resp: [description]
+    """
+    try:
+        resp.raise_for_status()
+        return resp
+    except requests.exceptions.HTTPError as e:
+        if resp.status_code in (402,):
+            return resp
+        elif resp.status_code in (400,):
+            raise NotLoggedIn
+        elif resp.status_code in (502, 403):
+            raise InvalidCredentials
+        elif resp.status_code != 200:
+            raise e
+        # if service == "auth":
+        #     pass
+        # elif service == "prism":
+        #     pass
