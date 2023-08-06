46898ae09eddf3b066963835c9c252817c343f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:25 +0000

    Add package awsmldistributionsjb/0.2/awsmldistributionsjb-0.2.tar.gz

---
+++ packages/awsmldistributionsjb/awsmldistributionsjb-0.2.tar.gz/awsmldistributionsjb-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='awsmldistributionsjb',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['awsmldistributionsjb'],
+      author='Joseph Berchmans',
+      zip_safe=False)
