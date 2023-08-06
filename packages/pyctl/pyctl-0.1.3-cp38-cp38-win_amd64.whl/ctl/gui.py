8b1c750a0d792cc837dc5e1fef2f13a34b351b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:19 +0000

    Add package pyctl/0.1.3/pyctl-0.1.3-cp38-cp38-win_amd64.whl

---
+++ packages/pyctl/pyctl-0.1.3-cp38-cp38-win_amd64.whl/ctl/gui.py
@@ -0,0 +1,15 @@
+import _ctl
+
+_ctl.gui.init_qapp()
+
+__all__ = [name for name in dir(_ctl.gui) if name[0] != '_' and name != 'init_qapp']
+
+def _add_ctl_attr():
+    for name in dir(_ctl.gui):
+        if name[0] == '_' or name == 'init_qapp':
+            continue
+        globals()[name] = getattr(_ctl.gui, name)
+_add_ctl_attr()
+del _add_ctl_attr
+
+del _ctl
