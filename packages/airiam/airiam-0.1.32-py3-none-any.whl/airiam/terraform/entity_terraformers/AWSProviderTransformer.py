e8a0f250ba83aea089e4c02cd4cfc62cbf1e0380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:53 +0000

    Add package airiam/0.1.32/airiam-0.1.32-py3-none-any.whl

---
+++ packages/airiam/airiam-0.1.32-py3-none-any.whl/airiam/terraform/entity_terraformers/AWSProviderTransformer.py
@@ -0,0 +1,25 @@
+from airiam.terraform.entity_terraformers.BaseEntityTransformer import BaseEntityTransformer
+
+
+class AWSProviderTransformer(BaseEntityTransformer):
+    def __init__(self, entity_json: dict):
+        super().__init__('aws_provider', 'default_provider', entity_json)
+
+    def _generate_hcl2_code(self, entity_json) -> str:
+        if 'profile' in entity_json and entity_json['profile'] is not None:
+            provider_code = f"""provider "aws" {{
+  region  = "{entity_json['region']}"
+  profile = "{entity_json['profile']}"
+}}
+
+"""
+        else:
+            provider_code = f"""provider "aws" {{
+  region = "{entity_json['region']}"
+}}
+
+"""
+        return provider_code
+
+    def entities_to_import(self) -> list:
+        return []
