ca5a61ac8c848ad4d9c1418126ca572ef01b54ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package py-mob/0.2.6/py_mob-0.2.6.tar.gz

---
+++ packages/py-mob/py_mob-0.2.6.tar.gz/py_mob-0.2.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+ 
+setuptools.setup(
+  name = "py_mob",
+  version = "0.2.6",
+  author = "WenSui Liu",
+  author_email = "liuwensui@gmail.com",
+  description = "Python Implementation of Monotonic Optimal Binning",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/statcompute/py_mob",
+  packages = setuptools.find_packages(),
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
