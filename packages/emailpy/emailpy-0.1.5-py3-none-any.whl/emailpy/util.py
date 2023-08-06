cc53f51e938bfc51d99f66e6b32724424603c7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:29 +0000

    Add package emailpy/0.1.5/emailpy-0.1.5-py3-none-any.whl

---
+++ packages/emailpy/emailpy-0.1.5-py3-none-any.whl/emailpy/util.py
@@ -0,0 +1,36 @@
+from base64 import *
+from .send import sendmail
+from .read import readmail
+
+class EmailSender:
+    def __init__(self, email, pwd):
+        self.email = email
+        self.pwd = b85encode(pwd.encode())
+
+    def send(self, toemails, subject = '', body = '', html = None,
+             attachments = None, nofileattach = None):
+        return sendmail(self.email, b85decode(self.pwd).decode(),
+                        toemails, subject, body, html,
+                        attachments, nofileattach)
+
+class EmailReader:
+    def __init__(self, email, pwd):
+        self.email = email
+        self.pwd = b85encode(pwd.encode())
+
+    def read(self, foldername = 'INBOX'):
+        return readmail(self.email, b85decode(self.pwd).decode(),
+                        foldername)
+
+class EmailManager:
+    def __init__(self, email, pwd):
+        self.sender = EmailSender(email, pwd)
+        self.reader = EmailReader(email, pwd)
+
+    def send(self, toemails, subject = '', body = '', html = None,
+             attachments = None, nofileattach = None):
+        return self.sender.send(toemails, subject, body, html,
+                        attachments, nofileattach)
+
+    def read(self, foldername = 'INBOX'):
+        return self.reader.read(foldername)
