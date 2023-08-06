660892f2a7ea8ba714c88fef9aec549635be0041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:49 +0000

    Add package distributions-helper/1.0/distributions_helper-1.0.tar.gz

---
+++ packages/distributions-helper/distributions_helper-1.0.tar.gz/distributions_helper-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_helper',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_helper'],
+      author = 'Samantha Sam-Inimgba',
+      author_email = 's.sinimgba@gmail.com',
+      zip_safe=False)
