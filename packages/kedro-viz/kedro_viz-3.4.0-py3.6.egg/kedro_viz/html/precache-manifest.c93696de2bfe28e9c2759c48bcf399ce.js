f3e49d4a2ffd9fb74238fdfe656c5993000ae64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package kedro-viz/3.4.0/kedro_viz-3.4.0-py3.6.egg

---
+++ packages/kedro-viz/kedro_viz-3.4.0-py3.6.egg/kedro_viz/html/precache-manifest.c93696de2bfe28e9c2759c48bcf399ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a2ea294c463ab0ac47dcaf50084faf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1db4435801a00e5585af",
+    "url": "./static/css/2.fcf86715.chunk.css"
+  },
+  {
+    "revision": "493519879fbf30cb6acb",
+    "url": "./static/css/main.9d0ba680.chunk.css"
+  },
+  {
+    "revision": "1db4435801a00e5585af",
+    "url": "./static/js/2.4c6c8980.chunk.js"
+  },
+  {
+    "revision": "54120cd43cbbd1b3b4c14b86ca02f064",
+    "url": "./static/js/2.4c6c8980.chunk.js.LICENSE"
+  },
+  {
+    "revision": "493519879fbf30cb6acb",
+    "url": "./static/js/main.6ce5e558.chunk.js"
+  },
+  {
+    "revision": "c88a607ed4c298f69081",
+    "url": "./static/js/runtime-main.6cb937c6.js"
+  },
+  {
+    "revision": "6259cc84bb236512f69bc1d20397254c",
+    "url": "./static/media/brodyquest.6259cc84.mp3"
+  },
+  {
+    "revision": "e702bd5fc3663eda7c4459232f5a76c0",
+    "url": "./static/media/easter-egg.e702bd5f.png"
+  }
+]);
---
+++ packages/kedro-viz/kedro_viz-3.4.0-py3.6.egg/kedro_viz/html/precache-manifest.f1a53259ee0440f873e75c0d09e6221f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743a00ea0a28129d09236e5753af77f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "446a4ffa7b83e949002a",
+    "url": "./static/css/2.fcf86715.chunk.css"
+  },
+  {
+    "revision": "5ac756b7a7b4a541ecee",
+    "url": "./static/css/main.9d0ba680.chunk.css"
+  },
+  {
+    "revision": "446a4ffa7b83e949002a",
+    "url": "./static/js/2.2932cbd5.chunk.js"
+  },
+  {
+    "revision": "54120cd43cbbd1b3b4c14b86ca02f064",
+    "url": "./static/js/2.2932cbd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ac756b7a7b4a541ecee",
+    "url": "./static/js/main.83ed6429.chunk.js"
+  },
+  {
+    "revision": "c88a607ed4c298f69081",
+    "url": "./static/js/runtime-main.6cb937c6.js"
+  },
+  {
+    "revision": "6259cc84bb236512f69bc1d20397254c",
+    "url": "./static/media/brodyquest.6259cc84.mp3"
+  },
+  {
+    "revision": "e702bd5fc3663eda7c4459232f5a76c0",
+    "url": "./static/media/easter-egg.e702bd5f.png"
+  }
+]);
