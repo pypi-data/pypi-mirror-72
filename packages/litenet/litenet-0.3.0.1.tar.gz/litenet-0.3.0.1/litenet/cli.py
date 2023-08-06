668c4eb0e0d39099b98daeacf367dde1ad304c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package litenet/0.3.0.1/litenet-0.3.0.1.tar.gz

---
+++ packages/litenet/litenet-0.3.0.1.tar.gz/litenet-0.3.0.1/litenet/cli.py
@@ -0,0 +1,35 @@
+import click
+from litenet.server import LiteNetServer
+from litenet.client import LiteNetClient
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command(help="Launch a LiteNet Server")
+@click.option("-h", "--host", required=True, type=str, help="The ip to run the server on. (Must have access to it.)")
+@click.option("-p", "--port", default=5050, type=int, help="The port to run the server on. (Must be a valid port.)")
+@click.option("--header", "header", default=64, type=int, help="The size of the server message header.")
+@click.option("-d", "--debug", is_flag=True, help="Enters debug mode.")
+def server(host, port, header, debug):
+    litenet = LiteNetServer(host, port, header, debug=debug)
+    litenet.start()
+
+
+@click.command(help="Launch the LiteNet Client")
+@click.option("-h", "--host", required=True, type=str, help="The ip the server's running on. (Must have access to it.)")ip
+@click.option("-p", "--port", default=5050, type=int, help="The port the server's running on. (Must be a valid port.)")
+@click.option("--header", "header", default=64, type=int, help="The size of the server message header. (Must match the server's.)")
+@click.option("-d", "--debug", is_flag=True, help="Enters debug mode.")
+def client(host, port, header, debug):
+    client = LiteNetClient(host, port, header, debug=debug)
+    client.start()
+
+
+cli.add_command(server)
+cli.add_command(client)
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/litenet/litenet-0.3.0.1.tar.gz/litenet-0.3.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='litenet',
+    version='0.3.0.1',
+    packages=['litenet'],
+    url='https://github.com/GrandMoff100/LiteNet',
+    license='MIT LIcence',
+    author='',
+    author_email='',
+    description='A socket-based client messaging service.',
+    install_requires=["click", "keyboard"],
+    entry_points={
+        "console_scripts":["litenet=litenet.cli:cli"]
+    }
+)
