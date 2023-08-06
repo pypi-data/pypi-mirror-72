f552946c7f5a6b1f660b8f62cbb4c487548ac588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:58 +0000

    Add package ivadomed/2.0.2/ivadomed-2.0.2.tar.gz

---
+++ packages/ivadomed/ivadomed-2.0.2.tar.gz/ivadomed-2.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+import ivadomed
+
+here = path.abspath(path.dirname(__file__))
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+#Get README
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ivadomed',
+    version=ivadomed.__version__,
+    description='Feature conditioning for IVADO medical imaging project.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/neuropoly/ivadomed',
+    author='NeuroPoly and Mila',
+    author_email='none@none.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(exclude=['docs', 'tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'ivadomed=ivadomed.main:run_main',
+        ],
+    },
+)
