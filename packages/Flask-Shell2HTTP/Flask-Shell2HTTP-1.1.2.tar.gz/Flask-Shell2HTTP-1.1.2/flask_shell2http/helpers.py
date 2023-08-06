482bba9a2f56a33a2baa6cecdcaabbb0909086db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:03 +0000

    Add package Flask-Shell2HTTP/1.1.2/Flask-Shell2HTTP-1.1.2.tar.gz

---
+++ packages/Flask-Shell2HTTP/Flask-Shell2HTTP-1.1.2.tar.gz/Flask-Shell2HTTP-1.1.2/flask_shell2http/helpers.py
@@ -0,0 +1,27 @@
+import logging
+
+
+def list_replace(lst: list, old, new) -> None:
+    """
+    replace list elements (inplace)
+    """
+    idx = -1
+    try:
+        while True:
+            i = lst.index(old, idx + 1)
+            lst[i] = new
+    except ValueError:
+        pass
+
+
+def calc_hash(lst: list) -> str:
+    """
+    Internal use only.
+    Calculates md5sum of given command with it's byte-string.
+    """
+    to_hash = " ".join(lst).encode("ascii")
+    return __import__("hashlib").md5(to_hash).hexdigest()
+
+
+def get_logger() -> logging.Logger:
+    return logging.getLogger("flask_shell2http")
