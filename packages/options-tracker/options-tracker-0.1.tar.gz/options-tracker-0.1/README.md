55e96f27088a917d3d454e1ce3e3d46ceead1186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:58 +0000

    Add package options-tracker/0.1/options-tracker-0.1.tar.gz

---
+++ packages/options-tracker/options-tracker-0.1.tar.gz/options-tracker-0.1/README.md
@@ -0,0 +1,24 @@
+Options Tracker
+===============
+
+How much are my stock options worth today?
+
+Vested and unvested.
+
+Installation
+------------
+
+You need python 3.6+
+
+`pip install options-tracker`
+
+Usage
+-----
+
+Basic (this should work for your Mirriad options):
+
+`options-tracker <granted quantity> <grant price>`
+
+Have a look at the help for more options:
+
+`options-tracker --help`
---
+++ packages/options-tracker/options-tracker-0.1.tar.gz/options-tracker-0.1/optionstracker/vesting.py
@@ -0,0 +1,19 @@
+from dateutil.relativedelta import relativedelta
+
+def vested(quantity, check_date, grant_date, vest_rate):
+    elapsed = relativedelta(check_date, grant_date)
+    if elapsed.years >= 3:
+        return (quantity, 0)
+    v = int((12 * elapsed.years + elapsed.months) * vest_rate * quantity)
+    return (v, quantity - v)
+
+def vesting_profile(grant_quantity, grant_date, vesting_rate,
+        profile_interval=relativedelta(months=+1)):
+    d = grant_date
+    p = []
+    uv = grant_quantity
+    while uv > 0:
+        v, uv = vested(grant_quantity, d, grant_date, vesting_rate)
+        p.append((d, v, uv))
+        d = d + profile_interval
+    return p
