1a7639071946b51dab89c4cf61dce1a737f17e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:39 +0000

    Add package python-scuttle/0.2.4/python_scuttle-0.2.4-py3-none-any.whl

---
+++ packages/python-scuttle/python_scuttle-0.2.4-py3-none-any.whl/scuttle/versions/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import requests
+
+
+class BaseApi:
+    version = None
+
+    def __init__(self, domain, api_key):
+        self.endpoint = "https://{}.scuttle.bluesoul.net/api/v{}".format(
+            domain, type(self).version
+        )
+        self.api_key = api_key
+
+    def request(self, namespace, value=None, data=None):
+        method = 'get' if data is None else 'post'
+        send = {'headers': {"Authorization": "Bearer {}".format(self.api_key)}}
+        if data is not None:
+            send['data'] = data
+        response = getattr(requests, method)(
+            "{}/{}".format(self.endpoint, namespace.format(value)), **send
+        )
+        return response.json()
