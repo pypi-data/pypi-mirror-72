0531a68d3cb164c6e373a9bd7dbde5d9c215c90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:44 +0000

    Add package mtna-rds/0.2.1/mtna_rds-0.2.1-py3-none-any.whl

---
+++ packages/mtna-rds/mtna_rds-0.2.1-py3-none-any.whl/rds/utility.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 10:13:22 2020
+
+@author: seanlucas
+"""
+
+import urllib
+import sys
+import json
+
+
+def get_response(api_call):
+    if sys.version_info > (3, 0):
+        return urllib.request.urlopen(api_call)
+    else:
+        return urllib.urlopen(api_call)
+
+
+def check_valid(api_call, message, is_json=False):
+    try:
+        response = get_response(api_call)
+        if is_json:
+            return json.load(response)
+    except AttributeError as e:
+        print(e)
+        sys.exit
+    except Exception as e:
+        print(message)
+        print(e)
+        sys.exit
