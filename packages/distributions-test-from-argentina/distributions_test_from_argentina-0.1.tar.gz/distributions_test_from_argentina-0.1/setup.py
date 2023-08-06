ac749d1fedfc4de82ed8d88c67b4eb8fc2f59a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:29 +0000

    Add package distributions-test-from-argentina/0.1/distributions_test_from_argentina-0.1.tar.gz

---
+++ packages/distributions-test-from-argentina/distributions_test_from_argentina-0.1.tar.gz/distributions_test_from_argentina-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_test_from_argentina',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_test_from_argentina'],
+      zip_safe=False)
