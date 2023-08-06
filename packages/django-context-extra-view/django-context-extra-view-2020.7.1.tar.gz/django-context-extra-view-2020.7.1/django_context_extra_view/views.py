f63d4afd452de59c293e055d8a0dba4e65063af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:35 +0000

    Add package django-context-extra-view/2020.7.1/django-context-extra-view-2020.7.1.tar.gz

---
+++ packages/django-context-extra-view/django-context-extra-view-2020.7.1.tar.gz/django-context-extra-view-2020.7.1/django_context_extra_view/views.py
@@ -0,0 +1,14 @@
+
+
+class ContextExtraViewMixin:
+    context_extra = {}
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        if hasattr(super(),'get_context_data'):
+            context = super().get_context_data(**kwargs)
+        if self.context_extra:
+            context.update(self.context_extra)
+        return context
+
+
