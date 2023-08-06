4b24377f06521c53869a422888a737305f659b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:40 +0000

    Add package wbia-vtool/3.0.1/wbia-vtool-3.0.1.tar.gz

---
+++ packages/wbia-vtool/wbia-vtool-3.0.1.tar.gz/wbia-vtool-3.0.1/scripts/_install_opencv_build_dependencies_macports_on_macos.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+###############################
+# Install MacPorts dependencies
+
+set -ex
+
+sudo port selfupdate
+
+# Install ports if MacPorts install location is not present
+sudo port install \
+     clang-5.0 \
+     clang_select \
+     llvm-5.0 \
+     llvm_select \
+     libomp \
+     gflags \
+     libpng \
+     jasper \
+     ilmbase \
+     openexr \
+     webp \
+     jpeg \
+     tiff \
+     tbb \
+     libdc1394 \
+     eigen3-devel \
+     boost \
+     bzip2 \
+     zlib \
+     ffmpeg \
+     freeglut \
+     mesa \
+     gstreamer1-gst-plugins-base \
+     qt5-qtbase \
+     gtk2 \
+     ade
+
+# OpenMP
+sudo port select clang mp-clang-5.0
+sudo port select llvm mp-llvm-5.0
+
+#: needed for python3 bindings
+python -m pip install numpy
