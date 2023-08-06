cc8e016a74650d25b9bddf6e38c17befa3d3a10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package lee-sndistributions/0.1/lee_sndistributions-0.1.tar.gz

---
+++ packages/lee-sndistributions/lee_sndistributions-0.1.tar.gz/lee_sndistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lee_sndistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['lee_sndistributions'],
+      author = "Louies Lee",
+      maintainer = "louies lee",
+      zip_safe=False)
