56bca6f8990619ecb68a14deeb19456a34843103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:43 +0000

    Add package laredoyinprobability/0.1.6/laredoyinprobability-0.1.6.tar.gz

---
+++ packages/laredoyinprobability/laredoyinprobability-0.1.6.tar.gz/laredoyinprobability-0.1.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='laredoyinprobability',
+      version='0.1.6',
+      description='Gaussian and Binomial distributions',
+      packages=['laredoyin_probability'],
+      author='Damilare Adedoyin',
+      author_email='damilare182@gmail.com',
+      zip_safe=False)
