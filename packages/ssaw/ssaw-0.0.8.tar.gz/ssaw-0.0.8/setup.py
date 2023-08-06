1a35b44a02ad5886042c6c67f263087b2681ce23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:32 +0000

    Add package ssaw/0.0.8/ssaw-0.0.8.tar.gz

---
+++ packages/ssaw/ssaw-0.0.8.tar.gz/ssaw-0.0.8/setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup
+
+packages = [
+    'ssaw',
+    'ssaw.designer',
+]
+
+requires = [
+    'requests',
+    'sgqlc',
+]
+
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'vcrpy',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'ssaw', '__about__.py'), 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    packages=packages,
+    install_requires=requires,
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license="",
+    keywords="Survey Solutions",
+    url=about['__url__'],
+    tests_require=test_requirements,
+)
