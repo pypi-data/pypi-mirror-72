93dd7550ed23901424f6ba14e115237aadedd5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:43 +0000

    Add package graphdot/0.5a7/graphdot-0.5a7.tar.gz

---
+++ packages/graphdot/graphdot-0.5a7.tar.gz/graphdot-0.5a7/graphdot/kernel/marginalized/basekernel.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import warnings
+
+warnings.warn(
+    "The basekernel module has been relocated to graphdot.kernel.basekernel.\n"
+    "Please update relevant import statements from\n"
+    "'from graphdot.kernel.marginalized.basekernel import *'\n"
+    "to\n"
+    "'from graphdot.kernel.basekernel import *'"
+)
