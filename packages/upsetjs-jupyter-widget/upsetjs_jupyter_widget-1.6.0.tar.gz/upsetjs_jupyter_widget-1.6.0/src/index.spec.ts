1482c589c5e19cbfeade494801a3834ac80bd8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:03 +0000

    Add package upsetjs-jupyter-widget/1.6.0/upsetjs_jupyter_widget-1.6.0.tar.gz

---
+++ packages/upsetjs-jupyter-widget/upsetjs_jupyter_widget-1.6.0.tar.gz/upsetjs_jupyter_widget-1.6.0/src/index.spec.ts
@@ -0,0 +1,30 @@
+/**
+ * @upsetjs/jupyter_widget
+ * https://github.com/upsetjs/upsetjs_jupyter_widget
+ *
+ * Copyright (c) 2020 Samuel Gratzl <sam@sgratzl.com>
+ */
+
+// import // Add any needed widget imports here (or from controls)
+// '@jupyter-widgets/base';
+
+import { createTestModel } from './__tests__/utils';
+
+import { UpSetModel } from '.';
+
+describe('UpSet', () => {
+  describe('UpSetModel', () => {
+    it('should be createable', () => {
+      let model = createTestModel(UpSetModel);
+      expect(model).toBeInstanceOf(UpSetModel);
+      expect(model.get('value')).toBeUndefined();
+    });
+
+    // it('should be createable with a value', () => {
+    //   let state = { value: 'Foo Bar!' };
+    //   let model = createTestModel(UpSetModel, state);
+    //   expect(model).toBeInstanceOf(UpSetModel);
+    //   expect(model.get('value')).toBe('Foo Bar!');
+    // });
+  });
+});
