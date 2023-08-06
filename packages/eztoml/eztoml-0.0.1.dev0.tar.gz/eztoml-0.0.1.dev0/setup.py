53c928cdc27cacb5ef46ab371a43292c3be5b996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:18 +0000

    Add package eztoml/0.0.1.dev0/eztoml-0.0.1.dev0.tar.gz

---
+++ packages/eztoml/eztoml-0.0.1.dev0.tar.gz/eztoml-0.0.1.dev0/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+"""Perform setup of the package for build."""
+import re
+import io
+
+from setuptools import setup
+
+
+with io.open('eztoml/__init__.py', 'rt', encoding='utf8') as f:
+    __version__ = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+
+setup(
+    name='eztoml',
+    version=__version__,
+    description='Easy TOML',
+    author='Ross Wolf',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    url='https://eztoml.readthedocs.io',
+    packages=['eztoml'],
+    tests_require=["PyYAML~=5.3"]
+)
