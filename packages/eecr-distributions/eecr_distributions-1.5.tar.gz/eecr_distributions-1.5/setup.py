824d3b6e32a22d1e0f8de0ef6b4312ca437e343a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:05 +0000

    Add package eecr-distributions/1.5/eecr_distributions-1.5.tar.gz

---
+++ packages/eecr-distributions/eecr_distributions-1.5.tar.gz/eecr_distributions-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eecr_distributions',
+      version='1.5',
+      description='Gaussian and Binomial distributions',
+      packages=['eecr_distributions'],
+      author='Ernesto Cornejo - Udacity course 2020',
+      author_email='ernestocornejo@hotmail.com',
+      zip_safe=False)
