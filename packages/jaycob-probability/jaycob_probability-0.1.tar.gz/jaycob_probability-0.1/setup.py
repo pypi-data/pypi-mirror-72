88ae609f20ee75683df6f87d11bdecde1b13d7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package jaycob-probability/0.1/jaycob_probability-0.1.tar.gz

---
+++ packages/jaycob-probability/jaycob_probability-0.1.tar.gz/jaycob_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from keyring import get_keyring
+
+setup(name='jaycob_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jaycob_probability'],
+      author = 'Jaycob Clavel',
+      author_email = 'jaycobclavel101@gmail.com',
+      zip_safe=False)
