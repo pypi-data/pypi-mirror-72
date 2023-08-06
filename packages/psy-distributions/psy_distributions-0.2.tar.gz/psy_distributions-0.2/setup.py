55f0953da168e5d444fb00eb37a83862b7d6146e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package psy-distributions/0.2/psy_distributions-0.2.tar.gz

---
+++ packages/psy-distributions/psy_distributions-0.2.tar.gz/psy_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='psy_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['psy_distributions'],
+      author = 'Nazım Önder Orhan',
+      autor_email = 'nazim54@gmail.com',
+      zip_safe=False)
