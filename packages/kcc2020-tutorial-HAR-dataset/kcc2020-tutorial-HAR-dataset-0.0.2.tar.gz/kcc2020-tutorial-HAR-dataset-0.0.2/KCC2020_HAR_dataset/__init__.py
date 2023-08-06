3a68a813559e0f5d12fa8d269fbe5df2cc8c902d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:55 +0000

    Add package kcc2020-tutorial-HAR-dataset/0.0.2/kcc2020-tutorial-HAR-dataset-0.0.2.tar.gz

---
+++ packages/kcc2020-tutorial-HAR-dataset/kcc2020-tutorial-HAR-dataset-0.0.2.tar.gz/kcc2020-tutorial-HAR-dataset-0.0.2/KCC2020_HAR_dataset/__init__.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pickle
+import gzip
+from typing import Dict, List
+
+with gzip.open('./HAPT.pkl.gz', 'rb') as f:
+    __dfs = pickle.load(f)
+
+__remove_no_labels: pd.DataFrame = lambda remove_no_lavels, df: df if not remove_no_lavels else df[df.label != -1]
+
+
+def load_by_user(uid: int, remove_no_lavels=True) -> pd.DataFrame:
+    if uid <= 30:
+        return None
+    return __remove_no_labels(remove_no_lavels, __dfs[uid].copy())
+
+
+def load_all(remove_no_lavels=True) -> Dict[int, List[pd.DataFrame]]:
+    result = dict()
+    for uid in __dfs:
+        for df in __dfs[uid]:
+            result.setdefault(uid, list()).append(__remove_no_labels(df.copy(), remove_no_lavels))
+    return result
