6904319f35d002b1bde4c28453ef2cdf464c3c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:21 +0000

    Add package yamale/2.2.0/yamale-2.2.0.tar.gz

---
+++ packages/yamale/yamale-2.2.0.tar.gz/yamale-2.2.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+if PY2:
+    readme = open('README.md').read()
+    license = open('LICENSE').read()
+elif PY3:
+    readme = open('README.md', encoding='utf-8').read()
+    license = open('LICENSE', encoding='utf-8').read()
+
+setup(
+    name='yamale',
+    version='2.2.0',
+    url='https://github.com/23andMe/Yamale',
+    author='Bo Lopker',
+    author_email='blopker@23andme.com',
+    description='A schema and validator for YAML.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pyyaml'],
+    entry_points={
+        'console_scripts': ['yamale=yamale.command_line:main'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+    ]
+)
---
+++ packages/yamale/yamale-2.2.0.tar.gz/yamale-2.2.0/yamale/schema/validationresults.py
@@ -0,0 +1,40 @@
+import sys
+
+PY2 = sys.version_info[0] == 2
+
+
+class Result(object):
+    def __init__(self, errors):
+        self.errors = errors
+
+    def __str__(self):
+        error_str = '\n'.join(self.errors)
+        if PY2:
+            error_str = error_str.encode('utf-8')
+        return error_str
+
+    def isValid(self):
+        return len(self.errors) == 0
+
+
+class ValidationResult(Result):
+    def __init__(self, data, schema, errors):
+        super(ValidationResult, self).__init__(errors)
+        self.data = data
+        self.schema = schema
+
+    def __str__(self):
+        if self.isValid():
+            error_str = "'%s' is Valid" % self.data
+        else:
+            head_line_bits = ["Error validating data"]
+            if self.data:
+                head_line_bits.append("'{}'".format(self.data))
+            if self.schema:
+                head_line_bits.append("with schema '{}'".format(self.schema))
+            head_line = ' '.join(head_line_bits)
+            head_line += '\n\t'
+            error_str = head_line + '\n\t'.join(self.errors)
+        if PY2:
+            error_str = error_str.encode('utf-8')
+        return error_str
