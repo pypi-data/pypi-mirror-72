d909974647a2c1680fdb0c5b7ac22293fb72eaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:02 +0000

    Add package dnn/0.6.3.2/dnn-0.6.3.2-py3-none-any.whl

---
+++ packages/dnn/dnn-0.6.3.2-py3-none-any.whl/dnn/__init__.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+if tf.__version__ [0] == '2':
+    tf.compat.v1.disable_v2_behavior ()
+from .dnn import DNN
+from .multidnn import MultiDNN
+
+__version__ = "0.6.3.2"
+def subprocess (train_func, *args):
+    from multiprocessing import Pool
+    with Pool(1) as p:
+        return p.apply (train_func, args)
