5eb59460b7920693443d9db92468085ced954b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:08 +0000

    Add package rainbow-print/1.0.5/rainbow_print-1.0.5-py3-none-any.whl

---
+++ packages/rainbow-print/rainbow_print-1.0.5-py3-none-any.whl/rainbow_print/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+| **@created on:** 6/19/20,
+| **@author:** prathyushsp,
+| **@version:** v1.0.0
+|
+| **Description:**
+|
+|
+| **Sphinx Documentation Status:**
+"""
+
+from rainbow_print.rainbow import printr, rlogging
+
+
+
+
