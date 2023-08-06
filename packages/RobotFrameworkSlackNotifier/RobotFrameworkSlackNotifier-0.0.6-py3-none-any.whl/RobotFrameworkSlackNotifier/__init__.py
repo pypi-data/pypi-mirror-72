62f68c29b1e05de52daed1004c1ba130b7f3ab0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:07 +0000

    Add package RobotFrameworkSlackNotifier/0.0.6/RobotFrameworkSlackNotifier-0.0.6-py3-none-any.whl

---
+++ packages/RobotFrameworkSlackNotifier/RobotFrameworkSlackNotifier-0.0.6-py3-none-any.whl/RobotFrameworkSlackNotifier/__init__.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="RobotFrameworkSlackNotifier",
+    version="0.0.6",
+    packages=find_packages(),
+    scripts=["RobotFrameworkSlackNotifier.py"],
+
+
+    # metadata to display on PyPI
+    author="PJ Gevana",
+    author_email="patdroidz@gmail.com",
+    description="RobotFrameworkSlackNotifier"
+)
