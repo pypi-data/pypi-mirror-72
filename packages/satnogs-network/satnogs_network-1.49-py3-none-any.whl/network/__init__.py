fefc47ca1d4fa2e4b4826f2e11e154a2afc15dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:03 +0000

    Add package satnogs-network/1.49/satnogs_network-1.49-py3-none-any.whl

---
+++ packages/satnogs-network/satnogs_network-1.49-py3-none-any.whl/network/__init__.py
@@ -0,0 +1,10 @@
+"""The core django app for SatNOGS Network"""
+from __future__ import absolute_import
+
+from ._version import get_versions
+from .celery import APP as celery_app
+
+__all__ = ['celery_app']
+
+__version__ = get_versions()['version']
+del get_versions
---
+++ packages/satnogs-network/satnogs_network-1.49-py3-none-any.whl/network/base/migrations/0002_auto_20151011_1406.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='antenna',
+            name='antenna_type',
+            field=models.CharField(max_length=15, choices=[('dipole', 'Dipole'), ('yagi', 'Yagi'), ('helical', 'Helical'), ('parabolic', 'Parabolic'), ('vertical', 'Verical')]),
+        ),
+    ]
