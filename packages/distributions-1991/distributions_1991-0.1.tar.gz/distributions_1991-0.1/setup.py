edf43edd4625077af1344f94a07cc6d879a37398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:20 +0000

    Add package distributions-1991/0.1/distributions_1991-0.1.tar.gz

---
+++ packages/distributions-1991/distributions_1991-0.1.tar.gz/distributions_1991-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_1991',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_1991'],
+      zip_safe=False)
