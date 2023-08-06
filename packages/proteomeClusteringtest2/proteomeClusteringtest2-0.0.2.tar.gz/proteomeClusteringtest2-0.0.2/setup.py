48a0f483a610e63296f436799f6707a8430dd7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:10 +0000

    Add package proteomeClusteringtest2/0.0.2/proteomeClusteringtest2-0.0.2.tar.gz

---
+++ packages/proteomeClusteringtest2/proteomeClusteringtest2-0.0.2.tar.gz/proteomeClusteringtest2-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proteomeClusteringtest2", # Replace with your own username
+    version="0.0.2",
+    author="Tse-Ming Chen",
+    author_email="benben5514@gmail.com",
+    description="proteomeClustering test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zytyz/ProteomeClustering",
+    project_urls={
+            "Documentation": "https://pillow.readthedocs.io" },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
