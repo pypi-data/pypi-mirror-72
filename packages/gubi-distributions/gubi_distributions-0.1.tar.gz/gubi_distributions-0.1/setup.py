902ae8163a2cdcde4c723ee833768258e7a52e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:41 +0000

    Add package gubi-distributions/0.1/gubi_distributions-0.1.tar.gz

---
+++ packages/gubi-distributions/gubi_distributions-0.1.tar.gz/gubi_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gubi_distributions',
+      version='0.1',
+      description='Normal/Gaussian and Binomial distributions',
+      packages=['gubi_distributions'],
+      author = 'Samson Adetutu',
+      zip_safe=False)
