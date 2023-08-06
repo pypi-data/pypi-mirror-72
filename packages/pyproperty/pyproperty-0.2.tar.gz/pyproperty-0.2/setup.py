0a0c7768b52075adf44bbd6aa54cbc42a97dd5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:58 +0000

    Add package pyproperty/0.2/pyproperty-0.2.tar.gz

---
+++ packages/pyproperty/pyproperty-0.2.tar.gz/pyproperty-0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md") as ld:
+    long_description = ld.read()
+
+setuptools.setup(
+    name = "pyproperty",
+    version = "0.2",
+    author = "raifpy",
+    author_email = "omerto12.45y@gmail.com",
+    description="Python Explore Modules Func Tool *",
+    long_description=long_description,
+    url = "https://github.com/raifpy/pyproperty",
+    packages=setuptools.find_packages(),
+    install_requires = ["colorama"],
+    python_requires= ">=3.5",
+    scripts = ["pyproperty/pyproperty"]
+        )
