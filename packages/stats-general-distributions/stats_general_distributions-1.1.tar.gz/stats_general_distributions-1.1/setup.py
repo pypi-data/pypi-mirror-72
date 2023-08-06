0dfd918a5678ee72cecfca860164ef6c659a7fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package stats-general-distributions/1.1/stats_general_distributions-1.1.tar.gz

---
+++ packages/stats-general-distributions/stats_general_distributions-1.1.tar.gz/stats_general_distributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stats_general_distributions',
+      version='1.01',
+      description='Gaussian and Binomial distributions',
+      packages=['stats_general_distributions'],
+      zip_safe=False)
