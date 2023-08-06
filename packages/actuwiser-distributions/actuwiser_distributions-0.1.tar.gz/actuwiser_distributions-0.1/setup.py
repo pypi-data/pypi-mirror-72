4d38e9fda18af2eb2c208620098551977de984ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:11 +0000

    Add package actuwiser-distributions/0.1/actuwiser_distributions-0.1.tar.gz

---
+++ packages/actuwiser-distributions/actuwiser_distributions-0.1.tar.gz/actuwiser_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='actuwiser_distributions',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['actuwiser_distributions'],
+      author='Victor Mombeyarara',
+      author_email='vmombeyarara@gmail.com',
+      zip_safe=False)
