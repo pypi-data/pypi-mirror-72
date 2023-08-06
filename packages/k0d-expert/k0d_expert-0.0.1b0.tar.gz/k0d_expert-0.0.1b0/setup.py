afeab4822c7cbdca21e4583ef20d4c74f76b40d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:28 +0000

    Add package k0d-expert/0.0.1b0/k0d_expert-0.0.1b0.tar.gz

---
+++ packages/k0d-expert/k0d_expert-0.0.1b0.tar.gz/k0d_expert-0.0.1b0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['package']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'k0d-expert',
+    'version': '0.0.1b0',
+    'description': 'Cli для школы k0d.expert',
+    'long_description': '# Cli для школы k0d.expert\n\n',
+    'author': 'Nikolay Baryshnikov',
+    'author_email': 'root@k0d.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://k0d.expert',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
