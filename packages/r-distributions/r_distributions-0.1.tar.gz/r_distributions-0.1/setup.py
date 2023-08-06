9e5f763b5a1efd65d91e8b666afca81c844bde5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:33 +0000

    Add package r-distributions/0.1/r_distributions-0.1.tar.gz

---
+++ packages/r-distributions/r_distributions-0.1.tar.gz/r_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='r_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['r_distributions'],
+      zip_safe=False)
