8a3eb3960815dfe83b7db0227629b87cd26b0e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:05 +0000

    Add package mh-universal-functions/2020.6.29.1/mh_universal_functions-2020.6.29.1.tar.gz

---
+++ packages/mh-universal-functions/mh_universal_functions-2020.6.29.1.tar.gz/mh_universal_functions-2020.6.29.1/mh_universal_functions/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['database_update_universal',
+           'database_read',
+           'read_config',
+           'exit_critical',
+           'send_telegram_message',
+           'test']
+from .mh_universal_functions import *
