3ebb7f24f4cb3c7b19c926272179c409421a73c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package django-snakeoil/1.0b6/django_snakeoil-1.0b6-py3-none-any.whl

---
+++ packages/django-snakeoil/django_snakeoil-1.0b6-py3-none-any.whl/snakeoil/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="SEOPath",
+            fields=[
+                ("meta_tags", models.JSONField(default=dict, verbose_name="meta tags")),
+                (
+                    "path",
+                    models.CharField(
+                        max_length=255,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="path",
+                    ),
+                ),
+            ],
+            options={"verbose_name": "SEO path", "verbose_name_plural": "SEO paths"},
+        ),
+    ]
