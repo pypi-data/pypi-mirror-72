ef599faf918ea17cfe9381ef9104d314c419029e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:06 +0000

    Add package itk-hasi/0.1.0/itk_hasi-0.1.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/itk-hasi/itk_hasi-0.1.0-cp38-cp38-manylinux1_x86_64.whl/itk/Configuration/HASIConfig.py
@@ -0,0 +1,17 @@
+depends = ('ITKPyBase', 'ITKThresholding', 'ITKTestKernel', 'ITKSpatialObjects', 'ITKSmoothing', 'ITKRegistrationCommon', 'ITKRegionGrowing', 'ITKLabelMap', 'ITKImageSources', 'ITKImageGrid', 'ITKDistanceMap', 'ITKConnectedComponents', 'BoneEnhancement', )
+templates = (
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterISS3ISS3', True, 'itk::Image< signed short,3 >, itk::Image< signed short,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterIUC3IUC3', True, 'itk::Image< unsigned char,3 >, itk::Image< unsigned char,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterIUS3IUS3', True, 'itk::Image< unsigned short,3 >, itk::Image< unsigned short,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterID3ID3', True, 'itk::Image< double,3 >, itk::Image< double,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterISS3ISS3', True, 'itk::Image< signed short,3 >, itk::Image< signed short,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterISS3IUC3', True, 'itk::Image< signed short,3 >, itk::Image< unsigned char,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterISS3IUS3', True, 'itk::Image< signed short,3 >, itk::Image< unsigned short,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterISS3IF3', True, 'itk::Image< signed short,3 >, itk::Image< float,3 >'),
+  ('LandmarkAtlasSegmentationFilter', 'itk::LandmarkAtlasSegmentationFilter', 'itkLandmarkAtlasSegmentationFilterISS3ID3', True, 'itk::Image< signed short,3 >, itk::Image< double,3 >'),
+  ('SegmentBonesInMicroCTFilter', 'itk::SegmentBonesInMicroCTFilter', 'itkSegmentBonesInMicroCTFilterISS3ISS3', True, 'itk::Image< signed short,3 >, itk::Image< signed short,3 >'),
+  ('SegmentBonesInMicroCTFilter', 'itk::SegmentBonesInMicroCTFilter', 'itkSegmentBonesInMicroCTFilterISS3IUC3', True, 'itk::Image< signed short,3 >, itk::Image< unsigned char,3 >'),
+  ('SegmentBonesInMicroCTFilter', 'itk::SegmentBonesInMicroCTFilter', 'itkSegmentBonesInMicroCTFilterISS3IUS3', True, 'itk::Image< signed short,3 >, itk::Image< unsigned short,3 >'),
+)
+snake_case_functions = ('landmark_atlas_segmentation_filter', 'segment_bones_in_micro_ct_filter', )
