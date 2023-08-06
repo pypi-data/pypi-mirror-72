aa67e0503d11bd3b64e72a66ba794309ad3ae6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:59 +0000

    Add package ssnp/0.0.1rc2/ssnp-0.0.1rc2.tar.gz

---
+++ packages/ssnp/ssnp-0.0.1rc2.tar.gz/ssnp-0.0.1rc2/src/ssnp/__init__.py
@@ -0,0 +1,28 @@
+"""
+SSNP
+
+=====
+
+Features
+
+* Forward model calculation based on TensorFlow 2.x
+* Common type of file read/write
+* Gradient calculation
+* Image reconstruction (working)
+"""
+
+import sys
+
+if sys.version_info.major < 3:
+    raise ImportError("SSNP package only support python3")
+
+import warnings
+
+if sys.version_info < (3, 6):
+    warnings.warn("Untested python version. Please use python>=3.6")
+
+VERSION = '0.0.1rc2'
+import pycuda
+import pycuda.autoinit
+from .data import read, write
+from .calc import ssnp_step, tilt, pure_forward_d, binary_pupil, merge_prop, split_prop
