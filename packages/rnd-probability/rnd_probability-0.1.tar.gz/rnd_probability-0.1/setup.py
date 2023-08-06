c8e6e28b500d517d5f5d43ac5c4e184a89111143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:42 +0000

    Add package rnd-probability/0.1/rnd_probability-0.1.tar.gz

---
+++ packages/rnd-probability/rnd_probability-0.1.tar.gz/rnd_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rnd_probability',
+      version='0.1',
+      description='Gaussian distributions and Binomial distribution',
+      packages=['rnd_probability'],
+      zip_safe=False)
