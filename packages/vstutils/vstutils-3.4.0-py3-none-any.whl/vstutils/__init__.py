81ee0656976783157a2cb5f793d7624ab90a812c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package vstutils/3.4.0/vstutils-3.4.0-py3-none-any.whl

---
+++ packages/vstutils/vstutils-3.4.0-py3-none-any.whl/vstutils/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=django-not-available
+__version__: str = '3.4.0'
---
+++ packages/vstutils/vstutils-3.4.0-py3-none-any.whl/vstutils/models.pyi
@@ -0,0 +1,50 @@
+import typing as _t
+from django.db import models
+from .utils import Paginator
+
+
+class ObjectDoesNotExist(Exception):
+    """The requested object does not exist"""
+    silent_variable_failure = True
+
+
+class BQuerySet(models.QuerySet):
+    use_for_related_fields: _t.ClassVar[bool] = True
+    custom_iterable_class: _t.ClassVar[_t.Any]
+
+    def paged(self, *args, **kwargs) -> _t.Iterable:
+        ...
+
+    def get_paginator(self, *args, **kwargs) -> Paginator:
+        ...
+
+    def cleared(self) -> BQuerySet:
+        ...
+
+    def _find(self, field_name, tp_name, *args, **kwargs) -> BQuerySet:
+        ...
+
+    @classmethod
+    def as_manager(cls) -> models.Manager:
+        ...
+
+
+class BaseManager(models.Manager):
+    ...
+
+
+class Manager(BaseManager):
+    ...
+
+
+class BaseModel(models.Model):
+    objects: Manager
+    DoesNotExist: ObjectDoesNotExist
+
+    def __unicode__(self) -> _t.Text:
+        ...
+
+
+class BModel(BaseModel):
+    id: _t.Union[_t.SupportsInt, models.AutoField]
+    hidden: _t.Union[bool, models.BooleanField]
