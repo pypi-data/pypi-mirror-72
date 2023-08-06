4f1ed92d6cc8143abd8a1d5b901662edfb3db50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:20 +0000

    Add package djangorestframework-simplejwt-casng/0.0.2/djangorestframework_simplejwt_casng-0.0.2.tar.gz

---
+++ packages/djangorestframework-simplejwt-casng/djangorestframework_simplejwt_casng-0.0.2.tar.gz/djangorestframework_simplejwt_casng-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="djangorestframework_simplejwt_casng",
+    version="0.0.2",
+    author="Changsheng Yan",
+    author_email="cs.yan@outlook.com",
+    description="A JSON Web Token authentication plugin for the Django REST Framework, "
+                "integrated with CAS Authentication.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChangshengYan/djangorestframework-simplejwt-casng",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'django>=3.0',
+        'django-cas-ng',
+        'djangorestframework',
+        'djangorestframework-simplejwt',
+    ]
+)
