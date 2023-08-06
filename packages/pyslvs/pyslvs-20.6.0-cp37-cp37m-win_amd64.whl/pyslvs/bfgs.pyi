50827b008bbe488515211c863f513131f1659e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package pyslvs/20.6.0/pyslvs-20.6.0-cp37-cp37m-win_amd64.whl

---
+++ packages/pyslvs/pyslvs-20.6.0-cp37-cp37m-win_amd64.whl/pyslvs/bfgs.pyi
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from typing import Tuple, List, Sequence, FrozenSet, Dict, Optional, Union
+from .expression import VPoint, Coordinate
+
+_Coord = Tuple[float, float]
+_Inputs = Dict[Tuple[int, int], float]
+
+class SolverSystem:
+
+    def __init__(
+        self,
+        vpoints: Sequence[VPoint],
+        inputs: Optional[Dict[Tuple[int, int], float]] = None,
+        data_dict: Optional[Dict[Union[int, Tuple[int, int]], Union[Coordinate, float]]] = None
+    ):
+        """The expression `vpoints` solver function of BFGS method by
+        giving the input pairs `inputs` and link length `data_dict` requirements.
+
+        !!! note
+            The format of input pairs:
+
+            + Revolut joints: `{(base, driver): angle}`
+            + Slider joints: `{(base, base): offset}`
+
+        The format of `data_dict`:
+
+        + Specific coordinates: Dict\[int, [Coordinate]]
+        + Specific link length: Dict\[Tuple[int, int], float]
+
+        The `data_dict` parameter will reformat its keys into `frozenset` type.
+        """
+        ...
+
+    def same_points(self, vpoints_: Sequence[VPoint]) -> bool:
+        ...
+
+    def show_inputs(self) -> FrozenSet[Tuple[int, int]]:
+        ...
+
+    def show_data(self) -> FrozenSet[Union[int, Tuple[int, int]]]:
+        ...
+
+    def set_inputs(self, inputs: Dict[Tuple[int, int], float]) -> None:
+        ...
+
+    def set_data(self, data_dict: Union[_Inputs, Dict[int, Coordinate]]) -> None:
+        ...
+
+    def solve(self) -> List[Union[_Coord, Tuple[_Coord, _Coord]]]:
+        ...
---
+++ packages/pyslvs/pyslvs-20.6.0-cp37-cp37m-win_amd64.whl/pyslvs/triangulation.pxd
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# cython: language_level=3
+
+"""Triangular expressions.
+
+author: Yuan Chang
+copyright: Copyright (C) 2016-2020
+license: AGPL
+email: pyslvs@gmail.com
+"""
+
+from libcpp.vector cimport vector
+from libcpp.pair cimport pair
+
+# Label, int
+ctypedef pair[int, int] sym
+
+cdef str symbol_str(sym p)
+cpdef EStack t_config(object vpoints_, object inputs, object status=*)
+
+cdef enum Label:
+    P_LABEL
+    L_LABEL
+    I_LABEL
+    A_LABEL
+    S_LABEL
+
+cdef enum Func:
+    PXY
+    PLA
+    PLAP
+    PLLP
+    PLPP
+    PALP
+
+cdef struct Expr:
+    bint op
+    Func func
+    sym v1
+    sym v2
+    sym c1
+    sym c2
+    sym c3
+    sym target
+
+cdef class EStack:
+
+    cdef vector[Expr] stack
+
+    cdef void add_pxy(self, sym c1, sym v1, sym v2, sym t) nogil
+    cdef void add_pla(self, sym c1, sym v1, sym v2, sym t) nogil
+    cdef void add_plap(self, sym c1, sym v1, sym v2, sym c2, sym t) nogil
+    cdef void add_pllp(self, sym c1, sym v1, sym v2, sym c2, sym t) nogil
+    cdef void add_plpp(self, sym c1, sym v1, sym c2, sym c3, sym t, bint op) nogil
+    cdef void add_palp(self, sym c1, sym v1, sym v2, sym c2, sym t, bint op) nogil
+    cpdef list as_list(self)
