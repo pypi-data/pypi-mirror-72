86f3293367da64226cec3e8ea0f38422c6fa7985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:29 +0000

    Add package awsml-probability/0.2/awsml_probability-0.2.tar.gz

---
+++ packages/awsml-probability/awsml_probability-0.2.tar.gz/awsml_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='awsml_probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['awsml_probability'],
+      author = 'Jung Soon Noh',
+      author_email = 'gracewm892@gmail.com',
+      zip_safe=False)
