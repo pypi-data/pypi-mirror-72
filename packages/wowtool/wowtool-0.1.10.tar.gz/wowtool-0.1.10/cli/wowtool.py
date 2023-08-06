300fa6c2922922f07d3d06fb41917138131de625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package wowtool/0.1.10/wowtool-0.1.10.tar.gz

---
+++ packages/wowtool/wowtool-0.1.10.tar.gz/wowtool-0.1.10/cli/wowtool.py
@@ -0,0 +1,30 @@
+import logging
+from logging import Logger
+import click
+from cli.__version__ import VERSION
+from cli.commands.livestream import live
+from cli.commands.transcoder import trans
+from cli.commands.configure import configure
+from cli.commands.resource import resource 
+from cli.commands.recording import recording
+
+
+@click.group()
+@click.version_option(version=VERSION)
+def cli():
+    pass
+
+cli.add_command(live)
+cli.add_command(configure)
+cli.add_command(trans)
+cli.add_command(resource)
+cli.add_command(recording)
+
+
+# Logger setup
+loglevel = 'ERROR'
+logger = Logger('')
+logger.setLevel(loglevel)
+ch = logging.StreamHandler()
+ch.setLevel(loglevel)
+logger.addHandler(ch)
