b704b9337241679c78289475433d8a21df1cd83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package zwack/2.2.0/zwack-2.2.0-py3-none-any.whl

---
+++ packages/zwack/zwack-2.2.0-py3-none-any.whl/zwack/__init__.py
@@ -0,0 +1,18 @@
+from .ascii import *
+from .praise import *
+import random
+async def exist():
+  while 1:
+    if(random.randint(0,99999999)==random.randint(0,99999999)):
+      break
+class Zwack:
+  class ascii():
+    @property
+    def zwack(self):
+      return(zwack_ascii)
+    @property
+    def overflow(self):
+      return(zwack_overflow)
+    @property
+    def overflow_small(self):
+      return(zwack_overflow_small)
---
+++ packages/zwack/zwack-2.2.0-py3-none-any.whl/zwack/ascii.py
@@ -0,0 +1,57 @@
+zwack_ascii = """                                          
+               `!riVI3PP3Kzu]r!`          
+           -|Ug#@@@@@@@@@@@@@@@#0z^`      
+        `vO#@@@@@@@@@@@@@@@@@@@@@@@BV-    
+      .cB@@@@@@@@@@@@@@@@@@@@@@@@@@@@#Y`  
+     (B@@@@B0$#@@@@@@@@@@@@@@@@@@@@@@@@b. 
+   'P@@@@@z^~~|#@@@@@@@@QQ#@@@@@@@@@@@@@d`
+  -E@@@@@Q<;~;v#@@@@@@8|=~*D@@@@@@@@@@@@@}
+ `6@@@@@@@0owM#@@@@@@@w~~~~K@@@@@@@@@@@@@g
+ z@@@@@@@@@@@@@@@@@@@@BVv]a#@@@@@@@@@@@@@#
+=#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
+H@@@@@@@@@UrvTwKMO0QB##@@@@@@@@@@@@@@@@@@E
+Q@@@@@@@@@@#BgRMazV}x\(j@@@@@@@@@@@@@@@@@x
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@L
+B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Z
+\#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B<
+ -T0@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#O]' 
+    -rVMg#@@@@@@@@@@@@@@@@@@@@@@#$Gl^.    
+         `_=rv}VjeaPPP3aUkcYv*!-          
+                                           """
+
+zwack_overflow_small = """               `_:!!!!"-        
+            .!rxTTTkd5c}(!.     
+          `<iTTTcV}MB$cTuli!`   
+          .xuullkQk}uTTTlllT^`  
+           !YTTTTy8PTcB@3lul}=  
+           `!YllTTcdVTksullTT~  
+         `'..!xTllluuluuuull\.  
+       -*mw}i(^*}luuullTluL^.   
+      !LysX3PTT!-<xTTlTx~_`     
+      "|TlkVTTv-  `_!~!'        
+  `    `=r)rr*-``  ``           
+ "*! `!luzL^`-vi}r_~*_          
+ "r= _icyc}}"rTcc}^;*,          
+ "r! `~rr*~,`.:!!:.~*,          
+ "r*====~~====~===~*r,          
+ `---__-_--________--`          """
+zwack_overflow = '''                                                                                                                                                     
+                       `._::::,-`                                                                                                                    
+                    ':*vLYYT3GuLx^_`                                                                                                                 
+                   ~xYYYYuYw$0wYY}Lr-                                                                                                                
+                   \YY}Y}O3LLYYLYYY}x"                                                                                                               
+                   !LYYYL}RHYL3Qg}YYYx                                                                                                               
+                   `~LY}YY}MzLy3zYYYYx`                                                                                                              
+                  `'`,|L}Y}YYY}YL}YYL~                                                                                                               
+               .~ljiv*=<xY}}YYYYYYL\:`                                                                           !mE$^x$m`                           
+              ,x}kVksLi:.=vYYY}L)=_`                                   k~          `                `           `D@d<.w@$`     `                     
+              "vYYVTY}v-  `_=^^_      ":!!!'.-   __   ,' :r)!`  `=vv!  3*  `=` `x9B#Q5^>8Q<  xBI-TDBBQa! vB8ZQB0"8@B0<w@$` `YEB#QG>VQZ` !gQ; `KQI`   
+               `_<r)v)-               ^**u9::G` -OM' -q-,)::}h`'3x""v; K* !c!  V@Q*:u@#*k@$."BB!G@d!_Y##!Y@@V!^:`8@m,`w@$` k@Q*:V@#;#@x`b@@0'*@#~    
+         _~"  -)\Lv!``~**^__~:          rT.  }Y y\iT Vv .?xxLg,^G      sVI3    Z@H  -B@}'E@jM@\_B@QddOgQ)Y@#_   `8@V  w@$` R@h  _#@,u@gy@GI@o9@z     
+         ,*! .xVuy}i-^cuyY|!*!        :c!    `mjI `UIh  M^  .$::d.  `_ IV`vc'  Y@BL*U@#= _Q@@R `K@$(~rc^ Y@B_   `8@V  u@Bv:c@Bx*K@#``d@@$.`O@@0'     
+         ,*! `^?\)*:`-====_,*!        G}vvv!  !Z:  :M:  rVxxxj! ^cxxT! x~  ^V-  *a0g9k:   ^DO:  `vG08DH)`r$Z-   `K$v  'TZET`rP0g6w:  !EE~  :OD<      
+         ,*;"""::""""""""""=*!                                                                                                                       
+         ',,,,,,,,,,,,,,,,,,,-                                                                                                                       
+                                                                                                                                                     
+                                                                                                                                                     '''
