e20927a3d991e446caf790e4b57e782f6c99a4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package faceMongo/0.0.4/faceMongo-0.0.4.tar.gz

---
+++ packages/faceMongo/faceMongo-0.0.4.tar.gz/faceMongo-0.0.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 15:15:06 2020
+
+@author: teeja
+"""
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="faceMongo", 
+    version="0.0.4",
+    author="Tajudeen Abdulazeez",
+    author_email="toabdula@syr.edu",
+    description="Scrape facebook public page to mongo database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toraaglobal/faceMongo",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'facebook_scraper','pymongo','tqdm',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
