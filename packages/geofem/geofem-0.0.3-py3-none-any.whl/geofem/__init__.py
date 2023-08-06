660cc05c707da222d1b793318e629b041b829db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:33 +0000

    Add package geofem/0.0.3/geofem-0.0.3-py3-none-any.whl

---
+++ packages/geofem/geofem-0.0.3-py3-none-any.whl/geofem/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+import discretize as Mesh
+from discretize import Tests
+
+from . import emg3d
+from . import SimPEG
+
+# Geofem/MiGEO Joint
+
+__version__   = '0.0.3'
+__author__    = 'Isadora'
+__license__   = 'MIT'
