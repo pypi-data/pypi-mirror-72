f8c23f06a1ba71eb2f3ccfc28c1a5870b725c13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:41 +0000

    Add package CC-probability/1.0/CC_probability-1.0.tar.gz

---
+++ packages/CC-probability/CC_probability-1.0.tar.gz/CC_probability-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='CC_probability',
+      version='1.0',
+      description='Gaussian and Binomial Distributions',
+      packages=['CC_probability'],
+      zip_safe=False)
