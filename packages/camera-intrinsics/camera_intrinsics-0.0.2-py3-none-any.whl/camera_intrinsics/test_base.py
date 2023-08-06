5bedd50d364a23ca939be6f5ad1ffb78eb9209e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:42 +0000

    Add package camera-intrinsics/0.0.2/camera_intrinsics-0.0.2-py3-none-any.whl

---
+++ packages/camera-intrinsics/camera_intrinsics-0.0.2-py3-none-any.whl/camera_intrinsics/test_base.py
@@ -0,0 +1,35 @@
+import unittest
+from .base import crop, resize, intrinsics
+from .base import _width, _height, _fx, _fy, _cx, _cy
+
+class TestBase(unittest.TestCase):
+
+    def test_crop(self):
+        intr = intrinsics(128, 96, 128.0, 128.0, 0.5 * 128 -
+                          0.5, 0.5 * 96 - 0.5, half_pixel_centers=False)
+        intr = crop(intr, 80, 50, 10, 20)
+
+        self.assertEqual(_width(intr), 80)
+        self.assertEqual(_height(intr), 50)
+        self.assertEqual(_cx(intr), 0.5 * 128 - 0.5 - 10)
+        self.assertEqual(_cy(intr), 0.5 * 96 - 0.5 - 20)
+
+    def test_resize(self):
+        intr = intrinsics(128, 96, 128.0, 128.0, 0.5 * 128 -
+                          0.5, 0.5 * 96 - 0.5, half_pixel_centers=False)
+
+        w = 80
+        h = 170
+
+        intr = resize(intr, w, h)
+
+        self.assertEqual(_width(intr), w)
+        self.assertEqual(_height(intr), h)
+        self.assertEqual(_fx(intr), 128/128*80)
+        self.assertEqual(_fy(intr), 128/96*170)
+        self.assertEqual(_cx(intr), 0.5 * w - 0.5)
+        self.assertEqual(_cy(intr), 0.5 * h - 0.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
