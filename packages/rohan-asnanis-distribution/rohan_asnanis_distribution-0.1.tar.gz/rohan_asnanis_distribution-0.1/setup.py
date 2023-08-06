e9942bf8d9c7e2b657ade97ff39839c8749007e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:35 +0000

    Add package rohan-asnanis-distribution/0.1/rohan_asnanis_distribution-0.1.tar.gz

---
+++ packages/rohan-asnanis-distribution/rohan_asnanis_distribution-0.1.tar.gz/rohan_asnanis_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rohan_asnanis_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Python Package(PyPi) - rohan_asnanis_distribution'],
+      zip_safe=False)
