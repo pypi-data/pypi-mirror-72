846d45a1b7748a487f7d0fbfd8a1e44baedba7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:55 +0000

    Add package edb-distributions/0.1/edb_distributions-0.1.tar.gz

---
+++ packages/edb-distributions/edb_distributions-0.1.tar.gz/edb_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='edb_distributions',
+      version='0.1',
+      description='Module for defining and inspecting Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
