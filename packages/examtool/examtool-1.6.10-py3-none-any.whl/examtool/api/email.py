f2096c87af6914ceb4b3232468cd8658d1428a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:40 +0000

    Add package examtool/1.6.10/examtool-1.6.10-py3-none-any.whl

---
+++ packages/examtool/examtool-1.6.10-py3-none-any.whl/examtool/api/email.py
@@ -0,0 +1,31 @@
+import os
+
+from examtool.api.server_delegate import server_only
+
+from sendgrid import SendGridAPIClient
+
+
+@server_only
+def send_email(*, exam, data):
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    sg.client.mail.send.post(data)
+
+
+@server_only
+def send_email_batch(*, exam, data_list):
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    for data in data_list:
+        sg.client.mail.send.post(data)
+
+
+@server_only
+def get_api_key(*, exam):
+    return os.environ.get("SENDGRID_API_KEY")
+
+
+def send_email_local(key, data):
+    sg = SendGridAPIClient(key)
+    response = sg.client.mail.send.post(data)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
