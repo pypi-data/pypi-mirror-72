091fbd986987f083f744fc3da7ad609b300ca223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:15 +0000

    Add package pysecurityspy/0.1.0/pysecurityspy-0.1.0.tar.gz

---
+++ packages/pysecurityspy/pysecurityspy-0.1.0.tar.gz/pysecurityspy-0.1.0/pysecurityspy/__init__.py
@@ -0,0 +1,13 @@
+"""Init file for pysecurityspy."""
+from pysecurityspy.server import SecuritySpyServer
+from pysecurityspy.errors import (
+    InvalidCredentials,
+    RequestError,
+    ResultError,
+)
+from pysecurityspy.const import (
+    RECORDING_MODE_ALWAYS,
+    RECORDING_MODE_MOTION,
+    RECORDING_MODE_ACTION,
+    RECORDING_MODE_NEVER,
+)
