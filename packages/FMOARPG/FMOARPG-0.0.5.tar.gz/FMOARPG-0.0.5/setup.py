5322b13d086f7ef69e324d0cceb0bb4e27e71724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:45 +0000

    Add package FMOARPG/0.0.5/FMOARPG-0.0.5.tar.gz

---
+++ packages/FMOARPG/FMOARPG-0.0.5.tar.gz/FMOARPG-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+  'Natural Language :: Portuguese (Brazilian)',
+  'Topic :: Software Development :: Libraries'
+]
+
+setup(
+  name='FMOARPG',
+  version='0.0.5',
+  description='Biblioteca para integrar Forge e Power BI',
+  long_description='Esta biblioteca foi desenvolvida para integrar as APIs da plataforma Forge com o Power BI de maneira direta e simplificada',
+  url='https://github.com/Jpornelas/MAPyForg/tree/master/MAPyForg',
+  author='John Martins',
+  author_email='jpornelas@poli.ufrj.br',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='Forge',
+  packages=find_packages(),
+  install_requires=['']
+)
