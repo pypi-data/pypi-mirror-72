57daafed9d7f121dc2a08440c10be9b843acbbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:36 +0000

    Add package KCE/3.1415926/KCE-3.1415926.tar.gz

---
+++ packages/KCE/KCE-3.1415926.tar.gz/KCE-3.1415926/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = "KCE",
+    version = "3.1415926",
+    license = "KCE",
+    author = "Taebin, Yang",
+    author_email = "ksa.yangtaebin@gmail.com",
+    description = "This is a project of the group Mathcome",
+    long_description = open("README.md", encoding="utf-8").read(),
+    url = "",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ]
+)
