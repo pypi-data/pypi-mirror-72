997114cd1362621381e93e62922d3acf71a37f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package pyprob-distributions/0.1/pyprob_distributions-0.1.tar.gz

---
+++ packages/pyprob-distributions/pyprob_distributions-0.1.tar.gz/pyprob_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pyprob_distributions',
+      version='0.1',
+      description='Gaussian and Binominal distributions',
+      packages=['pyprob_distributions'],
+      author = 'Adithya Yelloju',
+      author_email = 'adithyayelloju@gmail.com',
+      zip_safe=False)
