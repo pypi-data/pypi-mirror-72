7e435e3e97471a9987f5fcc71ee3a114efbfe1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:48 +0000

    Add package dst-gausbin/0.2/dst_gausbin-0.2.tar.gz

---
+++ packages/dst-gausbin/dst_gausbin-0.2.tar.gz/dst_gausbin-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dst_gausbin',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['distributions'],
+      author = 'Philipp Schmalen',
+      author_email = 'philbf@gmx.de',
+      zip_safe=False)
