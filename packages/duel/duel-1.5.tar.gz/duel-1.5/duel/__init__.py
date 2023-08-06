a108b2b6e252d6eda3a361371cd2ab5934d7969e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:06 +0000

    Add package duel/1.5/duel-1.5.tar.gz

---
+++ packages/duel/duel-1.5.tar.gz/duel-1.5/duel/__init__.py
@@ -0,0 +1,16 @@
+
+import duel.camera
+import duel.gameEngine
+import duel.gameFrame
+import duel.mainMenuFrame
+import duel.map
+import duel.mapSelectionList
+import duel.menuButtons
+import duel.pauseButtons
+import duel.player
+import duel.playerSkinsList
+import duel.redForestMap
+import duel.swordHitBoxes
+import due.theMain
+import duel.window
+
