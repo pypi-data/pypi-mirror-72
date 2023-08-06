ce32196a621a463c8722d5f859246ca6b3006e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:37 +0000

    Add package ldis/0.0.1/ldis-0.0.1-py3-none-any.whl

---
+++ packages/ldis/ldis-0.0.1-py3-none-any.whl/ldis/config.py
@@ -0,0 +1,35 @@
+"""Constants and configuration."""
+
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+import secrets
+from typing import Any, Dict, List, Optional, Union
+
+
+class Settings(BaseSettings):
+    SEED: int = 100
+
+    # POSTGRES_SERVER: str
+    # POSTGRES_USER: str
+    # POSTGRES_PASSWORD: str
+    # POSTGRES_DB: str
+    # SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+    SQLALCHEMY_DATABASE_URI: str = "asd.db"
+
+    # @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    # def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+    #     if isinstance(v, str):
+    #         return v
+    #     return PostgresDsn.build(
+    #         scheme="postgresql",
+    #         user=values.get("POSTGRES_USER"),
+    #         password=values.get("POSTGRES_PASSWORD"),
+    #         host=values.get("POSTGRES_SERVER"),
+    #         path=f"/{values.get('POSTGRES_DB') or ''}",
+    #     )
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
