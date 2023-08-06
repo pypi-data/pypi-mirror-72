26c7c4e0ce7b557f3dcdbdc3e59b86444ad3f519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:48 +0000

    Add package menday.distributions/0.1/menday.distributions-0.1.tar.gz

---
+++ packages/menday.distributions/menday.distributions-0.1.tar.gz/menday.distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='menday.distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Stewart Menday',
+      author_email = 'stewart@menday.com.au',
+      zip_safe=False)
