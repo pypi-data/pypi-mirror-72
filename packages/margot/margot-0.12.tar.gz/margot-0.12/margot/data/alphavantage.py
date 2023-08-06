8533613abf587551eba9d8740ae4066c5e3b83a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:10 +0000

    Add package margot/0.12/margot-0.12.tar.gz

---
+++ packages/margot/margot-0.12.tar.gz/margot-0.12/margot/data/alphavantage.py
@@ -0,0 +1,59 @@
+import os
+import logging
+
+from alpha_vantage.timeseries import TimeSeries
+
+from margot.data.columns import BaseColumn, DailyMixin
+
+logger = logging.getLogger(__name__)
+
+
+class DailyAdjusted(BaseColumn, DailyMixin):
+    """A daily time series from AlphaVantage.
+
+    Example::
+
+        from margot import alphavantage as av
+
+        volume = av.DailyAdjusted(time_series='adjusted_close')
+
+    Args:
+        time_series (str): the name of the time-series that will be returned.
+            Can be one of: 'open', 'high', 'low', 'close', 'adjusted_close',
+            'volume', 'dividend_amount' or 'split_coefficient'.
+    """
+
+    def clean(self, df):
+        """
+        Clean the dataframe.
+
+        Alphavantage has odd column names, so we'll fix those.
+        """
+        df = df.rename(mapper={
+            '1. open': 'open',
+            '2. high': 'high',
+            '3. low': 'low',
+            '4. close': 'close',
+            '5. adjusted close': 'adjusted_close',
+            '6. volume': 'volume',
+            '7. dividend amount': 'divident_amount',
+            '8. split coefficient': 'split_coefficient'
+        }, axis='columns')
+        return super().clean(df)
+
+    def fetch(self, symbol: str):
+        """
+        Fetch from remote - this could be the only service specific thing.
+
+        Args:
+            symbol (str): the name of the symbol to fetch
+
+        """
+        logger.info('fetching ({}) from alphavantage'.format(symbol))
+        ts = TimeSeries(
+            key=self.env.get(
+                'ALPHAVANTAGE_API_KEY',
+                os.environ.get('ALPHAVANTAGE_API_KEY')),
+            output_format='pandas')
+        df, _ = ts.get_daily_adjusted(symbol, outputsize='full')
+        return self.clean(df)
