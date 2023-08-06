2bbf3903ed19a6e135d6081c0e173df000e8a046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:24 +0000

    Add package EZlogR/0.2.0.dev3/EZlogR-0.2.0.dev3.tar.gz

---
+++ packages/EZlogR/EZlogR-0.2.0.dev3.tar.gz/EZlogR-0.2.0.dev3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='EZlogR',
+    version='0.2.0.dev3',
+    author='Jeremy Gillespie',
+    author_email='jeremy@kodeslingr.com',
+    url='http://www.ezlogr.com',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    description='This is a simple logging tool to help devs focus on writing code instead of writing logs.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6'
+)
