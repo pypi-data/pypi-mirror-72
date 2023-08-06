ce6af888d09dc8448fc36554431fcadf529321eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:04 +0000

    Add package hinpy/0.1.10/hinpy-0.1.10.tar.gz

---
+++ packages/hinpy/hinpy-0.1.10.tar.gz/hinpy-0.1.10/setup.py
@@ -0,0 +1,31 @@
+"""`setup.py`"""
+from setuptools import setup, find_packages
+
+# # Package requirements
+# with open('requirements.txt') as f:
+#     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+INSTALL_REQUIRES=[
+'numpy',
+'scikit-learn',
+'pandas',
+'surprise',
+'scipy',
+'scikit-surprise',
+'networkx',
+'matplotlib'
+]
+
+setup(name='hinpy',
+      version='0.1.10',
+      description='A python framework for Heterogeneous Information Networks.',
+      author='Pedro Ramaciotti Morales',
+      author_email='pedro.ramaciotti@gmail.com',
+      url = 'https://github.com/pedroramaciotti/HINPy',
+      download_url = 'https://github.com/pedroramaciotti/HINPy/archive/0.1.9.tar.gz',
+      keywords = ['Heterogeneous Information Networks','Recommender Systems','Diversity','Echo Chamber','Filter Bubble'],
+      packages=find_packages(),
+      #license='apache-2.0',
+      #classifiers=["License :: OSI Approved :: Apache License, Version 2.0 (Apache-2.0)"],
+      data_files=[('', ['LICENSE'])],
+      install_requires=INSTALL_REQUIRES)
