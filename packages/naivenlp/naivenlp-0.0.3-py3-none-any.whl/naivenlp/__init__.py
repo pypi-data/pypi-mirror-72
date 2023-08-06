0095c528c6774c71da6835ea3a9b7dc3ae583e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:22 +0000

    Add package naivenlp/0.0.3/naivenlp-0.0.3-py3-none-any.whl

---
+++ packages/naivenlp/naivenlp-0.0.3-py3-none-any.whl/naivenlp/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+from .tokenizers.abstract_tokenizer import AbstractTokenizer, VocabBasedTokenizer
+from .tokenizers.jieba_tokenizer import ACCURATE_MODE, FULL_MODE, SEARCH_MODE, JiebaTokenizer
+from .tokenizers.language_model_tokenizer import LanguageModelTokenizer
+
+__version__ = "0.0.3"
+__name__ = "naivenlp"
+
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s",
+    level=logging.INFO
+)
