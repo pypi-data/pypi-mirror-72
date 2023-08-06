e370325ea26c246ebde9d0d7974f7160f7238f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:09 +0000

    Add package google-speech-addons/1.0.0/google_speech_addons-1.0.0-py3-none-any.whl

---
+++ packages/google-speech-addons/google_speech_addons-1.0.0-py3-none-any.whl/speech_addons/test_synch_addons.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import pytest
+from synth_addons import sentences_splitter
+
+
+class TestSplit:
+    def test_dot(self):
+        assert sentences_splitter("abc") == ['abc']
+        assert sentences_splitter("abc.") == ['abc.']
+        assert sentences_splitter("abc", 2) == ['ab', 'c']
+        assert sentences_splitter("a. bc", 3) == ['a.', 'bc']
+        assert sentences_splitter("a. bc", 2) == ['a.', 'bc']
+        assert sentences_splitter("a. bcd", 2) == ['a.', 'bc', 'd']
+        assert sentences_splitter("a hj. bc d.", 6) == ['a hj.', 'bc d.']
+        assert sentences_splitter("c. ab... d", 7) == ['c.', 'ab... d']
+        assert sentences_splitter("c. ab... d", 8) == ['c.', 'ab... d']
