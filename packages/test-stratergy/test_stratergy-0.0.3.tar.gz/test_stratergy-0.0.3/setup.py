250e093bc37b3a140e928cc022ebceff5461a078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:07 +0000

    Add package test-stratergy/0.0.3/test_stratergy-0.0.3.tar.gz

---
+++ packages/test-stratergy/test_stratergy-0.0.3.tar.gz/test_stratergy-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test_stratergy", # Replace with your own username
+    version="0.0.3",
+    author="Harshit Agarwal",
+    author_email="9arshit@gmail.com",
+    description="This package is used to test you trading stragey integrated with deep learning models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/9harshit/trading_stratergy_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+
+    
+)
+
+
