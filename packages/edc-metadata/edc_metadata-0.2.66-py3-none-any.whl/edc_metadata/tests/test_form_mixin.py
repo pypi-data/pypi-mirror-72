105635b5e2af641da1d601aa02d9cfcb8866b336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:00 +0000

    Add package edc-metadata/0.2.66/edc_metadata-0.2.66-py3-none-any.whl

---
+++ packages/edc-metadata/edc_metadata-0.2.66-py3-none-any.whl/edc_metadata/tests/test_form_mixin.py
@@ -0,0 +1,31 @@
+from .test_view_mixin import TestViewMixin, MyView
+from django.test.client import RequestFactory
+from django.test.utils import tag
+from pprint import pprint
+from edc_metadata.form_validators.metadata_form_validator_mixin import (
+    MetaDataFormValidatorMixin,
+)
+from edc_form_validators.form_validator import FormValidator
+
+
+class MyForm(MetaDataFormValidatorMixin, FormValidator):
+
+    pass
+
+
+class TestForm(TestViewMixin):
+    def test_(self):
+        request = RequestFactory().get("/?f=f&e=e&o=o&q=q")
+        request.user = self.user
+        view = MyView(request=request)
+        view.appointment = self.appointment
+        view.subject_identifier = self.subject_identifier
+        view.kwargs = {}
+        context_data = view.get_context_data()
+        self.assertEqual(len(context_data.get("crfs")), 5)
+        form = MyForm(cleaned_data={}, instance=view.appointment)
+        self.assertTrue(form.crf_metadata_exists)
+        self.assertTrue(form.crf_metadata_required_exists)
+        self.assertTrue(form.requisition_metadata_exists)
+        self.assertTrue(form.requisition_metadata_required_exists)
+        form.validate()
