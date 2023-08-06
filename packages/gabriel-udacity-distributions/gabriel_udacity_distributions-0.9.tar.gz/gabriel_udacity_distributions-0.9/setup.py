f758c7c44bb8882303fb58952e6711efa90603e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:17 +0000

    Add package gabriel-udacity-distributions/0.9/gabriel_udacity_distributions-0.9.tar.gz

---
+++ packages/gabriel-udacity-distributions/gabriel_udacity_distributions-0.9.tar.gz/gabriel_udacity_distributions-0.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabriel_udacity_distributions',
+      version='0.9',
+      description='Gaussian and Binomial distributions',
+      packages=['gabriel_udacity_distributions'],
+      author='Gabriel Francis',
+      author_email='gncis8@gmail.com',
+      zip_safe=False)
