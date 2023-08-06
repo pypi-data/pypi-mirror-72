d43462f3395b6f9991e2365823e5195d9159acef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:21 +0000

    Add package Hazem1/0.1/Hazem1-0.1.tar.gz

---
+++ packages/Hazem1/Hazem1-0.1.tar.gz/Hazem1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Hazem1',
+      version='0.1',
+      description='Gaussian distributions and Bionomial ',
+      packages=['Hazem1'],
+      author='Hazem Nuseir',
+      author_email='hazem.nuseir@gmail.com',
+      zip_safe=False)
+
