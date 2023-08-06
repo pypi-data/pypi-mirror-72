afacc6c6b422cb9e9fd46867399f41d2f124176f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:15 +0000

    Add package django-activity-stream-action-expanded/0.9.2a1/django-activity-stream-action-expanded-0.9.2a1.tar.gz

---
+++ packages/django-activity-stream-action-expanded/django-activity-stream-action-expanded-0.9.2a1.tar.gz/django-activity-stream-action-expanded-0.9.2a1/actstream/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from actstream.signals import action
+except:
+    pass
+
+__version__ = '0.9.2a1'
+__author__ = 'Asif Saif Uddin, Justin Quick <justquick@gmail.com>'
+default_app_config = 'actstream.apps.ActstreamConfig'
