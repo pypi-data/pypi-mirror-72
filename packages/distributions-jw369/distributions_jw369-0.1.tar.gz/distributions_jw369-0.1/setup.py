3ccc18f6619ba66fe904ac9a38539a23760baf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:30 +0000

    Add package distributions-jw369/0.1/distributions_jw369-0.1.tar.gz

---
+++ packages/distributions-jw369/distributions_jw369-0.1.tar.gz/distributions_jw369-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jw369',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_jw369'],
+      zip_safe=False)
