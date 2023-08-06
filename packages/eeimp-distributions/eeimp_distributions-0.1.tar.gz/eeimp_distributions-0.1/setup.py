450331420a0845cbb7b2b30f03f92ea28f4863f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:02 +0000

    Add package eeimp-distributions/0.1/eeimp_distributions-0.1.tar.gz

---
+++ packages/eeimp-distributions/eeimp_distributions-0.1.tar.gz/eeimp_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eeimp_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['eeimp_distributions'],
+      zip_safe=False)
