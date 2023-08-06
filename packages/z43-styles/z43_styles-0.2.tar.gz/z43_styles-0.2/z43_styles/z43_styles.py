046bdab593f9268b09db31572e89d13b9bb6f247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:44 +0000

    Add package z43-styles/0.2/z43_styles-0.2.tar.gz

---
+++ packages/z43-styles/z43_styles-0.2.tar.gz/z43_styles-0.2/z43_styles/z43_styles.py
@@ -0,0 +1,83 @@
+import plotly.graph_objs as go
+import plotly.io as pio
+
+pio.templates["z43_base"] = go.layout.Template(
+    layout=dict(
+        font=dict(
+            size=14,
+            family="Roboto"
+        ),
+        title=dict(
+            x=0.5,
+            font=dict(
+                size=22,
+                family="Roboto"
+            ),
+        ),
+        xaxis=dict(
+            title=dict(
+                font=dict(
+                    size=16,
+                    family="Roboto"
+                )
+            ),
+            ticks="outside"
+        ),
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    size=16,
+                    family="Roboto"
+                )
+            ),
+            ticks="outside"
+        ),
+        images=[dict(
+            x=1,
+            y=0,
+            sizex=0.1,
+            sizey=0.1,
+            source="https://github.com/odeimaiz/z43_plot_styles/blob/master/imgs/z43_logo.png?raw=true",
+            opacity=0.5,
+            visible=True
+        )]
+    )
+)
+
+pio.templates["z43_dark"] = go.layout.Template(
+    layout=dict(
+        font=dict(
+            color="#FFFFFF"
+        ),
+        plot_bgcolor="#202020",
+        paper_bgcolor="#202020",
+        xaxis=dict(
+            title=dict(
+                font=dict(
+                    color="#BFBFBF"
+                )
+            ),
+            tickfont=dict(
+                color="#BFBFBF"
+            ),
+            tickcolor="#BFBFBF",
+            gridcolor="#373737",
+            linecolor="#373737",
+            zerolinecolor="#373737"
+        ),
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    color="#BFBFBF"
+                )
+            ),
+            tickfont=dict(
+                color="#BFBFBF"
+            ),
+            tickcolor="#BFBFBF",
+            gridcolor="#373737",
+            linecolor="#373737",
+            zerolinecolor="#373737"
+        )
+    )
+)
