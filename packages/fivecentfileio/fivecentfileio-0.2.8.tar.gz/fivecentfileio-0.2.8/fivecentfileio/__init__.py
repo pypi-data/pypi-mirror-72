391285660f606dbe0868ff6539805cf5f800aadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:30 +0000

    Add package fivecentfileio/0.2.8/fivecentfileio-0.2.8.tar.gz

---
+++ packages/fivecentfileio/fivecentfileio-0.2.8.tar.gz/fivecentfileio-0.2.8/fivecentfileio/__init__.py
@@ -0,0 +1,10 @@
+__author__    = 'Steve Nicholes'
+__copyright__ = 'Copyright (C) 2017 Steve Nicholes'
+__license__   = 'GPLv3'
+__version__   = '0.2.8'
+__url__       = 'https://github.com/endangeredoxen/fileio'
+
+from . config import ConfigFile
+from . html import Dir2HTML
+from . reader import FileReader
+from . utilities import *
