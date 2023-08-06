4c39496adeb3adf07158cbbd56da7d29ab01ca7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:27 +0000

    Add package holi-distributions/0.1/holi-distributions-0.1.tar.gz

---
+++ packages/holi-distributions/holi-distributions-0.1.tar.gz/holi-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'holi-distributions',
+      version = '0.1',
+      description = 'Gaussian distributions',
+      packages = ['holi-distributions'],
+      author = 'Holiser Ogah',
+      author_email = 'holisterogah@gmail.com',
+      zip_safe=False)
