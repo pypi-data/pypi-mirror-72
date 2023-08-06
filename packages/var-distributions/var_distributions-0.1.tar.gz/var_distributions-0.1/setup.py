46df562043b076fc5ea6d33943cad59179a1fbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package var-distributions/0.1/var_distributions-0.1.tar.gz

---
+++ packages/var-distributions/var_distributions-0.1.tar.gz/var_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='var_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['var_distributions'],
+      zip_safe=False)
