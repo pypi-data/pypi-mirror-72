303857696ff6d5208d78c8e6c0381235ca7ea997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:50 +0000

    Add package OBRequests/0.1.0/OBRequests-0.1.0-py3-none-any.whl

---
+++ packages/OBRequests/OBRequests-0.1.0-py3-none-any.whl/OBRequests/__init__.py
@@ -0,0 +1,11 @@
+from .request import Request
+from .methods import Methods
+from .resp_function import RespFunction
+
+
+__version__ = "0.1.0"
+
+
+assert Request
+assert Methods
+assert RespFunction
