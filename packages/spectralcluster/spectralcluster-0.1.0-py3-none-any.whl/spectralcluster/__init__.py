0d63e9eee88c3c811c5a49320a554078b9940558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:29 +0000

    Add package spectralcluster/0.1.0/spectralcluster-0.1.0-py3-none-any.whl

---
+++ packages/spectralcluster/spectralcluster-0.1.0-py3-none-any.whl/spectralcluster/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from . import spectral_clusterer
+
+SpectralClusterer = spectral_clusterer.SpectralClusterer
+DEFAULT_REFINEMENT_SEQUENCE = spectral_clusterer.DEFAULT_REFINEMENT_SEQUENCE
