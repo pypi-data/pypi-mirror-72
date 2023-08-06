ae8f765ae3216dd82d5fb574a9bb4e0252611123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package first-distributions/0.2/first_distributions-0.2.tar.gz

---
+++ packages/first-distributions/first_distributions-0.2.tar.gz/first_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='first_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['first_distributions'],
+      zip_safe=False)
