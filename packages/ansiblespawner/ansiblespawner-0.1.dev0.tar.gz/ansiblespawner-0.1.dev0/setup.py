f9bd1b313b2aac1125130a58bde9f058c160f0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:13 +0000

    Add package ansiblespawner/0.1.dev0/ansiblespawner-0.1.dev0.tar.gz

---
+++ packages/ansiblespawner/ansiblespawner-0.1.dev0.tar.gz/ansiblespawner-0.1.dev0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name="ansiblespawner",
+    packages=["ansiblespawner"],
+    description="Spawn JupyterHub single user notebook servers using Ansible",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    use_scm_version={"write_to": "ansiblespawner/_version.py"},
+    setup_requires=["setuptools_scm"],
+    author="Simon Li",
+    url="https://github.com/manics/jupyterhub-ansiblespawner",
+    license="BSD-3-Clause",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
