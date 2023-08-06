084d3ae5fc94daa9baa55c25cae28c50f641b6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:50 +0000

    Add package dnn/0.6/dnn-0.6-py3-none-any.whl

---
+++ packages/dnn/dnn-0.6-py3-none-any.whl/dnn/__init__.py
@@ -0,0 +1,9 @@
+from .dnn import DNN
+from .multidnn import MultiDNN
+
+__version__ = "0.6"
+
+def subprocess (train_func, *args):
+    from multiprocessing import Pool
+    with Pool(1) as p:
+        return p.apply (train_func, args)
