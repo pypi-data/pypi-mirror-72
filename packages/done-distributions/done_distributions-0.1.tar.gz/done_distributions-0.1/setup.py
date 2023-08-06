f0a346dad78f54113854ff1e9e8e714183c19400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:26 +0000

    Add package done-distributions/0.1/done_distributions-0.1.tar.gz

---
+++ packages/done-distributions/done_distributions-0.1.tar.gz/done_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='done_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['done_distributions'],
+      zip_safe=False)
