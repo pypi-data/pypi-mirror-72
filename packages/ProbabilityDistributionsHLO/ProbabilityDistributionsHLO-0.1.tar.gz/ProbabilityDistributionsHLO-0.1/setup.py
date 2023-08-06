134bac1d44be4c56740b0232b0f525f985b668a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:42 +0000

    Add package ProbabilityDistributionsHLO/0.1/ProbabilityDistributionsHLO-0.1.tar.gz

---
+++ packages/ProbabilityDistributionsHLO/ProbabilityDistributionsHLO-0.1.tar.gz/ProbabilityDistributionsHLO-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ProbabilityDistributionsHLO',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ProbabilityDistributionsHLO'],
+      zip_safe=False)
