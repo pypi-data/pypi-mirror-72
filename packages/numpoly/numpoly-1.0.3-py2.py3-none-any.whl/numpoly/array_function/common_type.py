d5cb0fc1d5194619a62a6d9b8473bac8de278389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package numpoly/1.0.3/numpoly-1.0.3-py2.py3-none-any.whl

---
+++ packages/numpoly/numpoly-1.0.3-py2.py3-none-any.whl/numpoly/array_function/common_type.py
@@ -0,0 +1,43 @@
+"""Return a scalar type which is common to the input arrays."""
+import numpy
+import numpoly
+
+from ..dispatch import implements
+
+
+@implements(numpy.common_type)
+def common_type(*arrays):
+    """
+    Return a scalar type which is common to the input arrays.
+
+    The return type will always be an inexact (i.e. floating point) scalar
+    type, even if all the arrays are integer arrays. If one of the inputs is an
+    integer array, the minimum precision type that is returned is a 64-bit
+    floating point dtype.
+
+    All input arrays except int64 and uint64 can be safely cast to the
+    returned dtype without loss of information.
+
+    Args:
+        arrays (numpoly.ndpoly):
+            Input arrays.
+
+    Return:
+        out (numpy.generic):
+            Data type code.
+
+    Examples:
+        >>> numpoly.common_type(
+        ...     numpy.array(2, dtype=numpy.float32)).__name__
+        'float32'
+        >>> numpoly.common_type(
+        ...     numpoly.variable()).__name__
+        'float64'
+        >>> numpoly.common_type(
+        ...     numpy.arange(3), 1j*numpoly.variable(), 45).__name__
+        'complex128'
+
+    """
+    arrays = [numpoly.aspolynomial(array) for array in arrays]
+    arrays = [array[array.keys[0]] for array in arrays]
+    return numpy.common_type(*arrays)
