3e4bdaff58a7538782b6951f333e57ef7542d5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:16 +0000

    Add package testwizard.core/3.2.1/testwizard.core-3.2.1-py3-none-any.whl

---
+++ packages/testwizard.core/testwizard.core-3.2.1-py3-none-any.whl/testwizard/core/models/CreateTestRunRequest.py
@@ -0,0 +1,32 @@
+class CreateTestRunRequest:
+    def __init__(self, scriptFilePath, metadata):
+        self.__scriptFilePath = scriptFilePath
+        self.__tester = metadata.get("tester")
+        self.__parameters = metadata.get("parameters")
+        self.__resources = metadata.get("resources", {})
+        self.__customProperties = metadata.get("customProperties")
+        self.__outputFolder = metadata.get("outputFolder")
+
+    @property
+    def scriptFilePath(self):
+        return self.__scriptFilePath
+
+    @property
+    def tester(self):
+        return self.__tester
+
+    @property
+    def parameters(self):
+        return self.__parameters
+
+    @property
+    def resources(self):
+        return self.__resources
+
+    @property
+    def customProperties(self):
+        return self.__customProperties
+
+    @property
+    def outputFolder(self):
+        return self.__outputFolder
