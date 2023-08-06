2f95afbc2a854d96ae303d8e10fa7a723d71918f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:53 +0000

    Add package ftw.billboard/1.6.0/ftw.billboard-1.6.0.tar.gz

---
+++ packages/ftw.billboard/ftw.billboard-1.6.0.tar.gz/ftw.billboard-1.6.0/ftw/billboard/hooks.py
@@ -0,0 +1,44 @@
+from Products.CMFCore.utils import getToolByName
+import logging
+
+logger = logging.getLogger('ftw.billboard')
+
+INDEXES = (
+    ('get_ad_expiration_date', 'DateIndex'),
+)
+
+
+def imported(site):
+    add_catalog_indexes(site, INDEXES)
+
+
+def add_catalog_indexes(portal, INDEXES):
+    """Method to add our wanted indexes to the portal_catalog.
+    """
+
+    catalog = getToolByName(portal, 'portal_catalog')
+    indexes = catalog.indexes()
+    indexables = []
+
+    for name, meta_type in INDEXES:
+        if name not in indexes:
+            catalog.addIndex(name, meta_type)
+            indexables.append(name)
+            logger.info("Added %s for field %s.", meta_type, name)
+
+    if len(indexables) > 0:
+        logger.info("Indexing new indexes %s.", ', '.join(indexables))
+        catalog.manage_reindexIndex(ids=indexables)
+
+
+def uninstalled(portal):
+    remove_catalog_indexes(portal)
+
+
+def remove_catalog_indexes(portal):
+    catalog = portal.portal_catalog
+    indexes = catalog.indexes()
+
+    for name, meta_type in INDEXES:
+        if name in indexes:
+            catalog.delIndex(name)
---
+++ packages/ftw.billboard/ftw.billboard-1.6.0.tar.gz/ftw.billboard-1.6.0/setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+import os
+
+
+version = '1.6.0'
+maintainer = 'Julian Infanger'
+
+
+tests_require = [
+    'zope.testing',
+    'plone.app.testing',
+    'ftw.builder',
+    'ftw.testbrowser',
+    'ftw.testing',
+    'unittest2',
+    ]
+
+
+setup(name='ftw.billboard',
+      version=version,
+      description='Simple market place addon for plone.',
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Zope2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='plone billboard ads advertisement ftw',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.billboard',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'Plone',
+        'Products.CMFCore',
+        'ftw.calendarwidget',
+        'ftw.colorbox',
+        'ftw.lawgiver',
+        'ftw.profilehook',
+        'ftw.upgrade>=1.14.5',
+        'plone.api',
+        'plone.app.registry',
+        'setuptools',
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points='''
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
