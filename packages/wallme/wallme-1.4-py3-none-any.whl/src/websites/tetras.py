c13b0fc7197d84e010dcc6224f2a4a17c26df354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:46 +0000

    Add package wallme/1.4/wallme-1.4-py3-none-any.whl

---
+++ packages/wallme/wallme-1.4-py3-none-any.whl/src/websites/tetras.py
@@ -0,0 +1,13 @@
+KEY = 'tetras'
+TEST_KEY = KEY
+DESCRIPTION = 'Amateur pictures of the Alpes and the Dauphine'
+URL = 'http://tetras.org/Semaine.jpg'
+
+def pre_process():
+	return None
+    
+def process(date):
+	return URL
+
+def post_process(image):
+	return None
