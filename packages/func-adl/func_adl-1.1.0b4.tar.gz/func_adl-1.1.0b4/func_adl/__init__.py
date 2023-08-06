2d1d4be4a3f643c0c8fa8fe9d0296381b81a3768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package func-adl/1.1.0b4/func_adl-1.1.0b4.tar.gz

---
+++ packages/func-adl/func_adl-1.1.0b4.tar.gz/func_adl-1.1.0b4/func_adl/__init__.py
@@ -0,0 +1,12 @@
+# Build a nice single cohesive version of the module with
+# simple exports for the main items.
+
+# flake8: noqa
+# Because of how we pull things in, they are not used below, and so generate errors. We'll
+# just turn off checking here.
+
+# Main streaming object
+from .ObjectStream import ObjectStream, ObjectStreamException  # NOQA
+
+# Dataset accessors
+from .EventDataset import EventDataset, EventDatasetURLException  # NOQA
