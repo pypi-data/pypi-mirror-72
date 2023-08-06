a6cb3ecd405da029598c30c4095404f462361701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:56 +0000

    Add package github-description/2020.7.1/github-description-2020.7.1.tar.gz

---
+++ packages/github-description/github-description-2020.7.1.tar.gz/github-description-2020.7.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='github-description',
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=setuptools.find_packages(),
+    scripts=['scripts/github-description']
+)
