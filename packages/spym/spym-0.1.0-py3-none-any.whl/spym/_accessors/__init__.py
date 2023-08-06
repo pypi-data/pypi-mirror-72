bb0e197b130a56cfb20afec8da569a03d988143e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:42 +0000

    Add package spym/0.1.0/spym-0.1.0-py3-none-any.whl

---
+++ packages/spym/spym-0.1.0-py3-none-any.whl/spym/_accessors/__init__.py
@@ -0,0 +1,46 @@
+import xarray as xr
+import numpy as np
+
+from .filters import SpymFilters
+from .level import SpymLevel
+
+@xr.register_dataarray_accessor("spym")
+class _SpymDataArray:
+    '''spym class extending xarray DataArray
+    '''
+
+    def __init__(self, xarray_dr):
+        self._dr = xarray_dr
+
+        self._bkg = np.zeros(self._dr.data.shape)
+        self._mask = np.zeros(self._dr.data.shape, dtype=bool)
+
+        # Initialize spym classes
+        self.level = SpymLevel(self)
+        self.filters = SpymFilters(self)
+
+        # Expose some useful methods
+        self.fixzero = self.level.fixzero
+        self.plane = self.level.plane
+        self.align = self.level.align
+        self.destripe = self.filters.destripe
+
+    @property
+    def background(self):
+        ''' Background.
+        '''
+        return self._bkg
+
+    @property
+    def mask(self):
+        ''' Mask.
+        '''
+        return self._mask
+
+@xr.register_dataset_accessor("spym")
+class _SpymDataset:
+    '''spym class extending xarray Dataset
+    '''
+
+    def __init__(self, xarray_ds):
+        self._ds = xarray_ds
