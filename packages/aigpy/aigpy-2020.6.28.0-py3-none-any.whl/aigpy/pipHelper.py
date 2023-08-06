4e55bf767bef4b7b291251784304fb5692c6f3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package aigpy/2020.6.28.0/aigpy-2020.6.28.0-py3-none-any.whl

---
+++ packages/aigpy/aigpy-2020.6.28.0-py3-none-any.whl/aigpy/pipHelper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   pipHelper.py
+@Time    :   2019/03/11
+@Author  :   Yaron Huang 
+@Version :   1.0
+@Contact :   yaronhuang@qq.com
+@Desc    :   pip server tool
+'''
+
+import aigpy.netHelper as netHelper
+
+def getInfo(projectName):
+    """Get project information from pypi
+    - Return: json or None                              
+    """
+    url = 'https://pypi.org/pypi/' + projectName + '/json'
+    ret = netHelper.downloadJson(url,None)
+    return ret
+    
+def getLastVersion(projectName):
+    """Get project version from pypi
+    - Return: str or None                              
+    """
+    try:
+        ret = getInfo(projectName)
+        if ret is None:
+            return None
+        return ret['info']['version']
+    except:
+        return None
+
+def getVersionList(projectName):
+    """Get project all versions from pypi
+    - Return: json or None                              
+    """
+    try:
+        ret = getInfo(projectName)
+        if ret is None:
+            return None
+        return ret['releases']
+    except:
+        return None
