38b1922558e51ab60417940c87f0e1f9ae13a716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:04 +0000

    Add package duel/1.0.7/duel-1.0.7.tar.gz

---
+++ packages/duel/duel-1.0.7.tar.gz/duel-1.0.7/duel/__init__.py
@@ -0,0 +1,17 @@
+
+__all__ = ["camera", "gameEngine","gameFrame","mainMenuFrame","map","mapSelectionList","menuButtons","pauseButtons","player","playerSkinsList","redForestMap","swordHitBoxes","theMain","window"]
+
+from duel.camera import *
+from duel.gameEngine import *
+from duel.gameFrame import *
+from duel.mainMenuFrame import *
+from duel.map import *
+from duel.mapSelectionList import *
+from duel.menuButtons import *
+from duel.pauseButtons import *
+from duel.player import *
+from duel.playSkinsList import *
+from duel.redForestMap import *
+from duel.swordHitBoxes import *
+from duel.theMain import *
+from duel.window import *
