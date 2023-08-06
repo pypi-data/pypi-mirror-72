121fdfc879bf7218a5a809e4b2a585c1c6c295b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:56 +0000

    Add package netbox-plugin-test/0.4/netbox-plugin-test-0.4.tar.gz

---
+++ packages/netbox-plugin-test/netbox-plugin-test-0.4.tar.gz/netbox-plugin-test-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='netbox-plugin-test',
+    version='0.4',
+    description='Test',
+    author='Sean Collins',
+    license='Apache 2.0',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+)
+
