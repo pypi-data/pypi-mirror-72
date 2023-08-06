fd9b8eddbdfde8e65b5a2bfea1a956a2290a9373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:53 +0000

    Add package thorsky/1.1/thorsky-1.1.tar.gz

---
+++ packages/thorsky/thorsky-1.1.tar.gz/thorsky-1.1/setup.py
@@ -0,0 +1,19 @@
+#  #!/usr/bin/env python
+  
+# from distutils.core import setup,Extension
+from setuptools import setup
+
+setup(name="thorsky",
+        version="1.1",
+        description="Python based skycalc GUI",
+        url="http://www.dartmouth.edu",
+        author = "John Thorstensen",
+        author_email = "John.Thorstensen@dartmouth.edu",
+        packages=['thorsky'],
+        scripts=['bin/pyskycalc3.py'],
+        include_package_data=True,
+        license = 'BSD 2-Clause License')
+#        scripts = ['pyskycalc3.py'],
+#        py_modules=['thorskyclasses3','thorskyutil'],
+#        data_files=[('/usr/local/share/skycalc',['observatories_rev.dat','cartesian_bright.dat'])])
+
