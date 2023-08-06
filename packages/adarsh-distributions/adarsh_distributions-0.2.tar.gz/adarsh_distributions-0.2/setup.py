723733fb0092366c3d6235bbc9330c816adf3f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:09 +0000

    Add package adarsh-distributions/0.2/adarsh_distributions-0.2.tar.gz

---
+++ packages/adarsh-distributions/adarsh_distributions-0.2.tar.gz/adarsh_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='adarsh_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['adarsh_distributions'],
+      author="Adarsh Ghimire",
+      email="adarsh.ghimire1@gmail.com",
+      zip_safe=False)
