31d1b15a4d2dcb86926eb0503fe7270e2bb1563b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package schema-checker/1.0/schema_checker-1.0-py3-none-any.whl

---
+++ packages/schema-checker/schema_checker-1.0-py3-none-any.whl/schema_checker/extras.py
@@ -0,0 +1,24 @@
+
+from typing import Callable
+import functools
+
+from .jschema import validate
+
+
+def decorator_constructor(getter: Callable, setter: Callable):
+    def validator(schema):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrap(*a, **b):
+                a, b = setter(validate(getter(*a, **b), schema), a, b)
+                return func(*a, **b)
+            return wrap
+        return decorator
+    return validator
+
+
+# ignores positional args
+kw_validator = decorator_constructor(
+    getter=lambda *a, **b: b,
+    setter=lambda data, *a, **b: (a, data),
+)
