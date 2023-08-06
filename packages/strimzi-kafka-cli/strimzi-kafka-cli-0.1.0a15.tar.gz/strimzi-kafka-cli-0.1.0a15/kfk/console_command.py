c6ce4d9824c997057f4c08be06dc71534287475c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package strimzi-kafka-cli/0.1.0a15/strimzi-kafka-cli-0.1.0a15.tar.gz

---
+++ packages/strimzi-kafka-cli/strimzi-kafka-cli-0.1.0a15.tar.gz/strimzi-kafka-cli-0.1.0a15/kfk/console_command.py
@@ -0,0 +1,31 @@
+import click
+import os
+
+from kfk.command import kfk
+from kfk.kubectl_command_builder import Kubectl
+
+
+@click.option('-n', '--namespace', help='Namespace to use', required=True)
+@click.option('-c', '--cluster', help='Cluster to use', required=True)
+@click.option('--topic', help='Topic Name', required=True)
+@click.option('--from-beginning', help='Consumes messages from beginning', is_flag=True)
+@kfk.command()
+def console_consumer(topic, cluster, from_beginning, namespace):
+    """The console consumer is a tool that reads data from Kafka and outputs it to standard output."""
+    native_command = "bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic {topic} {from_beginning}"
+    os.system(
+        Kubectl().exec("-it", "{cluster}-kafka-0").container("kafka").namespace(namespace).exec_command(
+            native_command).build().format(cluster=cluster, topic=topic,
+                                           from_beginning=(from_beginning and '--from-beginning' or '')))
+
+
+@click.option('-n', '--namespace', help='Namespace to use', required=True)
+@click.option('-c', '--cluster', help='Cluster to use', required=True)
+@click.option('--topic', help='Topic Name', required=True)
+@kfk.command()
+def console_producer(topic, cluster, namespace):
+    """The console producer is a tool that reads data from standard input and publish it to Kafka."""
+    native_command = "bin/kafka-console-producer.sh --broker-list localhost:9092 --topic {topic}"
+    os.system(
+        Kubectl().exec("-it", "{cluster}-kafka-0").container("kafka").namespace(namespace).exec_command(
+            native_command).build().format(cluster=cluster, topic=topic))
