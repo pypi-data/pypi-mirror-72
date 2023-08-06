1beee05d9e738fe0de4be3f0f36bb244ae2688a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:44 +0000

    Add package jmeter-metrics/1.0.6/jmeter_metrics-1.0.6-py3-none-any.whl

---
+++ packages/jmeter-metrics/jmeter_metrics-1.0.6-py3-none-any.whl/jmeter_metrics/runner.py
@@ -0,0 +1,69 @@
+import os
+import argparse
+from .jmetermetrics import generate_report
+from .version import __version__
+
+
+def parse_options():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    general = parser.add_argument_group("General")
+    parser.add_argument(
+        '-v', '--version',
+        action='store_true',
+        dest='version',
+        help='Display application version information'
+    )
+    general.add_argument(
+        '--logo',
+        dest='logo',
+        default='https://i.ibb.co/9qBkwDF/Testing-Fox-Logo.png',
+        help="User logo (default: dummy image )"
+    )
+
+    general.add_argument(
+        '-I', '--inputpath',
+        dest='path',
+        default=os.path.curdir,
+        help="Path of result files"
+    )
+
+    general.add_argument(
+        '-M', '--metrics-report-name',
+        dest='metrics_report_name',
+        help="Output name of the generate metrics report"
+    )
+
+    general.add_argument(
+        '-O', '--output',
+        dest='output',
+        default="result.jtl",
+        help="Name of *.jtl or *.csv file"
+    )
+
+    general.add_argument(
+        '-K', '--ignoretableresult',
+        dest='ignoretableresult',
+        default="False",
+        help="Ignore table report in metrics report"
+    )
+
+    general.add_argument(
+        '-S', '--seperator',
+        dest='seperator',
+        default=",",
+        help="Seperator of columns"
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_options()
+
+    if args.version:
+        print(__version__)
+        exit(0)
+
+    generate_report(args)
