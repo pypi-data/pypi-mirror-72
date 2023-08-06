bf5a15c90a6b4f8b6d764d868dea79a700a351a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:07 +0000

    Add package pckByHM/1.0/pckByHM-1.0.tar.gz

---
+++ packages/pckByHM/pckByHM-1.0.tar.gz/pckByHM-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pckByHM',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pckByHM'],
+      author = 'Harshit Malhotra',
+      author_email = '18ucs207@lnmiit.ac.in',
+      zip_safe=False)
