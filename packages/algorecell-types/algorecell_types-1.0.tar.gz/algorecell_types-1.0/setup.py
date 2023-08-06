2678a043e89f6b4d873f82539c7209c533df99c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package algorecell-types/1.0/algorecell_types-1.0.tar.gz

---
+++ packages/algorecell-types/algorecell_types-1.0.tar.gz/algorecell_types-1.0/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+NAME = 'algorecell_types'
+
+setup(name=NAME,
+    version='1.0',
+    author = "Loïc Paulevé",
+    author_email = "loic.pauleve@labri.fr",
+    url = "https://github.com/algorecell/algorecell_types",
+    description = 'Generic types for reprogramming predictions from logical models',
+    install_requires = [
+        "colomoto_jupyter",
+        "pandas",
+        "pydot",
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    packages = find_packages()
+)
+
