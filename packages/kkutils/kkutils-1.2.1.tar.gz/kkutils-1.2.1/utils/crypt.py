9b6dfadf11aa93a0062d4e5fa7c03c11e8c34f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:35 +0000

    Add package kkutils/1.2.1/kkutils-1.2.1.tar.gz

---
+++ packages/kkutils/kkutils-1.2.1.tar.gz/kkutils-1.2.1/utils/crypt.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: zhangkai
+Last modified: 2020-06-14 19:33:33
+'''
+import binascii
+
+import rsa
+from Crypto.Cipher import DES
+
+from .utils import to_bytes
+
+
+class Crypt:
+
+    @staticmethod
+    def _unpad(s):
+        ''' 删除 PKCS#7 方式填充的字符串
+        '''
+        return s[:-ord(s[len(s) - 1:])]
+
+    @staticmethod
+    def _pad(text, size=8):
+        length = len(text)
+        val = size - (length % size)
+        pad = f'{val:02x}' * val
+        return text + binascii.unhexlify(pad)
+
+    @classmethod
+    def des_encrypt(cls, message, key):
+        message = cls._pad(to_bytes(message), DES.block_size)
+        des = DES.new(to_bytes(key), DES.MODE_ECB)
+        encrypt_data = des.encrypt(message)
+        return binascii.b2a_hex(encrypt_data)
+
+    @ classmethod
+    def des_decrypt(cls, ciphertext, key):
+        data = binascii.a2b_hex(ciphertext)
+        des = DES.new(to_bytes(key), DES.MODE_ECB)
+        return cls._unpad(des.decrypt(data))
+
+    @ classmethod
+    def gen_rsa_key(cls, length=1024):
+        pubkey, privkey = rsa.newkeys(length)
+        return pubkey.save_pkcs1(), privkey.save_pkcs1()
+
+    @ classmethod
+    def rsa_encrypt(cls, message, pubkey):
+        if not isinstance(pubkey, rsa.PublicKey):
+            pubkey = rsa.PublicKey.load_pkcs1(to_bytes(pubkey))
+        encrypt_data = rsa.encrypt(to_bytes(message), pubkey)
+        return binascii.b2a_hex(encrypt_data)
+
+    @ classmethod
+    def rsa_decrypt(cls, ciphertext, privkey):
+        if not isinstance(privkey, rsa.PrivateKey):
+            privkey = rsa.PrivateKey.load_pkcs1(to_bytes(privkey))
+        data = binascii.a2b_hex(ciphertext)
+        return rsa.decrypt(data, privkey)
