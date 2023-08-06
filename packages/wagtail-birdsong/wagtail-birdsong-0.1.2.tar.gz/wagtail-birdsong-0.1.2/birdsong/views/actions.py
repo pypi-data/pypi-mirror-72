cb22fbad0c9666fab7d715f202fed8788251c2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:08 +0000

    Add package wagtail-birdsong/0.1.2/wagtail-birdsong-0.1.2.tar.gz

---
+++ packages/wagtail-birdsong/wagtail-birdsong-0.1.2.tar.gz/wagtail-birdsong-0.1.2/birdsong/views/actions.py
@@ -0,0 +1,28 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+from wagtail.contrib.modeladmin.helpers.url import AdminURLHelper
+
+from birdsong.models import CampaignStatus
+
+
+def redirect_helper(campaign):
+    url_helper = AdminURLHelper(type(campaign))
+    campaign_list_url = url_helper.get_action_url('index')
+
+    return redirect(campaign_list_url)
+
+
+def send_campaign(backend, request, campaign, contacts):
+    campaign.status = CampaignStatus.SENDING
+    campaign.save()
+    backend.send_campaign(request, campaign, contacts)
+
+    return redirect_helper(campaign)
+
+
+def send_test(backend, request, campaign, test_contact):
+    campaign.subject = f"[TEST] {campaign.subject}"
+    backend.send_campaign(request, campaign, [test_contact])
+    messages.success(request, "Test email sent, please check your inbox")
+
+    return redirect_helper(campaign)
