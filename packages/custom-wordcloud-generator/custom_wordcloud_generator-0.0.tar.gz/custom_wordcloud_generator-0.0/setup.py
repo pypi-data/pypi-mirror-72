d42ac020108243aed7c0ab8dbef0b36fd660e22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:52 +0000

    Add package custom-wordcloud-generator/0.0/custom_wordcloud_generator-0.0.tar.gz

---
+++ packages/custom-wordcloud-generator/custom_wordcloud_generator-0.0.tar.gz/custom_wordcloud_generator-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='custom_wordcloud_generator',
+      version='0.0',
+      description='Slightly modified Muellers Wordcloud generator',
+      packages=['custom_wordcloud_generator'],
+      author_email='ekertdenis@gmail.com',
+      zip_safe=False)
