3ebe04b5c54f88565d19ef78972e352a9efec2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:56 +0000

    Add package netbox-plugin-test/0.4/netbox_plugin_test-0.4-py3.7.egg

---
+++ packages/netbox-plugin-test/netbox_plugin_test-0.4-py3.7.egg/netbox-plugin-test/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='netbox-plugin-test',
+    version='0.2',
+    description='A Netbox plugin testing build',
+    author='Sean Collins',
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
