b374514ef00aa83dfecf081b64e6529ac09186d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:41 +0000

    Add package dsnd-by-cs/0.1/dsnd_by_cs-0.1.tar.gz

---
+++ packages/dsnd-by-cs/dsnd_by_cs-0.1.tar.gz/dsnd_by_cs-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_by_cs',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_by_cs'],
+      author = 'Camila Santos',
+      zip_safe=False)
