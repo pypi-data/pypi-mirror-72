a41bb6b057718c42e074c6f62353473920b7bbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:08 +0000

    Add package mostafa-zahran-distributions/0.1/mostafa-zahran-distributions-0.1.tar.gz

---
+++ packages/mostafa-zahran-distributions/mostafa-zahran-distributions-0.1.tar.gz/mostafa-zahran-distributions-0.1/README.md
@@ -0,0 +1,1 @@
+README file about distribution package
---
+++ packages/mostafa-zahran-distributions/mostafa-zahran-distributions-0.1.tar.gz/mostafa-zahran-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mostafa-zahran-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mostafa-zahran-distributions'],
+      zip_safe=False)
