6d8b252278a82f903412d4127b9163a89892db58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:54 +0000

    Add package detectem/0.7.3/detectem-0.7.3-py2.py3-none-any.whl

---
+++ packages/detectem/detectem-0.7.3-py2.py3-none-any.whl/detectem/cli.py
@@ -0,0 +1,141 @@
+import json
+import logging
+import sys
+import tempfile
+
+from operator import attrgetter
+
+import click
+import click_log
+
+from detectem.core import Detector
+from detectem.exceptions import DockerStartError, NoPluginsError, SplashError
+from detectem.plugin import load_plugins
+from detectem.response import get_response
+from detectem.settings import CMD_OUTPUT, JSON_OUTPUT, SPLASH_TIMEOUT
+from detectem.utils import create_printer
+
+# Set up logging
+logger = logging.getLogger("detectem")
+click_log.basic_config(logger)
+
+
+@click.command()
+@click.option(
+    "--timeout",
+    default=SPLASH_TIMEOUT,
+    type=click.INT,
+    help="Timeout for Splash (in seconds).",
+)
+@click.option(
+    "--format",
+    default=CMD_OUTPUT,
+    type=click.Choice([CMD_OUTPUT, JSON_OUTPUT]),
+    help="Set the format of the results.",
+)
+@click.option(
+    "--metadata",
+    default=False,
+    is_flag=True,
+    help="Include this flag to return plugin metadata.",
+)
+@click.option("--list-plugins", is_flag=True, help="List registered plugins")
+@click.option("--save-har", is_flag=True, help="Save har to file")
+@click.option("-i", "--input-file", type=click.File("r"), help="Read URLs from file")
+@click_log.simple_verbosity_option(logger, default="error")
+@click.argument("input_url", required=False)
+def main(timeout, format, metadata, list_plugins, save_har, input_file, input_url):
+    # Gather urls
+    urls = []
+    if input_file:
+        urls += input_file.read().splitlines()
+    if input_url:
+        urls.append(input_url)
+
+    OPTIONS_WITHOUT_URLS = [list_plugins]
+
+    # Exit if neither urls were defined nor an option that works without urls
+    if not urls and not any(OPTIONS_WITHOUT_URLS):
+        click.echo(click.get_current_context().get_help())
+        sys.exit(1)
+
+    printer = create_printer(format)
+    results = []
+
+    if list_plugins:
+        try:
+            results = get_plugins(metadata)
+        except NoPluginsError as e:
+            printer(str(e))
+            sys.exit(1)
+    else:
+        for url in urls:
+            logger.debug(f"[+] Starting to process {url} ..")
+
+            try:
+                result = get_detection_results(url, timeout, metadata, save_har)
+            except (NoPluginsError, DockerStartError, SplashError) as e:
+                result = {"url": url, "error": str(e)}
+
+            results.append(result)
+
+    printer(results)
+
+
+def get_detection_results(url, timeout, metadata=False, save_har=False):
+    """ Return results from detector.
+
+    This function prepares the environment loading the plugins,
+    getting the response and passing it to the detector.
+
+    In case of errors, it raises exceptions to be handled externally.
+
+    """
+    plugins = load_plugins()
+    if not plugins:
+        raise NoPluginsError("No plugins found")
+
+    logger.debug("[+] Starting detection with %(n)d plugins", {"n": len(plugins)})
+
+    response = get_response(url, plugins, timeout)
+
+    # Save HAR
+    if save_har:
+        fd, path = tempfile.mkstemp(suffix=".har")
+        logger.info(f"Saving HAR file to {path}")
+
+        with open(fd, "w") as f:
+            json.dump(response["har"], f)
+
+    det = Detector(response, plugins, url)
+    softwares = det.get_results(metadata=metadata)
+
+    output = {"url": url, "softwares": softwares}
+
+    return output
+
+
+def get_plugins(metadata):
+    """ Return the registered plugins.
+
+    Load and return all registered plugins.
+    """
+    plugins = load_plugins()
+    if not plugins:
+        raise NoPluginsError("No plugins found")
+
+    results = []
+    for p in sorted(plugins.get_all(), key=attrgetter("name")):
+        if metadata:
+            data = {"name": p.name, "homepage": p.homepage}
+            hints = getattr(p, "hints", [])
+            if hints:
+                data["hints"] = hints
+            results.append(data)
+        else:
+            results.append(p.name)
+    return results
+
+
+if __name__ == "__main__":
+    main()
