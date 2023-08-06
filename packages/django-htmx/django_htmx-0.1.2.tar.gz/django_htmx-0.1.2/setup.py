7fa897b444797c7e94bf51225cbb17cec03d4cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:11 +0000

    Add package django-htmx/0.1.2/django_htmx-0.1.2.tar.gz

---
+++ packages/django-htmx/django_htmx-0.1.2.tar.gz/django_htmx-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_htmx', 'django_htmx.templatetags']
+
+package_data = \
+{'': ['*'], 'django_htmx': ['static/htmx/*', 'templates/htmx/*']}
+
+install_requires = \
+['django>=2.2']
+
+setup_kwargs = {
+    'name': 'django-htmx',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'nicokant',
+    'author_email': 'niccolocantu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
