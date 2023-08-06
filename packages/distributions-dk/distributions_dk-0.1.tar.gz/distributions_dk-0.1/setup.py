66510b64af65f332474fa2667add2da05fd44198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:48 +0000

    Add package distributions-dk/0.1/distributions_dk-0.1.tar.gz

---
+++ packages/distributions-dk/distributions_dk-0.1.tar.gz/distributions_dk-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_dk',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Deirdre Kong',
+      author_email='deirdre.y.kong@gmail.com',
+      packages=['distributions_dk'],
+      zip_safe=False)
