ee16c8042067d3864f8359d14b88e2e5d4b1cffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package lspr/0.20.6.7/lspr-0.20.6.7.tar.gz

---
+++ packages/lspr/lspr-0.20.6.7.tar.gz/lspr-0.20.6.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lspr",
+    version="0.20.6.7",
+    author="Angantyr Krzysztof Orlinski",
+    author_email="kakoori@gmail.com",
+    description="lspr is a Python 3 scientific package for modelling UV-Vis spectral behaviour of metallic nanoparticle-dieletric composites.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Arghantyr/LSPR-glass-transmittance",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+    install_requires=install_requires
+)
