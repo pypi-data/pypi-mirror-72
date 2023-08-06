96c9e014a467fa04488079abffa6391aeeb83ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:44 +0000

    Add package Frontiersman/1.0.3/Frontiersman-1.0.3.tar.gz

---
+++ packages/Frontiersman/Frontiersman-1.0.3.tar.gz/Frontiersman-1.0.3/setup.py
@@ -0,0 +1,28 @@
+import io
+from setuptools import setup, find_packages
+
+setup(
+    name="Frontiersman",
+    version="1.0.3",
+    author="Brian Snow",
+    author_email="snowb@ufl.edu",
+    description="Network Multiplayer Board Game",
+    install_requires=['pygame>=2.0.0.dev10', 'pygame_gui'],
+    long_description=io.open(
+        './README.md', 'r', encoding='utf-8').read(),
+    url="https://github.com/ForgedSnow/Frontiersman",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'FMClient = frontiersman.ClientMain:main',
+            'FMServer = frontiersman.server:main'
+        ],
+    }
+)
