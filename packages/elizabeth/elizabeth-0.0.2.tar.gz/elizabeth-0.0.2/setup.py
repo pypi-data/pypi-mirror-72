29dc16fe077b8201503b9156244cb92bdf20fa3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:55 +0000

    Add package elizabeth/0.0.2/elizabeth-0.0.2.tar.gz

---
+++ packages/elizabeth/elizabeth-0.0.2.tar.gz/elizabeth-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="elizabeth",
+    version="0.0.2",
+    author="Shuhei Kishi",
+    author_email="marubontan@gmail.com",
+    description=("Machine leaning pipeline"),
+    license="MIT",
+    url="https://github.com/marubontan/elizabeth",
+    packages=['elizabeth']
+)
