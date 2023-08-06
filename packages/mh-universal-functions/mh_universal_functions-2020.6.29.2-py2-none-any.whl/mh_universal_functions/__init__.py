70d7a64d6d8682eb93ed6ac74987c062f9da34e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:08 +0000

    Add package mh-universal-functions/2020.6.29.2/mh_universal_functions-2020.6.29.2-py2-none-any.whl

---
+++ packages/mh-universal-functions/mh_universal_functions-2020.6.29.2-py2-none-any.whl/mh_universal_functions/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['database_update_universal',
+           'database_read',
+           'read_config',
+           'exit_critical',
+           'send_telegram_message',
+           'test']
+from .mh_universal_functions import *
+from ._version import __version__
