429a05ebf4545fa83db4cf66822cc92bd2b9a657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package confuse/1.3.0/confuse-1.3.0.tar.gz

---
+++ packages/confuse/confuse-1.3.0.tar.gz/confuse-1.3.0/docs/conf.py
@@ -0,0 +1,23 @@
+from __future__ import division, absolute_import, print_function
+
+
+extensions = [
+    'sphinx.ext.autosectionlabel',
+]
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'Confuse'
+copyright = u'2012, Adrian Sampson'
+
+version = '1.3'
+release = '1.3.0'
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+# -- Options for HTML output --------------------------------------------------
+
+html_theme = 'default'
+htmlhelp_basename = 'Confusedoc'
