cdbe09c633b29d405960bae6013d8d28185983a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:30 +0000

    Add package gethash/0.6/gethash-0.6-py3-none-any.whl

---
+++ packages/gethash/gethash-0.6-py3-none-any.whl/gethash/getsha256.py
@@ -0,0 +1,22 @@
+import hashlib
+
+import click
+
+from ._script import script_main
+
+
+@click.command()
+@click.option('-c', '--check', is_flag=True,
+              help='read SHA256 from the FILES and check them.')
+@click.option('--no-stdout', is_flag=True,
+              help='Do not output to stdout.')
+@click.option('--no-stderr', is_flag=True,
+              help='Do not output to stderr.')
+@click.version_option()
+@click.argument('files', nargs=-1)
+def main(check, files, **kwargs):
+    script_main(main, hashlib.sha256(), '.sha256', check, files, **kwargs)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
