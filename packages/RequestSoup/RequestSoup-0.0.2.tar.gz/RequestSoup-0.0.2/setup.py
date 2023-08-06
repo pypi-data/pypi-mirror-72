1d022438fc2a07d07cc0f183d2fa4761b273f5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:23 +0000

    Add package RequestSoup/0.0.2/RequestSoup-0.0.2.tar.gz

---
+++ packages/RequestSoup/RequestSoup-0.0.2.tar.gz/RequestSoup-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+with open("README.MD","r") as fh:
+    long = fh.read()
+setup(
+    name = 'RequestSoup',
+    version='0.0.2',
+    description='A wrapper created to make using requests and BeautifulSoup in conjunction easier',
+    long_description=long,
+    long_description_content_type="text/markdown",
+    py_modules=["RequestSoup"],
+    package_dir={'':'src'},
+    install_requires = [
+        "requests",
+        "bs4"
+    ],
+)
