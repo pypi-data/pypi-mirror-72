3cac1374c2e71e1a24e9c03f4adebf7efafc2c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:27 +0000

    Add package lo-aws-tools/0.0.2/lo-aws-tools-0.0.2.tar.gz

---
+++ packages/lo-aws-tools/lo-aws-tools-0.0.2.tar.gz/lo-aws-tools-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lo-aws-tools",
+    version="0.0.2",
+    author="Dyllan Pascoe",
+    author_email="dyllan@lookopen.com",
+    description="AWS Python API custom tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/dyllan/lo-aws-tools",
+    install_requires=['boto3'],
+    packages=setuptools.find_packages(),
+    scripts=['bin/lo-aws-tools'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
