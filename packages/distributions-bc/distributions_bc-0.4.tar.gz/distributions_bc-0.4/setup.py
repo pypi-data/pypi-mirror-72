677374d0bd58413367db0f1723f50688446a28ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:49 +0000

    Add package distributions-bc/0.4/distributions_bc-0.4.tar.gz

---
+++ packages/distributions-bc/distributions_bc-0.4.tar.gz/distributions_bc-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(name='distributions_bc',
+      version='0.4',
+      author='Charles Gauthey',
+      author_email='cjgauthey@gmail.com',
+      description='Gaussian and Binomial distributions',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+      ],
+      zip_safe=False,
+      python_requires='>=3.6'
+      )
