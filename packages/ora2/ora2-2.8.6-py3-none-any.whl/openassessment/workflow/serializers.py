7e779b8207c23c8539b3ab8418bb3eb407ba5887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:42 +0000

    Add package ora2/2.8.6/ora2-2.8.6-py3-none-any.whl

---
+++ packages/ora2/ora2-2.8.6-py3-none-any.whl/openassessment/workflow/serializers.py
@@ -0,0 +1,62 @@
+"""
+Serializers are created to ensure models do not have to be accessed outside the
+scope of the ORA2 APIs.
+"""
+
+
+from rest_framework import serializers
+
+from openassessment.workflow.models import AssessmentWorkflow, AssessmentWorkflowCancellation, TeamAssessmentWorkflow
+
+
+class AssessmentWorkflowSerializer(serializers.ModelSerializer):
+    """
+    Serialize a AssessmentWorkflow' model.
+    """
+    score = serializers.ReadOnlyField(required=False)
+
+    class Meta:
+        model = AssessmentWorkflow
+        fields = (
+            'submission_uuid',
+            'status',
+            'created',
+            'modified',
+
+            # Computed
+            'score'
+        )
+
+
+class TeamAssessmentWorkflowSerializer(serializers.ModelSerializer):
+    """
+    Serialize a TeamAssessmentWorkflow model.
+    """
+    score = serializers.ReadOnlyField(required=False)
+
+    class Meta:
+        model = TeamAssessmentWorkflow
+        fields = (
+            'team_submission_uuid',
+            'submission_uuid',
+            'status',
+            'created',
+            'modified',
+
+            # Computed
+            'score'
+        )
+
+
+class AssessmentWorkflowCancellationSerializer(serializers.ModelSerializer):
+    """
+    Serialize a `AssessmentWorkflowCancellation` model.
+    """
+
+    class Meta:
+        model = AssessmentWorkflowCancellation
+        fields = (
+            'comments',
+            'cancelled_by_id',
+            'created_at',
+        )
