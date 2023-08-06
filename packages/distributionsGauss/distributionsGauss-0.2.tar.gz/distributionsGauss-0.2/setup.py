5eec74c69537a4a1c36749b5df41b889f2d952e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:37 +0000

    Add package distributionsGauss/0.2/distributionsGauss-0.2.tar.gz

---
+++ packages/distributionsGauss/distributionsGauss-0.2.tar.gz/distributionsGauss-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsGauss',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
