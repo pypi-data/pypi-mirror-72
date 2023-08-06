ae00d6058e324e6fd57e012c6de37bbf1560a824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:24 +0000

    Add package topdown/0.2.1/topdown-0.2.1-py3-none-any.whl

---
+++ packages/topdown/topdown-0.2.1-py3-none-any.whl/topdown/__init__.py
@@ -0,0 +1,7 @@
+from topdown._actomizer import Actor
+from topdown._actomizer import Circle
+from topdown._actomizer import Square
+from topdown._actomizer import Triangle
+from topdown._animator import Animation
+
+__version__ = '0.2.0'
