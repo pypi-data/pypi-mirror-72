754d72273770133667a10af814640358b82d1884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package pycamunda/0.0.1b2/pycamunda-0.0.1b2.tar.gz

---
+++ packages/pycamunda/pycamunda-0.0.1b2.tar.gz/pycamunda-0.0.1b2/pycamunda/base.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+import abc
+import datetime as dt
+import typing
+
+import requests
+
+import pycamunda.request
+
+
+__all__ = ['isoformat', 'from_isoformat']
+
+
+def value_is_true(self, obj: typing.Any, obj_type: typing.Any) -> bool:
+    return bool(obj.__dict__[self.name])
+
+
+class CamundaRequest(pycamunda.request.Request):
+
+    @abc.abstractmethod
+    def __call__(self, *args, **kwargs):
+        return NotImplementedError
+
+    def body_parameters(self, apply: typing.Callable = ...):
+        if apply is Ellipsis:
+            return super().body_parameters(apply=prepare)
+        return super().body_parameters(apply=apply)
+
+    def query_parameters(self, apply: typing.Callable = ...):
+        if apply is Ellipsis:
+            return super().query_parameters(apply=prepare)
+        return super().query_parameters(apply=apply)
+
+
+def isoformat(datetime_: typing.Union[dt.date, dt.datetime]) -> str:
+    """Convert a datetime object to the isoformat string Camunda expects. Datetime objects are
+    expected to contain timezoneinformation.
+
+    :param datetime_: Datetime or date object to convert.
+    :return: Isoformat datetime or date string.
+    """
+    if isinstance(datetime_, dt.datetime):
+        dt_str = datetime_.strftime('%Y-%m-%dT%H:%M:%S.{ms}%z')
+        ms = datetime_.microsecond // 1000
+        dt_str = dt_str.format(ms=str(ms).zfill(3))
+    else:
+        dt_str = datetime_.strftime('%Y-%m-%d')
+
+    return dt_str
+
+
+def from_isoformat(datetime_str: str) -> dt.datetime:
+    """Convert an isoformat string to a datetime object.
+
+    :param datetime_str: String to convert.
+    :return: Converted datetime.
+    """
+    return dt.datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+
+def prepare(value: typing.Any) -> typing.Any:
+    """Prepare parameter values for Camunda.
+
+    :param value: Value to prepare.
+    """
+    if isinstance(value, dt.datetime):
+        return isoformat(datetime_=value)
+    try:  # support for enums
+        value = value.value
+    except AttributeError:
+        pass
+    return value
+
+
+def _raise_for_status(response: requests.Response) -> None:
+    """Handle http errors regarding Camunda.
+
+    :param response: Response coming from Camunda.
+    """
+    try:
+        message = response.json()['message']
+    except KeyError:
+        message = ''
+    except TypeError:
+        message = ''
+
+    if response.status_code == 400:
+        raise pycamunda.BadRequest(message)
+    elif response.status_code == 403:
+        raise pycamunda.Forbidden(message)
+    elif response.status_code == 404:
+        raise pycamunda.NotFound(message)
+
+    raise pycamunda.NoSuccess(response.text)
