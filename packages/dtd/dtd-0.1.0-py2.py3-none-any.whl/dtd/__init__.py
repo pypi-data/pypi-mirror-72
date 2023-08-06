2e604bb92e1caf741f961bf59c725ce36e3a0fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:35 +0000

    Add package dtd/0.1.0/dtd-0.1.0-py2.py3-none-any.whl

---
+++ packages/dtd/dtd-0.1.0-py2.py3-none-any.whl/dtd/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Death to DS_Store."""
+
+__author__ = """Mudit Bachhawat"""
+__email__ = 'mudit5bachhawat@gmail.com'
+__version__ = '0.1.0'
+
+
+DS_STORE_FILENAME = '.DS_Store'
+
+from .dtd import patch_all
