65e04169c7c45ac029df19e291c02ad8db877cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:42 +0000

    Add package commitizen-emoji/0.1.3/commitizen_emoji-0.1.3.tar.gz

---
+++ packages/commitizen-emoji/commitizen_emoji-0.1.3.tar.gz/commitizen_emoji-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="commitizen_emoji",
+    version="0.1.3",
+    py_modules=["cz_commitizen_emoji"],
+    license="MIT",
+    long_description="Commitizen with emojis!",
+    install_requires=["commitizen"],
+)
