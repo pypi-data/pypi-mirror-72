10b0036706e75c4b4f190ab5599b3c0f7fd6ebfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:53 +0000

    Add package epyk/1.0.16/epyk-1.0.16-py2.py3-none-any.whl

---
+++ packages/epyk/epyk-1.0.16-py2.py3-none-any.whl/epyk/core/cli/cli_export.py
@@ -0,0 +1,38 @@
+"""
+
+epyk_export.exe
+"""
+
+
+def angular():
+  """
+  Generate an Angular Application from the Epyk page
+  """
+
+
+def vue():
+  pass
+
+
+def react():
+  pass
+
+
+def node():
+  pass
+
+
+def jupyter():
+  pass
+
+
+def deno():
+  pass
+
+
+def html():
+  pass
+
+
+def serve():
+  pass
---
+++ packages/epyk/epyk-1.0.16-py2.py3-none-any.whl/epyk/core/cli/cli_project.py
@@ -0,0 +1,41 @@
+"""
+Main command lines for Epyk
+
+epyk.exe
+"""
+
+
+def new():
+  """
+  Create a new Epyk Structure.
+
+  THis structure should be easy to integrate any Python exnsting project:
+
+  The structure can be the ones below
+  ui/
+    pages/
+    views/
+    packages/
+    settings.py
+    transpile.py
+    serve.py
+  """
+
+
+def page():
+  """
+  Create a new page in the current project
+
+  basically it can be something like the below:
+
+  def page():
+    '''
+    '''
+
+  """
+
+
+def transpile():
+  """
+  Transpile to HTML a report
+  """
