5eef01496a37e517045b7cd89e0814573af9455e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:13 +0000

    Add package cngi-prototype/0.0.57/cngi_prototype-0.0.57.tar.gz

---
+++ packages/cngi-prototype/cngi_prototype-0.0.57.tar.gz/cngi_prototype-0.0.57/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open('README.md', "r") as fid:   #encoding='utf-8'
+    long_description = fid.read()
+
+setup(
+    name='cngi_prototype',
+    version='0.0.57',
+    description='CASA Next Generation Infrastructure Prototype',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='National Radio Astronomy Observatory',
+    author_email='casa-feedback@nrao.edu',
+    url='https://github.com/casangi/cngi_prototype',
+    license='Apache-2.0',
+    packages=find_packages(),
+    install_requires=['numpy>=1.18.1',
+                      'dask>=2.13.0',
+                      'distributed>=2.9.3',
+                      'bokeh>=1.4.0',
+                      'pandas>=0.25.2',
+                      'xarray>=0.15.0',
+                      'zarr>=2.4.0',
+                      'numcodecs>=0.6.4',
+                      'matplotlib>=3.1.2',
+                      'scipy>=1.4.1',
+                      'scikit-learn>=0.22.2',
+                      'sparse>=0.9.1',
+                      'toolz>=0.10.0'],
+    extras_require={
+        'dev': [
+            'pytest>=5.3.5',
+            'black>=19.10.b0',
+            'flake8>=3.7.9',
+            'isort>=4.3.21',
+            's3fs>=0.4.0',
+            'pylint>=2.4.4',
+            #'pytest-pep8',
+            #'pytest-cov'
+        ]
+    }
+
+)
