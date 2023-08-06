6b457a7a216b52fe34130db3dd0b445d81d94706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:46 +0000

    Add package pytest-chalice/0.0.5/pytest-chalice-0.0.5.tar.gz

---
+++ packages/pytest-chalice/pytest-chalice-0.0.5.tar.gz/pytest-chalice-0.0.5/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-chalice',
+    license='MIT',
+
+    author='Satoshi SUZUKI',
+    author_email='studio3104.com@gmail.com',
+    maintainer='Satoshi SUZUKI',
+    maintainer_email='studio3104.com@gmail.com',
+
+    url='https://github.com/studio3104/pytest-chalice',
+    description='A set of py.test fixtures for AWS Chalice',
+    long_description=read('README.rst'),
+    packages=find_packages(exclude=['docs', 'tests']),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=[
+        'chalice>=1.8.0',
+        'pytest>=3.5.0',
+    ],
+
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    entry_points={
+        'pytest11': [
+            'chalice = pytest_chalice.plugin',
+        ],
+    },
+)
---
+++ packages/pytest-chalice/pytest-chalice-0.0.5.tar.gz/pytest-chalice-0.0.5/tests/test_chalice.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from pytest_chalice.handlers import RequestHandler
+
+
+class TestRequest:
+    @pytest.mark.parametrize('method',  (
+        'get', 'head', 'post', 'put', 'delete', 'trace', 'patch', 'link', 'unlink',
+    ))
+    def test_json_response(self, method, client):
+        response = getattr(client, method)('/')
+        assert response.status_code == 200
+        assert response.json == {'hello': 'world'}
+
+    def test_invalid_method(self, client):
+        # type: (RequestHandler) -> None
+        with pytest.raises(AttributeError, match=r' object has no attribute '):
+            client.invalid_method('/')
+
+    def test_string_response_dont_have_json_attribute(self, client):
+        # type: (RequestHandler) -> None
+        response = client.get('/string')
+        assert not hasattr(response, 'json')
+
+
+class TestCustomContext:
+    def test_check_default_context(self, client):
+        # type: (RequestHandler) -> None
+        response = client.get('/context')
+        assert response.json == {
+            'context': {
+                'httpMethod': 'GET',
+                'identity': {'sourceIp': '127.0.0.1'},
+                'path': '/context',
+                'resourcePath': '/context',
+            }
+        }
+
+    def test_custom_context(self, client):
+        # type: (RequestHandler) -> None
+        client.custom_context = {
+            'authorizer': {'claims': {}},
+        }
+
+        response = client.get('/context')
+        response_context = response.json['context']
+        assert 'httpMethod' in response_context
+        assert 'identity' in response_context
+        assert 'path' in response_context
+        assert 'resourcePath' in response_context
+        assert 'authorizer' in response_context
