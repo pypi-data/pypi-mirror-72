988f64440e2321ac028d1720bcacb972b169d27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:40 +0000

    Add package taobao-sdk-python3/1.0.2/taobao_sdk_python3-1.0.2-py2.py3-none-any.whl

---
+++ packages/taobao-sdk-python3/taobao_sdk_python3-1.0.2-py2.py3-none-any.whl/top/api/rest/__init__.py
@@ -0,0 +1,3 @@
+from top.api.rest.TbkTpwdCreateRequest import TbkTpwdCreateRequest
+from top.api.rest.TbkCouponGetRequest import TbkCouponGetRequest
+from top.api.rest.TbkItemInfoGetRequest import TbkItemInfoGetRequest
