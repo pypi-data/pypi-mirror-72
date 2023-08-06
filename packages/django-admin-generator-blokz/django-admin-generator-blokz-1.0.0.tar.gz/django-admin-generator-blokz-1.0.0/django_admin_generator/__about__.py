ade28c044775d63cb7c83fd7a52da95df0228fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:22 +0000

    Add package django-admin-generator-blokz/1.0.0/django-admin-generator-blokz-1.0.0.tar.gz

---
+++ packages/django-admin-generator-blokz/django-admin-generator-blokz-1.0.0.tar.gz/django-admin-generator-blokz-1.0.0/django_admin_generator/__about__.py
@@ -0,0 +1,9 @@
+__package_name__ = 'django-admin-generator-blokz'
+__version__ = '1.0.0'
+__author__ = 'Mohamed Abdulaziz'
+__author_email__ = 'mo@blok-z.com'
+__description__ = ' '.join(('''
+Django Admin Generator is a management command to automatically generate a
+Django `admin.py` file for given apps/models.
+'''.strip().split()))
+__url__ = 'https://github.com/blok-z/django-admin-generator/'
