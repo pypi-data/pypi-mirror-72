cc630542cb585baa06ca894fd8b507c0fbcfe61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:12 +0000

    Add package dsnd-probability-oz/0.1/dsnd_probability_oz-0.1.tar.gz

---
+++ packages/dsnd-probability-oz/dsnd_probability_oz-0.1.tar.gz/dsnd_probability_oz-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_oz',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_oz'],
+      author = 'Ozzy Campos',
+      author_email = 'ozzycampos@yahoo.com', 
+      zip_safe=False)
