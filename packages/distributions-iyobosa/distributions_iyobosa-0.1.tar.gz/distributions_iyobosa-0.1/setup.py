a9e68fcf3fa27d0907b3df23b37d549ae9e34a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:08 +0000

    Add package distributions-iyobosa/0.1/distributions_iyobosa-0.1.tar.gz

---
+++ packages/distributions-iyobosa/distributions_iyobosa-0.1.tar.gz/distributions_iyobosa-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_iyobosa',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_iyobosa'],
+      author = "Iyobosa Evbayowieru",
+      author_email = "iyobo247@gmail.com",
+      zip_safe=False)
