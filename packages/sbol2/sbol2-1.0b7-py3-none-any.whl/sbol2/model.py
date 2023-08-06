0ded1acf6ed5d840a31ad6e4d9d9ac7676988d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:47 +0000

    Add package sbol2/1.0b7/sbol2-1.0b7-py3-none-any.whl

---
+++ packages/sbol2/sbol2-1.0b7-py3-none-any.whl/sbol2/model.py
@@ -0,0 +1,18 @@
+from rdflib import URIRef
+
+from .toplevel import TopLevel
+from .property import URIProperty
+from .constants import *
+
+
+class Model(TopLevel):
+    def __init__(self, rdf_type=SBOL_MODEL, uri=URIRef('example'), source='',
+                 language=EDAM_SBML, framework=SBO_CONTINUOUS,
+                 version=VERSION_STRING):
+        super().__init__(rdf_type, uri, version)
+        self.source = URIProperty(self, SBOL_SOURCE,
+                                  '0', '1', [], source)
+        self.language = URIProperty(self, SBOL_LANGUAGE,
+                                    '0', '1', [], language)
+        self.framework = URIProperty(self, SBOL_FRAMEWORK,
+                                     '0', '1', [], framework)
