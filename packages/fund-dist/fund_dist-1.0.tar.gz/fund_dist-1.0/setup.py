1c11d80185b53917fbb0c294b7eac2d8d63ed719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package fund-dist/1.0/fund_dist-1.0.tar.gz

---
+++ packages/fund-dist/fund_dist-1.0.tar.gz/fund_dist-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fund_dist',
+      version='1.0',
+      description='Fundamental distributions: Guassian, Binomial, Poisson',
+      packages=['fund_dist'],
+      author='Ozan Ersoy',
+      author_email='ozan.ersoy+pypi@gmail.com',
+      zip_safe=False)
