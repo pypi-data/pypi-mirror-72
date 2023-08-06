1dc3b71dc4e6b01e3ca78523372153f9226434f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:33 +0000

    Add package osascript/2020.7.1/osascript-2020.7.1.tar.gz

---
+++ packages/osascript/osascript-2020.7.1.tar.gz/osascript-2020.7.1/osascript/__init__.py
@@ -0,0 +1,24 @@
+__all__ = ['run', 'osascript']
+
+
+import os
+import runcmd
+import temp
+
+
+def run(applescript, background=False):
+    """run applescript file/code"""
+    if os.path.exists(applescript):
+        path = applescript
+    else:
+        path = temp.tempfile()
+        open(path, "w").write(applescript)
+    cmd = ["osascript", path]
+    r = runcmd.run(cmd, background=background)
+    return r.code, r.out, r.err
+
+
+@public.add
+def osascript(applescript, background=False):
+    """run applescript file/code. deprecated"""
+    return run(applescript, background=False)
