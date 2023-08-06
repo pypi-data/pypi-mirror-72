5eec349336738dfbd6de4198c026ecb8a647f078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:54 +0000

    Add package vedo/2020.3.4/vedo-2020.3.4.tar.gz

---
+++ packages/vedo/vedo-2020.3.4.tar.gz/vedo-2020.3.4/vedo/examples/other/run_all.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# source run_all.sh
+#
+echo #############################################
+echo    Press Esc at anytime to skip example
+echo #############################################
+echo
+echo
+
+echo Running flag_labels.py
+python flag_labels.py
+
+echo Running icon.py
+python icon.py
+
+echo Running inset.py
+python inset.py
+
+echo Running matplotlib_tri.py
+python matplotlib_tri.py
+
+echo Running qt_window.py # needs qt5
+python qt_window1.py
+
+echo Running qt_window_split.py # needs qt5
+python qt_window2.py
+
+echo Running qt_tabs.py # needs qt5
+python qt_tabs.py
+
+echo Running self_org_maps2d.py
+python self_org_maps2d.py
+
+echo Running value-iteration.py
+python value-iteration.py
+
+echo Running remesh_meshfix.py
+python remesh_meshfix.py
+
+echo Running spherical_harmonics1.py
+python spherical_harmonics1.py 
+
+echo Running create_logo.py
+python create_logo.py
+
+echo Running export_numpy.py
+python export_numpy.py
