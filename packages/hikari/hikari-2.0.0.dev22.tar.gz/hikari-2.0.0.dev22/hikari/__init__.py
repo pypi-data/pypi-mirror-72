7b20a8d396332b1af6c7d1868617197fbd34fdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:54 +0000

    Add package hikari/2.0.0.dev22/hikari-2.0.0.dev22.tar.gz

---
+++ packages/hikari/hikari-2.0.0.dev22.tar.gz/hikari-2.0.0.dev22/hikari/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright © Nekoka.tt 2019-2020
+#
+# This file is part of Hikari.
+#
+# Hikari is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hikari is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Hikari. If not, see <https://www.gnu.org/licenses/>.
+"""A sane Python framework for writing modern Discord bots."""
+
+from __future__ import annotations
+
+__all__: typing.Final[typing.Sequence[str]] = ["RESTClientFactory", "Bot"]
+
+# noinspection PyUnresolvedReferences
+import typing
+
+from hikari._about import __author__
+from hikari._about import __ci__
+from hikari._about import __copyright__
+from hikari._about import __discord_invite__
+from hikari._about import __docs__
+from hikari._about import __email__
+from hikari._about import __issue_tracker__
+from hikari._about import __license__
+from hikari._about import __url__
+from hikari._about import __version__
+
+from hikari.impl.bot import BotAppImpl as Bot
+from hikari.impl.rest import RESTClientFactoryImpl as RESTClientFactory
