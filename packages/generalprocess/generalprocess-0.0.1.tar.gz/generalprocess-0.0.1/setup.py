a22f370d8d6b856f299a716b055409fe7a0ebb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package generalprocess/0.0.1/generalprocess-0.0.1.tar.gz

---
+++ packages/generalprocess/generalprocess-0.0.1.tar.gz/generalprocess-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='generalprocess',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Rickard "Mandera" Abraham',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
