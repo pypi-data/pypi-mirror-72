80b7f4e7536a7578962a4d5f3e1044f5a438033b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:29 +0000

    Add package pyannote.metrics/3.0.1/pyannote.metrics-3.0.1.tar.gz

---
+++ packages/pyannote.metrics/pyannote.metrics-3.0.1.tar.gz/pyannote.metrics-3.0.1/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# The MIT License (MIT)
+
+# Copyright (c) 2014-2020 CNRS
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# AUTHORS
+# Herve BREDIN - http://herve.niderb.fr
+
+import versioneer
+
+versioneer.versionfile_source = "pyannote/metrics/_version.py"
+versioneer.versionfile_build = versioneer.versionfile_source
+versioneer.tag_prefix = ""
+versioneer.parentdir_prefix = "pyannote-metrics-"
+
+from setuptools import setup, find_packages
+
+setup(
+    # package
+    namespace_packages=["pyannote"],
+    packages=find_packages(),
+    entry_points={"console_scripts": ["pyannote-metrics=pyannote.metrics.cli:main",],},
+    install_requires=[
+        "pyannote.core >= 4.1",
+        "pyannote.database >= 4.0.1",
+        "pandas >= 0.19",
+        "scipy >= 1.1.0",
+        "scikit-learn >= 0.17.1",
+        "docopt >= 0.6.2",
+        "tabulate >= 0.7.7",
+        "matplotlib >= 2.0.0",
+        "sympy >= 1.1",
+    ],
+    # versioneer
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    # PyPI
+    name="pyannote.metrics",
+    description=(
+        "a toolkit for reproducible evaluation, diagnostic, and error analysis of speaker diarization systems"
+    ),
+    author="Herve Bredin",
+    author_email="bredin@limsi.fr",
+    url="https://pyannote.github.io/pyannote-metrics",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+    ],
+)
