3aa53fdc4fdd816032204cde406dc42a6f2359eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package tamersts/1.1/tamersts-1.1.tar.gz

---
+++ packages/tamersts/tamersts-1.1.tar.gz/tamersts-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tamersts',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tamersts'],
+      zip_safe=False)
