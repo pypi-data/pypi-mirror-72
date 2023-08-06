747174c820a329f36152a73bf02a438c05146394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:48 +0000

    Add package awsfnd-probability/0.1/awsfnd_probability-0.1.tar.gz

---
+++ packages/awsfnd-probability/awsfnd_probability-0.1.tar.gz/awsfnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='awsfnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['awsfnd_probability'],
+      author='Ahmad Saepulloh',
+      author_email='myemail@somthing.com',
+      zip_safe=False)
