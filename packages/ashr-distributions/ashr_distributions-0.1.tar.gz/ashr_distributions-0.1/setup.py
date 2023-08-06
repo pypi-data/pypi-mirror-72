583396f06bf067ebb4f28fff977aba4b945cc4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package ashr-distributions/0.1/ashr_distributions-0.1.tar.gz

---
+++ packages/ashr-distributions/ashr_distributions-0.1.tar.gz/ashr_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ashr_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ashr_distributions'],
+      zip_safe=False)
