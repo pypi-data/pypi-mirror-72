1fe76c9c97e0ed95bdac4c02a77fdbb73bc134c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:05 +0000

    Add package get-province-city-county/0.2.0/get_province_city_county-0.2.0.tar.gz

---
+++ packages/get-province-city-county/get_province_city_county-0.2.0.tar.gz/get_province_city_county-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="get_province_city_county", # Replace with your own username
+    version="0.2.0",
+    author="白旭东",
+    author_email="2216403312@qq.com",
+    description="这是一个从文本提取中文地区的包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oldlwhite/get_province_city_county",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
