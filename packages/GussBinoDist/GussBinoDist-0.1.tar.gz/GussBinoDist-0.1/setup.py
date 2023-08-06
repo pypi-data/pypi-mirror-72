87534a1ca36957728281549ac1bc62f48cd6cdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:37 +0000

    Add package GussBinoDist/0.1/GussBinoDist-0.1.tar.gz

---
+++ packages/GussBinoDist/GussBinoDist-0.1.tar.gz/GussBinoDist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GussBinoDist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['GussBinoDist'],
+      auther='sana sabah',
+      auther_email='sana_sabah@uoitc.edu.iq',
+      zip_safe=False)
