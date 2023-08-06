c2baa05fe13ed8c3b0444be40ce4f1ae8dcda112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package mh-universal-functions/2020.6.27.1/mh_universal_functions-2020.6.27.1.tar.gz

---
+++ packages/mh-universal-functions/mh_universal_functions-2020.6.27.1.tar.gz/mh_universal_functions-2020.6.27.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mh_universal_functions',
+      version='2020.6.27.1',
+      description='General functions',
+      url='',
+      author='MH',
+      author_email='mh@example.com',
+      license='MIT',
+      packages=['mh_universal_functions'],
+      zip_safe=False)
