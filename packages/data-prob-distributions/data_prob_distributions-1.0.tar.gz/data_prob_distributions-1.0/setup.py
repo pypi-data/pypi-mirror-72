5bdc22da29ce4ffcb237aa95f1d8850d09967f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:38 +0000

    Add package data-prob-distributions/1.0/data_prob_distributions-1.0.tar.gz

---
+++ packages/data-prob-distributions/data_prob_distributions-1.0.tar.gz/data_prob_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='data_prob_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['data_prob_distributions'],
+      auther = 'Thilina Kariyawasam',
+      zip_safe=False)
