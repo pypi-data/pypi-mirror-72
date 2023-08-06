0ac50f61d3e44d9060b968fbd420174bd943f95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:15 +0000

    Add package bilard-sta-distributions/1.0/bilard_sta-distributions-1.0.tar.gz

---
+++ packages/bilard-sta-distributions/bilard_sta-distributions-1.0.tar.gz/bilard_sta-distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bilard_sta-distributions',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['bilard_sta-distributions'],
+      author = "Adebayo Osinulu",
+      author_email = "bilardosinulu@gmail.com",
+      zip_safe=False)
