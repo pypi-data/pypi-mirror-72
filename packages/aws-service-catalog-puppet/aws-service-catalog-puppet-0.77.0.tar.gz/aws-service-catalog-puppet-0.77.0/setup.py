969d082f5ecdf87fc5eb90fd94b1696cc1b1be33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package aws-service-catalog-puppet/0.77.0/aws-service-catalog-puppet-0.77.0.tar.gz

---
+++ packages/aws-service-catalog-puppet/aws-service-catalog-puppet-0.77.0.tar.gz/aws-service-catalog-puppet-0.77.0/setup.py
@@ -0,0 +1,60 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aws-service-catalog-puppet",
+    version="0.77.0",
+    author="Eamonn Faherty",
+    author_email="aws-service-catalog-tools@amazon.com",
+    description="Making it easier to deploy ServiceCatalog products",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/awslabs/aws-service-catalog-puppet-framework",
+    packages=find_packages(),
+    package_data={"servicecatalog_puppet": ["*", "*/*", "*/*/*"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    entry_points={
+        "console_scripts": ["servicecatalog-puppet = servicecatalog_puppet.cli:cli"]
+    },
+    install_requires=[
+        "better-boto==0.28.0",
+        "boto3==1.14.14",
+        "botocore==1.17.14",
+        "certifi==2020.6.20",
+        "cfn-flip==1.2.1",
+        "chardet==3.0.4",
+        "click==7.0",
+        "colorclass==2.2.0",
+        "docopt==0.6.2",
+        "docutils==0.14",
+        "idna==2.8",
+        "jinja2==2.10.1",
+        "jmespath==0.10.0",
+        "lockfile==0.12.2",
+        "luigi==2.8.9",
+        "markupsafe==1.1.1",
+        "progressbar==2.5",
+        "psutil==5.7.0",
+        "pykwalify==1.7.0",
+        "python-daemon==2.1.2",
+        "python-dateutil==2.8.1",
+        "pyyaml==5.1",
+        "requests==2.22.0",
+        "s3transfer==0.3.3",
+        "six==1.15.0",
+        "terminaltables==3.1.0",
+        "tornado==5.1.1",
+        "urllib3==1.25.9; python_version != '3.4'",
+    ],
+)
