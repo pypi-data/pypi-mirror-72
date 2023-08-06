17d4aef6070c1b539c4dfb5735cd5dc6cd41af3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:30 +0000

    Add package pyboin/0.1.1/pyboin-0.1.1-py3-none-any.whl

---
+++ packages/pyboin/pyboin-0.1.1-py3-none-any.whl/pyboin/__init__.py
@@ -0,0 +1,11 @@
+from . import pyboin
+
+__copyright__    = 'Copyright (C) 2020 Tatsuya Abe'
+__license__      = 'MIT'
+__author__       = 'Tatsuya Abe'
+__author_email__ = 'abe12@mccc.jp'
+__url__          = 'https://github.com/AjxLab/pyboin'
+
+__all__ = ['text2boin']
+
+text2boin = pyboin.text2boin
