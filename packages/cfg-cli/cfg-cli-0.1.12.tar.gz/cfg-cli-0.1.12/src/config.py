04017eea20c7de40dc67fa765943276420648da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:09 +0000

    Add package cfg-cli/0.1.12/cfg-cli-0.1.12.tar.gz

---
+++ packages/cfg-cli/cfg-cli-0.1.12.tar.gz/cfg-cli-0.1.12/src/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+api_url = "https://api.cofactorgenomics.com"
+
+
+# UNIT TESTING
+test_project = os.getenv("TEST_PROJECT")
+test_username = os.getenv("TEST_USERNAME")
+test_password = password = os.getenv("TEST_PASSWORD")
