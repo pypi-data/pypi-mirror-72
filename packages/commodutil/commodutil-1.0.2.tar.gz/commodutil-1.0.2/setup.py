3d803bf96f942d2aff7672fcc369f13fcd5f4fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package commodutil/1.0.2/commodutil-1.0.2.tar.gz

---
+++ packages/commodutil/commodutil-1.0.2.tar.gz/commodutil-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
+
+setuptools.setup(
+    name="commodutil",
+    version=version,
+    author="aeorxc",
+    author_email="author@example.com",
+    description="common commodity/oil analytics utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
+
