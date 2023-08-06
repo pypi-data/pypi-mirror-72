fcdbfdcb329b095fe856859e33808240a27a95b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:41 +0000

    Add package tinker-access-client/2020.6.27.23/tinker-access-client-2020.6.27.23.tar.gz

---
+++ packages/tinker-access-client/tinker-access-client-2020.6.27.23.tar.gz/tinker-access-client-2020.6.27.23/tinker_access_client/State.py
@@ -0,0 +1,8 @@
+
+class State(object):
+    INITIALIZED = 'initialized'
+    ESTOP = 'estop'
+    IDLE = 'idle'
+    IN_USE = 'in_use'
+    IN_TRAINING = 'in_training'
+    TERMINATED = 'terminated'
