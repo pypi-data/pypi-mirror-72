2d31fe9ececf47d520f7cf5736d8baaa4fa18d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:01 +0000

    Add package discrete-distrib/0.1/discrete_distrib-0.1.tar.gz

---
+++ packages/discrete-distrib/discrete_distrib-0.1.tar.gz/discrete_distrib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='discrete_distrib',
+      version='0.1',
+      description='Gaussian Distributions and Binomial Distributions',
+      packages=['discrete_distrib'],
+      author = 'Patrick Jose Roxas',
+      author_email = 'pjcroxas@neu.edu.ph',
+      zip_safe=False)
