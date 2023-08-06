85009c515dcd930b77d7349b67140d4d8b389165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:33 +0000

    Add package shizen-gengo/0.1.1/shizen_gengo-0.1.1.tar.gz

---
+++ packages/shizen-gengo/shizen_gengo-0.1.1.tar.gz/shizen_gengo-0.1.1/shizen_gengo/explore/__init__.py
@@ -0,0 +1,7 @@
+"""
+Functions to search for text in a pandas dataframe column.
+"""
+
+from .search_utils import search
+
+__all__ = ['search']
---
+++ packages/shizen-gengo/shizen_gengo-0.1.1.tar.gz/shizen_gengo-0.1.1/shizen_gengo/preprocess_dataframe/__init__.py
@@ -0,0 +1,7 @@
+"""
+Functions to modify a pandas dataframe.
+"""
+
+from .dataframe_utils import rename_col
+
+__all__ = ['rename_col']
