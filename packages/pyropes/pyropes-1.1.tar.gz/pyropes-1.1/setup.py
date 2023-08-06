d1fa3df64858ba331c033564fb324ee59795a268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:48 +0000

    Add package pyropes/1.1/pyropes-1.1.tar.gz

---
+++ packages/pyropes/pyropes-1.1.tar.gz/pyropes-1.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyropes",
+    version="1.1",
+    author="Mradul Tiwari",
+    author_email="complex.m15@gmail.com",
+    description="An implementation of Rope Data Structure in Python3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mradultiw/pyropes",
+    project_urls={
+    'Say Thanks!': 'https://www.paypal.me/mradultiw?locale.x=en_GB',
+    'Source': 'https://github.com/mradultiw/pyropes',
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        'Topic :: Utilities'
+        
+    ],
+    python_requires='>=3.6',
+)
