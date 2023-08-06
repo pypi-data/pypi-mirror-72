30531aad1616364d2444f4f2c10be9b006e42435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package randomgen/1.19.1/randomgen-1.19.1-cp37-cp37m-win32.whl

---
+++ packages/randomgen/randomgen-1.19.1-cp37-cp37m-win32.whl/randomgen/src/rdrand/rdrand.h
@@ -0,0 +1,42 @@
+#ifndef _RANDOMDGEN__RDRAND_H_
+#define _RANDOMDGEN__RDRAND_H_
+
+#include "../common/randomgen_config.h"
+#include "../common/randomgen_immintrin.h"
+
+typedef struct s_rdrand_state {
+  int status;
+} rdrand_state;
+
+
+int rdrand_capable(void);
+
+
+static INLINE uint64_t rdrand_next64(rdrand_state* state){
+#if defined(__RDRND__) && __RDRND__
+    uint64_t val;
+#if defined(__x86_64__) || defined(_M_X64)
+    state->status &= _rdrand64_step((long long unsigned int *)&val);
+#else
+    uint32_t low, high;
+    state->status &= _rdrand32_step(&low);
+    state->status &= _rdrand32_step(&high);
+    val = ((uint64_t)high)<< 32 | low;
+#endif
+    return val;
+#else
+    return UINT64_MAX;
+#endif
+}
+
+static INLINE uint32_t rdrand_next32(rdrand_state* state){
+#if defined(__RDRND__) && __RDRND__
+    uint32_t val;
+    state->status &= _rdrand32_step(&val);
+    return val;
+#else
+    return UINT32_MAX;
+#endif
+}
+
+#endif /* _RANDOMDGEN__RDRAND_H_ */
