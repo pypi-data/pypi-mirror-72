c2171f7e6f60a4f9afa0839170ebc605873a9f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package aquvitae/0.1.4/aquvitae-0.1.4.tar.gz

---
+++ packages/aquvitae/aquvitae-0.1.4.tar.gz/aquvitae-0.1.4/aquvitae/algo/base_torch.py
@@ -0,0 +1,49 @@
+from abc import ABCMeta, abstractmethod
+
+import torch
+import ignite
+
+
+class BaseTorch(object, metaclass=ABCMeta):
+    def __init__(self, config):
+        self.metrics = {"accuracy": ignite.metrics.Accuracy()}
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def set_model(self, teacher, student, optimizer):
+        self.teacher = teacher.to(self.device)
+        self.student = student.to(self.device)
+        self.optimizer = optimizer
+
+    @abstractmethod
+    def teach_step(self, x, y):
+        pass
+
+    def logging_metrics(self, labels, logits):
+        for name in self.metrics.keys():
+            self.metrics[name].update([logits, labels])
+
+    def reset_metrics(self):
+        for name in self.metrics.keys():
+            self.metrics[name].reset()
+
+    def set_metrics(self, metrics):
+        for name in metrics.keys():
+            assert isinstance(metrics.keys()[name], ignite.metrics.Metric)
+            self.metrics[name] = metrics
+
+    def get_metrics(self):
+        result = {}
+        for name in self.metrics.keys():
+            result[name] = self.metrics[name].compute()
+        return result
+
+    def test(self, dataset):
+        self.reset_metrics()
+        with torch.set_grad_enabled(False):
+            for x, y in dataset:
+                x, y = x.to(device), y.to(device)
+                logits = self.student(x)
+                self.logging_metrics(y, logits)
+        result = self.get_metrics()
+        self.reset_metrics()
+        return result
