f44467156cf02495a6e55869f9be5e93ccd3ee1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:17 +0000

    Add package exo-predict/1.0.4/exo_predict-1.0.4.tar.gz

---
+++ packages/exo-predict/exo_predict-1.0.4.tar.gz/exo_predict-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name='exo_predict',
+    version= '1.0.4',
+    description='exo_predict will help you determine how likely you are to find an exoplanet!',
+    url='https://github.com/Rob685/codeastro_ml_planets',
+    author='Jea Adams, Rob Tejada, Sofia Covarrubias',
+    packages=find_packages()
+)
