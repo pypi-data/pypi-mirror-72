bc4e16dca999e6c76a3f7d48285e20897a061253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:25 +0000

    Add package alphamini/0.1.3/alphamini-0.1.3.tar.gz

---
+++ packages/alphamini/alphamini-0.1.3.tar.gz/alphamini-0.1.3/mini/apis/__init__.py
@@ -0,0 +1,9 @@
+from ..apis import *
+from .cmdid import *
+from .errors import *
+from .base_api import MiniApiResultType
+from .api_action import MoveRobotDirection, RobotActionType, RobotAudioRecordControlType
+from .api_expression import RobotExpressionType, RobotBehaviorControlType, MouthLampColor, MouthLampMode
+from .api_observe import RobotPosture, HeadRacketType
+from .api_sence import ObjectRecogniseType, TakePictureType
+from .api_sound import TTSControlType, AudioStorageType, AudioSearchType
