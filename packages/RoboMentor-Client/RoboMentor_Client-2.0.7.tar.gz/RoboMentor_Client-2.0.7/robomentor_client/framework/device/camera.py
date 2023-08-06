4310be2d00b84ec0dfaa3b6e9a150e7ca0509888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:38 +0000

    Add package RoboMentor-Client/2.0.7/RoboMentor_Client-2.0.7.tar.gz

---
+++ packages/RoboMentor-Client/RoboMentor_Client-2.0.7.tar.gz/RoboMentor_Client-2.0.7/robomentor_client/framework/device/camera.py
@@ -0,0 +1,22 @@
+import os
+import threading
+
+class Camera:
+
+    def __init__(self, device):
+        self.status = False
+        self.device = device
+        self.micro = None
+
+        if not os.path.exists(device):
+            return
+
+        self.camera_task_thread = threading.Thread(target=self.camera_task)
+        self.camera_task_thread.start()
+
+    def camera_task(self):
+        self.status = True
+        self.micro = os.system("sudo /usr/local/lib/python3.7/dist-packages/robomentor_client/framework/micro/micro --device " + self.device)
+
+
+
