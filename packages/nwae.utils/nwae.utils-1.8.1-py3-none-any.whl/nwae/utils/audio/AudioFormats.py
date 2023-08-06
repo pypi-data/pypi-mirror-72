431668059b7aefec7c8426ef1148e66a177f4a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:15 +0000

    Add package nwae.utils/1.8.1/nwae.utils-1.8.1-py3-none-any.whl

---
+++ packages/nwae.utils/nwae.utils-1.8.1-py3-none-any.whl/nwae/utils/audio/AudioFormats.py
@@ -0,0 +1,55 @@
+# Need ffmpeg
+# > brew install ffmpeg
+from pydub import AudioSegment
+import re
+from nwae.utils.Log import Log
+from inspect import getframeinfo, currentframe
+
+
+class AudioFormats:
+
+    def __init__(self):
+        return
+
+    def convert_format(
+            self,
+            filepath,
+            to_format = 'wav'
+    ):
+        file_extension = re.sub(pattern='(.*[.])([a-zA-Z0-9]+$)', repl='\\2', string=filepath)
+        new_filepath = re.sub(pattern='[.][a-zA-Z0-9]+$', repl='.wav', string=filepath)
+        Log.info(
+            str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Convert "' + str(filepath) + '" with extension "' + str(file_extension)
+            + '" New filepath "' + str(new_filepath) + '"'
+        )
+        try:
+            track = AudioSegment.from_file(
+                file   = filepath,
+                format = file_extension
+            )
+            Log.info(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Converting "' + str(filepath) + '" to "' + str(new_filepath) + '"..'
+            )
+            file_handle = track.export(new_filepath, format=to_format)
+            file_handle.close()
+            Log.info(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Successful Conversion from "' + str(filepath) + '" to "' + str(new_filepath) + '"..'
+            )
+        except Exception as ex:
+            raise Exception(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Exception converting "' + str(filepath) + '" to "' + str(new_filepath)
+                + '": ' + str(ex)
+            )
+        return
+
+
+if __name__ == '__main__':
+    audio_file = 'music.m4a'
+    AudioFormats().convert_format(
+        filepath = audio_file
+    )
+    exit(0)
