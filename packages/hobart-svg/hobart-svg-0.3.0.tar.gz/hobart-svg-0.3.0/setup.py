10339bbac5416675d26334235a048a1d65f8d0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:56 +0000

    Add package hobart-svg/0.3.0/hobart-svg-0.3.0.tar.gz

---
+++ packages/hobart-svg/hobart-svg-0.3.0.tar.gz/hobart-svg-0.3.0/setup.py
@@ -0,0 +1,32 @@
+import importlib
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("requirements.txt") as f:
+    install_requires = f.read()
+
+setup(
+    name="hobart-svg",
+    version=importlib.import_module("hobart_svg").__version__,
+    description="Render polygons, polylines, and mesh cross sections to SVG",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Metabolize",
+    author_email="github@paulmelnikow.com",
+    url="https://github.com/lace/hobart-svg",
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
