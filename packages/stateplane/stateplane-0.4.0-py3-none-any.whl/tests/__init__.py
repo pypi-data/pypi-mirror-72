b9473ab6fcae955debafbe9ee25975cfcf5abde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:49 +0000

    Add package stateplane/0.4.0/stateplane-0.4.0-py3-none-any.whl

---
+++ packages/stateplane/stateplane-0.4.0-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of stateplane.
+# https://github.com/fitnr/stateplane
+
+# Licensed under the GPLv3 license:
+# http://http://opensource.org/licenses/GPL-3.0
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
+from . import test_stateplane
