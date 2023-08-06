17350a95a215e443d3db5f4e1780013c441bd57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:49 +0000

    Add package mem-test-probability/0.1.1/mem_test_probability-0.1.1.tar.gz

---
+++ packages/mem-test-probability/mem_test_probability-0.1.1.tar.gz/mem_test_probability-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mem_test_probability',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mem_test_probability'],
+      zip_safe=False)
