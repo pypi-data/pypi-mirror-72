b0ac7b938c2a55c23f372d17c36818f87caedcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package simple-distributions-v11/1.1/simple_distributions_v11-1.1.tar.gz

---
+++ packages/simple-distributions-v11/simple_distributions_v11-1.1.tar.gz/simple_distributions_v11-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='simple_distributions_v11',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['simple_distributions_v11'],
+      author = 'Aram hamidi',
+      author_email = 'aram1hamidi@gmail.com',
+      zip_safe=False)
