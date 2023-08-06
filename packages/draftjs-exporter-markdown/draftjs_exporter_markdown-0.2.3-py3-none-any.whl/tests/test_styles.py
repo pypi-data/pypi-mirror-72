9d0e60bee8aa50a0e85f068d0642085968e8dff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:47 +0000

    Add package draftjs-exporter-markdown/0.2.3/draftjs_exporter_markdown-0.2.3-py3-none-any.whl

---
+++ packages/draftjs-exporter-markdown/draftjs_exporter_markdown-0.2.3-py3-none-any.whl/tests/test_styles.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from draftjs_exporter.dom import DOM
+
+from draftjs_exporter_markdown.styles import inline_style
+
+
+class test_inline_style(TestCase):
+    def test_works(self):
+        self.assertEqual(DOM.render(inline_style('*')({
+            'children': 'test'
+        })), '*test*')
