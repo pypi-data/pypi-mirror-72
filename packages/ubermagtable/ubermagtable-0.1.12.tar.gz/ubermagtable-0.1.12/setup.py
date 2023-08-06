154b9dc87756338aed02b221b420785676e2e421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:08 +0000

    Add package ubermagtable/0.1.12/ubermagtable-0.1.12.tar.gz

---
+++ packages/ubermagtable/ubermagtable-0.1.12.tar.gz/ubermagtable-0.1.12/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ubermagtable',
+    version='0.1.12',
+    description=('Python package for manipulating '
+                 'OOMMF and mumax3 tabular data.'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://ubermag.github.io',
+    author=('Marijan Beg, Vanessa Nehruji, Sergii Mamedov, '
+            'Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr'),
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=['ubermagutil==0.2.6',
+                      'matplotlib>=3.2',
+                      'pandas>=1.0',
+                      'jupyterlab>=2.1',
+                      'ipywidgets>=7.5'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: English',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: Unix',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Topic :: Scientific/Engineering :: Visualization']
+)
---
+++ packages/ubermagtable/ubermagtable-0.1.12.tar.gz/ubermagtable-0.1.12/ubermagtable/util/__init__.py
@@ -0,0 +1,1 @@
+from .util import columns, units, data
