27ebbc700f6add8f851ba41624a9492f13299e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:33 +0000

    Add package ss-dist/0.1/ss-dist-0.1.tar.gz

---
+++ packages/ss-dist/ss-dist-0.1.tar.gz/ss-dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ss-dist',
+      version='0.1',
+      author="Shrangi soni",
+      author_email="ss.darksiders@gmail.com",
+      description="A small example package that calculates probability distributions",
+      packages=['ss-dist'],
+      zip_safe=False)
