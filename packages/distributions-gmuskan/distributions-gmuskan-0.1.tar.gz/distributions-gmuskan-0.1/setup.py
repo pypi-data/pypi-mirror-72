9ff47ffc6440663f961d307824ce71083aaca71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package distributions-gmuskan/0.1/distributions-gmuskan-0.1.tar.gz

---
+++ packages/distributions-gmuskan/distributions-gmuskan-0.1.tar.gz/distributions-gmuskan-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-gmuskan',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Muskan',
+      zip_safe=False)
