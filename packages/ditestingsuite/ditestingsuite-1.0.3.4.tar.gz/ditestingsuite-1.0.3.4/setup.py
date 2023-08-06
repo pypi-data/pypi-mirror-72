29378c7f7c020998c9832c7f5ab96d16b9346915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:58 +0000

    Add package ditestingsuite/1.0.3.4/ditestingsuite-1.0.3.4.tar.gz

---
+++ packages/ditestingsuite/ditestingsuite-1.0.3.4.tar.gz/ditestingsuite-1.0.3.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ditestingsuite',
+      version='1.0.3.4',
+      author='Tong Wang',
+      author_email='tong.wang@di.net.au',
+      license='MIT',
+      packages=['ditestingsuite'],
+      install_requires=['atlassian-python-api','PyYAML'],
+      zip_safe=False)
