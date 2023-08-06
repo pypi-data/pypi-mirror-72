73783f1bfd4536ade5e9fde323ea07eff54c1d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package binom-gaus-dist/0.2/binom_gaus_dist-0.2.tar.gz

---
+++ packages/binom-gaus-dist/binom_gaus_dist-0.2.tar.gz/binom_gaus_dist-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binom_gaus_dist',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['binom_gaus_dist'],
+      author='Boni Oloff Nugraha',
+      author_email='bonioloff@gmail.com',
+      zip_safe=False)
