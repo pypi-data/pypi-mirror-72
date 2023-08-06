2708cf727aaef0b69a3360d9bf62bea45ead5aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:15 +0000

    Add package RNdistributions/0.1/RNdistributions-0.1.tar.gz

---
+++ packages/RNdistributions/RNdistributions-0.1.tar.gz/RNdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='RNdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'MH Rajon',
+      author_email = 'rajon789@gmail.com',
+      zip_safe=False)
