9a0399f7e2bf133f93cd77668ca3f12d3646be39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:32 +0000

    Add package py3fdfsv2/2.2.4/py3fdfsv2-2.2.4.tar.gz

---
+++ packages/py3fdfsv2/py3fdfsv2-2.2.4.tar.gz/py3fdfsv2-2.2.4/test/test_config_file.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+CONF_FILE = '~/.local/etc/fdfs/client.conf'
+
+
+def test_use_storage_id():
+    from fdfs_client import client
+    client.get_tracker_conf(CONF_FILE)
+
+
+def test_upload():
+    from fdfs_client import client
+    cli = client.Fdfs_client(client.get_tracker_conf(CONF_FILE))
+    ret = cli.upload_appender_by_buffer(b'a', 'test')
+    remote_file_id = ret['Remote file_id']
+    cli.delete_file(remote_file_id)
