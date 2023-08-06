290ec9c64395681bbba5ed687c52cdb6842e6f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:23 +0000

    Add package sholz-distributions/0.1/sholz_distributions-0.1.tar.gz

---
+++ packages/sholz-distributions/sholz_distributions-0.1.tar.gz/sholz_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sholz_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sholz_distributions'],
+      author= 'Solamipe Akinola',
+      zip_safe=False)
