6bc47da0b212b6e8290fb8f5a64ef33376f20cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:43 +0000

    Add package napoleontoolbox/2.5.35/napoleontoolbox-2.5.35.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.5.35.tar.gz/napoleontoolbox-2.5.35/napoleontoolbox/forecasting/lgbm_model_wrapper.py
@@ -0,0 +1,60 @@
+from napoleontoolbox.mixing import mixing_utility
+
+import lightgbm as lgb
+
+import numpy as np
+
+from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, RandomizedSearchCV, train_test_split
+from napoleontoolbox.forecasting import preprocessing_features
+
+
+
+class LGBMModel():
+
+    def __init__(self, preprocessing = False, threshold = 0.6, number_per_year=252):
+        self.model = lgb.LGBMRegressor()
+        self.preprocessing = preprocessing
+        self.threshold = threshold
+        self.columns_to_drop = np.array([])
+        self.binarization_threshold = dict()
+
+    def calibrate(self, X, y):#, method = 'standard'):
+        param_grid = {
+            'boosting_type': ['gbdt'],
+            'num_leaves': [int(x) for x in np.linspace(start=10, stop=30, num=10)],
+            'max_depth': [int(x) for x in np.linspace(start=-1, stop=7, num=3)],
+            'learning_rate': [0.001,0.01,0.2],
+            'n_estimators': [int(x) for x in np.linspace(start=40, stop=120, num=40)]
+        }
+        gbm_grid = GridSearchCV(self.model, param_grid, cv=2)
+        gbm_grid.fit(X.fillna(0), y.fillna(0))
+        print('Best parameters found by grid search are:', gbm_grid.best_params_)
+        best_params = gbm_grid.best_params_
+        self.model.set_params(**best_params)
+        return best_params
+
+    def fit(self, X_train, y_train, X_val, y_val):#, method = 'standard'):
+        if self.preprocessing:
+            self.columns_to_drop = preprocessing_features.most_correlated_features(X_train,self.threshold)
+            X_train = X_train.drop(self.columns_to_drop,axis=1)
+            X_val = X_val.drop(self.columns_to_drop,axis=1)
+
+            self.binarization_threshold = preprocessing_features.get_binarization_threshold(X_train)
+            X_train = preprocessing_features.binarize_features(X_train,self.binarization_threshold)
+            X_val = preprocessing_features.binarize_features(X_val, self.binarization_threshold)
+
+        self.model.fit(X_train, y_train, eval_set=[(X_val, y_val)], eval_metric=['l1', 'l2'], early_stopping_rounds=5,verbose=0)
+
+    def predict(self, X_test):#, method = 'standard'):
+        if self.preprocessing:
+            X_test = X_test.drop(self.columns_to_drop, axis=1)
+            X_test = preprocessing_features.binarize_features(X_test, self.binarization_threshold)
+
+        y_pred = self.model.predict(X_test)
+        return y_pred
+
+    def get_features_importance(self, features_names):#, method = 'standard'):
+        run_importances = {}
+        for (name, imp) in zip(features_names, self.model.feature_importances_):
+            run_importances[name] = imp
+        return run_importances
