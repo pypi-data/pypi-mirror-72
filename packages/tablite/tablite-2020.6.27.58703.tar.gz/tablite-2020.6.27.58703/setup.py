0317dfe464c834d386541ac1943dc2dd37938c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:42 +0000

    Add package tablite/2020.6.27.58703/tablite-2020.6.27.58703.tar.gz

---
+++ packages/tablite/tablite-2020.6.27.58703.tar.gz/tablite-2020.6.27.58703/setup.py
@@ -0,0 +1,50 @@
+"""
+tablite
+"""
+build_tag = "0c4b1cc32003d9b99d5617a9eb4db64253cddc992ada3236402d9abf3d0d938c"
+from setuptools import setup
+from pathlib import Path
+
+
+folder = Path(__file__).parent
+file = "README.md"
+readme = folder / file
+assert isinstance(readme, Path)
+assert readme.exists(), readme
+with open(str(readme), encoding='utf-8') as f:
+    long_description = f.read()
+
+keywords = list({
+    'tables'
+})
+
+keywords.sort(key=lambda x: x.lower())
+
+
+setup(
+    name="tablite",
+    version="2020.6.27.58703",
+    url="https://github.com/root-11/tablite",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="bjorn.madsen@operationsresearchgroup.com",
+    description="A table crunching library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=keywords,
+    packages=["table"],
+    include_package_data=True,
+    data_files=[(".", ["LICENSE", "README.md"])],
+    platforms="any",
+    install_requires=['xlrd'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
+
+
