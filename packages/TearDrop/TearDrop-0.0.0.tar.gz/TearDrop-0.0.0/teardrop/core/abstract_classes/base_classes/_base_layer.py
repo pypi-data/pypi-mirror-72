b1e8d3c5d66153f6f63385ea574b0b0fe55eea83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:19 +0000

    Add package TearDrop/0.0.0/TearDrop-0.0.0.tar.gz

---
+++ packages/TearDrop/TearDrop-0.0.0.tar.gz/TearDrop-0.0.0/teardrop/core/abstract_classes/base_classes/_base_layer.py
@@ -0,0 +1,14 @@
+import teardrop.core.math.activations as math
+from abc import ABCMeta
+
+
+class Layer(metaclass=ABCMeta):
+
+    def _forward(self, X):
+        raise NotImplementedError("Unfortunately, the function hasn't been added yet.")
+
+    def initialize(self, neurons):
+        raise NotImplementedError("Unfortunately, the function hasn't been added yet.")
+
+    def _backward(self, last_derivative, lr, optimizer):
+        raise NotImplementedError("Unfortunately, the function hasn't been added yet.")
