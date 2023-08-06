2c98b0031768742e204dac6f4c931a5d60660af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:19 +0000

    Add package mal-api/0.1.1/mal-api-0.1.1.tar.gz

---
+++ packages/mal-api/mal-api-0.1.1.tar.gz/mal-api-0.1.1/mal/config.py
@@ -0,0 +1,5 @@
+_MAL_ENDPOINT = "https://myanimelist.net/"
+_NOT_FOUND_TITLE = "404 Not Found - MyAnimeList.net "
+_NO_BACKGROUND_INFO = "No background information has been added to this title. Help improve our database by adding background information here."
+
+TIMEOUT = 5
