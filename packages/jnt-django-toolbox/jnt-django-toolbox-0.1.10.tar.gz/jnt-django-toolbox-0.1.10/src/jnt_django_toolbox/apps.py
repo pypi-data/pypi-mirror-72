774c5aa4aaef0819fe795cd41fc98103ab64a855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package jnt-django-toolbox/0.1.10/jnt-django-toolbox-0.1.10.tar.gz

---
+++ packages/jnt-django-toolbox/jnt-django-toolbox-0.1.10.tar.gz/jnt-django-toolbox-0.1.10/src/jnt_django_toolbox/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig as DjangoAppConfig
+
+
+class AppConfig(DjangoAppConfig):
+    name = "jnt_django_toolbox"
+    verbose_name = "Django toolbox"
