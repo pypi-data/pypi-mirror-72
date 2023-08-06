4dc8a9eb1ba79233d0cbc1a39f636c9eed97f6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:30 +0000

    Add package rvnewop/0.5.6/rvnewop-0.5.6.tar.gz

---
+++ packages/rvnewop/rvnewop-0.5.6.tar.gz/rvnewop-0.5.6/rvnewop/Program.py
@@ -0,0 +1,36 @@
+from . import RV32
+
+import sys
+
+
+class Program:
+    """A Program is a collection of instructions which are mapped to pc values"""
+
+    def __init__(self, isa="32I"):
+        self.rv = RV32(isa)
+        self.instructions = {}  # maps pc value -> RVInstruction
+        self.frequencies = {}  # maps pc value -> frequency of instruction
+
+    def addInstruction(self, pc, hexd, freq):
+        """Adds an instruction to a Program given a PC value
+        and the instruction hex value
+
+        pc - (int) program counter (pc)
+        hexd - (str) instruction encoded in hexadecimal
+        freq - (int) amount of times instruction shows up"""
+        self.instructions[pc] = self.rv.decodeHex(hexd)
+
+        # TODO decouple storing frequencies from program?
+        self.frequencies[pc] = freq
+
+    def getInstructionNameSet(self):
+        if not self.instructionNameSet:
+            self.instructionNameSet = {
+                self.instructions[pc].name for pc in self.instructions
+            }
+        return self.instructionNameSet
+
+    def printAll(self, file=sys.stdout):
+        """Prints out all instructions to file (default is stdout)"""
+        for pc in self.instructions:
+            print("{}: {}".format(hex(pc), self.instructions[pc]), file=file)
