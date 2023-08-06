2b8b8fe45710cbcd412f7d9f1c926a1ee752a625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:19 +0000

    Add package cfdi-admin/0.0.7/cfdi_admin-0.0.7.tar.gz

---
+++ packages/cfdi-admin/cfdi_admin-0.0.7.tar.gz/cfdi_admin-0.0.7/cfdi_admin/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides cfdi_admin version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update cfdi_admin` to change this file.
+
+from incremental import Version
+
+__version__ = Version('cfdi_admin', 0, 0, 7)
+__all__ = ["__version__"]
