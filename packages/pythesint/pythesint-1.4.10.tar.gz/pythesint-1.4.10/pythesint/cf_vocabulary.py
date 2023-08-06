7cd76c2de5718bc77dc26a68173d1c53f4f80fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:52 +0000

    Add package pythesint/1.4.10/pythesint-1.4.10.tar.gz

---
+++ packages/pythesint/pythesint-1.4.10.tar.gz/pythesint-1.4.10/pythesint/cf_vocabulary.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+
+from xml.dom.minidom import parseString
+import requests
+from pythesint.json_vocabulary import JSONVocabulary
+
+
+class CFVocabulary(JSONVocabulary):
+    def _fetch_online_data(self):
+        # Note the version number... Would probably be better to make it always
+        # take the last version..
+        try:
+            r = requests.get(self.url)
+        except requests.RequestException:
+            print("Could not get the vocabulary file at '{}'".format(self.url))
+            raise
+        dom = parseString(r.text.encode('utf-8').strip())
+        # should only contain the standard_name_table:
+        node = dom.childNodes[0]
+
+        cf_list = []
+        for entry in node.getElementsByTagName('entry'):
+            standard_name = entry.attributes['id'].value
+            units = ''
+            if entry.getElementsByTagName(
+                    'canonical_units')[0].childNodes:
+                units = entry.getElementsByTagName(
+                    'canonical_units')[0].childNodes[0].nodeValue
+            grib = ''
+            if entry.getElementsByTagName('grib')[0].childNodes:
+                grib = entry.getElementsByTagName(
+                    'grib')[0].childNodes[0].nodeValue
+            amip = ''
+            if entry.getElementsByTagName('amip')[0].childNodes:
+                amip = entry.getElementsByTagName(
+                    'amip')[0].childNodes[0].nodeValue
+            if entry.getElementsByTagName('description')[0].childNodes:
+                descr = entry.getElementsByTagName(
+                    'description')[0].childNodes[0].nodeValue
+            stdname = {
+                    'standard_name': standard_name,
+                    'canonical_units': units,
+                    'grib': grib,
+                    'amip': amip,
+                    'description': descr
+                }
+            cf_list.append(stdname)
+        return cf_list
+
