06ea14d96f815a74b0812a675d5a7e2b07716c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:23 +0000

    Add package gibiga-distributions/0.0.2/gibiga_distributions-0.0.2.tar.gz

---
+++ packages/gibiga-distributions/gibiga_distributions-0.0.2.tar.gz/gibiga_distributions-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+    
+setup(name='gibiga_distributions',
+      version='0.0.2',
+      description='Gaussian distributions',
+      author="Greg Izzo",
+      author_email="greg.izzo@gmail.com",
+      long_description="GIBIGA Gaussian and Binomial Distribution",
+      packages=['gibiga_distributions'],
+      zip_safe=False)
