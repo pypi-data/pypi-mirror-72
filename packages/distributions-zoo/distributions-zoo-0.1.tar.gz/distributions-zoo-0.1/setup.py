38969afd11c1561ea6fbf2e49ff39617bc05bfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package distributions-zoo/0.1/distributions-zoo-0.1.tar.gz

---
+++ packages/distributions-zoo/distributions-zoo-0.1.tar.gz/distributions-zoo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-zoo',
+      version='0.1',
+      description='Gaussian, Binomial distributions package',
+      packages=['distributions'],
+      zip_safe=False)
