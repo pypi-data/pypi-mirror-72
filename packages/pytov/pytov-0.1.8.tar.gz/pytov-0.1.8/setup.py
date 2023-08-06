ee3f0b3f8be6af6dcf5277b9617772695e85a934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package pytov/0.1.8/pytov-0.1.8.tar.gz

---
+++ packages/pytov/pytov-0.1.8.tar.gz/pytov-0.1.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+print(setuptools.find_packages())
+
+setuptools.setup(
+    name="pytov",
+    version="0.1.8",
+    license='MIT',
+    author="Yuval Rosen",
+    author_email="yuv.rosen@gmail.com",
+    description="Write python with C like syntax",
+    long_description=''.join(open('README.txt', encoding='utf-8').readlines()),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yuvix25/pytov",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+
+    entry_points={
+        'console_scripts': [
+            'pytov=pytov.pytov:run',
+        ],
+    },
+)
