ac4d828ff9529952089e181e4236198f7fefdd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:30 +0000

    Add package nit-distributions/0.1/nit-distributions-0.1.tar.gz

---
+++ packages/nit-distributions/nit-distributions-0.1.tar.gz/nit-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nit-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nit-distributions'],
+      author = 'Nitin Bhandari',
+      zip_safe=False)
