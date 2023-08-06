9e166ab9d6d3ae18af3419be122047fef8843492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:38 +0000

    Add package camera-intrinsics/0.0.1/camera_intrinsics-0.0.1.tar.gz

---
+++ packages/camera-intrinsics/camera_intrinsics-0.0.1.tar.gz/camera_intrinsics-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="camera_intrinsics",
+    version="0.0.1",
+    author="Lukas Koestler",
+    author_email="lkskstlr@gmail.com",
+    description="A package for applying image transformations to the camera intrinsics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://lukaskoestler.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
