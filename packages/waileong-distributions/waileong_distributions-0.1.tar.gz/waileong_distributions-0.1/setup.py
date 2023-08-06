fc5efd29d8134e53b6d0b4cf767e90d2abc1d8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package waileong-distributions/0.1/waileong_distributions-0.1.tar.gz

---
+++ packages/waileong-distributions/waileong_distributions-0.1.tar.gz/waileong_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='waileong_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['waileong_distributions'],
+      zip_safe=False)
