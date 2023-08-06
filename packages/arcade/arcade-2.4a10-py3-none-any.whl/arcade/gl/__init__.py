2565ba23519a6d838d174aed830983b3a58c3fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:51 +0000

    Add package arcade/2.4a10/arcade-2.4a10-py3-none-any.whl

---
+++ packages/arcade/arcade-2.4a10-py3-none-any.whl/arcade/gl/__init__.py
@@ -0,0 +1,9 @@
+
+# Thank you Benjamin Moran for writing part of this code!
+# https://bitbucket.org/HigashiNoKaze/pyglet/src/shaders/pyglet/graphics/shader.py
+
+
+from .context import Context
+from .types import BufferDescription
+from .exceptions import ShaderException
+from .enums import *
