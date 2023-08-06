de66d54156ee6deab578980d4e9295e1a8d7638e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package a5orchestrator/1.0.1/A5Orchestrator-1.0.1.tar.gz

---
+++ packages/a5orchestrator/A5Orchestrator-1.0.1.tar.gz/A5Orchestrator-1.0.1/A5Package/OrchestratorAPI.py
@@ -0,0 +1,44 @@
+import requests, json
+import json
+
+
+def get_authentication_key(source_url,tenancy_name,user_name,password):
+    url = source_url+"/api/Account/Authenticate"
+    data = {
+        "tenancyName": tenancy_name,
+        "usernameOrEmailAddress": user_name,
+        "password": password
+        }
+    responce = requests.post(url,data)
+    if(responce.status_code == 200):
+        responce_json = responce.json()
+        return responce_json
+    else:
+        return responce.status_code
+
+def get_api_call(url,key,file_case = False):
+    head = {
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer " + key
+    }
+    responce = requests.get(url,headers=head)
+    
+    if(responce.status_code == 200):
+        if file_case == False:
+            return responce.json()
+        elif file_case == True:
+            return responce
+    else:
+        return responce.status_code
+
+def post_api_call(url,key,data = None):
+    head = {
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer " + key
+    }
+    responce = requests.post(url,headers=head,data=json.dumps(data))
+    
+    if(responce.status_code == 200):
+        return responce.json()
+    else:
+        return responce.status_code
---
+++ packages/a5orchestrator/A5Orchestrator-1.0.1.tar.gz/A5Orchestrator-1.0.1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+# specify requirements of your package here 
+REQUIREMENTS = ['requests','json'] 
+
+# some more details 
+CLASSIFIERS = [ 
+	'Development Status :: 1 - Planning', 
+	'Intended Audience :: Developers', 
+	'Topic :: Software Development :: Libraries', 
+	'License :: OSI Approved :: MIT License', 
+    'Framework :: Flask',
+    'Programming Language :: Python', 
+	'Programming Language :: Python :: 2', 
+	'Programming Language :: Python :: 2.6', 
+	'Programming Language :: Python :: 2.7', 
+	'Programming Language :: Python :: 3', 
+	'Programming Language :: Python :: 3.3', 
+	'Programming Language :: Python :: 3.4', 
+	'Programming Language :: Python :: 3.5', 
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Typing :: Typed'
+	] 
+
+# calling the setup function 
+setuptools.setup(name='A5Orchestrator', 
+	version='1.0.1', 
+	description='This module helps to intigrate Orchestrator with python scripts ', 
+	long_description='This module helps to intigrate Orchestrator with python scripts ', 
+	long_description_content_type="text/markdown",
+	url='https://github.com/akvdkharnath/A5Orchestrator.git', 
+	author='Harnath Atmakuri', 
+	author_email='akvdkharnath@gmail.com', 
+	license='MIT', 
+	packages=['A5Package'], 
+	include_package_data=True,
+	classifiers=CLASSIFIERS, 
+	install_requires=REQUIREMENTS
+	) 
