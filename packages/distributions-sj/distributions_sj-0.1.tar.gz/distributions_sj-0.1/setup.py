849064263fe220ab50cd6b129d90212b36ecf71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package distributions-sj/0.1/distributions_sj-0.1.tar.gz

---
+++ packages/distributions-sj/distributions_sj-0.1.tar.gz/distributions_sj-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sj',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sj'],
+      zip_safe=False)
