b8c0a4dcdd197dfad3225d163688a00a7c6bbdaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:20 +0000

    Add package trustii-ml/0.0.5/trustii-ml-0.0.5.tar.gz

---
+++ packages/trustii-ml/trustii-ml-0.0.5.tar.gz/trustii-ml-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trustii-ml", # Replace with your own username
+    version="0.0.5",
+    author="tiwars",
+    author_email="mockinbird24@gmail.com",
+    description="To add Trustii Class to Python Path",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
