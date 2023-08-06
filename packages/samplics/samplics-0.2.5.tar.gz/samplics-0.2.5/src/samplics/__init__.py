b924b25879fe4825a7768d485a973a241cafc135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package samplics/0.2.5/samplics-0.2.5.tar.gz

---
+++ packages/samplics/samplics-0.2.5.tar.gz/samplics-0.2.5/src/samplics/__init__.py
@@ -0,0 +1,11 @@
+from samplics.estimation import *
+from samplics.sampling import *
+from samplics.weighting import *
+from samplics.sae import *
+
+from samplics.utils.formats import array_to_dict
+from samplics.utils.basic_functions import transform
+
+# __all__ = ["sampling", "weighting", "estimation", "sae"]
+
+__version__ = "0.2.5"
