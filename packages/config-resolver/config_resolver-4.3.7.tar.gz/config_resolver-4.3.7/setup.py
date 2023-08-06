e2ecb4ad2fccd0d70ff658332016d1a57594f97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:15 +0000

    Add package config-resolver/4.3.7/config_resolver-4.3.7.tar.gz

---
+++ packages/config-resolver/config_resolver-4.3.7.tar.gz/config_resolver-4.3.7/setup.py
@@ -0,0 +1,46 @@
+import sys
+
+from setuptools import find_packages, setup
+
+PACKAGE = "config_resolver"
+NAME = "config_resolver"
+DESCRIPTION = "A small package to automatically find a configuration file."
+AUTHOR = "Michel Albert"
+AUTHOR_EMAIL = "michel@albert.lu"
+VERSION = open("config_resolver/version.txt").read().strip()
+
+DEPENDENCIES = []  # type: ignore
+if sys.version_info < (3, 0):
+    DEPENDENCIES.append('typing')
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    url='https://github.com/exhuma/config_resolver',
+    long_description=open("README.rst").read(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="MIT",
+    include_package_data=True,
+    packages=['config_resolver'],
+    install_requires=DEPENDENCIES,
+    requires=DEPENDENCIES,
+    package_data={
+        'config_resolver': ['py.typed'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 '
+        '(LGPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
