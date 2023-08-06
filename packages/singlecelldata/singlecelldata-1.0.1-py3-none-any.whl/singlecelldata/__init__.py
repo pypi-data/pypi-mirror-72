cc417d5fa86dee4ae6aa1db8af093aa1613004bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:22 +0000

    Add package singlecelldata/1.0.1/singlecelldata-1.0.1-py3-none-any.whl

---
+++ packages/singlecelldata/singlecelldata-1.0.1-py3-none-any.whl/singlecelldata/__init__.py
@@ -0,0 +1,13 @@
+"""
+SingleCellData 
+==============
+
+A Python package that contains a class for managing single-cell RNA-seq datasets.
+See https://edwinv87.github.io/singlecelldata/ for more information and documentation. 
+
+"""
+
+from .singlecell import SingleCell
+
+
+__all__ = ['SingleCell']
