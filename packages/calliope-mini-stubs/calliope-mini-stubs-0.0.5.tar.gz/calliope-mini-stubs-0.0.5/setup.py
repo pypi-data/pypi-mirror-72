0dd3b4e82d61ef6704b7e10b3a43407fe9fdfc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:40 +0000

    Add package calliope-mini-stubs/0.0.5/calliope-mini-stubs-0.0.5.tar.gz

---
+++ packages/calliope-mini-stubs/calliope-mini-stubs-0.0.5.tar.gz/calliope-mini-stubs-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="calliope-mini-stubs",
+    version="0.0.5",
+    author="kopp",
+    description="Provide stubs with documentation for the MicroPython running on Calliope Mini",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kopp/calliope-mini-stubs",
+    packages=["calliope_mini", "radio", "utime"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
