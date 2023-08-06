dd8285d953df4549a62c46a5e09470a84e53f977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:13 +0000

    Add package probability-dist-gauss-binomial/1.0/probability_dist_gauss_binomial-1.0.tar.gz

---
+++ packages/probability-dist-gauss-binomial/probability_dist_gauss_binomial-1.0.tar.gz/probability_dist_gauss_binomial-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_dist_gauss_binomial',
+      version='1.0',
+      description='Gaussian distribution and Binomial distribution',
+      packages=['probability_dist_gauss_binomial'],
+      author = 'Akhil Kumar',
+      author_email='akhilkumar.ganji24@gmail.com',
+      zip_safe=False)
