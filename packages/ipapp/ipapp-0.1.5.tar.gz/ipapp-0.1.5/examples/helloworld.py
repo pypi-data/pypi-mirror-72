8c6b1ce27d1168c8fc32562951f804492d1f8c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:52 +0000

    Add package ipapp/0.1.5/ipapp-0.1.5.tar.gz

---
+++ packages/ipapp/ipapp-0.1.5.tar.gz/ipapp-0.1.5/examples/helloworld.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+from aiohttp import web
+
+from ipapp import BaseApplication, BaseConfig, main
+from ipapp.http.server import Server, ServerConfig, ServerHandler
+
+
+class Config(BaseConfig):
+    http: ServerConfig
+
+
+class HttpHandler(ServerHandler):
+    async def prepare(self) -> None:
+        self.server.add_route('GET', '/', self.home)
+
+    async def home(self, request: web.Request) -> web.Response:
+        return web.Response(text='Hello, world!')
+
+
+class App(BaseApplication):
+    def __init__(self, cfg: Config) -> None:
+        super().__init__(cfg)
+        self.add('srv', Server(cfg.http, HttpHandler()))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main(sys.argv, '0.0.1', App, Config)
