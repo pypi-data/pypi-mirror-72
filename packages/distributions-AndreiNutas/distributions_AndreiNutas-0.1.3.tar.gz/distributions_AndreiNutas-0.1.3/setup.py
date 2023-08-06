0be4bc8ddf52fda4bdc58dae0aaf26e387632cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:26 +0000

    Add package distributions-AndreiNutas/0.1.3/distributions_AndreiNutas-0.1.3.tar.gz

---
+++ packages/distributions-AndreiNutas/distributions_AndreiNutas-0.1.3.tar.gz/distributions_AndreiNutas-0.1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_AndreiNutas',
+      version='0.1.3',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_AndreiNutas'],
+      author = 'Andrei Nutas',
+      zip_safe=False)
