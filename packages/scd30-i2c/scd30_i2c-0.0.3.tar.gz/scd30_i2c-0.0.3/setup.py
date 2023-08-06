f277eba5dfded916c70a37f0af39ede863217bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:30 +0000

    Add package scd30-i2c/0.0.3/scd30_i2c-0.0.3.tar.gz

---
+++ packages/scd30-i2c/scd30_i2c-0.0.3.tar.gz/scd30_i2c-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scd30_i2c",
+    version="0.0.3",
+    author="RequestForCoffee",
+    description="SCD30 CO₂ sensor Python driver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RequestForCoffee/scd30",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.7.3',
+)
