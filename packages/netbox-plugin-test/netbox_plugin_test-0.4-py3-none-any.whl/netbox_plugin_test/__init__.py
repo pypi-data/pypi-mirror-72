b31acb47e112aca4e9e5b0ea7080590571ea9a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:56 +0000

    Add package netbox-plugin-test/0.4/netbox_plugin_test-0.4-py3-none-any.whl

---
+++ packages/netbox-plugin-test/netbox_plugin_test-0.4-py3-none-any.whl/netbox_plugin_test/__init__.py
@@ -0,0 +1,13 @@
+from extras.plugins import PluginConfig
+
+class DeviceScannerConfig(PluginConfig):
+    name = 'netbox_plugin_test'
+    verbose_name = 'Plugin Tester'
+    decription = 'A Netbox plugin test'
+    version = '0.3'
+    author = 'Sean Collins'
+    author_email = 'collsean@umich.edu'
+    base_url = 'plugin-test'
+    required_settings = []
+
+config = DeviceScannerConfig
