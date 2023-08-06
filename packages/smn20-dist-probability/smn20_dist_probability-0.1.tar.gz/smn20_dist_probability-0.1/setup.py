52f2fa3143baeb9a6440a121a41113bbb9aa78bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:54 +0000

    Add package smn20-dist-probability/0.1/smn20_dist_probability-0.1.tar.gz

---
+++ packages/smn20-dist-probability/smn20_dist_probability-0.1.tar.gz/smn20_dist_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='smn20_dist_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['smn20_dist_probability'],
+      author='smn20',
+      zip_safe=False)
