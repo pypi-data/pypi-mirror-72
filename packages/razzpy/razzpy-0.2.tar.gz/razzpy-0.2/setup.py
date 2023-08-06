1abc365cc5082861c2ea5f85783a893e8d6a025f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:44 +0000

    Add package razzpy/0.2/razzpy-0.2.tar.gz

---
+++ packages/razzpy/razzpy-0.2.tar.gz/razzpy-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='razzpy',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['razzpy'],
+      author = 'Ridho Gani',
+      author_email = 'ridhoadhadigani@gmail.com',
+      zip_safe=False)
