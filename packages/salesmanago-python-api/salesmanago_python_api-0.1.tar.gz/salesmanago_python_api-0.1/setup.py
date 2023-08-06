957a48465bc47228d932d04868f00461fd133d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:54 +0000

    Add package salesmanago-python-api/0.1/salesmanago_python_api-0.1.tar.gz

---
+++ packages/salesmanago-python-api/salesmanago_python_api-0.1.tar.gz/salesmanago_python_api-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='salesmanago_python_api',
+      version='0.1',
+      description='Client for Salesmanago API.',
+      url='https://github.com/haloween/salesmanago-python-api',
+      keywords = "salesmanago, api",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      author='Tomasz Utracki-Janeta',
+      author_email='halgravity+githubrepo@gmail.com',
+      license='MIT',
+      zip_safe=True,
+      packages=['salesmanago_python_api'],
+      include_package_data=True,
+      install_requires=[
+          'requests'
+      ]
+      )
