74e9e3e53fb3e02a82a7319c66e088db7e504369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:34 +0000

    Add package margot/0.12.2/margot-0.12.2.tar.gz

---
+++ packages/margot/margot-0.12.2.tar.gz/margot-0.12.2/margot/tests/test_signals.py
@@ -0,0 +1,16 @@
+from margot import Symbol, MargotDataFrame, Ratio, BaseAlgo, Position, BackTest
+from margot import finance, cboe, alphavantage as av
+
+
+def test_algo():
+
+    class Index(Symbol):
+        close = cboe.Column(time_series='close')
+
+    class VXBasis(MargotDataFrame):
+        vixm = Index(symbol='VIX', trading_calendar='NYSE')
+
+    class MyAlgo(BaseAlgo):
+        data = VXBasis()
+
+    myalgo = MyAlgo()
