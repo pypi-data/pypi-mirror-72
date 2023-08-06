4478931936a0a3785209825917051618cd4c2297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:47 +0000

    Add package soft-404/0.3.1/soft-404-0.3.1.tar.gz

---
+++ packages/soft-404/soft-404-0.3.1.tar.gz/soft-404-0.3.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='soft-404',
+    version='0.3.1',
+    author='Konstantin Lopuhin',
+    author_email='kostia.lopuhin@gmail.com',
+    description='A classifier for detecting soft 404 pages',
+    license='MIT',
+    url='https://github.com/TeamHG-Memex/soft404',
+    packages=['soft404'],
+    include_package_data=True,
+    install_requires=[
+        'langdetect>=1.0.8',
+        'lxml>=4.5.0',
+        'numpy>=1.18.4',
+        'parsel>=1.6.0',
+        'scikit-learn>=0.23.0',
+        'joblib>=0.14.1',
+        'scipy>=1.4.1',
+        'webstruct>=0.6',
+        'html_text>=0.5.1',
+    ],
+    long_description=readme,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
