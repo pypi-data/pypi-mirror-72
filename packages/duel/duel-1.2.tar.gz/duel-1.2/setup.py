07e0c06ca5a566d9af63029a530eb3ad08c06eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:03 +0000

    Add package duel/1.2/duel-1.2.tar.gz

---
+++ packages/duel/duel-1.2.tar.gz/duel-1.2/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="duel",
+    version="1.2",
+    description="Locally Multiplayer Dueling game",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexisDougherty13/Duel",
+    author="Python Buds",
+    author_email="zachary.s1110@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["duel"],
+    include_package_data=True,
+    install_requires=["pygame"],
+    entry_points={
+        "console_scripts": [
+            "duel=duel.theMain:__main__",
+        ]
+    },
+)
