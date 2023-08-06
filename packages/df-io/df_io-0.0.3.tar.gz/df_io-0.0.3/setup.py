66931fc525a2eefcafd76cdc697219b8cc3ea3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:18 +0000

    Add package df-io/0.0.3/df_io-0.0.3.tar.gz

---
+++ packages/df-io/df_io-0.0.3.tar.gz/df_io-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import io
+import re
+import setuptools
+
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    long_description = f.read()
+
+with io.open("df_io/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+
+setuptools.setup(
+    name="df_io",
+    version=version,
+    author="NAGY, Attila",
+    author_email="nagy.attila@gmail.com",
+    description="Helpers for doing IO with Pandas DataFrames",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Mikata-Project/df_io',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 's3fs'],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
