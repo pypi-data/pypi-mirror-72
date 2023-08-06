3f696b77b716f2174868ea29d59fdc7b932addac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:18 +0000

    Add package duel/1.0.0.10/duel-1.0.0.10.tar.gz

---
+++ packages/duel/duel-1.0.0.10.tar.gz/duel-1.0.0.10/duel/__main__.py
@@ -0,0 +1,12 @@
+import duel.window
+import duel.gameEngine
+from duel import *
+import pygame
+
+def main():
+    duel.gameEngine.mainMenu(duel.window.startScreen())
+
+
+if __name__ == '__main__':
+    main()
+
