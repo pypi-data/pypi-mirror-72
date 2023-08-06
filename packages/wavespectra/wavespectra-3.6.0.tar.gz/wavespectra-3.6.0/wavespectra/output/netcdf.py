d55740edd59ecbe12c8df2bf1374f876ff1f8aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:39 +0000

    Add package wavespectra/3.6.0/wavespectra-3.6.0.tar.gz

---
+++ packages/wavespectra/wavespectra-3.6.0.tar.gz/wavespectra-3.6.0/wavespectra/output/netcdf.py
@@ -0,0 +1,32 @@
+"""Generic netCDF output plugin."""
+from wavespectra.core.attributes import attrs
+
+
+def to_netcdf(
+    self,
+    filename,
+    specname=attrs.SPECNAME,
+    ncformat="NETCDF4",
+    compress=True,
+    time_encoding={"units": "days since 1900-01-01"},
+):
+    """Write spectra in netCDF format using wavespectra conventions.
+
+    Args:
+        - filename (str): name of output netcdf file.
+        - specname (str): name of spectra variable in dataset.
+        - ncformat (str): netcdf format for output, see options in native
+          to_netcdf method.
+        - compress (bool): if True output is compressed, has no effect for
+          NETCDF3.
+        - time_encoding (dict): force standard time units in output files.
+
+    """
+    other = self.copy(deep=True)
+    encoding = {}
+    if compress:
+        for ncvar in other.data_vars:
+            encoding.update({ncvar: {"zlib": True}})
+    if attrs.TIMENAME in other:
+        other.time.encoding.update(time_encoding)
+    other.to_netcdf(filename, format=ncformat, encoding=encoding)
