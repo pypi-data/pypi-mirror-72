319c8ca6f9a46fb41be0fc444120e0803ecdc77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package rlrunner/1.0.1/rlrunner-1.0.1.tar.gz

---
+++ packages/rlrunner/rlrunner-1.0.1.tar.gz/rlrunner-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rlrunner",
+    version="1.0.1",
+    author="Miguel Martins",
+    author_email="mfmartins1996@gmail.com",
+    description="A framework for Reinforcement Learning experimentation and run simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PriestTheBeast/RLRunner",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=[
+          'numpy',
+          'gym',
+          'matplotlib'
+    ],
+    python_requires='>=3.6',
+)
