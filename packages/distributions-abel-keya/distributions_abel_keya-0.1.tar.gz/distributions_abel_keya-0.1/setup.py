05f5f5e67a02cdd5a9eec18e33964406caeabf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:41 +0000

    Add package distributions-abel-keya/0.1/distributions_abel_keya-0.1.tar.gz

---
+++ packages/distributions-abel-keya/distributions_abel_keya-0.1.tar.gz/distributions_abel_keya-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_abel_keya',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
