6cb44f028c1b370625493be938e34ca114c1e4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:34 +0000

    Add package replitdb/0.0.8a0/replitdb-0.0.8a0-py3-none-any.whl

---
+++ packages/replitdb/replitdb-0.0.8a0-py3-none-any.whl/replitdb/__main__.py
@@ -0,0 +1,21 @@
+from main import replitdb
+def main():
+    import sys
+    import os
+    del sys.argv[0]
+    url = False
+    setUrl = os.environ['REPLIT_DB_URL']
+    args = []
+    for i in sys.argv:
+      if(url):
+        url = False
+        setUrl = i
+      elif(i.lower()=='-url'):
+        url = True
+      else:
+        args.append(i)
+    replitdb.commands.commandHandler(args,setUrl)
+
+
+if __name__ == '__main__':
+    main()
