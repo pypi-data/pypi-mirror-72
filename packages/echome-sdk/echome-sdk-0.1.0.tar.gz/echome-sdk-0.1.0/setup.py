ad8754944935969df9dc6b4a0f0b955126a83324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:48 +0000

    Add package echome-sdk/0.1.0/echome-sdk-0.1.0.tar.gz

---
+++ packages/echome-sdk/echome-sdk-0.1.0.tar.gz/echome-sdk-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="echome-sdk",
+    version="0.1.0",
+    author="Marcus Gutierrez",
+    author_email="markg90@gmail.com",
+    description="EcHome python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mgtrrz/echome-python-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.24'
+    ]
+)
