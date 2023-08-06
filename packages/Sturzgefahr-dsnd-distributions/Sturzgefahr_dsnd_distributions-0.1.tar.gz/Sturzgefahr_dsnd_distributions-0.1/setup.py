52fafd8bcf37a1312f9006a7f7fa6ea21551a0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:00 +0000

    Add package Sturzgefahr-dsnd-distributions/0.1/Sturzgefahr_dsnd_distributions-0.1.tar.gz

---
+++ packages/Sturzgefahr-dsnd-distributions/Sturzgefahr_dsnd_distributions-0.1.tar.gz/Sturzgefahr_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Sturzgefahr_dsnd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Sturzgefahr_dsnd_distributions'],
+      author = 'Matthew Corsmeier',
+      author_email = 'matthew.corsmeier@gmail.com',
+      zip_safe=False)
