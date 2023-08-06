90e50845f30a54cc3b585ad796a58324bf2f6009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:30 +0000

    Add package amsterdam-schema-tools/0.8.13/amsterdam-schema-tools-0.8.13.tar.gz

---
+++ packages/amsterdam-schema-tools/amsterdam-schema-tools-0.8.13.tar.gz/amsterdam-schema-tools-0.8.13/dso_api/lib/schematools/db.py
@@ -0,0 +1,11 @@
+from django.db import connection
+
+from dso_api.datasets import schema_models_factory
+from amsterdam_schema.types import DatasetSchema
+
+
+def create_tables(dataset: DatasetSchema, tables=None):
+    """Create the database tables for a given schema."""
+    with connection.schema_editor() as schema_editor:
+        for model in schema_models_factory(dataset, tables=tables):
+            schema_editor.create_model(model)
