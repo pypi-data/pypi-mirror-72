15f898c14dbdfec1fafa7ddc6c1b00c5eb982971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package printp/0.1.2/printp-0.1.2.tar.gz

---
+++ packages/printp/printp-0.1.2.tar.gz/printp-0.1.2/printp/printp.py
@@ -0,0 +1,15 @@
+
+from input_util import I
+from print_dict import print_dict
+
+
+def printp(arg):
+
+    if I(arg).is_dict:
+        print_dict(arg)
+
+    else:
+        raise NotImplementedError
+
+    return True
+
