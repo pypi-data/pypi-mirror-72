373c718e10b02b58197c2d7e71826c2671838363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package normdistbiondist-probability/0.1/normdistbiondist_probability-0.1.tar.gz

---
+++ packages/normdistbiondist-probability/normdistbiondist_probability-0.1.tar.gz/normdistbiondist_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='normdistbiondist_probability',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['normdistbiondist_probability'],
+      author='Mahmudul Islam',
+      author_email='mahmudul.du20@gmail.com',
+     zip_safe=False)
