3352c0f53208c664e3cf98dc98cfd229c42b9fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:12 +0000

    Add package cr-distributions/0.1/cr_distributions-0.1.tar.gz

---
+++ packages/cr-distributions/cr_distributions-0.1.tar.gz/cr_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cr_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['cr_distributions'],
+      author = 'Carlos Rojas V',
+      author_email = 'carloseduardo866@hotmail.com',
+      zip_safe=False)
