3999d24a92e23ecaea2d25534c2684d251696baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:10 +0000

    Add package cfdi-admin/0.0.1/cfdi_admin-0.0.1.tar.gz

---
+++ packages/cfdi-admin/cfdi_admin-0.0.1.tar.gz/cfdi_admin-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    version="0.0.1",
+    name="cfdi_admin",
+    packages=setuptools.find_packages(),
+    scripts=["bin/cfdi-admin"],
+    url="http://jorgejuarez.net",
+    author="Jorge Juarez",
+    author_email="jorgejuarezmx@gmail.com"
+)
