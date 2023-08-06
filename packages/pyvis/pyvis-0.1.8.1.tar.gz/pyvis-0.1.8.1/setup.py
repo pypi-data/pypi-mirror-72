fe2c5a23bb8f2ea07a5865b3b40a5cbfe70b128b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:03 +0000

    Add package pyvis/0.1.8.1/pyvis-0.1.8.1.tar.gz

---
+++ packages/pyvis/pyvis-0.1.8.1.tar.gz/pyvis-0.1.8.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+exec(open('pyvis/_version.py').read())
+setup(
+    name="pyvis",
+    version=__version__,
+    description="A Python network visualization library",
+    url="https://github.com/WestHealth/pyvis",
+    author="Giancarlo Perrone",
+    author_email="gperrone@westhealth.org",
+    license="BSD",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "jinja2 >= 2.9.6",
+        "networkx >= 1.11",
+        "ipython >= 5.3.0"
+    ]
+)
