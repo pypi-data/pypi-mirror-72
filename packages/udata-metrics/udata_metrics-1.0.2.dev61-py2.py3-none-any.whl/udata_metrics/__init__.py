76f6320ad7a65554aa79bfa80036adc5c4eaab16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:58 +0000

    Add package udata-metrics/1.0.2.dev61/udata_metrics-1.0.2.dev61-py2.py3-none-any.whl

---
+++ packages/udata-metrics/udata_metrics-1.0.2.dev61-py2.py3-none-any.whl/udata_metrics/__init__.py
@@ -0,0 +1,14 @@
+'''
+udata-metrics
+
+Connexion handler to metrics service for udata
+'''
+
+
+__version__ = '1.0.2.dev'
+__description__ = 'Connexion handler to metrics service for udata'
+
+
+def init_app(app):
+    # Do whatever you want to initialize your plugin
+    pass
