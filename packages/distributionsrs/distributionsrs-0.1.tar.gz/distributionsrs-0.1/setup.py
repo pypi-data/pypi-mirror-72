e874826b6036fd2519d30ac61866334116043965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:42 +0000

    Add package distributionsrs/0.1/distributionsrs-0.1.tar.gz

---
+++ packages/distributionsrs/distributionsrs-0.1.tar.gz/distributionsrs-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsrs',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsrs'],
+      zip_safe=False)
