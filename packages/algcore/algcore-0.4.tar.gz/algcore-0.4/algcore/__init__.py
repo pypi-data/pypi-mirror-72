a126d17b0f77edbdbc8fbda42a545c104288f15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:13 +0000

    Add package algcore/0.4/algcore-0.4.tar.gz

---
+++ packages/algcore/algcore-0.4.tar.gz/algcore-0.4/algcore/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+def desc():
+    print ("This is a algcore package!")
+
+def solid():
+
+    pass
+
+if __name__ == '__main__':
+    desc()
---
+++ packages/algcore/algcore-0.4.tar.gz/algcore-0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'algcore',
+    version = '0.4',
+    description="algorithm tool and sdk",
+    long_description="algorithm tool and sdk",
+    author="vita.wang",
+    author_email="2508000696@qq.com",
+    packages = find_packages(),
+)
