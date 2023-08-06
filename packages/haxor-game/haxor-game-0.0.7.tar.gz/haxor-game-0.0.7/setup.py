b83d55a495a3ebb26f306112337d940a4ff8f969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:48 +0000

    Add package haxor-game/0.0.7/haxor-game-0.0.7.tar.gz

---
+++ packages/haxor-game/haxor-game-0.0.7.tar.gz/haxor-game-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="haxor-game",
+    version="0.0.7",
+    author="Angel Davila",
+    author_email="",
+    description="How good are you at haxoring?",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adavila0703/haxor-game-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires='>=3.8',
+)
+
+
