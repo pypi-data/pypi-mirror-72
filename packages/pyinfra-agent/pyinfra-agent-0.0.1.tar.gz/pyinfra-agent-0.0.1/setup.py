e86e453cdc610e654a88cbe3bc9177f5a3f9be4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:39 +0000

    Add package pyinfra-agent/0.0.1/pyinfra-agent-0.0.1.tar.gz

---
+++ packages/pyinfra-agent/pyinfra-agent-0.0.1.tar.gz/pyinfra-agent-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        version='0.0.1',
+        name='pyinfra-agent',
+        author='Nick / Fizzadar',
+        author_email='pointlessrambler@gmail.com',
+        license='MIT',
+        packages=[],
+    )
