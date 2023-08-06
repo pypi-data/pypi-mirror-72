84febe3d86933025834fae9c329688f2b893b7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:31 +0000

    Add package chocobo-acng/0.0.4/chocobo_acng-0.0.4-py2-none-any.whl

---
+++ packages/chocobo-acng/chocobo_acng-0.0.4-py2-none-any.whl/chocobo_acng/__init__.py
@@ -0,0 +1,6 @@
+
+"""chocobo is a python package for delicious Chocobo recipes."""
+
+from .chocobo import (
+  chocobo_roast,
+)
---
+++ packages/chocobo-acng/chocobo_acng-0.0.4-py2-none-any.whl/chocobo_acng/chocobo.py
@@ -0,0 +1,6 @@
+"""My chocobo cooking script."""
+
+import os
+
+def chocobo_roast(num_guests, hotness_level):
+    return num_guests*hotness_level
