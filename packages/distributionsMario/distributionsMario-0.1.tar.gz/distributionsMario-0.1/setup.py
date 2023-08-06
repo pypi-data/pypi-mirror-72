1c0ec9457f1b34f22aac5fe5dced98410733d265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:32 +0000

    Add package distributionsMario/0.1/distributionsMario-0.1.tar.gz

---
+++ packages/distributionsMario/distributionsMario-0.1.tar.gz/distributionsMario-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsMario',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsMario'],
+      zip_safe=False)
