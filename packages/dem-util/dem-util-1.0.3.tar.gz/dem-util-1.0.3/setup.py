2ab2ddb2db1ce725b55ca4034382a5d00b8c1355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:35 +0000

    Add package dem-util/1.0.3/dem-util-1.0.3.tar.gz

---
+++ packages/dem-util/dem-util-1.0.3.tar.gz/dem-util-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dem-util',
+    version='1.0.3',
+    url="https://github.com/BetaS/pydemutil",
+    author="BetaS",
+    author_email="thou1999@gmail.com",
+    description="DEM slope modeling and imaging with trilinear interpolation",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pyproj',
+        'shapely',
+        'cairocffi'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+            "License :: OSI Approved :: MIT License"
+    ]
+)
