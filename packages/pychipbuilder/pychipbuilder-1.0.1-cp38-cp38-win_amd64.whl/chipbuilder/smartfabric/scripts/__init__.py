9b4795ce98b34983d2991f45d3afa99e046c1a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:11 +0000

    Add package pychipbuilder/1.0.1/pychipbuilder-1.0.1-cp38-cp38-win_amd64.whl

---
+++ packages/pychipbuilder/pychipbuilder-1.0.1-cp38-cp38-win_amd64.whl/chipbuilder/smartfabric/scripts/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright 2020 zGlue, Inc.
+
+Licensed under zOH License version 1.0 ("the license") that is included in the accompanying repository.  
+You may not use this file except in compliance with the License. 
+You may obtain a copy of the license at <https://zglue.com/oci/zohl1v>.
+
+__init__.py
+"""
