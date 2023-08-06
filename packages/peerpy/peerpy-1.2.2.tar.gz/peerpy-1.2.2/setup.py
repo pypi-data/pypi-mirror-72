34a20458be10978ebcd7d68f814c0d3eb7df6e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:57 +0000

    Add package peerpy/1.2.2/peerpy-1.2.2.tar.gz

---
+++ packages/peerpy/peerpy-1.2.2.tar.gz/peerpy-1.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="peerpy",
+    version="1.2.2",
+    author="Rubilmax",
+    author_email="rmilon@gmail.com",
+    description="p2p connection over TCP made dead simple!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rubilmax/peerpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
