2f9156f759d659b1bfc1c9750743cdbfc4690166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:37 +0000

    Add package thumbor-icc/1.0.0/thumbor_icc-1.0.0.tar.gz

---
+++ packages/thumbor-icc/thumbor_icc-1.0.0.tar.gz/thumbor_icc-1.0.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="thumbor_icc",
+    packages=find_packages(),
+    version="1.0.0",
+    description="Color management filters for thumbor",
+    author="Lorenz Schori",
+    author_email="lo@znerol.ch",
+    keywords=["thumbor", "icc", "images"],
+    license="MIT",
+    url="https://github.com/znerol/thumbor-icc",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Multimedia :: Graphics :: Presentation"
+    ],
+    install_requires=[
+        "thumbor>7.0.0.dev0",
+        "Pillow"
+    ],
+    long_description="""\
+Thumbor is a smart imaging service. It enables on-demand crop, resizing and
+flipping of images. This module provide support for color management profiles.
+"""
+)
