9261ea95f91e302194b3ec0d84a5229e190fb116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:17 +0000

    Add package pi-team-frontoffice-client/1.2.0/pi-team-frontoffice-client-1.2.0.tar.gz

---
+++ packages/pi-team-frontoffice-client/pi-team-frontoffice-client-1.2.0.tar.gz/pi-team-frontoffice-client-1.2.0/frontoffice/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from frontoffice.api.contactgegevens_api import ContactgegevensApi
+from frontoffice.api.developer_tools_api import DeveloperToolsApi
+from frontoffice.api.persoonsgegevens_api import PersoonsgegevensApi
+from frontoffice.api.search_api import SearchApi
