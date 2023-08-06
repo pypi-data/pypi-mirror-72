389e2e920b5435c4117b27f7bf99a13926043b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:01 +0000

    Add package webspace/2.7.58/webspace-2.7.58-py3-none-any.whl

---
+++ packages/webspace/webspace-2.7.58-py3-none-any.whl/webspace/cms/pages/home.py
@@ -0,0 +1,16 @@
+from webspace.cms.bakery.abstract import WagtailPageBakeryModel
+from webspace.cms import constants
+from webspace.cms.amp.mixins import AmpMixin
+from webspace.loader import get_model
+from webspace.cms.forms.abstract import FormMixin
+
+GenericPage = get_model('cms', 'GenericPage')
+
+
+class AHomePage(AmpMixin, FormMixin, WagtailPageBakeryModel, GenericPage):
+    template = '%s/home_page.html' % constants.PAGES_TEMPLATES_PATH
+    content_panels = GenericPage.content_panels
+
+    class Meta:
+        abstract = True
+        app_label = 'cms'
