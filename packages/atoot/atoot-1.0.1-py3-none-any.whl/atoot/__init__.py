c8f8532a765ed85b777d53454e519609234cfdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:41 +0000

    Add package atoot/1.0.1/atoot-1.0.1-py3-none-any.whl

---
+++ packages/atoot/atoot-1.0.1-py3-none-any.whl/atoot/__init__.py
@@ -0,0 +1,16 @@
+from atoot.api import (
+    MastodonAPI, client,
+    MastodonError,
+    NetworkError,
+    ApiError,
+    ClientError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
+    ConflictError,
+    GoneError,
+    UnprocessedError,
+    RatelimitError,
+    ServerError,
+    UnavailableError
+)
