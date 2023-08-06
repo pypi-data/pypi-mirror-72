098062d6ca828e1b929a3534863ccd536e1f8425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:08 +0000

    Add package vin-distributions/0.1/vin_distributions-0.1.tar.gz

---
+++ packages/vin-distributions/vin_distributions-0.1.tar.gz/vin_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vin_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vin_distributions'],
+      author = 'Vinit Kanse',
+      author_email = 'randomwave45@gmail.com',
+      zip_safe=False)
