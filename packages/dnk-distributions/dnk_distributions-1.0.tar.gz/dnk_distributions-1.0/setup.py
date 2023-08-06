79dfa2893bb420fc55b7b4ce081d70986e0d8a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:46 +0000

    Add package dnk-distributions/1.0/dnk_distributions-1.0.tar.gz

---
+++ packages/dnk-distributions/dnk_distributions-1.0.tar.gz/dnk_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dnk_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dnk_distributions'],
+      zip_safe=False)
