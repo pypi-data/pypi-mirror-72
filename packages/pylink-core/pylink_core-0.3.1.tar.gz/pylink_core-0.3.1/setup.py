6c7b3ae811f031d965cfd5d6f959ac3f10aa3460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:25 +0000

    Add package pylink-core/0.3.1/pylink_core-0.3.1.tar.gz

---
+++ packages/pylink-core/pylink_core-0.3.1.tar.gz/pylink_core-0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+setup(name='pylink_core',
+      version='0.3.1',
+      description='a small example',
+      classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+      url='https://www.python.org/',
+      author='zhushaodong123',
+      author_email='',
+      license='NEU',
+      packages=find_packages(),
+      package_data={"":["*.pyc"]},
+      zip_safe=True
+     )
+
