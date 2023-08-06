a13dac1fcfcda747b81bbd801e639d8bddb7634b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:39 +0000

    Add package clvnshi-distributions/0.1/clvnshi_distributions-0.1.tar.gz

---
+++ packages/clvnshi-distributions/clvnshi_distributions-0.1.tar.gz/clvnshi_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='clvnshi_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['clvnshi_probability'],
+      author = 'Calvin Shi',
+      author_email = 'clvnshi@outlook.com',
+      zip_safe=False)
