3bc39f3a96479cb0e0c5a2487e5f562501811935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:29 +0000

    Add package spdx-py-build-tool-ek8/0.1/spdx_py_build_tool_ek8-0.1.tar.gz

---
+++ packages/spdx-py-build-tool-ek8/spdx_py_build_tool_ek8-0.1.tar.gz/spdx_py_build_tool_ek8-0.1/setup.py
@@ -0,0 +1,36 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import imp, os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_dict = dict(
+    name='spdx_py_build_tool_ek8',
+    version='0.1',
+    author='Ekong Obie Philip',
+    author_email='ekongobiephilip@gmail.com',
+    maintainer='Ekong Obie Philip',
+    maintainer_email='ekongobiephilip@gmail.com',
+    url='https://github.com/spdx/',
+    description='Support a continuous integration (CI) generation of SPDX files by creating a plugins or extensions to build tools',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["build_tool"],
+    entry_points={
+        'console_scripts': [
+            'spdx-build = build_tool.tool:entry_point',
+        ],
+    }
+)
+
+
+def main():
+    setup(**setup_dict)
+
+
+if __name__ == '__main__':
+    main()
