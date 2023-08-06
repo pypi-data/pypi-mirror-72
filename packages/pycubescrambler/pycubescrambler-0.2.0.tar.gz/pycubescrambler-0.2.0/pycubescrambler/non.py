78b88b191b5461b9ba8a70281733c396c51a3d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package pycubescrambler/0.2.0/pycubescrambler-0.2.0.tar.gz

---
+++ packages/pycubescrambler/pycubescrambler-0.2.0.tar.gz/pycubescrambler-0.2.0/pycubescrambler/non.py
@@ -0,0 +1,53 @@
+import random
+
+def get_ivy():
+    moves = ["L", "R", 'D', 'B']
+    turns= [' ', "' "]
+    scramble = []
+    usedmoves = []
+    usedmoves1 = []
+    
+    for i in range(8):
+
+        if i % 2 == 0:
+            
+            randmoves = random.choice(moves)
+            usedmoves.append(randmoves)
+            scramble.append(randmoves)
+            scramble.append(random.choice(turns))
+            moves.remove(randmoves)
+
+            if i != 0:
+                moves.append(usedmoves1[-1])
+
+        else:
+            randmoves1 = random.choice(moves)
+            scramble.append(randmoves1)
+            usedmoves1.append(randmoves1)
+            scramble.append(random.choice(turns))
+            moves.remove(randmoves1)
+            moves.append(usedmoves[-1])
+    return "".join(scramble)
+
+
+def get223():
+    num_moves = random.randint(8, 10)
+    double_moves = ["R", "F"]
+    single_moves = ["U", "D"]
+    turns = ["2 ", " ", "' "]
+    scramble = []
+
+    for i in range(num_moves):
+
+        if i % 2 == 0:
+            randmoves = random.choice(double_moves)
+            scramble.append(randmoves)
+            turn ="2 "
+
+            scramble.append(turn)
+        else:
+            randmoves1 = random.choice(single_moves)
+            scramble.append(randmoves1)
+            scramble.append(random.choice(turns))
+
+    return "".join(scramble)
