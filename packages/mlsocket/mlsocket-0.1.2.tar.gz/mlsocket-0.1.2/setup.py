e6f15bf727af5dbcf0a69eb99956973db44577af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:08 +0000

    Add package mlsocket/0.1.2/mlsocket-0.1.2.tar.gz

---
+++ packages/mlsocket/mlsocket-0.1.2.tar.gz/mlsocket-0.1.2/setup.py
@@ -0,0 +1,56 @@
+from os import path
+from io import open
+from setuptools import setup, find_packages
+
+
+def get_readme():
+    here = path.dirname(__file__)
+    with open(path.join(here, 'README.md'),
+              encoding='utf8') as readme_file:
+        readme = readme_file.read()
+        return readme
+
+
+def get_history():
+    here = path.dirname(__file__)
+    with open(path.join(here, 'HISTORY.md'),
+              encoding='utf8') as history_file:
+        history = history_file.read()
+        return history
+
+
+def get_requirements():
+    here = path.dirname(__file__)
+    with open(path.join(here, 'requirements.txt'),
+              encoding='utf8') as requirements_file:
+        return requirements_file.read().splitlines()
+
+
+setup(
+    name='mlsocket',
+    version='0.1.2',
+    author='SeBeom Lee',
+    author_email='moses97@gmail.com',
+    description='Python socket for machine learning data',
+    long_description=get_readme(),
+    long_description_content_type="text/markdown",
+    install_requires=get_requirements(),
+    license='MIT',
+    include_package_data=True,
+    keywords=["socket", "ml", "machine learning"],
+    packages=find_packages(include=['mlsocket']),
+    test_suite="tests",
+    url='https://github.com/k2sebeom/mlsocket',
+    classifiers=[
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
