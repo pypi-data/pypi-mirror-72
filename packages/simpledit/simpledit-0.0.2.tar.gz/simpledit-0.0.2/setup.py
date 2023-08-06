1f5ecb03bad7ad5928fcade3b6bb0ac9218f3dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:48 +0000

    Add package simpledit/0.0.2/simpledit-0.0.2.tar.gz

---
+++ packages/simpledit/simpledit-0.0.2.tar.gz/simpledit-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='simpledit',
+    version='0.0.2',
+    description='',
+    url='https://gitlab.com/torresed/simpledit',
+    author='Programmers',
+    author_email='noreply@ufl.edu',
+    license='GPL3',
+    packages=['simpledit'],
+    install_requires=['Pillow', 'PyQt5', 'PyQt5-sip'],
+    scripts=['simpledit/SimplEdit'])
