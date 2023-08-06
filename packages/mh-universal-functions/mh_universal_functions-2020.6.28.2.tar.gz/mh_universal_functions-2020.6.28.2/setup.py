bc208d65cb6f4587c607b1a6e34e9c8c46ca7811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:08 +0000

    Add package mh-universal-functions/2020.6.28.2/mh_universal_functions-2020.6.28.2.tar.gz

---
+++ packages/mh-universal-functions/mh_universal_functions-2020.6.28.2.tar.gz/mh_universal_functions-2020.6.28.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mh_universal_functions',
+      version='2020.6.28.2',
+      description='General functions library',
+      long_description='General functions library to be used with pir.py and rha.py',
+      url='https://pypi.org/project/mh-universal-functions',
+      author='MH',
+      author_email='ibhmduu1721nhg11.11@o2mail.de',
+      license='MIT',
+      packages=['mh_universal_functions'],
+      python_requires='>=3.7',
+      zip_safe=False)
