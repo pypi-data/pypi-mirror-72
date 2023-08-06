73cc20b349f3c74cefa9841e5961ae7893fe7a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package csi21-distributions/0.4/csi21_distributions-0.4.tar.gz

---
+++ packages/csi21-distributions/csi21_distributions-0.4.tar.gz/csi21_distributions-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='csi21_distributions',
+      version='0.4',
+      description='Gaussian distributions',
+      packages=['csi21_distributions'],
+      zip_safe=False)
