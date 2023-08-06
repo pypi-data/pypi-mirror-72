f4d4e512f60bf1ba3a9dab90e76f35a44222246f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:33 +0000

    Add package doughnuts/3.0.2/doughnuts-3.0.2.tar.gz

---
+++ packages/doughnuts/doughnuts-3.0.2.tar.gz/doughnuts-3.0.2/doughnuts/webshell_plugins/cd.py
@@ -0,0 +1,17 @@
+from libs.config import alias, color, gset, gget
+from libs.myapp import send, base64_encode
+
+
+@alias(True, d="dir")
+def run(directory: str = ''):
+    """
+    cd
+
+    Change the working directory.
+    """
+    res = send(f"chdir(base64_decode('{base64_encode(str(directory))}'));print(getcwd());")
+    if (not res):
+        return
+    pwd = res.r_text.strip()
+    gset("webshell.pwd", pwd, namespace="webshell")
+    gset("webshell.prompt", f"doughnuts ({color.cyan(gget('webshell.netloc', 'webshell'))}) > ")
