f96c5e98612056948637b75a41d90b228bed66fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:40 +0000

    Add package vest/0.0.1/vest-0.0.1.tar.gz

---
+++ packages/vest/vest-0.0.1.tar.gz/vest-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='vest',
+    version='0.0.1',
+    author='mattf1n',
+    long_description=long_description,
+    url='https://github.com/mattf1n/vest',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'robin_stocks',
+    ],
+    entry_points='''
+        [console_scripts]
+        vest=vest.cli:cli
+    ''',
+)
