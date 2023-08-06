6eb487316413935ee9f74bdbd3be9b1d1289614d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:05 +0000

    Add package probability-dist-calc/0.1/probability_dist_calc-0.1.tar.gz

---
+++ packages/probability-dist-calc/probability_dist_calc-0.1.tar.gz/probability_dist_calc-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_dist_calc',
+      version='0.1',
+      description='probability distributions calculation',
+      packages=['probability_dist_calc'],
+      zip_safe=False)
