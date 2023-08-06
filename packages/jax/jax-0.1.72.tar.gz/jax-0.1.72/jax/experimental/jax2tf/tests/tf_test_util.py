5bfc52ddce97e7c483c0c8538847e40e2530fb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:02 +0000

    Add package jax/0.1.72/jax-0.1.72.tar.gz

---
+++ packages/jax/jax-0.1.72.tar.gz/jax-0.1.72/jax/experimental/jax2tf/tests/tf_test_util.py
@@ -0,0 +1,73 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+import logging
+import numpy as np
+from typing import Any, Callable, Tuple
+import tensorflow as tf  # type: ignore[import]
+
+from jax.config import config
+from jax import dtypes
+from jax.experimental import jax2tf
+from jax import test_util as jtu
+
+class JaxToTfTestCase(jtu.JaxTestCase):
+
+  def setUp(self):
+    super().setUp()
+    # Ensure that all TF ops are created on the proper device (TPU or GPU or CPU)
+    # TODO(necula): why doesn't TF do this automatically?
+    tf_preferred_devices = (
+        tf.config.list_logical_devices("TPU") +
+        tf.config.list_logical_devices("GPU") +
+        tf.config.list_logical_devices())
+    self.tf_default_device = tf_preferred_devices[0]
+    logging.info(f"Running jax2tf converted code on {self.tf_default_device}.")
+    if jtu.device_under_test() != "gpu":
+      # TODO(necula): Change the build flags to ensure the GPU is seen by TF
+      # It seems that we need --config=cuda build flag for this to work?
+      self.assertEqual(jtu.device_under_test().upper(),
+                       self.tf_default_device.device_type)
+
+    with contextlib.ExitStack() as stack:
+      stack.enter_context(tf.device(self.tf_default_device))
+      self.addCleanup(stack.pop_all().close)
+
+  def assertDtypesMatch(self, x, y, *, canonicalize_dtypes=True):
+    """Compares dtypes across JAX and TF dtypes. Overrides super method."""
+    def to_numpy_dtype(dt):
+      return dt if isinstance(dt, np.dtype) else dt.as_numpy_dtype
+
+    if not config.FLAGS.jax_enable_x64 and canonicalize_dtypes:
+      self.assertEqual(dtypes.canonicalize_dtype(to_numpy_dtype(jtu._dtype(x))),
+                       dtypes.canonicalize_dtype(to_numpy_dtype(jtu._dtype(y))))
+    else:
+      self.assertEqual(to_numpy_dtype(jtu._dtype(x)),
+                       to_numpy_dtype(jtu._dtype(y)))
+
+  def ConvertAndCompare(self, func_jax: Callable, *args,
+                        with_function: bool = False,
+                        atol=None,
+                        rtol=None) -> Tuple[Any, Any]:
+    """Compares jax_func(*args) with convert(jax_func)(*args)."""
+    func_tf = jax2tf.convert(func_jax)
+    if with_function:
+      func_tf = tf.function(func_tf)
+    res_jax = func_jax(*args)
+    #logging.info(f"res_jax is {res_jax} on {res_jax.device_buffer.device()}")
+    res_tf = func_tf(*args)
+    #logging.info(f"res_tf is {res_tf} on {res_tf.backing_device}")
+    self.assertAllClose(res_jax, res_tf, atol=atol, rtol=rtol)
+    return (res_jax, res_tf)
