4a336d7512fd21f97e0b384896b3654ffecd1553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:34 +0000

    Add package distributions-mm/0.5/distributions_mm-0.5.tar.gz

---
+++ packages/distributions-mm/distributions_mm-0.5.tar.gz/distributions_mm-0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_mm',
+      version='0.5',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
