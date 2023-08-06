d9c84924d3338e2372cf612c106c6ce05d2f9aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:39 +0000

    Add package consoleme/0.0.0/consoleme-0.0.0.tar.gz

---
+++ packages/consoleme/consoleme-0.0.0.tar.gz/consoleme-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="consoleme",
+    version="0.0.0",
+    author="Netflix",
+    url="https://github.com/Netflix-Skunkworks/consoleme",
+    author_email="consoleme-maintainers@netflix.com",
+    description="Consoleme",
+    keywords="consoleme",
+)
