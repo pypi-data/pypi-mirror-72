3ab90770232553537ee4872b8a8578821363103c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package jourgen/0.0.9/jourgen-0.0.9.tar.gz

---
+++ packages/jourgen/jourgen-0.0.9.tar.gz/jourgen-0.0.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='jourgen',
+    packages=['jourgen'],
+    entry_points={
+        "console_scripts": ['jourgen = jourgen.jourgen:cli']
+    },
+    include_package_data=True,
+    version='0.0.9',
+    license='MIT',
+    description='Tiny but functional blog engine',
+    author='Pablo Toledo Margalef',
+    author_email='pabloatm980@gmail.com',
+    url='https://gitlab.com/papablo/journal-generator',
+    download_url='https://gitlab.com/papablo/journal-generator/-/archive/0.0.9/journal-generator-0.0.9.tar.gz',
+    keywords=['blogging', 'blog', 'writing'],
+    install_requires=[
+        'click',
+        'Jinja2',
+        'livereload',
+        'Markdown',
+        'Pygments',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Topic :: Office/Business :: News/Diary',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
