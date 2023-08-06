f6f62cc3e39c7cba6bfa76726392d551da828881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:39 +0000

    Add package zwack/2.1.0/zwack-2.1.0-py3-none-any.whl

---
+++ packages/zwack/zwack-2.1.0-py3-none-any.whl/zwack/__init__.py
@@ -0,0 +1,17 @@
+from .ascii import *
+import random
+async def exist():
+  while 1:
+    if(random.randint(0,99999999)==random.randint(0,99999999)):
+      break
+class Zwack:
+  class ascii():
+    @property
+    def zwack(self):
+      return(zwack_ascii)
+    @property
+    def overflow(self):
+      return(zwack_overflow)
+    @property
+    def overflow_small(self):
+      return(zwack_overflow_small)
