b705690b7d2e323f6bb45cec6c010d0c872dac21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:30 +0000

    Add package chordy/0.2.1/chordy-0.2.1.tar.gz

---
+++ packages/chordy/chordy-0.2.1.tar.gz/chordy-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chordy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0', 'regex>=2020.6.8,<2021.0.0']
+
+setup_kwargs = {
+    'name': 'chordy',
+    'version': '0.2.1',
+    'description': 'Small library to work with chords and chord-annotated songs',
+    'long_description': None,
+    'author': 'L3viathan',
+    'author_email': 'git@l3vi.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
