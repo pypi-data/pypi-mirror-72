40bf7b07233291936978fd8b9808875388498e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:38 +0000

    Add package stat-probability/0.1/stat_probability-0.1.tar.gz

---
+++ packages/stat-probability/stat_probability-0.1.tar.gz/stat_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stat_probability',
+      version='0.1',
+      description='Gaussian and Normal distributions.',
+      packages=['stat_probability'],
+      author = 'Aniket Hande',
+      author_email = 'anikethande779@gmail.com',
+      zip_safe=False)
