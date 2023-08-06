6794911a99ae9bfcc5fc98b7d380c28902cb61bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:36 +0000

    Add package mr-distributions/0.1/mr_distributions-0.1.tar.gz

---
+++ packages/mr-distributions/mr_distributions-0.1.tar.gz/mr_distributions-0.1/README.md
@@ -0,0 +1,16 @@
+# Mr-distributions package
+version 0.1
+
+The package lets you install the gaussian and binomial distribution package
+
+# files
+binomialdistribution.py
+gaussiandistribution.py
+generaldistribution.py
+license.txt
+setup.cfg
+setup.py
+__init__.py
+
+#installation
+pip install
---
+++ packages/mr-distributions/mr_distributions-0.1.tar.gz/mr_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mr_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mr_distributions'],
+      author = 'Prasil Adhikari',
+      author_email = 'adhikariprasil7@gmail.com',
+      zip_safe=False)
