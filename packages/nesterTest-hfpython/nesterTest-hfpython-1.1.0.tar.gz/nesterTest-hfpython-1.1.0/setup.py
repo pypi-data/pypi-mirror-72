29d99b5046704c7faf3be31e08d71e36381f4b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package nesterTest-hfpython/1.1.0/nesterTest-hfpython-1.1.0.tar.gz

---
+++ packages/nesterTest-hfpython/nesterTest-hfpython-1.1.0.tar.gz/nesterTest-hfpython-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            ='nesterTest-hfpython', 
+    version         ='1.1.0', 
+    py_modules      =['nester'], 
+    author          ='hfpython', 
+    author_email    ='hfpython@headfirstlabs.com',  
+    url             ='http://www.headfirstlabs.com', 
+    description     ='A simple printer of nester lists', 
+    )
