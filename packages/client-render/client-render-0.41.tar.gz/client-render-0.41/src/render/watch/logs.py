c3e15a45a8e0b091a9a6866d517171930a2f9177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:57 +0000

    Add package client-render/0.41/client-render-0.41.tar.gz

---
+++ packages/client-render/client-render-0.41.tar.gz/client-render-0.41/src/render/watch/logs.py
@@ -0,0 +1,25 @@
+import glob
+import itertools
+import logging
+import os
+from pathlib import PurePosixPath
+
+from render import RENDER_DIR
+from render.s3 import S3
+from render.util import user_data
+
+logger = logging.getLogger(__name__)
+
+
+def collect_instance_logs(client: S3):
+    logger.debug("Collecting log files...")
+    for file in itertools.chain(
+            # additional logs could be appended here
+            glob.glob(str(RENDER_DIR / '*.log')),
+    ):
+        logger.debug(f"Found log {file}")
+        client.upload_file(
+            PurePosixPath(user_data['instance_s3_key']) / 'logs' / os.path.basename(
+                file),
+            file
+        )
