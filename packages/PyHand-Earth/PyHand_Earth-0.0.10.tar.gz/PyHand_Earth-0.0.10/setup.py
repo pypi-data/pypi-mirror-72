b33b89c746ecc11578d526dac7520c6a7ff6569a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:57 +0000

    Add package PyHand-Earth/0.0.10/PyHand_Earth-0.0.10.tar.gz

---
+++ packages/PyHand-Earth/PyHand_Earth-0.0.10.tar.gz/PyHand_Earth-0.0.10/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+setuptools.setup(
+    name = "PyHand_Earth",
+    version = "0.0.10",
+    description = "Google Earth navigation driven by gesture recognition",
+    url = "https://github.com/luijohnj/PyHand_Earth",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    
+
+)
