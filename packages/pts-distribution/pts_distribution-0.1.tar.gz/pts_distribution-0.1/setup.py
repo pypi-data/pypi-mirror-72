2817e9e2de19b6f5ac820d0b528b4d193b7b9150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:00 +0000

    Add package pts-distribution/0.1/pts_distribution-0.1.tar.gz

---
+++ packages/pts-distribution/pts_distribution-0.1.tar.gz/pts_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pts_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pts_distribution'],
+      Author = 'Prathamesh',
+      Author_email = 'prathamesh272000@gmail.com',
+      zip_safe=False)
