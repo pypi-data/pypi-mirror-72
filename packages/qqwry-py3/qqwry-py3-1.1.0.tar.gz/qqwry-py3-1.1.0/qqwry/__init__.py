19abe314b2b4422492409784c468429601d07f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:29 +0000

    Add package qqwry-py3/1.1.0/qqwry-py3-1.1.0.tar.gz

---
+++ packages/qqwry-py3/qqwry-py3-1.1.0.tar.gz/qqwry-py3-1.1.0/qqwry/__init__.py
@@ -0,0 +1,8 @@
+from .qqwry import QQwry
+from .cz88update import updateQQwry
+
+__version__ = '1.1.0'
+
+__all__ = ('QQwry', 'updateQQwry')
+
+__doc__ = 'Document of this module: https://pypi.python.org/pypi/qqwry-py3'
