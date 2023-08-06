e6e4f5ce9c5970fa7beb510b1429be5adc52fe3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:58 +0000

    Add package dsnd-distributions-ppk/0.1/dsnd_distributions_ppk-0.1.tar.gz

---
+++ packages/dsnd-distributions-ppk/dsnd_distributions_ppk-0.1.tar.gz/dsnd_distributions_ppk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_ppk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_distributions_ppk'],
+      zip_safe=False)
