e39898046191b0a108224c1fa68aa9fd0415e4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package stpmex/3.5.0/stpmex-3.5.0.tar.gz

---
+++ packages/stpmex/stpmex-3.5.0.tar.gz/stpmex-3.5.0/setup.py
@@ -0,0 +1,53 @@
+from importlib.machinery import SourceFileLoader
+
+from setuptools import find_packages, setup
+
+version = SourceFileLoader('version', 'stpmex/version.py').load_module()
+
+install_requires = [
+    'clabe==1.2.0',
+    'dataclasses>=0.6;python_version<"3.7"',
+    'pydantic==1.5.1',
+    'pyopenssl==19.1.0',
+    'requests==2.24.0',
+]
+
+test_requires = [
+    'black==19.10b0',
+    'isort[pipfile]==4.3.21',
+    'flake8==3.8.3',
+    'mypy==0.782',
+    'pytest==5.4.3',
+    'pytest-vcr==1.0.2',
+    'pytest-cov==2.10.0',
+    'requests-mock==1.8.0',
+]
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='stpmex',
+    version=version.__version__,
+    author='Cuenca',
+    author_email='dev@cuenca.com',
+    description='Client library for stpmex.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cuenca-mx/stpmex-python',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data=dict(stpmex=['py.typed']),
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    setup_requires=['pytest-runner'],
+    tests_require=test_requires,
+    extras_require=dict(test=test_requires),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
