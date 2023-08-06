b39fe3f1bb29c82bb6db8d5abdd963f130337d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package Mobydoc/0.0.14/Mobydoc-0.0.14.tar.gz

---
+++ packages/Mobydoc/Mobydoc-0.0.14.tar.gz/Mobydoc-0.0.14/src/Mobydoc/__init__.py
@@ -0,0 +1,10 @@
+from .base import Version, checkVersion, dumps
+from .classification.classification import Classification
+from .classification.zone_contexte import ClassificationZoneContexte
+from .multimedia.multimedia import Multimedia
+from .multimedia.zone_generale import MultimediaZoneGenerale
+from .provenance.provenance import Provenance
+from .provenance.zone_contexte import ProvenanceZoneContexte
+from .specimen.champs.multimedia import ChampSpecimenZoneMultimediaMultimedia
+from .specimen.specimen import Specimen
+from .specimen.zone_identification import SpecimenZoneIdentification
