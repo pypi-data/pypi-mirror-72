79bd3135b8ab74978989fad5de2aca88e68237ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:47 +0000

    Add package lite-distributions/0.1/lite_distributions-0.1.tar.gz

---
+++ packages/lite-distributions/lite_distributions-0.1.tar.gz/lite_distributions-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='lite_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author_email='author@something.com',
+      packages=['distributions'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
