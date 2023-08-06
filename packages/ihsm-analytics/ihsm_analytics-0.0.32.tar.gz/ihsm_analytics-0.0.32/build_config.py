2208d4994d528d2e3c774d3d627cfa59b9ebf4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:56 +0000

    Add package ihsm-analytics/0.0.32/ihsm_analytics-0.0.32.tar.gz

---
+++ packages/ihsm-analytics/ihsm_analytics-0.0.32.tar.gz/ihsm_analytics-0.0.32/build_config.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+readme = ''
+with open('readme.md', mode='r', encoding='utf-8') as fd:
+    readme = fd.read()
+
+setup(
+    name='ihsm_analytics',
+    author='Huy Nguyen',
+    author_email='huy.nguyen@ihsmarkit.com',
+    packages=['ihsm_analytics'],
+    install_requires=['numpy', 'pandas', 'scipy', 'sklearn', 'xgboost'], # TODO: add required packages
+    url='https://ihsmarkit.com/',
+    long_description=readme,
+    include_package_data=True,
+    version='0.0.32', # TODO: update version for each upload
+    license='MIT',
+    description='The package provides advanced geo-analytics functionalities.',
+    classifiers=[
+        # The full list is here: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Software Development :: Libraries'
+    ],
+    python_requires='>=3.7'
+)
---
+++ packages/ihsm-analytics/ihsm_analytics-0.0.32.tar.gz/ihsm_analytics-0.0.32/ihsm_analytics/__init__.py
@@ -0,0 +1,3 @@
+from .eor import *
+# from .ar_module import *
+# from .ar import *
