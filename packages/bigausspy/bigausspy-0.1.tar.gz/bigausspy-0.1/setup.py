ac80cb1c742b3162f54bfdf377f2ed2d458df1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:58 +0000

    Add package bigausspy/0.1/bigausspy-0.1.tar.gz

---
+++ packages/bigausspy/bigausspy-0.1.tar.gz/bigausspy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bigausspy',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bigausspy'],
+      author= 'Obinna Iheanachor',
+      author_email= 'datascientistobi@mail.com',
+      zip_safe=False)
