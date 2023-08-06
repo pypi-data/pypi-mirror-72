8ebe60759c8e36e95a8130fa1adb39d98d58b788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:40 +0000

    Add package no-headers-target-csv/1.0.1/no_headers_target_csv-1.0.1-py2.py3-none-any.whl

---
+++ packages/no-headers-target-csv/no_headers_target_csv-1.0.1-py2.py3-none-any.whl/no_headers_target_csv.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+import argparse
+import io
+import os
+import sys
+import json
+import csv
+import threading
+import http.client
+import urllib
+from datetime import datetime
+import collections
+import pkg_resources
+import tracemalloc
+
+from jsonschema.validators import Draft4Validator
+import singer
+
+logger = singer.get_logger()
+
+def emit_state(state):
+    if state is not None:
+        line = json.dumps(state)
+        logger.debug('Emitting state {}'.format(line))
+        sys.stdout.write("{}\n".format(line))
+        sys.stdout.flush()
+
+def flatten(d, parent_key='', sep='__'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, str(v) if type(v) is list else v))
+    return dict(items)
+
+def persist_messages(delimiter, quotechar, messages, destination_path, without_headers):
+    state = None
+    schemas = {}
+    key_properties = {}
+    headers = {}
+    validators = {}
+
+    now = datetime.now().strftime('%Y%m%dT%H%M%S')
+
+    for message in messages:
+        tracemalloc.start()
+        try:
+            o = singer.parse_message(message).asdict()
+        except json.decoder.JSONDecodeError:
+            logger.error("Unable to parse:\n{}".format(message))
+            raise
+        message_type = o['type']
+        if message_type == 'RECORD':
+            if o['stream'] not in schemas:
+                raise Exception("A record for stream {}"
+                                "was encountered before a corresponding schema".format(o['stream']))
+
+            validators[o['stream']].validate(o['record'])
+
+            filename = o['stream'] + '-' + now + '.csv'
+            filename = os.path.expanduser(os.path.join(destination_path, filename))
+            file_is_empty = (not os.path.isfile(filename)) or os.stat(filename).st_size == 0
+
+            flattened_record = flatten(o['record'])
+
+            if o['stream'] not in headers and not file_is_empty:
+                with open(filename, 'r') as csvfile:
+                    reader = csv.reader(csvfile,
+                                        delimiter=delimiter,
+                                        quotechar=quotechar)
+                    first_line = next(reader)
+                    headers[o['stream']] = first_line if first_line else flattened_record.keys()
+            else:
+                headers[o['stream']] = flattened_record.keys()
+
+            with open(filename, 'a') as csvfile:
+                writer = csv.DictWriter(csvfile,
+                                        headers[o['stream']],
+                                        extrasaction='ignore',
+                                        delimiter=delimiter,
+                                        quotechar=quotechar)
+                if file_is_empty and not without_headers:
+                    writer.writeheader()
+
+                writer.writerow(flattened_record)
+                del flattened_record
+            state = None
+        elif message_type == 'STATE':
+            logger.debug('Setting state to {}'.format(o['value']))
+            state = o['value']
+        elif message_type == 'SCHEMA':
+            stream = o['stream']
+            schemas[stream] = o['schema']
+            validators[stream] = Draft4Validator(o['schema'])
+            key_properties[stream] = o['key_properties']
+        else:
+            logger.warning("Unknown message type {} in message {}"
+                            .format(o['type'], o))
+
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics('lineno')
+        for stat in top_stats[:20]:
+            print('[MEMORY USAGE] {}'.format(stat))
+
+        total = sum(stat.size for stat in top_stats)
+        print("[TOTAL MEMORY USAGE]: Total allocated size: {:.2f} MiB".format(total / 1024 ** 2))
+
+        tracemalloc.stop()
+    return state
+
+
+def send_usage_stats():
+    try:
+        version = pkg_resources.get_distribution('target-csv').version
+        conn = http.client.HTTPConnection('collector.singer.io', timeout=10)
+        conn.connect()
+        params = {
+            'e': 'se',
+            'aid': 'singer',
+            'se_ca': 'target-csv',
+            'se_ac': 'open',
+            'se_la': version,
+        }
+        conn.request('GET', '/i?' + urllib.parse.urlencode(params))
+        response = conn.getresponse()
+        conn.close()
+    except:
+        logger.debug('Collection request failed')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='Config file')
+    parser.add_argument('--without_headers',
+                        action='store_true',
+                        help='Do not export the headers.')
+    args = parser.parse_args()
+    if args.config:
+        with open(args.config) as input_json:
+            config = json.load(input_json)
+    else:
+        config = {}
+    without_headers = False
+    if args.without_headers:
+        without_headers = True
+    if not config.get('disable_collection', False):
+        logger.info('Sending version information to singer.io. ' +
+                    'To disable sending anonymous usage data, set ' +
+                    'the config parameter "disable_collection" to true')
+        threading.Thread(target=send_usage_stats).start()
+
+    input_messages = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+    state = persist_messages(config.get('delimiter', ','),
+                             config.get('quotechar', '"'),
+                             input_messages,
+                             config.get('destination_path', ''),
+                             without_headers)
+
+    emit_state(state)
+    logger.debug("Exiting normally")
+
+
+if __name__ == '__main__':
+    main()
