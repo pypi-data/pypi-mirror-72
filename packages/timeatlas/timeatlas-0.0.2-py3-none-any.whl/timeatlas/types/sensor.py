bd898bd18bcca8b11b3fb8e6231d50972b6dc0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:41 +0000

    Add package timeatlas/0.0.2/timeatlas-0.0.2-py3-none-any.whl

---
+++ packages/timeatlas/timeatlas-0.0.2-py3-none-any.whl/timeatlas/types/sensor.py
@@ -0,0 +1,9 @@
+
+class Sensor:
+
+    def __init__(self, sensor_id: int, name: str):
+        self.sensor_id = sensor_id
+        self.name = name
+
+    def __repr__(self):
+        return "Sensor ID {}".format(self.sensor_id)
