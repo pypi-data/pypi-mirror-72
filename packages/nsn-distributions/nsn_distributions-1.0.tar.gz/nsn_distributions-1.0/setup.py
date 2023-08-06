6612a2e541f48d0dfe62cfd805af7d3a44b6e8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package nsn-distributions/1.0/nsn_distributions-1.0.tar.gz

---
+++ packages/nsn-distributions/nsn_distributions-1.0.tar.gz/nsn_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nsn_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      author='Odeyale Kehinde',
+      author_email='odeyale_kehinde@yahoo.com',
+      packages=['nsn_distributions'],
+      zip_safe=False)
