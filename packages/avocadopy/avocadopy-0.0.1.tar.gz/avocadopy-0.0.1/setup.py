e0a3529578391ad4f68ce3d751792377d8e3ee5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:07 +0000

    Add package avocadopy/0.0.1/avocadopy-0.0.1.tar.gz

---
+++ packages/avocadopy/avocadopy-0.0.1.tar.gz/avocadopy-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="avocadopy", 
+    version="0.0.1",
+    author='B Niu',
+    author_email='shinji006@126.com',
+    description='Tools for medical statistics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/b-niu/avocadopy',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
