cc9927d66612f5207c94a1312ea62bd93a84b9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package pyIndego/2.0.13/pyIndego-2.0.13.tar.gz

---
+++ packages/pyIndego/pyIndego-2.0.13.tar.gz/pyIndego-2.0.13/pyIndego/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import DistributionNotFound
+from pkg_resources import get_distribution
+
+try:
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
+
+from .indego_client import IndegoClient
+from .indego_async_client import IndegoAsyncClient
