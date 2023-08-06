c1729aa438feb8790a231f369c9cea720e6965a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:49 +0000

    Add package bean/0.0.1/bean-0.0.1.tar.gz

---
+++ packages/bean/bean-0.0.1.tar.gz/bean-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+  name = 'bean',
+  version = '0.0.1',
+  description = 'A simple devops toolkit',
+  author = 'acegik',
+  license = 'GPL-3.0',
+  url = 'https://github.com/acegik/bean',
+  download_url = 'https://github.com/acegik/bean/downloads',
+  keywords = ['sysops', 'devops', 'tools'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
