09e27dc15e89bb7007b46428894a08d9c83a5e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:30 +0000

    Add package mojio-sdk/0.0.7/mojio_sdk-0.0.7.tar.gz

---
+++ packages/mojio-sdk/mojio_sdk-0.0.7.tar.gz/mojio_sdk-0.0.7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="mojio_sdk",
+    version="0.0.7",
+    author="Simon Tenbeitel",
+    author_email="open-source@simontb.de",
+    description="Moj.io API Python SDK",
+    url="https://github.com/simontb/mojio-py",
+    packages=['mojio_sdk'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.6',
+)
