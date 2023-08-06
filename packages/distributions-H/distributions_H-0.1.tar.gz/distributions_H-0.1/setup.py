1fc3ce12ad27514216d55a3a70043962bd72553c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:30 +0000

    Add package distributions-H/0.1/distributions_H-0.1.tar.gz

---
+++ packages/distributions-H/distributions_H-0.1.tar.gz/distributions_H-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_H',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_H'],
+      zip_safe=False)
