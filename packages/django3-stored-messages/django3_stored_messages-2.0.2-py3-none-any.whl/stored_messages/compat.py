5ab6e887e697d96236424e7974dfd5be6dfd133a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:22 +0000

    Add package django3-stored-messages/2.0.2/django3_stored_messages-2.0.2-py3-none-any.whl

---
+++ packages/django3-stored-messages/django3_stored_messages-2.0.2-py3-none-any.whl/stored_messages/compat.py
@@ -0,0 +1,6 @@
+"""
+The `compat` module provides support for backwards compatibility with older
+versions of Django and Python. Please, add in this module the compatibility code.
+"""
+
+from __future__ import unicode_literals
