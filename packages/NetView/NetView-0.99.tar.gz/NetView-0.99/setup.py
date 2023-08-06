379a6f3512ed8feaf1a4151310d95e5ccf9c73c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:38 +0000

    Add package NetView/0.99/NetView-0.99.tar.gz

---
+++ packages/NetView/NetView-0.99.tar.gz/NetView-0.99/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import setup
+
+
+setup(
+    name = "NetView",
+    version = 0.99,
+    packages= ["netview","sample","graph"],
+    description='Network Grapher',
+    install_requires=["networkx","Django","matplotlib"],
+
+    entry_points =
+    { 'console_scripts':
+        [
+            'runmyserver = sample.run:main',
+            'initmigrate = sample.init:main',
+        ]
+    },
+)
