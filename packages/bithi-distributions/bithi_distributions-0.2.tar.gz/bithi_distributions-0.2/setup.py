13f7851ccd4d0a40e588b81fbd285c8b157dd2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package bithi-distributions/0.2/bithi_distributions-0.2.tar.gz

---
+++ packages/bithi-distributions/bithi_distributions-0.2.tar.gz/bithi_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bithi_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['bithi_distributions'],
+      author='Bithi',
+      zip_safe=False)
