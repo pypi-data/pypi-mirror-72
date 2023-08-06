13d6ef8af03a3f9053cd29269ab123ecb1ba492f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:51 +0000

    Add package rayvision-api/2.1.1/rayvision_api-2.1.1-py2.py3-none-any.whl

---
+++ packages/rayvision-api/rayvision_api-2.1.1-py2.py3-none-any.whl/rayvision_api/operators/tag.py
@@ -0,0 +1,73 @@
+"""API operation on tags."""
+
+
+class TagOperator(object):
+    """Task tag settings."""
+
+    def __init__(self, connect):
+        """Initialize instance.
+
+        Args:
+            connect (rayvision_api.api.connect.Connect): The connect instance.
+
+        """
+        self._connect = connect
+
+    def add_label(self, new_name, status=1):
+        """Add a custom label.
+
+        Args:
+            new_name (str): Label name.
+            status (int, optional): Label status,0 or 1,default is 1.
+
+        """
+        data = {
+            'newName': new_name,
+            'status': int(status)
+        }
+        return self._connect.post(self._connect.url.addLabel, data)
+
+    def delete_label(self, del_name):
+        """Delete custom label.
+
+        Args:
+            del_name (str): The name of the label to be deleted.
+
+        """
+        data = {'delName': del_name}
+        return self._connect.post(self._connect.url.deleteLabel, data)
+
+    def get_label_list(self):
+        """Get custom labels.
+
+        Returns:
+            dict: Label list info.
+                e.g.:
+                    {
+                        "projectNameList": [
+                            {
+                                "projectId": 3671,
+                                "projectName": "myLabel"
+                            }
+                        ]
+                    }
+
+        """
+        return self._connect.post(self._connect.url.getLabelList,
+                                  validator=False)
+
+    def get_project_list(self):
+        """Get custom labels.
+
+        Returns:
+            list: Label list info.
+                e.g.:
+                    [
+                        {
+                            "projectId": 3671,
+                            "projectName": "myLabel"
+                        }
+                    ]
+
+        """
+        return self.get_label_list()['projectNameList']
