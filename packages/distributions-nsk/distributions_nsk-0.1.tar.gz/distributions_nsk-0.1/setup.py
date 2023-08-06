e2586ebe998f6f3665aa6de0ca1bf0a19396d1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package distributions-nsk/0.1/distributions_nsk-0.1.tar.gz

---
+++ packages/distributions-nsk/distributions_nsk-0.1.tar.gz/distributions_nsk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_nsk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_nsk'],
+      zip_safe=False)
