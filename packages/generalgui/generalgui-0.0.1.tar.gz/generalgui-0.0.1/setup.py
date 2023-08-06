55759afce1a8cda09c0d0d60f8155ec109d34c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:14 +0000

    Add package generalgui/0.0.1/generalgui-0.0.1.tar.gz

---
+++ packages/generalgui/generalgui-0.0.1.tar.gz/generalgui-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='generalgui',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Rickard "Mandera" Abraham',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
